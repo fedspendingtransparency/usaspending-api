1fa7ccac032275bfed550c99379b14699c244cf3
Kaitlin Devine <kaitlin.devine@gsa.gov>
2016-09-01 21:27:00 -0400

    added basic test

--- usaspending_api/awards/tests.py
+++ usaspending_api/awards/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class AwardTests(TestCase):
+
+    #load the test fixture
+    fixtures = ['awards']
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_award_list(self):
+        """
+        Ensure the awards endpoint lists the right number of awards
+        """
+        resp = self.c.get('/api/v1/awards/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 2)
