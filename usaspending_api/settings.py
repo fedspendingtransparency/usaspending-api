850a199435ca9858b1ffc74754820ad8a70a45c6
Kaitlin Devine <kaitlin.devine@gsa.gov>
2016-09-01 21:49:15 -0400

    updated readme, cribbed from broker

--- usaspending_api/settings.py
+++ usaspending_api/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'usaspending_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
