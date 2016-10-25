bc363d798e64645cd3544d9fa19481d8f4c85f1c
playmakervi <dyer_bernard@bah.com>
2016-10-25 10:59:53 -0400

    Update Test for CFDA Loader

--- usaspending_api/references/test_cfda_loader.py
+++ usaspending_api/references/test_cfda_loader.py
@@ -15,16 +15,19 @@ class CDFALoadTests(TestCase):
         """
         Ensure cfda data can can be loaded from source file
         """
+        call_command('loadcfda')
 
     def test_program_number(self):
         """
         Make sure an instance of a program number is properly created
         """
 
+        program_number = CFDAProgram.objects.get(program_number='98.012', program_title='USAID Development Partnerships for University Cooperation and Development)
 
     def test_account_identification(self):
         """
         Make sure a account identication is properly mapped to program_number
         """
 
+        account_identification = CFDAProgram.objects.get(program_number='15.918', account_identification='14-1042-0-1-303.')
 #        assert(subtier.department == department)
