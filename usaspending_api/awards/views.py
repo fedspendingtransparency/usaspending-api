d3c922001e377efbb465ee6853baff89a338fd6a
Arman Frasier <armanfrasier@gmail.com>
2016-10-25 14:39:34 -0400

    Arbitrary get and post filters
    Specify anything in post/get requests and have the filters dynamically
    generated

--- usaspending_api/awards/views.py
+++ usaspending_api/awards/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Q, Sum
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from usaspending_api.awards.models import FinancialAccountsByAwardsTransactionObligations, Award
 from usaspending_api.awards.serializers import FinancialAccountsByAwardsTransactionObligationsSerializer, AwardSerializer
+from usaspending_api.common.api_request_utils import FilterGenerator, FiscalYear, ResponsePaginator
 
 
 class AwardList(APIView):
@@ -38,64 +38,40 @@ class AwardListSummary(APIView):
     List all awards (summary level)
     """
     def get(self, request, uri=None, piid=None, fain=None, format=None):
-        # Because these are all GET requests and mutually exclusive, we chain an
-        # if statement here. We could use some nifty Q object nonsense but for
-        # clarity we skip that here. For POST filters we will want to set that up
-        awards = None
+        filter_map = {
+            'awarding_fpds': 'awarding_agency__fpds_code',
+            'funding_fpds': 'funding_agency__fpds_code',
+        }
+        fg = FilterGenerator(filter_map=filter_map, ignored_parameters=['fy'])
+        filter_arguments = fg.create_from_get(request.GET)
+        # We need to parse the FY to be the appropriate value
+        if 'fy' in request.GET:
+            fy = FiscalYear(request.GET.get('fy'))
+            fy_arguments = fy.get_filter_object('date_signed', as_dict=True)
+            filter_arguments = {**filter_arguments, **fy_arguments}
+
         if uri:
-            awards = Award.objects.filter(uri=uri)
+            filter_arguments['uri'] = uri
         elif piid:
-            awards = Award.objects.filter(piid=piid)
+            filter_arguments['piid'] = piid
         elif fain:
-            awards = Award.objects.filter(fain=fain)
-        else:
-            awards = Award.objects.all()
+            filter_arguments['fain'] = fain
 
-        agency = request.GET.get('agency')
-        fy = request.GET.get('fy')
-        query = Q()
+        awards = Award.objects.all().filter(**filter_arguments)
 
-        if agency:
-            query = Q(awarding_agency__fpds_code=agency)
-            query |= Q(funding_agency__fpds_code=agency)
-        if fy:
-            query &= Q(date_signed__year=fy)
+        paged_data = ResponsePaginator.get_paged_data(awards, request_parameters=request.GET)
 
-        awards = awards.filter(query)
-
-        # In the future this should be refactored out to accept arbitrary QS and
-        # be reused in other views
-        limit = request.GET.get('limit')
-        page = request.GET.get('page')
-        if limit:
-            limit = int(limit)
-        else:
-            limit = 100
-
-        paginator = Paginator(awards, limit)
-
-        try:
-            paged_awards = paginator.page(page)
-        except PageNotAnInteger:
-            # Either no page or garbage page
-            paged_awards = paginator.page(1)
-            page = 1
-        except EmptyPage:
-            # Page is too far, give last page
-            paged_awards = paginator.page(paginator.num_pages)
-            page = paginator.num_pages
-
-        serializer = AwardSerializer(paged_awards, many=True)
+        serializer = AwardSerializer(paged_data, many=True)
         response_object = {
             "total_metadata": {
                 "count": awards.count(),
                 "total_obligation_sum": awards.aggregate(Sum('total_obligation'))["total_obligation__sum"],
             },
             "page_metadata": {
-                "page_number": paged_awards.number,
-                "page_max_length": limit,
-                "count": len(paged_awards),
-                "total_obligation_sum": paged_awards.object_list.aggregate(Sum('total_obligation'))["total_obligation__sum"],
+                "page_number": paged_data.number,
+                "num_pages": paged_data.paginator.num_pages,
+                "count": len(paged_data),
+                "total_obligation_sum": paged_data.object_list.aggregate(Sum('total_obligation'))["total_obligation__sum"],
             },
             "results": serializer.data
         }
