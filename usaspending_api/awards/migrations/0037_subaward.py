1ead52893af65b95f414560dc7966193382b4f40
playmakervi <dyer_bernard@bah.com>
2016-11-01 13:15:33 -0400

    Removed Comment Line

--- usaspending_api/awards/migrations/0037_subaward.py
+++ usaspending_api/awards/migrations/0037_subaward.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-# ('references', '0033_auto_20161101_0948'),
         ('awards', '0036_auto_20161027_1403'),
     ]
 
