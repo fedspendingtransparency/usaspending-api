b785ecf27dd1e09a0284338232c2f4b1b0cb778d
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-12-09 16:54:31 -0500

    Add post method to financial activity endpoint
    Expand the functionality of the financial activity endpoint
    by enabling a post request that uses our generic data query
    handler (i.e., adds complex filters, unique values, etc.).

--- usaspending_api/financial_activities/serializers.py
+++ usaspending_api/financial_activities/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
-from usaspending_api.financial_activities.models import FinancialAccountsByProgramActivityObjectClass
+from usaspending_api.common.serializers import LimitableSerializer
 from usaspending_api.accounts.serializers import AppropriationAccountBalancesSerializer
+from usaspending_api.financial_activities.models import FinancialAccountsByProgramActivityObjectClass
 
 
-class FinancialAccountsByProgramActivityObjectClassSerializer(serializers.ModelSerializer):
+class FinancialAccountsByProgramActivityObjectClassSerializer(LimitableSerializer):
     appropriation_account_balances = AppropriationAccountBalancesSerializer(read_only=True)
 
     class Meta:
