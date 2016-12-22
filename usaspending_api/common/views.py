c444784805f44bbcf1f3570d4eb2b694d87968bf
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-12-22 15:10:56 -0500

    Add a custom ViewSet for our detailed data endpoints
    Since we're currently overriding the ReadOnlyModelViewSet's
    list method to add custom metadata, this commit creates
    a custom ViewSet class to abstract that. This way, we can
    base our views on the custom ViewSet rather than explicitly
    add a list override to each view.

--- usaspending_api/common/views.py
+++ usaspending_api/common/views.py
@@ -1,5 +1,4 @@
-from rest_framework import generics
-from rest_framework import mixins
+from rest_framework import generics, mixins, viewsets
 from rest_framework.response import Response
 
 from usaspending_api.common.api_request_utils import ResponsePaginator
@@ -51,3 +50,28 @@ class AggregateView(mixins.ListModelMixin,
         }
         response_object.update({'results': serialized_data})
         return Response(response_object)
+
+
+class DetailViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Handles the views for endpoints that request a detailed
+    view of model objects (either in the form of a single
+    object or a list of objects).
+
+    Note that the only reason our views don't use Django
+    Rest Frameworks ReadOnlyModelViewSet directly is that
+    we're overriding the list method to tack metadata
+    on to our responses. Once we're able to customize
+    our metadata via DRF pagination, we probably won't
+    need this class at all and can just hook views directly
+    to viewsets.ReadOnlyModelViewSet.
+    """
+
+    def list(self, request, *args, **kwargs):
+        """
+        Override the parent list method so we can add metadata to response.
+        Once we're able to customize metadata via DRF pagination, we won't need this.
+        """
+        response = self.build_response(
+            self.request, queryset=self.get_queryset(), serializer=self.get_serializer_class())
+        return Response(response)
