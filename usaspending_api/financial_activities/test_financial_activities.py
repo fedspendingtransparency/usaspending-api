fe73e514b6875cdffba8c54d3f1cd38d7a7f04a2
Catherine Devlin <catherine.devlin@gsa.gov>
2017-01-25 12:12:07 -0500

    Simplest possible conversion of most TestCases to py.test

--- usaspending_api/financial_activities/test_financial_activities.py
+++ usaspending_api/financial_activities/test_financial_activities.py
@@ -1,18 +1,17 @@
-from django.test import TestCase, Client
 import pytest
 
 from model_mommy import mommy
 
+@pytest.fixture(scope="session")
+def fin_act_models():
+    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', _quantity=2)
 
-class FinancialActivitiesTests(TestCase):
-    def setUp(self):
-        self.award = mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', _quantity=2)
 
-    @pytest.mark.django_db
-    def test_financial_activities_list(self):
-        """
-        Ensure the financial activities endpoint lists the right number of entities
-        """
-        resp = self.client.get('/api/v1/financial_activities/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(len(resp.data), 3)
+@pytest.mark.django_db
+def test_financial_activities_list(fin_act_models, client):
+    """
+    Ensure the financial activities endpoint lists the right number of entities
+    """
+    resp = client.get('/api/v1/financial_activities/')
+    assert resp.status_code == 200
+    assert len(resp.data) == 3
