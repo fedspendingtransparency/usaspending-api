0686b025b7e1223b148c40bb9e87fcfd55072a6c
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-01-26 10:27:22 -0500

    Add single-transaction endpoint
    Similar to the previous commit for a single award, this
    adds a way to retrieve information about a single transaction.

--- usaspending_api/awards/urls_transactions.py
+++ usaspending_api/awards/urls_transactions.py
@@ -3,12 +3,15 @@ from django.conf.urls import url
 from usaspending_api.awards import views
 
 # map reqest types to viewset method; replace this with a router
-transaction = views.TransactionViewset.as_view(
+transaction_list = views.TransactionViewset.as_view(
     {'get': 'list', 'post': 'list'})
+transaction_detail = views.TransactionViewset.as_view(
+    {'get': 'retrieve', 'post': 'retrieve'})
 transaction_total = views.TransactionAggregateViewSet.as_view({
     'get': 'list', 'post': 'list'})
 
 urlpatterns = [
-    url(r'^$', transaction),
+    url(r'^$', transaction_list, name='transaction-list'),
+    url(r'(?P<pk>[0-9]+)/$', transaction_detail, name='transaction-detail'),
     url(r'^total/', transaction_total)
 ]
