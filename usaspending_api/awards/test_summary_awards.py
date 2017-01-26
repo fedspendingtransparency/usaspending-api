6b2307da32c717884fcdec03135f57a1b0679eb8
Catherine Devlin <catherine.devlin@gsa.gov>
2017-01-26 12:47:53 -0500

    yapf formatting applied

--- usaspending_api/awards/test_summary_awards.py
+++ usaspending_api/awards/test_summary_awards.py
@@ -3,10 +3,16 @@ import pytest
 from model_mommy import mommy
 from usaspending_api.awards.models import Award
 
+
 @pytest.fixture(scope="session")
 def award_models():
     mommy.make('awards.Award', total_obligation="2000", _quantity=2)
-    mommy.make('awards.Award', type="U", total_obligation=None, date_signed=None, recipient=None)
+    mommy.make('awards.Award',
+               type="U",
+               total_obligation=None,
+               date_signed=None,
+               recipient=None)
+
 
 @pytest.mark.django_db
 def test_null_awards(award_models):
