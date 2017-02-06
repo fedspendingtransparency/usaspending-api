f8d972e992d8659049ada68fc34a51c575f473f7
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-02-06 16:53:02 -0500

    Instruct pytest to add --keepdb when running tests
    I'm not sure that adding this speeds things up a great deal,
    but it also doesn't seem to be hurting anything.

--- usaspending_api/conftest.py
+++ usaspending_api/conftest.py
@@ -1,8 +1,13 @@
 from django.conf import settings
-
+import pytest
 
 def pytest_configure():
     # To make sure the test setup process doesn't try
     # to set up a data_broker test db, remove it
     # from the list of databases in django settings
     settings.DATABASES.pop('data_broker', None)
+
+
+@pytest.fixture(scope='session')
+def django_db_keepdb(request):
+    return True
