3a2bbe961496a18bd69f78a6cf7f7aed02aa87a9
Catherine Devlin <catherine.devlin@gsa.gov>
2017-02-08 11:55:21 -0500

    Added tests for matched_objects parameter

--- usaspending_api/awards/tests/test_awards.py
+++ usaspending_api/awards/tests/test_awards.py
@@ -140,18 +140,29 @@ def test_award_total_grouped(client, awards_data):
 def test_award_autocomplete(client, awards_data, fields, value, expected):
     """Test partial-text search."""
 
-    resp = client.post(
-        '/api/v1/awards/autocomplete/',
-        content_type='application/json',
-        data=json.dumps({
-            'fields': fields,
-            'value': value,
-        }))
-    assert resp.status_code == status.HTTP_200_OK
+    for match_objs in (0, 1):
+        resp = client.post(
+            '/api/v1/awards/autocomplete/',
+            content_type='application/json',
+            data=json.dumps({
+                'fields': fields,
+                'value': value,
+                'matched_objects': match_objs
+            }))
+        assert resp.status_code == status.HTTP_200_OK
 
-    results = resp.data['results']
-    for key in results:
-        sorted(results[key]) == expected[key]
+        results = resp.data['results']
+        for key in results:
+            sorted(results[key]) == expected[key]
+
+        # If matched_objects requested, verify they are present
+        assert ('matched_objects' in resp.data) == match_objs
+        if match_objs:
+            objs = resp.data['matched_objects']
+            for field in fields:
+                assert field in objs
+                # and return correct number of objects
+                assert len(objs[field]) == len(results[field])
 
 
 def test_bad_autocomplete_request(client):
