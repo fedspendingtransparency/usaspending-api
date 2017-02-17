0028cd76fbfbfb61ca96c4954e16d2c86983409b
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-02-16 22:18:02 -0500

    Convert endpoints and tests from AwardActions to Transaction
    This commit transitions the transaction endpoint to the new
    set of transaction tables. In addition, the old procurement_set
    and financial_assistance_set information in the awards endpoing
    has been updated to reflect the new model structure. We'll want
    to think more about how much transaction info we want to return
    in the awards endpoint (vs the specific transaction endpoint).
    All tests and data now reflect the new models, so a future
    commit will remove the old models.

--- usaspending_api/awards/serializers.py
+++ usaspending_api/awards/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
-from usaspending_api.awards.models import Award, FinancialAccountsByAwards, FinancialAccountsByAwardsTransactionObligations, FinancialAssistanceAward, Procurement
-from usaspending_api.accounts.serializers import AppropriationAccountBalancesSerializer, TreasuryAppropriationAccountSerializer
+from usaspending_api.awards.models import (
+    Award, FinancialAccountsByAwards,
+    FinancialAccountsByAwardsTransactionObligations,
+    Transaction, TransactionAssistance, TransactionContract)
+from usaspending_api.accounts.serializers import TreasuryAppropriationAccountSerializer
 from usaspending_api.common.serializers import LimitableSerializer
 from usaspending_api.references.serializers import AgencySerializer, LegalEntitySerializer, LocationSerializer
 from usaspending_api.common.helpers import fy
@@ -11,20 +14,6 @@ class FinancialAccountsByAwardsTransactionObligationsSerializer(LimitableSeriali
 
     class Meta:
         model = FinancialAccountsByAwardsTransactionObligations
-        fields = '__all__'
-
-
-class ProcurementSerializer(LimitableSerializer):
-
-    class Meta:
-        model = Procurement
-        fields = '__all__'
-
-
-class FinancialAssistanceAwardSerializer(LimitableSerializer):
-
-    class Meta:
-        model = FinancialAssistanceAward
         fields = '__all__'
 
 
@@ -43,6 +32,17 @@ class FinancialAccountsByAwardsSerializer(LimitableSerializer):
                 "kwargs": {"read_only": True, "many": True}
             },
         }
+
+
+class AwardTransactionSerializer(LimitableSerializer):
+    """Serialize basic transaction information to return in award/ endpoint."""
+
+    class Meta:
+        model = Transaction
+        fields = (
+            'id', 'federal_action_obligation',
+            'period_of_performance_start_date', 'period_of_performance_current_end_date',
+            'action_date', 'action_type')
 
 
 class AwardSerializer(LimitableSerializer):
@@ -64,12 +64,8 @@ class AwardSerializer(LimitableSerializer):
                 "class": AgencySerializer,
                 "kwargs": {"read_only": True}
             },
-            "procurement_set": {
-                "class": ProcurementSerializer,
-                "kwargs": {"read_only": True, "many": True}
-            },
-            "financialassistanceaward_set": {
-                "class": FinancialAssistanceAwardSerializer,
+            "transaction_set": {
+                "class": AwardTransactionSerializer,
                 "kwargs": {"read_only": True, "many": True}
             },
             "financial_set": {
@@ -88,45 +84,39 @@ class AwardSerializer(LimitableSerializer):
         return fy(obj.date_signed)
 
 
+class TransactionAssistanceSerializer(LimitableSerializer):
+
+    class Meta:
+        model = TransactionAssistance
+        fields = '__all__'
+
+
+class TransactionContractSerializer(LimitableSerializer):
+
+    class Meta:
+        model = TransactionContract
+        fields = '__all__'
+
+
 class TransactionSerializer(LimitableSerializer):
-    """Serializer transactions."""
-    # Note: until we update the AwardAction abstract class to
-    # a physical Transaction table, the transaction serializer's first
-    # iteration will include procurement data only. This will let us
-    # demo and test the endpoint while avoiding the work
-    # of combining the separate procurement/assistance tables (that work
-    # won't be needed once we make the AwardAction-->Transaction change).
-
-    # The following code simulates having a single
-    # Tranaction table with contract and assistance tables that can point back
-    # to it and render as nested data in the transaction response.
-    contract_data = serializers.SerializerMethodField()
-    assistance_data = serializers.SerializerMethodField()
-
-    def get_contract_data(self, procurement):
-        return {
-            "cost_or_pricing_data": procurement.cost_or_pricing_data,
-            "naics": procurement.naics,
-            "naics_description": procurement.naics_description,
-            "product_or_service_code": procurement.product_or_service_code,
-        }
-
-    def get_assistance_data(self, procurement):
-        return {}
-
-    # End of temporary code to mock up nexted contract and assistance data.
+    """Serialize complete transactions, including assistance and contract data."""
 
     class Meta:
 
-        model = Procurement
-        # Manually setting the field list below to mock up the nested
-        # contract and assistance data parts of the response
-        fields = (
-            'award', 'type', 'type_description', 'modification_number',
-            'federal_action_obligation', 'action_date', 'description',
-            'update_date', 'contract_data', 'assistance_data', 'type_of_contract_pricing',
-            'type_of_contract_pricing_description')
+        model = Transaction
+        fields = '__all__'
+
         nested_serializers = {
+            # name below must match related_name in TransactionAssistance
+            "assistance_data": {
+                "class": TransactionAssistanceSerializer,
+                "kwargs": {"read_only": True}
+            },
+            # name below must match related_name in TransactionContract
+            "contract_data": {
+                "class": TransactionContractSerializer,
+                "kwargs": {"read_only": True}
+            },
             "recipient": {
                 "class": LegalEntitySerializer,
                 "kwargs": {"read_only": True}
@@ -137,6 +127,10 @@ class TransactionSerializer(LimitableSerializer):
             },
             "funding_agency": {
                 "class": AgencySerializer,
+                "kwargs": {"read_only": True}
+            },
+            "place_of_performance": {
+                "class": LocationSerializer,
                 "kwargs": {"read_only": True}
             }
         }
