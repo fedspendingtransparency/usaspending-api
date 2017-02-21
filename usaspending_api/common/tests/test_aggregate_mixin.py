fad92533faacf590b0aa508dea881f67d46a5b58
Catherine Devlin <catherine.devlin@gsa.gov>
2017-02-21 17:28:46 -0500

    Added as-yet-unfulfilled test for aggregating __fy over FK traversal

--- usaspending_api/common/tests/test_aggregate_mixin.py
+++ usaspending_api/common/tests/test_aggregate_mixin.py
@@ -50,6 +50,11 @@ def aggregate_models():
 
     award_recipe.make(_quantity=4)
 
+    # make each award parent of the next
+    parent = None
+    for award in Award.objects.order_by('period_of_performance_start_date'):
+        award.parent_award = parent
+
 
 @pytest.mark.django_db
 def test_agg_fields(monkeypatch, aggregate_models):
@@ -155,12 +160,52 @@ _expected_fy_aggregated = [{
 }]
 
 
+
 @pytest.mark.django_db
 @pytest.mark.parametrize('model, request_data, expected', [(Award, {
     'field': 'total_obligation',
     'group': 'period_of_performance_start_date__fy'
 }, _expected_fy_aggregated)])
 def test_aggregate_fy(monkeypatch, aggregate_models, model, request_data,
+                      expected):
+    request = Mock()
+    request.query_params = {}
+    request.data = request_data
+    a = AggregateQuerysetMixin()
+    a.get_queryset = lambda: model.objects.all()
+    agg = a.aggregate(request=request)
+
+    agg_list = [a for a in agg]
+    if 'order' not in request_data:
+        # this isn't an 'order by' request, (i.e., we're not testing
+        # the result order), so sort the actual and expected results
+        # to ensure a good comparison
+        agg_list.sort(key=itemgetter('item'))
+        expected.sort(key=itemgetter('item'))
+
+    assert agg_list == expected
+
+
+_expected_parent_fy_aggregated = [{
+    'item': None,
+    'aggregate': Decimal('1000.01')
+}, {
+    'item': 2016,
+    'aggregate': Decimal('2000.00')
+}, {
+    'item': 2017,
+    'aggregate': Decimal('4000.02')
+}]
+
+
+# TODO: support applying __fy through a FK traversal
+@pytest.mark.skip
+@pytest.mark.django_db
+@pytest.mark.parametrize('model, request_data, expected', [(Award, {
+    'field': 'total_obligation',
+    'group': 'parent_award__period_of_performance_start_date__fy'
+}, _expected_fy_aggregated)])
+def test_aggregate_fy_with_traversal(monkeypatch, aggregate_models, model, request_data,
                       expected):
     request = Mock()
     request.query_params = {}
