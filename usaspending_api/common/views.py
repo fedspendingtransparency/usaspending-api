3b787b2cd1b4f93ca801e523ec63a92a17e91346
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-03-09 16:17:53 -0500

    Add caching decorator from drf-extensions
    This does not address the environment-based caching control
    that we had talked about, but it's so easy to implement that
    it seemed worth testing and getting eyes on.

--- usaspending_api/common/views.py
+++ usaspending_api/common/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_extensions.cache.decorators import cache_response
 
 from usaspending_api.common.api_request_utils import ResponsePaginator
 from usaspending_api.common.serializers import AggregateSerializer
@@ -24,6 +25,7 @@ class AggregateView(AggregateQuerysetMixin,
 
     exception_logger = logging.getLogger("exceptions")
 
+    @cache_response()
     def list(self, request, *args, **kwargs):
         """
         Override the parent list method so we can aggregate the data
@@ -82,6 +84,7 @@ class DetailViewSet(viewsets.ReadOnlyModelViewSet):
 
     exception_logger = logging.getLogger("exceptions")
 
+    @cache_response()
     def list(self, request, *args, **kwargs):
         try:
             response = self.build_response(
