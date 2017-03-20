23319cba380f09e4ef875f1e957c337601d9718e
Arman Frasier <armanfrasier@gmail.com>
2017-03-20 17:10:18 -0400

    Adding caching * Set the CACHE_ENVIRONMENT to one of the specified options: ‘local’ does local memcaching, ‘disabled’ disables caching, and ‘elasticache’ enables elasticache, but needs further settings to be altered.

--- usaspending_api/common/views.py
+++ usaspending_api/common/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_extensions.cache.decorators import cache_response
 from django.views.generic import TemplateView
 
 from usaspending_api.common.api_request_utils import ResponsePaginator
@@ -25,6 +26,7 @@ class AggregateView(AggregateQuerysetMixin,
 
     exception_logger = logging.getLogger("exceptions")
 
+    @cache_response()
     def list(self, request, *args, **kwargs):
         """
         Override the parent list method so we can aggregate the data
@@ -83,6 +85,7 @@ class DetailViewSet(viewsets.ReadOnlyModelViewSet):
 
     exception_logger = logging.getLogger("exceptions")
 
+    @cache_response()
     def list(self, request, *args, **kwargs):
         try:
             response = self.build_response(
