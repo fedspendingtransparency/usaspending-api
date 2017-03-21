3a203a78af95c480cd850191036788e35ced914a
Arman Frasier <armanfrasier@gmail.com>
2017-03-21 12:30:38 -0400

    * Add tas autocomplete endpoint * Add tas autocomplete tests

--- usaspending_api/accounts/views.py
+++ usaspending_api/accounts/views.py
@@ -5,7 +5,7 @@ from usaspending_api.accounts.serializers import AppropriationAccountBalancesSer
 from usaspending_api.awards.serializers import FinancialAccountsByAwardsSerializer
 from usaspending_api.awards.models import FinancialAccountsByAwards
 from usaspending_api.common.mixins import FilterQuerysetMixin, ResponseMetadatasetMixin
-from usaspending_api.common.views import DetailViewSet
+from usaspending_api.common.views import DetailViewSet, AutocompleteView
 from usaspending_api.common.mixins import SuperLoggingMixin
 
 
@@ -23,6 +23,19 @@ class TreasuryAppropriationAccountViewSet(SuperLoggingMixin,
         filtered_queryset = self.filter_records(self.request, queryset=queryset)
         ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
         return ordered_queryset
+
+
+class TreasuryAppropriationAccountAutocomplete(FilterQuerysetMixin,
+                                               AutocompleteView):
+    """Handle autocomplete requests for appropriation account (i.e., TAS) information."""
+    serializer_class = TreasuryAppropriationAccountSerializer
+
+    def get_queryset(self):
+        """Return the view's queryset."""
+        queryset = TreasuryAppropriationAccount.objects.all()
+        queryset = self.serializer_class.setup_eager_loading(queryset)
+        filtered_queryset = self.filter_records(self.request, queryset=queryset)
+        return filtered_queryset
 
 
 class TreasuryAppropriationAccountBalancesViewSet(SuperLoggingMixin,
