0b43d7bf91e8c1bce571507cd97aa51f612f9d2c
Arman Frasier <armanfrasier@gmail.com>
2017-03-22 14:44:08 -0400

    Adding new TAS aggregation endpoints
    * Added `accounts/tas/balances` for File A
    (AppropriationAccountBalances)
    * Added `accounts/tas/categories` for File B
    (FinancialAccountsByProgramActivityObjectClass)
    * Added tests
    * Added documentation

--- usaspending_api/accounts/views.py
+++ usaspending_api/accounts/views.py
@@ -4,8 +4,10 @@ from usaspending_api.accounts.models import AppropriationAccountBalances
 from usaspending_api.accounts.serializers import AppropriationAccountBalancesSerializer
 from usaspending_api.awards.serializers import FinancialAccountsByAwardsSerializer
 from usaspending_api.awards.models import FinancialAccountsByAwards
+from usaspending_api.financial_activities.models import FinancialAccountsByProgramActivityObjectClass
+from usaspending_api.financial_activities.serializers import FinancialAccountsByProgramActivityObjectClassSerializer
 from usaspending_api.common.mixins import FilterQuerysetMixin, ResponseMetadatasetMixin
-from usaspending_api.common.views import DetailViewSet, AutocompleteView
+from usaspending_api.common.views import DetailViewSet, AutocompleteView, AggregateView
 from usaspending_api.common.mixins import SuperLoggingMixin
 
 
@@ -68,6 +70,43 @@ class FinancialAccountsByAwardListViewSet(
         """Return the view's queryset."""
         queryset = FinancialAccountsByAwards.objects.all()
         queryset = self.serializer_class.setup_eager_loading(queryset)
+        filtered_queryset = self.filter_records(self.request, queryset=queryset)
+        ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
+        return ordered_queryset
+
+
+class TASBalancesAggregate(SuperLoggingMixin,
+                           FilterQuerysetMixin,
+                           AggregateView):
+    """Return aggregated award information."""
+    def get_queryset(self):
+        queryset = AppropriationAccountBalances.objects.all()
+        filtered_queryset = self.filter_records(self.request, queryset=queryset)
+        ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
+        return ordered_queryset
+
+
+class TASCategoryList(SuperLoggingMixin,
+                      FilterQuerysetMixin,
+                      ResponseMetadatasetMixin,
+                      DetailViewSet):
+    """Handle requests for appropriation account balance information."""
+    serializer_class = FinancialAccountsByProgramActivityObjectClassSerializer
+
+    def get_queryset(self):
+        queryset = FinancialAccountsByProgramActivityObjectClass.objects.all()
+        queryset = self.serializer_class.setup_eager_loading(queryset)
+        filtered_queryset = self.filter_records(self.request, queryset=queryset)
+        ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
+        return ordered_queryset
+
+
+class TASCategoryAggregate(SuperLoggingMixin,
+                           FilterQuerysetMixin,
+                           AggregateView):
+    """Return aggregated award information."""
+    def get_queryset(self):
+        queryset = FinancialAccountsByProgramActivityObjectClass.objects.all()
         filtered_queryset = self.filter_records(self.request, queryset=queryset)
         ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
         return ordered_queryset
