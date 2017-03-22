e2595742b26e1ff8f5402591497a62c585335b7b
Arman Frasier <armanfrasier@gmail.com>
2017-03-22 10:00:05 -0400

    Adding path keybit to default key constructor

--- usaspending_api/common/cache.py
+++ usaspending_api/common/cache.py
@@ -2,6 +2,16 @@ from rest_framework_extensions.key_constructor import bits
 from rest_framework_extensions.key_constructor.constructors import DefaultKeyConstructor
 
 
+class PathKeyBit(bits.QueryParamsKeyBit):
+    """
+    Adds query path as a key bit
+    """
+
+    def get_source_dict(self, params, view_instance, view_method, request, args, kwargs):
+        path = request.path
+        return {"path": path}
+
+
 class GetPostQueryParamsKeyBit(bits.QueryParamsKeyBit):
     """
     Override QueryParamsKey method in drf-extensions to ensure that
@@ -21,6 +31,7 @@ class USAspendingKeyConstructor(DefaultKeyConstructor):
     more nuanced keys, see the drf-extensions documentation:
     http://chibisov.github.io/drf-extensions/docs/#default-key-constructor
     """
+    path_bit = PathKeyBit()
     request_params = GetPostQueryParamsKeyBit()
     unique_view_id = bits.UniqueMethodIdKeyBit()
 
