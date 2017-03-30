7c5f4980389db227d5a731df6661ceae9bc30ae0
Arman Frasier <armanfrasier@gmail.com>
2017-03-30 10:25:24 -0400

    Fix codeclimate

--- usaspending_api/common/management/commands/clear_usaspending_cache.py
+++ usaspending_api/common/management/commands/clear_usaspending_cache.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.cache import caches
 
+
 class Command(BaseCommand):
     """
     This command will clear the usaspending-cache (useful after a load or a deletion
