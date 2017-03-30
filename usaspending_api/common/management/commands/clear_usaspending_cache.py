12d4ee2b14522344f2359544eeb5ed3f2a4a3cd9
Arman Frasier <armanfrasier@gmail.com>
2017-03-30 10:25:03 -0400

    Add cache clear command

---
+++ usaspending_api/common/management/commands/clear_usaspending_cache.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import logging
+import os
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.cache import caches
+
+class Command(BaseCommand):
+    """
+    This command will clear the usaspending-cache (useful after a load or a deletion
+    to ensure end users don't see stale data)
+    """
+    help = "Clears the usaspending-cache"
+    logger = logging.getLogger('console')
+
+    def handle(self, *args, **options):
+        self.logger.info("Clearing usaspending-cache...")
+        cache = caches["usaspending-cache"]
+        cache.clear()
+        self.logger.info("Done.")
