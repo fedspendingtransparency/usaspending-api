524e140548117a313173865fa4b69574061dc2a6
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-04-05 17:37:36 -0400

    Add get_quarterly_numbers method to AppropriationAccountBalances
    This is the same procedure we followed with the TAS/object class/
    program activity balances. This first commit adds a method
    to return the quarterly numbers, which will later be used when
    loading the table that will house them.

--- usaspending_api/accounts/models.py
+++ usaspending_api/accounts/models.py
@@ -303,3 +303,72 @@ class AppropriationAccountBalances(DataSourceTrackedModel):
     def populate_final_of_fy(cls):
         with connection.cursor() as cursor:
             cursor.execute(cls.FINAL_OF_FY_SQL)
+
+    # TODO: is the self-joining SQL below do-able via the ORM?
+    # note: appropriation_account_balances_id is included in the quarterly
+    # query because Django's Manage.raw()/RawQuerySet require the underlying
+    # model's primary key
+    QUARTERLY_SQL = """
+        SELECT
+            current.appropriation_account_balances_id,
+            sub.submission_id,
+            current.treasury_account_identifier,
+            current.data_source,
+            COALESCE(current.budget_authority_unobligated_balance_brought_forward_fyb, 0) - COALESCE(previous.budget_authority_unobligated_balance_brought_forward_fyb, 0) AS budget_authority_unobligated_balance_brought_forward_fyb,
+            COALESCE(current.adjustments_to_unobligated_balance_brought_forward_cpe, 0) - COALESCE(previous.adjustments_to_unobligated_balance_brought_forward_cpe, 0) AS adjustments_to_unobligated_balance_brought_forward_cpe,
+            COALESCE(current.budget_authority_appropriated_amount_cpe, 0) - COALESCE(previous.budget_authority_appropriated_amount_cpe, 0) AS budget_authority_appropriated_amount_cpe,
+            COALESCE(current.borrowing_authority_amount_total_cpe, 0) - COALESCE(previous.borrowing_authority_amount_total_cpe, 0) AS borrowing_authority_amount_total_cpe,
+            COALESCE(current.contract_authority_amount_total_cpe, 0) - COALESCE(previous.contract_authority_amount_total_cpe, 0) AS contract_authority_amount_total_cpe,
+            COALESCE(current.spending_authority_from_offsetting_collections_amount_cpe, 0) - COALESCE(previous.spending_authority_from_offsetting_collections_amount_cpe, 0) AS spending_authority_from_offsetting_collections_amount_cpe,
+            COALESCE(current.other_budgetary_resources_amount_cpe, 0) - COALESCE(previous.other_budgetary_resources_amount_cpe, 0) AS other_budgetary_resources_amount_cpe,
+            COALESCE(current.budget_authority_available_amount_total_cpe, 0) - COALESCE(previous.budget_authority_available_amount_total_cpe, 0) AS budget_authority_available_amount_total_cpe,
+            COALESCE(current.gross_outlay_amount_by_tas_cpe, 0) - COALESCE(previous.gross_outlay_amount_by_tas_cpe, 0) AS gross_outlay_amount_by_tas_cpe,
+            COALESCE(current.deobligations_recoveries_refunds_by_tas_cpe, 0) - COALESCE(previous.deobligations_recoveries_refunds_by_tas_cpe, 0) AS deobligations_recoveries_refunds_by_tas_cpe,
+            COALESCE(current.unobligated_balance_cpe, 0) - COALESCE(previous.unobligated_balance_cpe, 0) AS unobligated_balance_cpe,
+            COALESCE(current.status_of_budgetary_resources_total_cpe, 0) - COALESCE(previous.status_of_budgetary_resources_total_cpe, 0) AS status_of_budgetary_resources_total_cpe,
+            COALESCE(current.obligations_incurred_total_by_tas_cpe, 0) - COALESCE(previous.obligations_incurred_total_by_tas_cpe, 0) AS obligations_incurred_total_by_tas_cpe
+        FROM
+            appropriation_account_balances AS current
+            JOIN submission_attributes AS sub
+            ON current.submission_id = sub.submission_id
+            LEFT JOIN appropriation_account_balances AS previous
+            ON current.treasury_account_identifier = previous.treasury_account_identifier
+            AND previous.submission_id = sub.previous_submission_id
+    """
+
+    @classmethod
+    def get_quarterly_numbers(cls, current_submission_id=None):
+        """
+        Return a RawQuerySet of quarterly financial numbers by tas
+        (aka treasury account symbol, aka appropriations account)
+
+        Because we receive financial data as YTD aggregates (i.e., Q3 numbers
+        represent financial activity in Q1, Q2, and Q3), we subtract previously
+        reported FY numbers when calculating discrete quarters.
+
+        For example, consider a Q3 submission in fiscal year 2017. Using
+        total_outlays (a simplified field name) as an example, we would get
+        discrete Q3 total_outlays by taking total_outlays as reported in
+        the Q3 submission and subtracting the total_outlays that were reported
+        in the Q2 submission (or the most recent prior-to-Q3 submission we
+        have on record for that agency in the current fiscal year).
+
+        If there is no previous submission matching for an agency in the
+        current fiscal year (for example, Q1 submission), quarterly numbers
+        are the same as the submission's YTD numbers). The COALESCE function
+        in the SQL above is what handles this scenario.
+
+        Args:
+            current_submission_id: the submission to retrieve quarterly data for
+                (if None, return quarterly data for all submisisons)
+
+        Returns:
+            A RawQuerySet of AppropriationAccountBalances objects
+        """
+        if current_submission_id is None:
+            return AppropriationAccountBalances.objects.raw(
+                cls.QUARTERLY_SQL)
+        else:
+            sql = cls.QUARTERLY_SQL + ' WHERE current.submission_id = %s'
+            return AppropriationAccountBalances.objects.raw(
+                sql, [current_submission_id])
