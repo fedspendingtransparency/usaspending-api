f2abb179e5dede356d80ff7e808d788200ae767e
Catherine Devlin <catherine.devlin@gsa.gov>
2017-04-20 14:50:27 -0400

    /account/awards/totals endpoint, aggregating over FinancialAccountsByAwards
    For DS-1137.  Not yet tested.

--- usaspending_api/accounts/views/common.py
+++ usaspending_api/accounts/views/common.py
@@ -1,8 +1,9 @@
 from usaspending_api.awards.serializers import FinancialAccountsByAwardsSerializer
 from usaspending_api.awards.models import FinancialAccountsByAwards
-from usaspending_api.common.mixins import FilterQuerysetMixin
+from usaspending_api.common.mixins import FilterQuerysetMixin, AggregateQuerysetMixin
 from usaspending_api.common.views import DetailViewSet, AutocompleteView
 from usaspending_api.common.mixins import SuperLoggingMixin
+from usaspending_api.common.serializers import AggregateSerializer
 
 
 class FinancialAccountsByAwardListViewSet(
@@ -22,3 +23,19 @@ class FinancialAccountsByAwardListViewSet(
         filtered_queryset = self.filter_records(self.request, queryset=queryset)
         ordered_queryset = self.order_records(self.request, queryset=filtered_queryset)
         return ordered_queryset
+
+
+class FinancialAccountsByAwardAggregateViewSet(SuperLoggingMixin,
+                                               FilterQuerysetMixin,
+                                               AggregateQuerysetMixin,
+                                               DetailViewSet):
+
+    serializer_class = AggregateSerializer
+
+    """Return aggregated FinancialAccountsByAward information."""
+    def get_queryset(self):
+        queryset = FinancialAccountsByAwards.objects.all()
+        queryset = self.filter_records(self.request, queryset=queryset)
+        queryset = self.aggregate(self.request, queryset=queryset)
+        queryset = self.order_records(self.request, queryset=queryset)
+        return queryset
