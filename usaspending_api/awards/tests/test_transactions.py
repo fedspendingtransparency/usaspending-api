495fb9b07ba7659aab935cc93e2863b6161ec155
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-04-20 15:23:17 -0400

    Stub out cases for txn get_or_create tests

--- usaspending_api/awards/tests/test_transactions.py
+++ usaspending_api/awards/tests/test_transactions.py
@@ -43,3 +43,23 @@ def test_txn_total_grouped(client):
     """Test award total endpoint with a group parameter."""
     # add this test once the transaction backend is refactored
     pass
+
+
+@pytest.mark.django_db
+def test_txn_get_or_create():
+    """Test Transaction.get_or_create method."""
+
+    # if txn doesn't exist yet, create it
+
+    # if txn already exists but has an action date < the incoming
+    # last modified date, update the existing txn
+
+    # if the txn already exists but has an action date > the incoming
+    # last modified date, do nothing
+
+    # if the txn already exists, does not have an action date and its
+    # certified date < the incoming ???action or certified??? date, update
+    # the existing txn
+
+    # if the txn already exists, does not have an action date and its
+    # certified date > the incoming ???action or certified??? date, do nothing
