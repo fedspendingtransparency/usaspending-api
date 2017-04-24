61d975e7128575ad41cbeecf4fa3df37c2f2d130
Arman Frasier <armanfrasier@gmail.com>
2017-04-24 16:43:33 -0400

    Get or create by duns method, tests for said method

---
+++ usaspending_api/references/tests/test_legal_entity.py
@@ -0,0 +1,17 @@
+import pytest
+
+from usaspending_api.references.models import LegalEntity
+
+
+@pytest.mark.django_db
+def test_get_or_create_by_duns():
+    # Null duns should always return True for created
+    assert LegalEntity.get_or_create_by_duns(duns=None)[1]  # Assert created is True
+    assert LegalEntity.get_or_create_by_duns(duns=None)[1]  # Assert created is True
+    assert LegalEntity.get_or_create_by_duns(duns=None)[1]  # Assert created is True
+
+    # Let's create a new record with a DUNS
+    entity, created = LegalEntity.get_or_create_by_duns(duns="123456789")
+    assert created
+    assert LegalEntity.get_or_create_by_duns(duns="123456789")[0] == entity
+    assert not LegalEntity.get_or_create_by_duns(duns="123456789")[1]
