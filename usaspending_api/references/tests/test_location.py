483f88f1707c7e7b5bea3e90b28dc4002659fba0
Rebecca Sweger <rebecca.sweger@gsa.gov>
2017-04-25 13:33:39 -0400

    Fix tests to accommodate the inability to fill optional Location fields
    The location model has a long unique index that cannot be created for
    records that have large numbers of characters in all of the model's
    optional fields. In reality, this scenario is unlikely to occur.
    In unit testing, however, it means that the model mommy
     _fill_optional param can't be used. We should almost certainly
    explore a different type of index in the future, but for now, I
    think it's ok to err on the side of guess that most incoming locations
    will be sparsely populated enough not to trigger the index error (versus
    keeping character limitations that risk a load error when incoming data
    exceeds it).

--- usaspending_api/references/tests/test_location.py
+++ usaspending_api/references/tests/test_location.py
@@ -41,24 +41,24 @@ def test_geocomplete_scope():
 @pytest.mark.django_db
 def test_geocomplete_limit():
     usa_code = mommy.make('references.RefCountryCode', country_code="USA", country_name="United States", _fill_optional=True)
-    locations = mommy.make('references.Location', location_country_code=usa_code, _fill_optional=True, _quantity=50)
+    locations = mommy.make('references.Location', location_country_code=usa_code, state_name='Pennsylvania', state_code='PA', city_name='Altoona', county_name='Lakawana', _quantity=50)
 
-    response_10 = GeoCompleteHandler({"value": "a", "limit": 10}).build_response()
-    response_25 = GeoCompleteHandler({"value": "a", "limit": 25}).build_response()
-    response_50 = GeoCompleteHandler({"value": "a", "limit": 50}).build_response()
+    response_2 = GeoCompleteHandler({"value": "a", "limit": 2}).build_response()
+    response_4 = GeoCompleteHandler({"value": "a", "limit": 4}).build_response()
+    response_5 = GeoCompleteHandler({"value": "a", "limit": 5}).build_response()
 
-    assert len(response_10) == 10
-    assert len(response_25) == 25
-    assert len(response_50) == 50
+    assert len(response_2) == 2
+    assert len(response_4) == 4
+    assert len(response_5) == 5
 
 
 @pytest.mark.django_db
 def test_geocomplete_congressional_codes():
     usa_code = mommy.make('references.RefCountryCode', country_code="USA", country_name="United States", _fill_optional=True)
-    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="00", state_code="VA", _fill_optional=True)
-    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="01", state_code="VA", _fill_optional=True)
-    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="02", state_code="VA", _fill_optional=True)
-    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="00", state_code="UT", _fill_optional=True)
+    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="00", state_code="VA")
+    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="01", state_code="VA")
+    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="02", state_code="VA")
+    locations = mommy.make('references.Location', location_country_code=usa_code, congressional_code="00", state_code="UT")
 
     response_va = GeoCompleteHandler({"value": "VA-"}).build_response()
     response_ut = GeoCompleteHandler({"value": "UT-"}).build_response()
