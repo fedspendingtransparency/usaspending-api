03bcc2843ecd58dc514a6377c703f10c13f5730b
Arman Frasier <armanfrasier@gmail.com>
2017-04-25 17:19:23 -0400

    Update legal entity tests
    Add case for duns="" to legal entity get_or_create_by_duns tests

--- usaspending_api/references/tests/test_legal_entity.py
+++ usaspending_api/references/tests/test_legal_entity.py
@@ -10,6 +10,10 @@ def test_get_or_create_by_duns():
     assert LegalEntity.get_or_create_by_duns(duns=None)[1]  # Assert created is True
     assert LegalEntity.get_or_create_by_duns(duns=None)[1]  # Assert created is True
 
+    assert LegalEntity.get_or_create_by_duns(duns="")[1]  # Assert created is True
+    assert LegalEntity.get_or_create_by_duns(duns="")[1]  # Assert created is True
+    assert LegalEntity.get_or_create_by_duns(duns="")[1]  # Assert created is True
+
     # Let's create a new record with a DUNS
     entity, created = LegalEntity.get_or_create_by_duns(duns="123456789")
     assert created
