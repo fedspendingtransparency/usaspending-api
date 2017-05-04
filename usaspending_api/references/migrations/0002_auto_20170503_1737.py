935ee8a250d5643b3f250be97304e450c6c662d0
TheRJB <bodine_russell@bah.com>
2017-05-04 12:18:11 -0400

    Migrations fixed

--- usaspending_api/references/migrations/0002_auto_20170503_1737.py
+++ usaspending_api/references/migrations/0002_auto_20170503_1737.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('references', '0070_auto_20170425_1357'),
+        ('references', '0001_initial'),
     ]
 
     operations = [
