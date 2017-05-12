d4b12769fe806fc6e5aea4ab845a8058a9cd4cfa
Arman Frasier <armanfrasier@gmail.com>
2017-05-12 17:42:13 -0400

    Legal Entity Category Flags

--- usaspending_api/references/urls.py
+++ usaspending_api/references/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     url(r'^locations/geocomplete', views.LocationGeoCompleteEndpoint.as_view()),
     url(r'^agency/$', views.AgencyEndpoint.as_view(mode_list)),
     url(r'^agency/autocomplete', views.AgencyAutocomplete.as_view()),
+    url(r'^recipients/$', views.RecipientViewSet.as_view(mode_list), name='recipient-list'),
+    url(r'^recipients/(?P<pk>[0-9]+)/$', views.RecipientViewSet.as_view(mode_detail), name='recipient-detail'),
     url(r'^recipients/autocomplete', views.RecipientAutocomplete.as_view()),
     url(r'^cfda/$', views.CfdaEndpoint.as_view(mode_list), name='cfda-list'),
     url(r'^cfda/(?P<program_number>[0-9]+\.[0-9]+)/', views.CfdaEndpoint.as_view(mode_detail), name='cfda-detail'),
