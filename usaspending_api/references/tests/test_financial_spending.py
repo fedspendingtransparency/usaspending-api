5f50cacc0c8125eadfa8087eede2919f599df021
TheRJB <bodine_russell@bah.com>
2017-06-06 12:01:47 -0400

    Test updates!

--- usaspending_api/references/tests/test_financial_spending.py
+++ usaspending_api/references/tests/test_financial_spending.py
@@ -11,18 +11,34 @@ from usaspending_api.references.models import Agency, ToptierAgency, SubtierAgen
 
 @pytest.fixture
 def financial_spending_data(db):
-    # agency w/ tt agency
-    ttagency1 = mommy.make('references.ToptierAgency', cgac_code="cgac_code", name="tta_name")
+    # Create 2 objects that should be returned and one that should not.
+    # Create AGENCY AND TopTier AGENCY For FinancialAccountsByProgramActivityObjectClass objects
+    ttagency1 = mommy.make('references.ToptierAgency', name="tta_name")
     mommy.make('references.Agency', id=1, toptier_agency=ttagency1)
-    # TAS w/ same ttagency
-    tas1 = mommy.make('accounts.TreasuryAppropriationAccount', awarding_toptier_agency=ttagency1, funding_toptier_agency=ttagency1)
 
+    # Object 1
+    tas1 = mommy.make('accounts.TreasuryAppropriationAccount', funding_toptier_agency=ttagency1)
     # Financial Account with Object class and submission
     object_class_1 = mommy.make('references.ObjectClass', major_object_class="mocCode",
                                 major_object_class_name="mocName", object_class="ocCode", object_class_name="ocName")
-    submission_1 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2017, cgac_code="cgac_code")
+    submission_1 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2017)
     mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', object_class=object_class_1,
                obligations_incurred_by_program_object_class_cpe=1000, submission=submission_1, treasury_account=tas1,
+               final_of_fy=True)
+
+    # Object 2
+    object_class_2 = mommy.make('references.ObjectClass', major_object_class="mocCode2",
+                                major_object_class_name="mocName2", object_class="ocCode2", object_class_name="ocName2")
+    submission_2 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2017)
+    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', object_class=object_class_2,
+               obligations_incurred_by_program_object_class_cpe=1000, submission=submission_2, treasury_account=tas1,
+               final_of_fy=True)
+
+    # not reported by api Object
+    tas3 = mommy.make('accounts.TreasuryAppropriationAccount', funding_toptier_agency=ttagency1)
+    submission_3 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2018)
+    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', object_class=object_class_1,
+               obligations_incurred_by_program_object_class_cpe=1000, submission=submission_3, treasury_account=tas3,
                final_of_fy=True)
 
 
@@ -33,7 +49,8 @@ def test_award_type_endpoint(client, financial_spending_data):
     resp = client.get('/api/v2/financial_spending/object_class/?fiscal_year=2017&funding_agency_id=1')
     print(resp.data)
     assert resp.status_code == status.HTTP_200_OK
-    assert len(resp.data['results']) > 0
+    assert len(resp.data['results']) == 2
 
+    # check for bad request due to missing params
     resp = client.get('/api/v2/financial_spending/object_class/')
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
