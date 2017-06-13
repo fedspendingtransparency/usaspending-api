2613b2a1dd8beea7444ada3612876c8b5cc8e626
TheRJB <bodine_russell@bah.com>
2017-06-13 10:51:22 -0400

    updates

--- usaspending_api/references/views_v2.py
+++ usaspending_api/references/views_v2.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.db.models import F, Sum
 from usaspending_api.references.models import Agency
 from usaspending_api.common.mixins import FilterQuerysetMixin, SuperLoggingMixin
 from usaspending_api.common.views import DetailViewSet, AutocompleteView
@@ -16,26 +16,23 @@ class AgencyEndpoint(SuperLoggingMixin,
     def get_queryset(self):
         """Return the view's queryset."""
         json_request = self.request.query_params
+        # get args
         agency_id = json_request.get('agency_id', None)
-
-        queryset = Agency.objects.filter(id=agency_id)
-
-        queryset = queryset.annotate(
-            agency_name=F('top_tier_agency__name'))
-        queryset = queryset.values('fiscal_year').annotate(
-            fiscal_year=Sum('budget_authority_available_amount_total_cpe'))
-
+        #
         queryset = AppropriationAccountBalances.final_objects.all()
-        # get the incoming agency's toptier agency, because that's what we'll
-        # need to filter on
-        # (used filter() instead of get() b/c we likely don't want to raise an
-        # error on a bad agency id)
         toptier_agency = Agency.objects.filter(id=agency_id).first().toptier_agency
+
         queryset = queryset.filter(
-            submission__reporting_fiscal_year=fiscal_year,
+            #submission__reporting_fiscal_year=fiscal_year,
             treasury_account_identifier__funding_toptier_agency=toptier_agency
         )
 
-        queryset = queryset.annotate(
+
+
+        queryset = queryset.values('agency_name').annotate(
             fiscal_year=F('submission__reporting_fiscal_year'))
+        # queryset = queryset.values('agency_name','fiscal_year').annotate(
+        #    fiscal_year=Sum('budget_authority_available_amount_total_cpe'))
+
+
         return queryset
