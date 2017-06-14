6cc6dbf1568cfe01e0ebe1daa8c1e62ccabc860f
Nipun Monga <nipunmnga@gmail.com>
2017-06-14 14:08:46 -0400

    Updating budget function autocomplete endpoint to use django's full text search to order by relevance

--- usaspending_api/references/views_v2/autocomplete.py
+++ usaspending_api/references/views_v2/autocomplete.py
@@ -1,11 +1,12 @@
-from usaspending_api.accounts.models import TreasuryAppropriationAccount
-from usaspending_api.references.serializers_v2.autocomplete import BudgetFunctionAutocompleteSerializer
-from usaspending_api.common.views import DetailViewSet, AutocompleteView
-from usaspending_api.common.exceptions import InvalidParameterException
-from django.db.models import F, Sum, Q
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from usaspending_api.accounts.models import TreasuryAppropriationAccount
 from usaspending_api.common.exceptions import status
+
+
+# SearchRank ranks a non-matching result with 1e-20
+SEARCH_RANK_NO_MATCH = 0.00000000000000000001
 
 
 @api_view(['GET'])
@@ -26,13 +27,19 @@ def budget_function_autocomplete_view(request):
 
     # Filter based on search text
     response = {}
-    function_queryset = queryset.values('budget_function_title').\
-        filter(Q(budget_function_title__icontains=search_text)).distinct()
-    subfunction_queryset = queryset.values('budget_subfunction_title').\
-        filter(Q(budget_subfunction_title__icontains=search_text)).distinct()
 
-    function_titles = function_queryset.values_list('budget_function_title', flat=True)
-    subfunction_titles = subfunction_queryset.values_list('budget_subfunction_title', flat=True)
+    function_vector = SearchVector('budget_function_title')
+    subfunction_vector = SearchVector('budget_subfunction_title')
+    query = SearchQuery(search_text)
+
+    function_results = queryset.annotate(rank=SearchRank(function_vector, query)).values('budget_function_title').\
+        distinct().filter(rank__gt=SEARCH_RANK_NO_MATCH).order_by('-rank')
+
+    subfunction_results = queryset.annotate(rank=SearchRank(subfunction_vector, query)).\
+        values('budget_subfunction_title').distinct().filter(rank__gt=SEARCH_RANK_NO_MATCH).order_by('-rank')
+
+    function_titles = function_results.values_list('budget_function_title', flat=True)
+    subfunction_titles = subfunction_results.values_list('budget_subfunction_title', flat=True)
 
     response['results'] = {'budget_function_title': function_titles,
                            'budget_subfunction_title': subfunction_titles}
