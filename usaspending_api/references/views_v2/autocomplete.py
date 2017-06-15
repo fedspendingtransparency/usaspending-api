2f2bffe439a58118aa3ea720c0d879c91da76eb9
Nipun Monga <nipunmnga@gmail.com>
2017-06-15 16:44:35 -0400

    Converting autocomplete budget function endpoint to POST and adding optional parameter "limit" which defaults to 10.

--- usaspending_api/references/views_v2/autocomplete.py
+++ usaspending_api/references/views_v2/autocomplete.py
@@ -12,17 +12,23 @@ class BaseAutocompleteViewSet(APIView):
 
 class BudgetFunctionAutocompleteViewSet(BaseAutocompleteViewSet):
 
-    def get(self, request):
+    def post(self, request):
         """Return all award spending by award type for a given fiscal year and agency id"""
 
-        json_request = request.query_params
+        json_request = request.data
 
         # retrieve search_text from request
         search_text = json_request.get('search_text', None)
 
+        try:
+            limit = int(json_request.get('limit', 10))
+        except ValueError:
+            return Response({'message': 'Limit request parameter is not a valid, positive integer'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
         # required query parameters were not provided
         if not search_text:
-            return Response({'message': 'Missing one or more required query parameters: search_text'},
+            return Response({'message': 'Missing one or more required request parameters: search_text'},
                             status=status.HTTP_400_BAD_REQUEST)
 
         queryset = TreasuryAppropriationAccount.objects.all()
@@ -40,8 +46,8 @@ class BudgetFunctionAutocompleteViewSet(BaseAutocompleteViewSet):
         subfunction_results = queryset.annotate(rank=SearchRank(subfunction_vector, query)). \
             values('budget_subfunction_title').distinct().filter(rank__gt=self.SEARCH_RANK_NO_MATCH).order_by('-rank')
 
-        function_titles = function_results.values_list('budget_function_title', flat=True)
-        subfunction_titles = subfunction_results.values_list('budget_subfunction_title', flat=True)
+        function_titles = function_results.values_list('budget_function_title', flat=True)[:limit]
+        subfunction_titles = subfunction_results.values_list('budget_subfunction_title', flat=True)[:limit]
 
         response['results'] = {'budget_function_title': function_titles,
                                'budget_subfunction_title': subfunction_titles}
