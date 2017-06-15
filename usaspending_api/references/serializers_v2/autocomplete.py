d984b9fac7cbe9fe99dc093d818e8c2ba8808605
Nipun Monga <nipunmnga@gmail.com>
2017-06-14 21:42:15 -0400

    Removing unused v2 serializer file

--- usaspending_api/references/serializers_v2/autocomplete.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class BudgetFunctionAutocompleteSerializer(serializers.Serializer):
-
-    budget_function_title = serializers.ListField()
-    budget_subfunction_title = serializers.ListField()
