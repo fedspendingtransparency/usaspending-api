d4b8a03b1eacad5b07ea51e55738bab408e72253
Jonathan Froiland <jon.froiland@gmail.com>
2017-06-28 11:20:58 -0400

    Removed route
    Unecessary route removed from federal obligations url

--- usaspending_api/accounts/urls_federal_obligations.py
+++ usaspending_api/accounts/urls_federal_obligations.py
@@ -5,10 +5,7 @@ from usaspending_api.accounts.views import federal_obligations as views
 # bind ViewSets to URLs
 federal_list = views.FederalAccountByObligationViewSet.as_view(
     {'get': 'list', 'post': 'list'})
-federal_detail = views.FederalAccountByObligationViewSet.as_view(
-    {'get': 'retrieve', 'post': 'retrieve'})
 
 urlpatterns = [
-    url(r'^$', federal_list),
-    url(r'(?P<pk>[0-9]+)/$', federal_detail)
+    url(r'^$', federal_list)
 ]
