c7689725ca296e26986fff03f5d529e32206c8c5
Nipun Monga <nipunmnga@gmail.com>
2017-07-04 00:56:09 -0400

    Adding v2 budget function autocomplete

--- usaspending_api/references/views_v2/autocomplete.py
+++ usaspending_api/references/views_v2/autocomplete.py
@@ -2,17 +2,22 @@ from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models.functions import Greatest
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from usaspending_api.awards.models import LegalEntity
+from usaspending_api.awards.models import LegalEntity, TreasuryAppropriationAccount
 from usaspending_api.common.exceptions import InvalidParameterException
 from usaspending_api.references.serializers_v2.autocomplete import RecipientAutocompleteSerializer
 
 
-class RecipientAutocompleteViewSet(APIView):
+class BaseAutocompleteViewSet(APIView):
 
-    serializer_class = RecipientAutocompleteSerializer
+    @staticmethod
+    def get_request_payload(request):
+        """
+            Retrieves all the request attributes needed for the autocomplete endpoints.
 
-    def post(self, request):
-        """Return all recipients matching the provided search text"""
+            Current attributes:
+            * search_text : string to search for
+            * limit : number of items to return
+        """
 
         json_request = request.data
 
@@ -27,6 +32,56 @@ class RecipientAutocompleteViewSet(APIView):
         # required query parameters were not provided
         if not search_text:
             raise InvalidParameterException('Missing one or more required request parameters: search_text')
+
+        return search_text, limit
+
+
+class BudgetFunctionAutocompleteViewSet(BaseAutocompleteViewSet):
+
+    def post(self, request):
+        """Return all budget function/subfunction titles matching the provided search text"""
+
+        search_text, limit = self.get_request_payload(request)
+
+        queryset = TreasuryAppropriationAccount.objects.all()
+
+        # Filter based on search text
+        response = {}
+
+        function_results = queryset.annotate(similarity=TrigramSimilarity('budget_function_title', search_text)).\
+            distinct().order_by('-similarity')
+
+        subfunction_results = queryset.annotate(similarity=TrigramSimilarity('budget_subfunction_title', search_text)).\
+            distinct().order_by('-similarity')
+
+        function_exact_match_queryset = function_results.filter(similarity=1.0)
+        if function_exact_match_queryset.count() > 0:
+            function_results = function_exact_match_queryset
+
+        subfunction_exact_match_queryset = subfunction_results.filter(similarity=1.0)
+        if subfunction_exact_match_queryset.count() > 0:
+            subfunction_results = subfunction_exact_match_queryset
+
+        function_titles = function_results.values_list('budget_function_title', flat=True)[:limit]
+        subfunction_titles = subfunction_results.values_list('budget_subfunction_title', flat=True)[:limit]
+
+        response['results'] = {'budget_function_title': function_titles,
+                               'budget_subfunction_title': subfunction_titles}
+
+        response['counts'] = {'budget_function_title': len(function_titles),
+                              'budget_subfunction_title': len(subfunction_titles)}
+
+        return Response(response)
+
+
+class RecipientAutocompleteViewSet(BaseAutocompleteViewSet):
+
+    serializer_class = RecipientAutocompleteSerializer
+
+    def post(self, request):
+        """Return all recipients matching the provided search text"""
+
+        search_text, limit = self.get_request_payload(request)
 
         queryset = LegalEntity.objects.all()
 
