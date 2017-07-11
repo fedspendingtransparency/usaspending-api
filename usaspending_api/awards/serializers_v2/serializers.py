95a85ffa9055f5bb3b88c7fe15b23400b4670400
Jonathan Froiland <jon.froiland@gmail.com>
2017-07-11 12:00:56 -0700

    Indentation
    Corrections.

--- usaspending_api/awards/serializers_v2/serializers.py
+++ usaspending_api/awards/serializers_v2/serializers.py
@@ -2,16 +2,15 @@ from rest_framework import serializers
 
 
 class AwardTypeAwardSpendingSerializer(serializers.Serializer):
-
     award_type = serializers.CharField()
     obligated_amount = serializers.DecimalField(None, 2)
 
 
 class RecipientSeriallizer(serializers.Serializer):
-        recipient_id = serializers.IntegerField()
-        recipient_name = serializers.CharField()
+    recipient_id = serializers.IntegerField()
+    recipient_name = serializers.CharField()
 
 
 class RecipientAwardSpendingSerializer(serializers.Serializer):
-        recipient = RecipientSeriallizer(source='*')
-        obligated_amount = serializers.DecimalField(None, 2)
+    recipient = RecipientSeriallizer(source='*')
+    obligated_amount = serializers.DecimalField(None, 2)
