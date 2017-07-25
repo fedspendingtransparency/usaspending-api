7ca2d24ec6bc555585420cc2181b89623abbf731
Jonathan Froiland <jon.froiland@gmail.com>
2017-07-25 11:11:49 -0400

    agency_recipient
    Nested obligated_amount with award_category

--- usaspending_api/awards/serializers_v2/serializers.py
+++ usaspending_api/awards/serializers_v2/serializers.py
@@ -9,9 +9,9 @@ class AwardTypeAwardSpendingSerializer(serializers.Serializer):
 class RecipientSerializer(serializers.Serializer):
     recipient_id = serializers.IntegerField()
     recipient_name = serializers.CharField()
-    obligated_amount = serializers.DecimalField(None, 2)
-
+    
 
 class RecipientAwardSpendingSerializer(serializers.Serializer):
     award_category = serializers.CharField()
+    obligated_amount = serializers.DecimalField(None, 2)
     recipient = RecipientSerializer(source='*')
