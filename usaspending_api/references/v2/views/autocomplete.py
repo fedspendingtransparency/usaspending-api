82a30bf934ff147b94ad94b65b414c4a399b4bd4
Jonathan Froiland <jon.froiland@gmail.com>
2017-07-31 18:20:48 -0400

    recipient_autocomplete_trigram
    Removed parent_recipient_unique_id from recipients queryset trigram search.

--- usaspending_api/references/v2/views/autocomplete.py
+++ usaspending_api/references/v2/views/autocomplete.py
@@ -228,8 +228,7 @@ class RecipientAutocompleteViewSet(BaseAutocompleteViewSet):
         recipients = queryset.annotate(
             similarity=Greatest(
                 TrigramSimilarity('recipient_name', search_text),
-                TrigramSimilarity('recipient_unique_id', search_text),
-                TrigramSimilarity('parent_recipient_unique_id', search_text))
+                TrigramSimilarity('recipient_unique_id', search_text))
         ).distinct().order_by('-similarity').exclude(
             parent_recipient_unique_id__in=F('recipient_unique_id'))
 
