cacbc7e2dc99b2182eb1b2fd49314edd4f136689
Daniel Boos <boos_daniel@bah.com>
2017-08-29 11:19:28 -0400

    Editing url regex for spending_over_time

--- usaspending_api/search/v2/urls_search.py
+++ usaspending_api/search/v2/urls_search.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from usaspending_api.search.v2.views.search import SpendingOverTimeVisualizationViewSet
 
 urlpatterns = [
-    url(r'^spending_over_time$', SpendingOverTimeVisualizationViewSet.as_view())
+    url(r'^spending_over_time', SpendingOverTimeVisualizationViewSet.as_view())
     ]
