f2a8f988189d9798269e87779c4e4fa9ffa3514b
Catherine Devlin <catherine.devlin@gsa.gov>
2017-08-30 16:58:14 -0400

    Added failing test to omit negative outlays

--- usaspending_api/references/tests/test_agency_v2.py
+++ usaspending_api/references/tests/test_agency_v2.py
@@ -24,12 +24,20 @@ def financial_spending_data(db):
     # submission_3 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2015, cgac_code='100')
     submission_1 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2017,
                               reporting_fiscal_quarter=2, cgac_code='100')
-    # submission_2 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2016, cgac_code='100')
+    submission_2 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2017,
+                              reporting_fiscal_quarter=2, cgac_code='100')
 
     # CREATE AppropriationAccountBalances
     aab = mommy.make('accounts.AppropriationAccountBalances', final_of_fy=True, reporting_period_start="2017-1-1",
                      submission=submission_1, budget_authority_available_amount_total_cpe=2,
                      obligations_incurred_total_by_tas_cpe=2, gross_outlay_amount_by_tas_cpe=2,
+                     treasury_account_identifier=tas)
+
+    # A negative appropriation account balance; this one should not
+    # show up in `obligated_amount`
+    mommy.make('accounts.AppropriationAccountBalances', final_of_fy=True, reporting_period_start="2016-1-1",
+                     submission=submission_2, budget_authority_available_amount_total_cpe=-0.5,
+                     obligations_incurred_total_by_tas_cpe=-0.5, gross_outlay_amount_by_tas_cpe=-0.5,
                      treasury_account_identifier=tas)
 
     # CREATE OverallTotals
@@ -43,8 +51,8 @@ def test_award_type_endpoint(client, financial_spending_data):
     resp = client.get('/api/v2/references/agency/1/')
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == {'results': {'agency_name': 'tta_name', 'active_fy': '2017', 'active_fq': '2',
-                                     'outlay_amount': '2.00', 'obligated_amount': '2.00',
-                                     'budget_authority_amount': '2.00',
+                                     'outlay_amount': '1.50', 'obligated_amount': '2.00',
+                                     'budget_authority_amount': '1.50',
                                      'current_total_budget_authority_amount': '3860000000.00',
                                      'website': 'http://test.com',
                                      'mission': 'test',
