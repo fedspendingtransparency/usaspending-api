6b0c3133ec4befb5a5b7523c160122d387323224
Nipun Monga <nipunmnga@gmail.com>
2017-09-01 16:46:26 -0400

    updates

--- usaspending_api/broker/management/commands/update_transactions.py
+++ usaspending_api/broker/management/commands/update_transactions.py
@@ -173,7 +173,7 @@ class Command(BaseCommand):
 
         place_of_performance_field_map = {
             # not sure place_of_performance_locat maps exactly to city name
-            "city_name": "place_of_performance_locat",
+            # "city_name": "place_of_performance_locat", # location id doesn't mean it's a city. Can't use this mapping
             "congressional_code": "place_of_performance_congr",
             "state_code": "place_of_performance_state",
             "zip4": "place_of_performance_zip4a",
