1335a32f755bea7b007892e5a083fe3514342007
hinson-sonia <hinson_sonia@bah.com>
2017-09-07 16:04:05 -0400

    Fixing the CodeClimate errors in federal accounts view

--- usaspending_api/accounts/views/federal_accounts_v2.py
+++ usaspending_api/accounts/views/federal_accounts_v2.py
@@ -31,22 +31,21 @@ class ObjectClassFederalAccountsViewSet(APIView):
         financial_account_queryset = \
             FinancialAccountsByProgramActivityObjectClass.objects.filter(treasury_account__in=tas_queryset) \
             .select_related('object_class').distinct('object_class') \
-            if tas_queryset.count() >=1 \
+            if tas_queryset.count() >= 1 \
             else FinancialAccountsByProgramActivityObjectClass.objects.filter(treasury_account=tas_queryset) \
             .select_related('object_class').distinct('object_class')
 
         # Retrieve only unique major class ids and names
         major_classes = set([(obj.object_class.major_object_class, obj.object_class.major_object_class_name)
-                        for obj in financial_account_queryset])
+                            for obj in financial_account_queryset])
         result = [
             {
                 'id': int(maj[0]),
                 'name': maj[1],
-                'minor_object_class': 
+                'minor_object_class':
                 [
                     {'id': obj[0], 'name': obj[1]}
                     for obj in set([(oc.object_class.object_class, oc.object_class.object_class_name)
-                    for oc in financial_account_queryset if oc.object_class.major_object_class == maj[0]])
                 ]
             }
             for maj in major_classes
