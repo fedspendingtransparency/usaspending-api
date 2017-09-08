4894c90d1f59c2fbb07a3c65e3399ff42b184990
hinson-sonia <hinson_sonia@bah.com>
2017-09-08 10:00:03 -0400

    Updated object filter view to return TAS as list

--- usaspending_api/accounts/views/federal_accounts_v2.py
+++ usaspending_api/accounts/views/federal_accounts_v2.py
@@ -20,35 +20,29 @@ class ObjectClassFederalAccountsViewSet(APIView):
         if fa is None:
             return Response(response)
 
-        # get list of relevant object classes to the FA
-        oc_list = []
-
         # get tas related to FA
-        tas_queryset = TreasuryAppropriationAccount.objects.filter(federal_account=fa)
+        tas_queryset = TreasuryAppropriationAccount.objects.filter(federal_account=fa) \
+            .values_list('treasury_account_identifier', flat=True)
 
-        # get fin based on tas, select oc, make distict values
-        # add conditions since '__in' will not work with non-querysets
+        # get fin based on tas, select oc, make distinct values
         financial_account_queryset = \
             FinancialAccountsByProgramActivityObjectClass.objects.filter(treasury_account__in=tas_queryset) \
-            .select_related('object_class').distinct('object_class') \
-            if tas_queryset.count() >= 1 \
-            else FinancialAccountsByProgramActivityObjectClass.objects.filter(treasury_account=tas_queryset) \
             .select_related('object_class').distinct('object_class')
 
         # Retrieve only unique major class ids and names
         major_classes = set([(obj.object_class.major_object_class, obj.object_class.major_object_class_name)
-                            for obj in financial_account_queryset])
+                             for obj in financial_account_queryset])
         result = [
             {
                 'id': int(maj[0]),
                 'name': maj[1],
                 'minor_object_class':
-                [
-                    {'id': obj[0], 'name': obj[1]}
-                    for obj in set([(oc.object_class.object_class, oc.object_class.object_class_name)
-                                    for oc in financial_account_queryset
-                                    if oc.object_class.major_object_class == maj[0]])
-                ]
+                    [
+                        {'id': obj[0], 'name': obj[1]}
+                        for obj in set([(oc.object_class.object_class, oc.object_class.object_class_name)
+                                        for oc in financial_account_queryset
+                                        if oc.object_class.major_object_class == maj[0]])
+                    ]
             }
             for maj in major_classes
         ]
