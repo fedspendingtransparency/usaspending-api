023be42c0858a5b067e92c34ad2e881c388b3aaa
TheRJB <bodine_russell@bah.com>
2017-09-08 11:51:07 -0400

    Fixed Test Case, changed minor object class id to int

--- usaspending_api/accounts/tests/test_federal_account_object_class_v2.py
+++ usaspending_api/accounts/tests/test_federal_account_object_class_v2.py
@@ -44,11 +44,16 @@ def test_federal_account_object_class_endpoint(client, financial_spending_data):
     resp = client.get('/api/v2/federal_accounts/1/available_object_classes')
     assert resp.status_code == status.HTTP_200_OK
 
-    assert resp.data["results"][0] in [
-        {'id': 10, 'name': 'mocName1', 'minor_object_class': [{'id': '111', 'name': 'ocName1'}]},
-        {'id': 20, 'name': 'mocName2', 'minor_object_class': [{'id': '222', 'name': 'ocName2'},
-                                                              {'id': '444', 'name': 'ocName4'}]}
-    ]
+    # test allows for arrays to be ordered in any way
+    assert (resp.data["results"][0] in [
+        {'id': 10, 'name': 'mocName1', 'minor_object_class': [{'id': 111, 'name': 'ocName1'}]},
+        {'id': 20, 'name': 'mocName2', 'minor_object_class': [{'id': 222, 'name': 'ocName2'},
+                                                              {'id': 444, 'name': 'ocName4'}]}
+    ] or resp.data["results"][0] in [
+        {'id': 10, 'name': 'mocName1', 'minor_object_class': [{'id': 111, 'name': 'ocName1'}]},
+        {'id': 20, 'name': 'mocName2', 'minor_object_class': [{'id': 444, 'name': 'ocName4'},
+                                                              {'id': 222, 'name': 'ocName2'}]}
+    ])
 
     # check for bad request due to missing params
     resp = client.get('/api/v2/federal_accounts/2/available_object_classes')
--- usaspending_api/accounts/views/federal_accounts_v2.py
+++ usaspending_api/accounts/views/federal_accounts_v2.py
@@ -38,7 +38,7 @@ class ObjectClassFederalAccountsViewSet(APIView):
                 'name': maj[1],
                 'minor_object_class':
                     [
-                        {'id': obj[0], 'name': obj[1]}
+                        {'id': int(obj[0]), 'name': obj[1]}
                         for obj in set([(oc.object_class.object_class, oc.object_class.object_class_name)
                                         for oc in financial_account_queryset
                                         if oc.object_class.major_object_class == maj[0]])
