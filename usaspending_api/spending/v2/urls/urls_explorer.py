b13e016e3666ade240929c68b4f804d86355a51e
Daniel Boos <boos_daniel@bah.com>
2017-09-12 17:46:35 -0400

    Updating SpendingExplorerViewSet url to stop at spending

--- usaspending_api/spending/v2/urls/urls_explorer.py
+++ usaspending_api/spending/v2/urls/urls_explorer.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from usaspending_api.spending.v2.views.spending_explorer import SpendingExplorerViewSet
 
 urlpatterns = [
-    url(r'^', SpendingExplorerViewSet.as_view())
+    url(r'^$', SpendingExplorerViewSet.as_view())
 ]
