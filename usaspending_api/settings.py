da97bf9ecbc4c5b77650bdde7d152905522dc195
Catherine Devlin <catherine.devlin@gsa.gov>
2017-09-12 14:18:41 -0400

    Default to saving locally unless DOWNLOAD_TO_S3 environment variable is set

--- usaspending_api/settings.py
+++ usaspending_api/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Define local flag to affect location of downloads
-IS_LOCAL = bool(os.getenv('DOWNLOAD_LOCALLY'))
+IS_LOCAL = not os.getenv('DOWNLOAD_TO_S3')
 
 # AWS locations for CSV files
 CSV_LOCAL_PATH = "/Volumes/exlinux/Users/catherine/werk/dataact/usaspending-api/downloads/"
