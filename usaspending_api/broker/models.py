86b6014c77c3f3d700170e87fe457e818ac03039
Nipun Monga <nipunmnga@gmail.com>
2017-09-19 20:32:54 -0400

    * Removing historical table logging for TransactionNormalized

--- usaspending_api/broker/models.py
+++ usaspending_api/broker/models.py
@@ -31,7 +31,6 @@ class TransactionNormalized(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, blank=True, null=True, help_text="The date this transaction was created in the API")
     update_date = models.DateTimeField(auto_now=True, null=True, help_text="The last time this transaction was updated in the API")
     fiscal_year = models.IntegerField(blank=True, null=True, help_text="Fiscal Year calculated based on Action Date")
-    history = HistoricalRecords()
 
     def __str__(self):
         return '%s award: %s' % (self.type_description, self.award)
