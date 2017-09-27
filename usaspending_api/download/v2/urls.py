94ecc4accb687e431ab558c2b40af4e7bbaba0ac
Will Jackson <willkjackson@gmail.com>
2017-09-27 15:24:10 -0400

    commenting out download v2 urls

--- usaspending_api/download/v2/urls.py
+++ usaspending_api/download/v2/urls.py
@@ -5,8 +5,8 @@ from usaspending_api.download.v2 import views
 
 
 urlpatterns = [
-    url(r'^awards', views.DownloadAwardsViewSet.as_view()),
+    # url(r'^awards', views.DownloadAwardsViewSet.as_view()),
     # url(r'^columns', DownloadColumnsViewSet.as_view()),
-    url(r'^status', views.DownloadStatusViewSet.as_view()),
-    url(r'^transactions', views.DownloadTransactionsViewSet.as_view()),
+    # url(r'^status', views.DownloadStatusViewSet.as_view()),
+    # url(r'^transactions', views.DownloadTransactionsViewSet.as_view()),
 ]
