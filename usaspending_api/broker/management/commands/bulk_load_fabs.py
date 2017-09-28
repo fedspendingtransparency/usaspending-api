d0ae3feb5b4a3c0df5f75f201f62bbbe0a3cef3f
Nipun Monga <nipunmnga@gmail.com>
2017-09-28 16:15:35 -0400

    Updating bulk loads to bulk create place of performance locations

--- usaspending_api/broker/management/commands/bulk_load_fabs.py
+++ usaspending_api/broker/management/commands/bulk_load_fabs.py
@@ -7,31 +7,34 @@ from django.db import connections, transaction as db_transaction
 from usaspending_api.etl.broker_etl_helpers import dictfetchall
 from usaspending_api.awards.models import TransactionFABS
 from usaspending_api.etl.management.load_base import load_data_into_model
-
+from usaspending_api.references.helpers import canonicalize_location_dict
+from usaspending_api.references.models import RefCountryCode, Location, RefCityCountyCode
 
 
 logger = logging.getLogger('console')
 exception_logger = logging.getLogger("exceptions")
 
+country_code_map = {country['country_code']: country['country_name'] for country in RefCountryCode.objects.values('country_code', 'country_name')}
+
+pop_lookup = {}
+pop_bulk = []
+
+lel_lookup = {}
+lel_bulk = []
 
 class Command(BaseCommand):
     help = "Update historical transaction data for a fiscal year from the Broker."
 
     @staticmethod
-    def update_transaction_assistance(db_cursor, fiscal_year=None, page=1, limit=500000):
-        logger.info('Starting bulk loading for FABS data')
-
-        query = "SELECT * FROM published_award_financial_assistance"
+    def get_fabs_data(db_cursor, fiscal_year=None, page=1, limit=500000):
+        query = "SELECT * FROM published_award_financial_assistance WHERE is_active=TRUE"
         arguments = []
 
         fy_begin = '10/01/' + str(fiscal_year - 1)
         fy_end = '09/30/' + str(fiscal_year)
 
         if fiscal_year:
-            if arguments:
-                query += " AND"
-            else:
-                query += " WHERE"
+            query += " AND"
             query += ' action_date::Date BETWEEN %s AND %s'
             arguments += [fy_begin]
             arguments += [fy_end]
@@ -43,15 +46,18 @@ class Command(BaseCommand):
         db_cursor.execute(query, arguments)
 
         logger.info("Running dictfetchall on db_cursor")
-        fabs_broker_data = dictfetchall(db_cursor)
+        return dictfetchall(db_cursor)
+
+    @staticmethod
+    def load_transaction_fabs(fabs_broker_data, total_rows):
+        logger.info('Starting bulk loading for FABS data')
 
         fabs_bulk = []
 
-        total_rows = len(fabs_broker_data)
         start_time = datetime.now()
         for index, row in enumerate(fabs_broker_data, 1):
-            if not (index % 100):
-                logger.info('D1 File Load: Loading row {} of {} ({})'.format(str(index),
+            if not (index % 10000):
+                logger.info('Transaction FABS: Loading row {} of {} ({})'.format(str(index),
                                                                              str(total_rows),
                                                                              datetime.now() - start_time))
 
@@ -64,8 +70,86 @@ class Command(BaseCommand):
             fabs_bulk.append(fabs_instance)
 
         with db_transaction.atomic():
-            logger.info('Bulk creating...')
+            logger.info('Bulk creating Transaction FABS...')
             TransactionFABS.objects.bulk_create(fabs_bulk)
+
+
+    @staticmethod
+    def load_pop_locations(fabs_broker_data, total_rows):
+
+        pop_field_map = {
+            "city_name": "place_of_performance_city",
+            "performance_code": "place_of_performance_code",
+            "congressional_code": "place_of_performance_congr",
+            "county_name": "place_of_perform_county_na",
+            "foreign_location_description": "place_of_performance_forei",
+            "state_name": "place_of_perform_state_nam",
+            "zip4": "place_of_performance_zip4a",
+            "location_country_code": "place_of_perform_country_c"
+
+        }
+
+        start_time = datetime.now()
+        for index, row in enumerate(fabs_broker_data, 1):
+            if not (index % 10000):
+                logger.info('Locations: Loading row {} of {} ({})'.format(str(index),
+                                                                             str(total_rows),
+                                                                             datetime.now() - start_time))
+            location_value_map = {"place_of_performance_flag": True}
+            row = canonicalize_location_dict(row)
+
+            country_code = row[pop_field_map.get('location_country_code')]
+            pop_code = row[pop_field_map.get('performance_code')]
+
+            # We can assume that if the country code is blank and the place of performance code is NOT '00FORGN', then
+            # the country code is USA
+            if not country_code and pop_code != '00FORGN':
+                row[pop_field_map.get('location_country_code')] = 'USA'
+
+            # Get country code obj
+            location_country_code = country_code_map.get(row[pop_field_map.get('location_country_code')])
+
+            # Fix state code periods
+            state_code = row.get(pop_field_map.get('state_code'))
+            if state_code is not None:
+                pop_field_map.update({'state_code': state_code.replace('.', '')})
+
+            if location_country_code:
+                location_value_map.update({
+                    'location_country_code': location_country_code,
+                    'country_name': country_code_map[location_country_code]
+                })
+
+                if location_country_code != 'USA':
+                    location_value_map.update({
+                        'state_code': None,
+                        'state_name': None
+                    })
+            else:
+                # no country found for this code
+                location_value_map.update({
+                    'location_country_code': None,
+                    'country_name': None
+                })
+
+            pop_instance_data = load_data_into_model(
+                Location(),
+                row,
+                value_map=location_value_map,
+                field_map=pop_field_map,
+                as_dict=True)
+
+            pop_instance = Location(**pop_instance_data)
+            pop_instance.load_country_data()
+            pop_instance.load_city_county_data()
+            pop_instance.fill_missing_state_data()
+
+            pop_bulk.append(pop_instance)
+            pop_lookup[index] = pop_instance
+
+        with db_transaction.atomic():
+            logger.info('Bulk creating Place of Performance Locations...')
+            Location.objects.bulk_create(pop_bulk)
 
     def add_arguments(self, parser):
 
@@ -110,7 +194,21 @@ class Command(BaseCommand):
         page = page[0] if page else 1
         limit = limit[0] if limit else 500000
 
+        logger.info('Get Broker FABS data...')
         start = timeit.default_timer()
-        self.update_transaction_assistance(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit)
+        fabs_broker_data = self.get_fabs_data(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit)
+        total_rows = len(fabs_broker_data)
+        end = timeit.default_timer()
+        logger.info('Finished getting Broker FABS data in ' + str(end - start) + ' seconds')
+
+        logger.info('Loading Transaction FABS data...')
+        start = timeit.default_timer()
+        self.load_transaction_fabs(fabs_broker_data, total_rows)
         end = timeit.default_timer()
         logger.info('Finished FABS bulk data load in ' + str(end - start) + ' seconds')
+
+        logger.info('Loading POP Location data...')
+        start = timeit.default_timer()
+        self.load_pop_locations(fabs_broker_data, total_rows)
+        end = timeit.default_timer()
+        logger.info('Finished POP Location bulk data load in ' + str(end - start) + ' seconds')
