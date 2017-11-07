f265407579f0e4f94bf8cc0ae12041032067092e
Nipun Monga <nipunmnga@gmail.com>
2017-11-07 14:03:32 -0500

    * Updated references to the last modified date to pull from contract/assistance data relationships as opposed to the top level award object * Updated loaders to use the right modified date to load into TransactionNormalized and thus properly propagate to Award

--- usaspending_api/broker/management/commands/bulk_load_fpds.py
+++ usaspending_api/broker/management/commands/bulk_load_fpds.py
@@ -418,6 +418,7 @@ class Command(BaseCommand):
                 "period_of_performance_start_date": format_date(row['period_of_performance_star']),
                 "period_of_performance_current_end_date": format_date(row['period_of_performance_curr']),
                 "action_date": format_date(row['action_date']),
+                "last_modified_date": row['last_modified']
             }
 
             contract_field_map = {
