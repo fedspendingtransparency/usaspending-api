db8f0edd3b5f6c20dd166a968918824bc11ab61f
Daniel Boos <boos_daniel@bah.com>
2017-11-27 10:29:30 -0500

    Filter only by awarding_agency for bulk download

--- usaspending_api/bulk_download/v2/views.py
+++ usaspending_api/bulk_download/v2/views.py
@@ -304,11 +304,9 @@ class BulkDownloadAwardsViewSet(BaseDownloadViewSet):
         # Agencies are to be OR'd together and then AND'd to the major query
         agencies_queryset = None
         if filters['agency'] != 'all':
-            agencies_queryset = (Q(awarding_agency__toptier_agency_id=filters['agency']) |
-                           Q(funding_agency__toptier_agency_id=filters['agency']))
+            agencies_queryset = Q(awarding_agency__toptier_agency_id=filters['agency'])
             if 'sub_agency' in filters and filters['sub_agency']:
-                agencies_queryset = (agencies_queryset & (Q(awarding_agency__subtier_agency_id=filters['sub_agency']) |
-                                                          Q(funding_agency__subtier_agency_id=filters['sub_agency'])))
+                agencies_queryset &= Q(awarding_agency__subtier_agency_id=filters['sub_agency'])
             queryset &= table.objects.filter(agencies_queryset)
 
         return queryset
