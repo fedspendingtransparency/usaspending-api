7023be575156c89e69541a8163f41ebb9a365f15
Will Jackson <willkjackson@gmail.com>
2017-11-29 11:44:50 -0500

    aligning with matview sql script

--- usaspending_api/awards/models_matviews.py
+++ usaspending_api/awards/models_matviews.py
@@ -109,47 +109,54 @@ class UniversalAwardView(models.Model):
     base_and_all_options_value = models.DecimalField(
         max_digits=20, decimal_places=2, blank=True,
         null=True)
+
     recipient_id = models.IntegerField()
     recipient_name = models.TextField()
     recipient_unique_id = models.TextField()
     parent_recipient_unique_id = models.TextField()
     business_categories = ArrayField(models.TextField(), default=list)
+
     issued_date = models.DateField()
     issued_date_fiscal_year = models.IntegerField()
+
     face_value_loan_guarantee = models.DecimalField(
         max_digits=23, decimal_places=2, blank=True,
         null=True)
     original_loan_subsidy_cost = models.DecimalField(
         max_digits=23, decimal_places=2, blank=True,
         null=True)
+
     awarding_toptier_agency_name = models.TextField()
     funding_toptier_agency_name = models.TextField()
     awarding_subtier_agency_name = models.TextField()
     funding_subtier_agency_name = models.TextField()
     awarding_agency_office_name = models.TextField()
     funding_agency_office_name = models.TextField()
+
     recipient_location_address_line1 = models.TextField()
     recipient_location_address_line2 = models.TextField()
     recipient_location_address_line3 = models.TextField()
     recipient_location_country_name = models.TextField()
     recipient_location_country_code = models.TextField()
     recipient_location_state_code = models.TextField()
-    recipient_location_foreign_province = models.TextField()
     recipient_location_county_name = models.TextField()
     recipient_location_county_code = models.TextField()
     recipient_location_city_name = models.TextField()
     recipient_location_zip5 = models.TextField()
     recipient_location_congressional_code = models.TextField()
+    recipient_location_foreign_province = models.TextField()
+
     pop_country_name = models.TextField()
     pop_country_code = models.TextField()
     pop_state_name = models.TextField()
     pop_state_code = models.TextField()
-    pop_congressional_code = models.TextField()
-    pop_zip5 = models.TextField()
-    pop_city_name = models.TextField()
-    pop_foreign_province = models.TextField()
     pop_county_name = models.TextField()
     pop_county_code = models.TextField()
+    pop_city_name = models.TextField()
+    pop_zip5 = models.TextField()
+    pop_congressional_code = models.TextField()
+    pop_foreign_province = models.TextField()
+
     cfda_number = models.TextField()
     pulled_from = models.TextField()
     type_of_contract_pricing = models.TextField()
