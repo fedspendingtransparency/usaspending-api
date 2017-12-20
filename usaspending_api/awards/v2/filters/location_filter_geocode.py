118fc30942b216ae794cbc015d67f7d1b9479979
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2017-12-20 16:00:26 -0500

    proof-of-concept for new sql structure

--- usaspending_api/awards/v2/filters/location_filter_geocode.py
+++ usaspending_api/awards/v2/filters/location_filter_geocode.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.db.models import Q
 from usaspending_api.common.exceptions import InvalidParameterException
 
 
@@ -20,30 +21,63 @@ def geocode_filter_locations(scope, values, model, use_matview=False, default_mo
     returns queryset
     """
     q_str, loc_dict = return_query_strings(use_matview)
-    queryset_init = False
-    or_queryset = None
+    # queryset_init = False
+    # or_queryset = None
 
-    for v in values:
-        fields = v.keys()
+    if type(model) == str:
+        model = apps.get_model(default_model, model)
 
-        check_location_fields(fields)
+    or_queryset = model.objects.filter(new_sql_filter(values, loc_dict, scope))
 
-        kwargs = {}
-        for loc_scope in fields:
-            if loc_dict.get(loc_scope) is not None:
-                key_str = q_str.format(scope, loc_dict.get(loc_scope))
-                kwargs[key_str] = get_fields_list(loc_dict.get(loc_scope), v.get(loc_scope), loc_dict)
+    # for v in values:
+    #     fields = v.keys()
 
-        if type(model) == str:
-            model = apps.get_model(default_model, model)
-        qs = model.objects.filter(**kwargs)
+    #     check_location_fields(fields)
 
-        if queryset_init:
-            or_queryset |= qs
-        else:
-            queryset_init = True
-            or_queryset = qs
+    #     kwargs = {}
+    #     for loc_scope in fields:
+    #         if loc_dict.get(loc_scope) is not None:
+    #             key_str = q_str.format(scope, loc_dict.get(loc_scope))
+    #             kwargs[key_str] = get_fields_list(loc_dict.get(loc_scope), v.get(loc_scope), loc_dict)
+
+    #     if type(model) == str:
+    #         model = apps.get_model(default_model, model)
+    #     print(f'+++++++++++++++++++++++++++++++++++++\n{kwargs}\n')
+    #     qs = model.objects.filter(**kwargs)
+
+    #     if queryset_init:
+    #         or_queryset |= qs
+    #     else:
+    #         queryset_init = True
+    #         or_queryset = qs
     return or_queryset
+
+
+def new_sql_filter(values, loc_dict, scope):
+    nested_locations = {}
+    for v in values:
+        if v['country'] not in nested_locations.keys():
+            nested_locations[v['country']] = {}
+            nested_locations[v['country']][v['state']] = [v['county']]
+        elif v['state'] not in nested_locations[v['country']].keys():
+            nested_locations[v['country']][v['state']] = [v['county']]
+        else:
+            nested_locations[v['country']][v['state']].append(v['county'])
+
+    country_list = Q()
+    for country, vals in nested_locations.items():
+        state_list = []
+        for state, county_list in vals.items():
+            counties = Q(**{f'{scope}_state_code__exact': state})
+            counties &= Q(**{f'{scope}_county_code__in': county_list})
+            state_list.append(counties)
+
+        temp = Q()
+        for x in state_list:
+            temp |= x
+        country_list |= Q(**{f'{scope}_country_code__exact': country}) & temp
+
+    return country_list
 
 
 def check_location_fields(fields):
