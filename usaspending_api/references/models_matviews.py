93285955c92b15012ea028843d88731f7e0b55d7
Daniel Boos <boos_daniel@bah.com>
2018-01-04 18:09:06 -0500

    ExecCompMatview (migration excluded)

---
+++ usaspending_api/references/models_matviews.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class ExecCompMatview(models.Model):
+    duns = models.TextField(blank=True, default='', null=True, verbose_name="DUNS Number")
+    officer_1_name = models.TextField(null=True, blank=True)
+    officer_1_amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    officer_2_name = models.TextField(null=True, blank=True)
+    officer_2_amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    officer_3_name = models.TextField(null=True, blank=True)
+    officer_3_amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    officer_4_name = models.TextField(null=True, blank=True)
+    officer_4_amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+    officer_5_name = models.TextField(null=True, blank=True)
+    officer_5_amount = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'exec_comp_matview'
