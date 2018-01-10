4bb6ce33fd9f81537821db44a9623856a63c4f5d
Daniel Boos <boos_daniel@bah.com>
2018-01-10 18:41:20 -0500

    Updating agency_lookup to combine relevant data from toptier, subtier, and office tables

--- usaspending_api/database_scripts/broker_matviews/agency_lookup_matview.sql
+++ usaspending_api/database_scripts/broker_matviews/agency_lookup_matview.sql
@@ -4,11 +4,22 @@ create materialized view agency_lookup as
 (     
 	select
 		agency.id as agency_id,
-		subtier_agency.subtier_code as subtier_code     
+		toptier_agency.cgac_code as toptier_cgac,
+		toptier_agency.name as toptier_name,
+		toptier_agency.abbreviation as toptier_abbr,
+		subtier_agency.subtier_code as subtier_code,
+		subtier_agency.name as subtier_name,
+		subtier_agency.abbreviation as subtier_abbr,
+		office_agency.aac_code as office_code,
+		office_agency.name as office_name
 	from
-		agency         
+		agency
+	inner join
+		toptier_agency on toptier_agency.toptier_agency_id=agency.toptier_agency_id
 	inner join
 		subtier_agency on subtier_agency.subtier_agency_id=agency.subtier_agency_id
+	left outer join
+		office_agency on office_agency.office_agency_id=agency.office_agency_id
 );
 
 create index agency_lookup_subtier_code_idx on agency_lookup using btree (subtier_code);
