51f18c99070cdbf82bcfd51318581750eccaba2e
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-01-21 02:40:37 -0500

    Added DB columns, improved comments, fixed bugs

--- usaspending_api/etl/management/commands/fetch_transaction_delta.py
+++ usaspending_api/etl/management/commands/fetch_transaction_delta.py
@@ -1,12 +1,11 @@
 import boto3
-import csv
-import datetime
 import os
 import pandas as pd
 import pytz
 import subprocess
 import tempfile
 
+from datetime import date, datetime
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection
@@ -16,13 +15,14 @@ from time import perf_counter
 TEMP_ES_DELTA_VIEW = '''
 CREATE OR REPLACE VIEW transaction_delta_view AS
 SELECT
-  UTM.awarding_agency_id,
   UTM.transaction_id,
   TM.modification_number,
   UAM.award_id,
-  AW.description AS award_description,
   UTM.piid,
   UTM.fain,
+  UTM.uri,
+  AW.description AS award_description,
+
   UTM.product_or_service_code,
   UTM.product_or_service_description,
   UTM.naics_code,
@@ -43,6 +43,8 @@ SELECT
   UAM.face_value_loan_guarantee,
   UAM.original_loan_subsidy_cost,
 
+  UTM.awarding_agency_id,
+  UTM.funding_agency_id,
   UAM.awarding_toptier_agency_name,
   UTM.funding_toptier_agency_name,
   UTM.awarding_subtier_agency_name,
@@ -60,12 +62,12 @@ SELECT
   UTM.pulled_from,
   UTM.type,
 
-  UTM.pop_country_name,
   UTM.pop_country_code,
-  UTM.pop_zip5,
+  UTM.pop_country_name,
+  UTM.pop_state_code,
   UTM.pop_county_code,
   UTM.pop_county_name,
-  UTM.pop_state_code,
+  UTM.pop_zip5,
   UTM.pop_congressional_code,
 
   UTM.recipient_location_country_code,
@@ -86,11 +88,11 @@ WHERE
   UTM.fiscal_year={fy}{update_date};'''
 
 UPDATE_DATE_SQL = ' AND TM.update_date >= \'{}\''
-COPY_SQL = '''
-"COPY (
+
+COPY_SQL = '''"COPY (
     SELECT *
     FROM transaction_delta_view
-) TO STDOUT DELIMITER ',' CSV HEADER" > '{dir}{fy}_transactions_{now}.csv'
+) TO STDOUT DELIMITER ',' CSV HEADER" > '{filename}'
 '''
 
 CHECK_IDS_SQL = '''
@@ -104,17 +106,17 @@ TEMP_SQL_FILE = os.path.join(HERE, 'temp_sql_cmds.sql')
 
 
 class Command(BaseCommand):
-    help = 'Creates a CSV dump of transactions'
+    help = 'Creates two CSV files: one a list of all transactions in a fiscal year and another of deleted transactions'
 
     def add_arguments(self, parser):
         parser.add_argument('--fiscal_year', type=int, required=True)
-        parser.add_argument('--diff_from', default='2008-10-01', type=str)
+        parser.add_argument('--since', default='2008-10-01', type=str)
         parser.add_argument('--verbose', action='store_true')
 
-    def create_sql_commands(self):
-        update_date_str = UPDATE_DATE_SQL.format(self.diff_from.strftime('%Y-%m-%d'))
+    def create_sql_commands(self, filename):
+        update_date_str = UPDATE_DATE_SQL.format(self.starting_date.strftime('%Y-%m-%d'))
         view_sql = TEMP_ES_DELTA_VIEW.format(fy=self.fiscal_year, update_date=update_date_str)
-        copy_sql = COPY_SQL.format(dir=HERE, fy=self.fiscal_year, now=self.formatted_now)
+        copy_sql = COPY_SQL.format(filename=filename)
 
         if self.deleted_ids:
             id_list = ','.join(["('{}')".format(x) for x in self.deleted_ids.keys()])
@@ -125,18 +127,25 @@ class Command(BaseCommand):
         return view_sql, copy_sql, id_sql
 
     def run_db_commands(self):
+        print('---------------------------------------------------------------')
+        print('Executing Postgres statements')
         start = perf_counter()
-        view_sql, copy_sql, id_sql = self.create_sql_commands()
+        filename = '{dir}{fy}_transactions_{now}.csv'.format(dir=HERE, fy=self.fiscal_year, now=self.formatted_now)
+        view_sql, copy_sql, id_sql = self.create_sql_commands(filename)
 
         self.execute_sql(view_sql)
-        # It is prefereable to not use shell=True, but this command works. Limited user-input so risk is low
+
+        print('storing FY transactions here:')
+        print(filename)
+        # It is preferable to not use shell=True, but this command works. Limited user-input so risk is low
         subprocess.Popen('psql "${{DATABASE_URL}}" -c {}'.format(copy_sql), shell=True).wait()
 
         if id_sql:
             restored_ids = self.execute_sql(id_sql, True)
-            print(restored_ids)
+            if self.verbose:
+                print(restored_ids)
             print('{} "deleted" IDs were found'.format(len(restored_ids)))
-            self.correct_deleted_ids()
+            self.correct_deleted_ids(restored_ids)
 
         self.execute_sql('DROP VIEW transaction_delta_view;')
         print("Database commands took {} seconds".format(perf_counter() - start))
@@ -160,16 +169,17 @@ class Command(BaseCommand):
         return rows
 
     def gather_deleted_ids(self):
+        print('---------------------------------------------------------------')
+        print('Gathering all deleted transactions from S3')
         start = perf_counter()
         s3 = boto3.resource('s3', region_name=settings.CSV_AWS_REGION)
         bucket = s3.Bucket(settings.CSV_2_S3_BUCKET_NAME)
         bucket.objects.all()
 
         if self.verbose:
-            print('=======================')
-            print('From {} to now...'.format(self.diff_from))
+            print('Gathering data from {} to now.'.format(self.starting_date))
 
-        to_datetime = datetime.datetime.combine(self.diff_from, datetime.datetime.min.time(), tzinfo=pytz.UTC)
+        to_datetime = datetime.combine(self.starting_date, datetime.min.time(), tzinfo=pytz.UTC)
         csv_list = [
             x for x in bucket.objects.all()
             if x.key.endswith('.csv') and x.last_modified >= to_datetime]
@@ -178,17 +188,20 @@ class Command(BaseCommand):
             print('Found {} csv files'.format(len(csv_list)))
 
         for obj in csv_list:
-            # Use temporary files to facilitate the csv files from S3 into pands
+            # Use temporary files to facilitate date moving from csv files on S3 into pands
             (file, file_path) = tempfile.mkstemp()
             bucket.download_file(obj.key, file_path)
+
             # Ingests the CSV into a dataframe. pandas thinks some ids are dates, so disable parsing
             data = pd.DataFrame.from_csv(file_path, parse_dates=False)
             new_ids = list(data['detached_award_proc_unique'].values)
+
             # Next statements are ugly, but properly handle the temp files
             os.close(file)
             os.remove(file_path)
+
             if self.verbose:
-                print('file {} has {} ids'.format(obj.key, len(new_ids)))
+                print('{:<55} ... {}'.format(obj.key, len(new_ids)))
 
             for uid in new_ids:
                 if uid in self.deleted_ids:
@@ -205,23 +218,29 @@ class Command(BaseCommand):
         print("Gathering deleted transactions took {} seconds".format(perf_counter() - start))
 
     def correct_deleted_ids(self, existing_ids):
+        '''
+            This function removeds transactions from the deleted list.
+            Necessary for when a transaction was previously deleted and then recreted
+        '''
+        print('Verifying deleted transactions')
         for record in existing_ids:
             uid = record['detached_award_proc_unique']
             if uid in self.deleted_ids:
                 if record['update_date'] > self.deleted_ids[uid]['timestamp']:
+                    # Remove recreated transaction from the final list
                     del self.deleted_ids[uid]
 
     def handle(self, *args, **options):
         start = perf_counter()
         self.deleted_ids = {}
-        self.formatted_now = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')  # ISO8601
+        self.formatted_now = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')  # ISO8601
         self.verbose = options['verbose']
         self.fiscal_year = options['fiscal_year']
 
         try:
-            self.diff_from = datetime.date(*[int(x) for x in options['diff_from'].split('-')])
+            self.starting_date = date(*[int(x) for x in options['since'].split('-')])
         except Exception:
-            print('Malformed date string provided for `diff_from`. Requires YYYY-MM-DD')
+            print('Malformed date string provided. `--since` requires YYYY-MM-DD')
             raise SystemExit
 
         # 1. Gather the list of deleted transactions
@@ -234,9 +253,14 @@ class Command(BaseCommand):
         self.gather_deleted_ids()
         self.run_db_commands()
 
+        print('---------------------------------------------------------------')
+        print('Writing deleted transactions csv')
         csv_file = HERE + 'deleted_ids_{}.csv'.format(self.formatted_now)
+        print('Filepath:')
+        print(csv_file)
 
         with open(csv_file, 'w') as f:
             f.writelines('\n'.join(self.deleted_ids.keys()))
 
+        print('---------------------------------------------------------------')
         print("Script completed in {} seconds".format(perf_counter() - start))
