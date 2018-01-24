7c173463e01a0acf7a423f5cecf81f16b31602e7
Michael Hess <hess_michael@bah.com>
2018-01-24 15:34:20 -0500

    Update tests

--- usaspending_api/awards/tests/test_awards.py
+++ usaspending_api/awards/tests/test_awards.py
@@ -213,77 +213,65 @@ def test_get_or_create_summary_award():
     assert t3 == m3
 
     # match on awarding agency and fain + uri (fain takes precedence, same uri)
-    m4 = mommy.make('awards.award', fain='DUT987', uri='123-abc-456', awarding_agency=a1)
-    t4 = Award.get_or_create_summary_award(fain='DUT987', uri='123-abc-456', awarding_agency=a1)[1]
+    m4 = mommy.make('awards.award', fain='DUT987', awarding_agency=a1)
+    t4 = Award.get_or_create_summary_award(fain='DUT987', uri='123-abc-456', record_type='2', awarding_agency=a1)[1]
     assert t4 == m4
 
-    # fain + uri are unique on their own
-    m5 = mommy.make('awards.award', fain='DUT123456', uri='123-abc-456', awarding_agency=a1)
-    t5 = Award.get_or_create_summary_award(fain='DUT123456', uri='123-abc-456-a-different-uri', awarding_agency=a1)[1]
-    assert t5 != m5
-
     # match on awarding agency + uri
-    m6 = mommy.make('awards.award', uri='abc-123-def', awarding_agency=a1)
-    t6 = Award.get_or_create_summary_award(uri='abc-123-def', awarding_agency=a1)[1]
-    assert t6 == m6
+    m5 = mommy.make('awards.award', uri='abc-123-def', awarding_agency=a1)
+    t5 = Award.get_or_create_summary_award(uri='abc-123-def', record_type='1', awarding_agency=a1)[1]
+    assert t5 == m5
 
     # match on awarding toptier agency only
-    m7 = mommy.make('awards.award', uri='kkk-bbb-jjj', awarding_agency=a2)
-    t7 = Award.get_or_create_summary_award(uri='kkk-bbb-jjj', awarding_agency=a2)[1]
-    assert m7 == t7
+    m6 = mommy.make('awards.award', uri='kkk-bbb-jjj', awarding_agency=a2)
+    t6 = Award.get_or_create_summary_award(uri='kkk-bbb-jjj', record_type='1', awarding_agency=a2)[1]
+    assert m6 == t6
 
     # match on no awarding agency, uri
-    m8 = mommy.make('awards.award', uri='mmm-fff-ddd')
-    t8 = Award.get_or_create_summary_award(uri='mmm-fff-ddd')[1]
-    assert m8 == t8
+    m7 = mommy.make('awards.award', uri='mmm-fff-ddd')
+    t7 = Award.get_or_create_summary_award(uri='mmm-fff-ddd', record_type='1')[1]
+    assert m7 == t7
 
     # match on no awarding agency, fain
-    m9 = mommy.make('awards.award', fain='DUTDUTDUT')
-    t9 = Award.get_or_create_summary_award(fain='DUTDUTDUT')[1]
-    assert m9 == t9
+    m8 = mommy.make('awards.award', fain='DUTDUTDUT')
+    t8 = Award.get_or_create_summary_award(fain='DUTDUTDUT', record_type='2')[1]
+    assert m8 == t8
 
     # non-match with piid creates new award record
-    m10 = mommy.make('awards.award', piid='imapiid')
-    t10 = Award.get_or_create_summary_award(piid='imadifferentpiid')
+    m9 = mommy.make('awards.award', piid='imapiid')
+    t9 = Award.get_or_create_summary_award(piid='imadifferentpiid')
+    assert len(t9[0]) == 1
+    assert m9 != t9[1]
+
+    # non-match with piid + matching parent award creates one new award
+    pa10 = mommy.make('awards.award', piid='thingmom')
+    m10 = mommy.make('awards.award', piid='thing1', parent_award=pa10)
+    t10 = Award.get_or_create_summary_award(piid='thing2', parent_award_id='thingmom')
     assert len(t10[0]) == 1
     assert m10 != t10[1]
 
-    # non-match with piid + non-matching parent award creates two new awards
-    pa11 = mommy.make('awards.award', piid='momofpiidsarefun')
-    m11 = mommy.make('awards.award', piid='piidsarefun', parent_award=pa11)
-    t11 = Award.get_or_create_summary_award(piid='piidsarefun', parent_award_id='dadofpiidsarefun')
-    assert len(t11[0]) == 2
-    assert m11 != t11[1]
-
-    # non-match with piid + matching parent award creates one new award
-    pa12 = mommy.make('awards.award', piid='thingmom')
-    m12 = mommy.make('awards.award', piid='thing1', parent_award=pa12)
-    t12 = Award.get_or_create_summary_award(piid='thing2', parent_award_id='thingmom')
-    assert len(t12[0]) == 1
-    assert m12 != t12[1]
-
     # matching piid + non-matching parent
-    pa13 = mommy.make('awards.award', piid='piidthing')
-    m13 = mommy.make('awards.award', piid='0005', parent_award=pa13)
-    t13 = Award.get_or_create_summary_award(piid='0005', parent_award_id='anotherpiidthing')[1]
-    assert t13 != m13
+    pa11 = mommy.make('awards.award', piid='piidthing')
+    m11 = mommy.make('awards.award', piid='0005', parent_award=pa11)
+    t11 = Award.get_or_create_summary_award(piid='0005', parent_award_id='anotherpiidthing')[1]
+    assert t11 != m11
 
     # matching piid and parent award id but mismatched subtier agency, same top tier agency
-    pa14 = mommy.make('awards.award', piid='imalittlepiid', awarding_agency=a1)
-    m14 = mommy.make('awards.award', piid='shortandstout', parent_award=pa14, awarding_agency=a1)
-    t14 = Award.get_or_create_summary_award(piid='shortandstout', parent_award_id='imalittlepiid', awarding_agency=a3)[1]
-    assert t14 != m14
+    pa12 = mommy.make('awards.award', piid='imalittlepiid', awarding_agency=a1)
+    m12 = mommy.make('awards.award', piid='shortandstout', parent_award=pa12, awarding_agency=a1)
+    t12 = Award.get_or_create_summary_award(piid='shortandstout', parent_award_id='imalittlepiid', awarding_agency=a3)[1]
+    assert t12 != m12
 
     # matching fain but mismatched subtier agency, same top tier agency
-    m15 = mommy.make('awards.award', fain='imalittlefain', parent_award=pa14, awarding_agency=a1)
-    t15 = Award.get_or_create_summary_award(fain='imalittlefain', awarding_agency=a3)[1]
-    assert t15 != m15
+    m13 = mommy.make('awards.award', fain='imalittlefain', parent_award=pa12, awarding_agency=a1)
+    t13 = Award.get_or_create_summary_award(fain='imalittlefain', record_type='2', awarding_agency=a3)[1]
+    assert t13 != m13
 
     # matching piid and parent award but mismatched subtier and toptier agency
-    pa16 = mommy.make('awards.Award', piid='imjustapiidparent', awarding_agency=a3)
-    m16 = mommy.make('awards.Award', piid='imjustapiidchild', awarding_agency=a3)
-    t16 = Award.get_or_create_summary_award(piid='imjustapiidchild', parent_award_id='imjustapiidparent', awarding_agency=a4)[1]
-    assert t16 != m16
+    pa14 = mommy.make('awards.Award', piid='imjustapiidparent', awarding_agency=a3)
+    m14 = mommy.make('awards.Award', piid='imjustapiidchild', awarding_agency=a3)
+    t14 = Award.get_or_create_summary_award(piid='imjustapiidchild', parent_award_id='imjustapiidparent', awarding_agency=a4)[1]
+    assert t14 != m14
 
 
 @pytest.mark.django_db
