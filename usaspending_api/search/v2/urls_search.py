e2dc6f3e7b8d0af7c1ac416a9b29bd1068b31fba
Melvin Dunn <mdunn1151@gmail.com>
2018-01-24 13:40:07 -0500

    adding urls search

--- usaspending_api/search/v2/urls_search.py
+++ usaspending_api/search/v2/urls_search.py
@@ -6,7 +6,7 @@ from usaspending_api.search.v2.views.search import SpendingByGeographyVisualizat
 from usaspending_api.search.v2.views.search import SpendingOverTimeVisualizationViewSet
 from usaspending_api.search.v2.views.search import TransactionSummaryVisualizationViewSet
 from usaspending_api.search.v2.views.search import SpendingByTransactionVisualizationViewSet
-from usaspending_api.search.v2.views.search import SpendingByTransactionCountvisualizaitonViewSet
+from usaspending_api.search.v2.views.search import SpendingByTransactionCountVisualizaitonViewSet
 
 urlpatterns = [
     url(r'^spending_over_time', SpendingOverTimeVisualizationViewSet.as_view()),
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^spending_by_geography', SpendingByGeographyVisualizationViewSet.as_view()),
     url(r'^spending_by_award_count', SpendingByAwardCountVisualizationViewSet.as_view()),
     url(r'^spending_by_award', SpendingByAwardVisualizationViewSet.as_view()),
-    url(r'^spending_by_transaction_count', SpendingByTransactionCountvisualizaitonViewSet.as_view()),
+    url(r'^spending_by_transaction_count', SpendingByTransactionCountVisualizaitonViewSet.as_view()),
     url(r'^spending_by_transaction', SpendingByTransactionVisualizationViewSet.as_view()),
     url(r'^transaction_spending_summary', TransactionSummaryVisualizationViewSet.as_view()),
 ]
