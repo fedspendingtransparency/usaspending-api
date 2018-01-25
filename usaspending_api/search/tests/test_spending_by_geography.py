b4a5e77e0c51b6755f8b137d0a279a9c9fcca6fc
alburde1 <Burdeyny_Alisa@bah.com>
2018-01-25 16:00:09 -0500

    Fixing flake8 in search folder

--- usaspending_api/search/tests/test_spending_by_geography.py
+++ usaspending_api/search/tests/test_spending_by_geography.py
@@ -1,71 +1,14 @@
 import json
 
 import pytest
-from model_mommy import mommy
 from rest_framework import status
 
-from usaspending_api.search.tests.test_mock_data_search \
-    import budget_function_data, all_filters
-
-
-@pytest.fixture
-def incorrect_location_data(db):
-    country_usa = mommy.make(
-        'references.RefCountryCode',
-        country_code='USA'
-    )
-
-    location_wrong_state = mommy.make(
-        'references.Location',
-        location_id=10,
-        location_country_code=country_usa,
-        state_code='*'
-    )
-
-    location_wrong_county = mommy.make(
-        'references.Location',
-        location_id=11,
-        location_country_code=country_usa,
-        state_code='AL',
-        county_code='9.0',
-        county_name='COUNTY'
-
-    )
-
-    location_wrong_district = mommy.make(
-        'references.Location',
-        location_id=12,
-        location_country_code=country_usa,
-        state_code='AL',
-        congressional_code='AL09',
-
-    )
-
-    transaction_state = mommy.make(
-        'awards.TransactionNormalized',
-        id=12345,
-        place_of_performance=location_wrong_state,
-        federal_action_obligation=50
-    )
-
-    transaction_county = mommy.make(
-        'awards.TransactionNormalized',
-        id=12346,
-        place_of_performance=location_wrong_county,
-        federal_action_obligation=100
-    )
-
-    transaction_county = mommy.make(
-        'awards.TransactionNormalized',
-        id=12347,
-        place_of_performance=location_wrong_district,
-        federal_action_obligation=0
-    )
+from usaspending_api.search.tests.test_mock_data_search import all_filters
 
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_spending_by_geography_success(client, budget_function_data):
+def test_spending_by_geography_success(client):
 
     # test for required filters
     resp = client.post(
@@ -106,7 +49,7 @@ def test_spending_by_geography_failure(client):
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_spending_by_geography_incorrect_state(client, incorrect_location_data):
+def test_spending_by_geography_incorrect_state(client):
     resp = client.post(
         '/api/v2/search/spending_by_geography/',
         content_type='application/json',
@@ -122,7 +65,7 @@ def test_spending_by_geography_incorrect_state(client, incorrect_location_data):
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_spending_by_geography_incorrect_county(client, incorrect_location_data):
+def test_spending_by_geography_incorrect_county(client):
     resp = client.post(
         '/api/v2/search/spending_by_geography/',
         content_type='application/json',
@@ -138,7 +81,7 @@ def test_spending_by_geography_incorrect_county(client, incorrect_location_data)
 
 @pytest.mark.skip
 @pytest.mark.django_db
-def test_spending_by_geography_incorrect_district(client, incorrect_location_data):
+def test_spending_by_geography_incorrect_district(client):
     resp = client.post(
         '/api/v2/search/spending_by_geography/',
         content_type='application/json',
