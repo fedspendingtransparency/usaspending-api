67ee5c6798e0e55b73fafb0182ecf9021aff6b4e
Nipun Monga <nipunmnga@gmail.com>
2018-01-25 16:48:53 -0500

    Using fully qualified table names

--- usaspending_api/broker/management/sql/update_award_ids.sql
+++ usaspending_api/broker/management/sql/update_award_ids.sql
@@ -1,4 +1,4 @@
 UPDATE transaction_normalized_new
-SET award_id = awards_new.id
+SET transaction_normalized_new.award_id = awards_new.id
 FROM awards_new
 WHERE awards_new.generated_unique_award_id = transaction_normalized_new.generated_unique_award_id;
