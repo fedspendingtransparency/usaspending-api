c2604809d9d54bde20b7e53e3f9676020d00b990
Nipun Monga <nipunmnga@gmail.com>
2018-01-25 09:39:05 -0500

    Adding indexes to help with performance

--- usaspending_api/broker/management/sql/update_transaction_ids.sql
+++ usaspending_api/broker/management/sql/update_transaction_ids.sql
@@ -1,3 +1,7 @@
+create index fabs_unique_id on transaction_fabs_new (afa_generated_unique);
+create index fpds_unique_id on transaction_fpds_new (detached_award_proc_unique);
+create index tx_unique_id on transaction_normalized_new (transaction_unique_id);
+
 UPDATE transaction_fabs_new
 SET transaction_id = tn_new.id
 FROM transaction_normalized_new AS tn_new
