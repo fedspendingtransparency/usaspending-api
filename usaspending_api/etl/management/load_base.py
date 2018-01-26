b738518b5732c3a3141e8c77a1d38084a5908345
Nipun Monga <nipunmnga@gmail.com>
2018-01-25 22:48:51 -0500

    adding logging to save method

--- usaspending_api/etl/management/load_base.py
+++ usaspending_api/etl/management/load_base.py
@@ -516,7 +516,7 @@ def load_data_into_model(model_instance, data, **kwargs):
             store_value(mod, field, data[field], reverse)
 
     if save:
-        logger.info("model % is being save" % model_instance)
+        logger.info("model %s is being save" % str(model_instance))
         model_instance.save()
     if as_dict:
         return mod
