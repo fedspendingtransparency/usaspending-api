6b337d8964a774c830a84a632f6ecc844f647b47
Catherine Devlin <catherine.devlin@gsa.gov>
2018-02-01 14:27:22 -0500

    initial /federal_accounts endpoint, just 200s

--- usaspending_api/references/v2/urls.py
+++ usaspending_api/references/v2/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from usaspending_api.references.v1 import views
-from usaspending_api.references.v2.views import agency, toptier_agencies
+from usaspending_api.references.v2.views import agency, toptier_agencies, federal_accounts
 
 glossary_router = DefaultRouter()
 glossary_router.register('glossary', views.GlossaryViewSet)
@@ -13,5 +13,6 @@ mode_detail = {'get': 'retrieve', 'post': 'retrieve'}
 
 urlpatterns = [
     url(r'^agency/(?P<pk>[0-9]+)/$', agency.AgencyViewSet.as_view()),
-    url(r'^toptier_agencies/$', toptier_agencies.ToptierAgenciesViewSet.as_view())
+    url(r'^toptier_agencies/$', toptier_agencies.ToptierAgenciesViewSet.as_view()),
+    url(r'^federal_accounts/$', federal_accounts.FederalAccountsViewSet.as_view())
 ]
