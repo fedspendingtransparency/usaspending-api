cfb96645e26edbdee712c314ea94d29849a561f2
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-02-02 21:48:47 -0500

    Cleaned up prints and made minor tweaks

--- usaspending_api/etl/management/commands/es_rapidloader.py
+++ usaspending_api/etl/management/commands/es_rapidloader.py
@@ -30,7 +30,7 @@ ES_CLIENT = Elasticsearch(settings.ES_HOSTNAME, timeout=300)
 
 class DataJob:
     def __init__(self, *args):
-        self._name = args[0]
+        self.name = args[0]
         self.index = args[1]
         self.fy = args[2]
         self.category = args[3]
@@ -78,7 +78,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         ''' Script execution of custom code starts in this method'''
         start = perf_counter()
-        print('\n\nStarting script\n')
+        printf({'msg': 'Starting script\n{}'.format('=' * 56)})
 
         self.config = set_config()
         self.config['formatted_now'] = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')  # ISO8601
@@ -91,27 +91,23 @@ class Command(BaseCommand):
         self.config['starting_date'] = date(2001, 1, 1)
 
         if not os.path.isdir(self.config['directory']):
-            print('Provided directory does not exist')
+            printf({'msg': 'Provided directory does not exist'})
             raise SystemExit
 
-        es_mapping_file = 'usaspending_api/etl/es_mapper.json'
-        with open(es_mapping_file) as f:
-            data = json.load(f)
-            self.config['mapping'] = json.dumps(data)
-
         self.controller()
-        print('---------------------------------------------------------------')
-        print("Script completed in {} seconds".format(perf_counter() - start))
-        print('---------------------------------------------------------------')
+        printf({'msg': '---------------------------------------------------------------'})
+        printf({'msg': 'Script completed in {} seconds'.format(perf_counter() - start)})
+        printf({'msg': '---------------------------------------------------------------'})
 
     def controller(self):
 
         new_queue = Queue()  # Queue for jobs whch need a csv downloaded
         ingest_queue = Queue()  # Queue for jobs which have a csv and are ready for ES ingest
 
-        job_id = 1
+        job_id = 0
         for fy in self.config['fiscal_years']:
             for awd_cat_idx in AWARD_DESC_CATEGORIES.keys():
+                job_id += 1
                 award_category = AWARD_DESC_CATEGORIES[awd_cat_idx]
                 index = '{}-{}-{}'.format(settings.TRANSACTIONS_INDEX_ROOT, award_category, fy)
                 filename = '{dir}{fy}_transactions_{type}.csv'.format(
@@ -120,7 +116,6 @@ class Command(BaseCommand):
                     type=awd_cat_idx.replace(' ', ''))
 
                 new_job = DataJob(job_id, index, fy, awd_cat_idx, filename)
-                job_id += 1
 
                 if self.config['stale'] is False:
                     if os.path.exists(filename):
@@ -132,7 +127,7 @@ class Command(BaseCommand):
                         new_job.count = count
                     ingest_queue.put(new_job)
 
-        print('There are {} jobs to process'.format(job_id))
+        printf({'msg': 'There are {} jobs to process'.format(job_id)})
 
         download_proccess = Process(target=download_db_records, args=(new_queue, ingest_queue, self.config))
         es_index_process = Process(target=es_data_loader, args=(new_queue, ingest_queue, self.config))
@@ -149,61 +144,65 @@ def es_data_loader(fetch_jobs, done_jobs, config):
     while loop_again:
         if not done_jobs.empty():
             job = done_jobs.get_nowait()
-            if job._name is None:
+            if job.name is None:
                 loop_again = False
                 break
 
-            print('[ES Ingest] Starting new job')
+            printf({'msg': 'Starting new job', 'job': job.name, 'f': 'ES Ingest'})
             post_to_elasticsearch(job, config)
             if os.path.exists(job.csv):
                 os.remove(job.csv)
         # elif fetch_jobs.empty() and done_jobs.empty():
         else:
-            print('[ES Ingest] No Job :-( Sleeping 15s')
+            printf({'msg': 'No Job :-( Sleeping 15s', 'f': 'ES Ingest'})
             sleep(15)
 
-    print('[ES Ingest] Completed Elasticsearch data load')
+    printf({'msg': 'Completed Elasticsearch data load', 'f': 'ES Ingest'})
     return
 
 
 def download_csv(count_sql, copy_sql, filename, job_id, verbose):
-    start = perf_counter()
-    count = execute_sql_statement(count_sql, True, verbose)
-    print('[Download {2}] Writing {0} transactions to this file: {1}'.format(count[0]['count'], filename, job_id))
+    count = execute_sql_statement(count_sql, True, verbose)[0]['count']
+    printf({'msg': 'Writing {} transactions to this file: {}'.format(count, filename), 'job': job_id, 'f': 'Download'})
     # It is preferable to not use shell=True, but this command works. Limited user-input so risk is low
     subprocess.Popen('psql "${{DATABASE_URL}}" -c {}'.format(copy_sql), shell=True).wait()
-    ####################################
-    # Potentially smart to perform basic validation that the number of CSV data rows match count
-    print("[Download {1}] Database download took {0} seconds".format(perf_counter() - start, job_id))
-    return count[0]['count']
+
+    with open(filename, 'r') as f:
+        download_count = len(f.readlines()) - 1
+    if count != download_count:
+        print('Download count doesn\'t match rows in DB!')
+        raise SystemExit
+    return count
 
 
 def download_db_records(fetch_jobs, done_jobs, config):
     while not fetch_jobs.empty():
         start = perf_counter()
         job = fetch_jobs.get_nowait()
-        print('[Download {}] Preparing to Download a new CSV'.format(job._name))
+        printf({'msg': 'Preparing to Download a new CSV', 'job': job.name, 'f': 'Download'})
 
-        c = {
+        sql_config = {
             'starting_date': config['starting_date'],
             'fiscal_year': job.fy,
             'award_category': job.category,
             'provide_deleted': False
         }
-        copy_sql, _, count_sql = configure_sql_strings(c, job.csv, [])
+        copy_sql, _, count_sql = configure_sql_strings(sql_config, job.csv, [])
 
         if os.path.isfile(job.csv):
             os.remove(job.csv)
 
-        job.count = download_csv(count_sql, copy_sql, job.csv, job._name, config['verbose'])
+        job.count = download_csv(count_sql, copy_sql, job.csv, job.name, config['verbose'])
         done_jobs.put(job)
-        print("[Download {1}] Data fetch job took {0} seconds.".format(perf_counter() - start, job._name))
+        printf({
+            'msg': 'Data fetch job took {} seconds'.format(perf_counter() - start),
+            'job': job.name,
+            'f': 'Download'
+        })
         sleep(1)
 
-    dummy_job = DataJob(None, None, None, None, None)
-    done_jobs.put(dummy_job)
-
-    print('[Download] All downloads from Postgres completed')
+    done_jobs.put(DataJob(None, None, None, None, None))
+    printf({'msg': 'All downloads from Postgres completed', 'f': 'Download'})
     return
 
 
@@ -216,11 +215,17 @@ def set_config():
         print('Missing environment variable `ES_HOSTNAME`')
         raise SystemExit
 
-    return {}
+    config = {}
+    es_mapping_file = 'usaspending_api/etl/es_mapper.json'
+    with open(es_mapping_file) as f:
+        data = json.load(f)
+        config['mapping'] = json.dumps(data)
+
+    return config
 
 
 def csv_chunk_gen(filename, chunksize, job_id):
-    print('[ES Ingest {2}] Opening {0} to batch read by {1} lines'.format(filename, chunksize, job_id))
+    printf({'msg': 'Opening {} batch size: {}'.format(filename, chunksize), 'job': job_id, 'f': 'ES Ingest'})
     # Panda's data type guessing causes issues for Elasticsearch. Set all cols to str
     dtype = {x: str for x in VIEW_COLUMNS}
 
@@ -240,13 +245,12 @@ def streaming_post_to_es(chunk, index_name, job_id):
         print('MASSIVE FAIL!!!\n\n{}\n\n{}'.format(e, '*' * 80))
         raise SystemExit
 
-    print('[ES Ingest {2}] Success: {0} | Fails: {1} '.format(success, failed, job_id))
-    #
+    printf({'msg': 'Success: {} | Fails: {}'.format(success, failed), 'job': job_id, 'f': 'ES Ingest'})
     return success, failed
 
 
 def post_to_elasticsearch(job, config, chunksize=250000):
-    print('[ES Ingest {1}] Populating ES Index : {0}'.format(job.index, job._name))
+    printf({'msg': 'Populating ES Index : {}'.format(job.index), 'job': job.name, 'f': 'ES Ingest'})
 
     try:
         does_index_exist = ES_CLIENT.indices.exists(job.index)
@@ -254,15 +258,30 @@ def post_to_elasticsearch(job, config, chunksize=250000):
         print(e)
         raise SystemExit
     if not does_index_exist:
-        print('[ES Ingest {1}] Creating {0} index'.format(job.index, job._name))
+        printf({'msg': 'Creating {} index'.format(job.index), 'job': job.name, 'f': 'ES Ingest'})
         ES_CLIENT.indices.create(index=job.index, body=config['mapping'])
     elif does_index_exist and config['recreate']:
-        print('[ES Ingest {1}] {0} exists... deleting first'.format(job.index, job._name))
+        printf({'msg': 'Deleting Existing index {}'.format(job.index), 'job': job.name, 'f': 'ES Ingest'})
         ES_CLIENT.indices.delete(job.index)
 
-    csv_generator = csv_chunk_gen(job.csv, chunksize, job._name)
+    csv_generator = csv_chunk_gen(job.csv, chunksize, job.name)
     for count, chunk in enumerate(csv_generator):
-        print('[ES Ingest {1}] Running chunk # {0}'.format(count, job._name))
         iteration = perf_counter()
-        streaming_post_to_es(chunk, job.index, job._name)
-        print('[ES Ingest {1}] Iteration took {0}s'.format(perf_counter() - iteration, job._name))
+        streaming_post_to_es(chunk, job.index, job.name)
+        printf({
+            'msg': 'Iteration on chunk {} took {}s'.format(count, perf_counter() - iteration),
+            'job': job.name,
+            'f': 'ES Ingest'
+        })
+
+
+def printf(items):
+    template = '[{time}] {complex:<20} | {msg}'
+    msg = items['msg']
+    func = '[' + items.get('f', 'main') + ']'
+    job = items.get('job', None)
+    j = ''
+    if job:
+        j = ' (#{})'.format(job)
+
+    print(template.format(time=datetime.utcnow().strftime('%H:%M:%S.%f'), complex=func + j, msg=msg))
