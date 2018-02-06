8a3dbb884d9500344f9696357f4577aea41f5c5b
Melvin Dunn <mdunn1151@gmail.com>
2018-02-06 11:34:51 -0500

    reflake8ing search.py

--- usaspending_api/search/v2/views/search.py
+++ usaspending_api/search/v2/views/search.py
@@ -28,7 +28,6 @@ from usaspending_api.awards.v2.filters.view_selector import get_view_queryset
 from usaspending_api.awards.v2.filters.view_selector import spending_by_award_count
 from usaspending_api.awards.v2.filters.view_selector import spending_by_geography
 from usaspending_api.awards.v2.filters.view_selector import spending_over_time
-from usaspending_api.awards.v2.filters.view_selector import transaction_spending_summary
 
 from usaspending_api.awards.v2.lookups.lookups import contract_type_mapping, loan_type_mapping, \
     non_loan_assistance_type_mapping
@@ -36,7 +35,8 @@ from usaspending_api.awards.v2.lookups.matview_lookups import award_contracts_ma
     non_loan_assistance_award_mapping
 from usaspending_api.references.abbreviations import code_to_state, fips_to_code, pad_codes
 from usaspending_api.references.models import Cfda
-from usaspending_api.search.v2.elasticsearch_helper import search_transactions, spending_by_transaction_count, spending_by_transaction_sum_and_count
+from usaspending_api.search.v2.elasticsearch_helper import search_transactions,\
+     spending_by_transaction_count, spending_by_transaction_sum_and_count
 
 
 logger = logging.getLogger(__name__)
@@ -823,6 +823,7 @@ class TransactionSummaryVisualizationViewSet(APIView):
         results = spending_by_transaction_sum_and_count(filters)
         # build response
         return Response({"results": results})
+
 
 class SpendingByTransactionCountVisualizaitonViewSet(APIView):
 
