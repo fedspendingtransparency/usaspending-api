206e6772bbc64347759d0eaacbff3189b12202cf
Daniel Boos <boos_daniel@bah.com>
2018-02-12 10:51:10 -0500

    Reverting delete message for dead letter queue testing

--- usaspending_api/bulk_download/management/commands/generate_bulk_zip.py
+++ usaspending_api/bulk_download/management/commands/generate_bulk_zip.py
@@ -98,9 +98,9 @@ class Command(BaseCommand):
                 # Handle uncaught exceptions in validation process.
                 logger.error(str(e))
 
-                # Delete failed jobs
-                message.delete()
-                processed_messages.append(message)
+                # # Delete failed jobs - commenting out for dead letter queue
+                # message.delete()
+                # processed_messages.append(message)
 
                 # csv-specific errors get a different job status and response code
                 if isinstance(e, ValueError) or isinstance(e, csv.Error) or isinstance(e, UnicodeDecodeError):
