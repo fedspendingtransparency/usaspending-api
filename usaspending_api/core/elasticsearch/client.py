e749371be6b91d0791728b03a2ca23a2ec63c489
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-02-15 15:46:29 -0500

    added more checks and tailored error log messages

--- usaspending_api/core/elasticsearch/client.py
+++ usaspending_api/core/elasticsearch/client.py
@@ -1,9 +1,19 @@
 import logging
 from django.conf import settings
+from elasticsearch import ConnectionError
+from elasticsearch import ConnectionTimeout
 from elasticsearch import Elasticsearch
+from elasticsearch import NotFoundError
+from elasticsearch import TransportError
 
 logger = logging.getLogger('console')
-CLIENT = Elasticsearch(settings.ES_HOSTNAME)
+
+if not settings.ES_HOSTNAME:
+    raise Exception('Elasticsearch cluster connection string isn\'t set!')
+try:
+    CLIENT = Elasticsearch(settings.ES_HOSTNAME)
+except Exception as e:
+    logger.exception('Error creating the elasticsearch client')
 
 
 def es_client_query(index, body, timeout='1m', retries=1):
@@ -12,11 +22,24 @@ def es_client_query(index, body, timeout='1m', retries=1):
     elif retries < 1:
         retries = 1
     for attempt in range(retries):
-        try:
-            response = CLIENT.search(index=index, body=body, timeout=timeout)
-        except Exception:
-            logger.exception('There was an error connecting to the ElasticSearch cluster.')
-            continue
-        return response
-    logger.error('Error connecting to elasticsearch. {} attempts made'.format(retries))
+        response = _es_search(index=index, body=body, timeout=timeout)
+        if response is not None:
+            return response
+    logger.error('Unable to reach elasticsearch cluster. {} attempt(s) made'.format(retries))
     return None
+
+
+def _es_search(index, body, timeout):
+    error_template = '[ERROR] ({type}) with ElasticSearch cluster: {e}'
+    result = None
+    try:
+        result = CLIENT.search(index=index, body=body, timeout=timeout)
+    except (ConnectionError, ConnectionTimeout) as e:
+        logger.error(error_template.format(type='Connection', e=str(e)))
+    except TransportError as e:
+        logger.error(error_template.format(type='Transport', e=str(e)))
+    except NotFoundError as e:
+        logger.error(error_template.format(type='404 Not Found', e=str(e)))
+    except Exception as e:
+        logger.error(error_template.format(type='Generic', e=str(e)))
+    return result
