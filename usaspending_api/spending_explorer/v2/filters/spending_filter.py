c05773866ec605bf78de8d63f814eb5981565098
Nipun Monga <nipunmnga@gmail.com>
2018-02-28 18:02:20 -0500

    Fixing linting issues

--- usaspending_api/spending_explorer/v2/filters/spending_filter.py
+++ usaspending_api/spending_explorer/v2/filters/spending_filter.py
@@ -1,6 +1,5 @@
 import logging
 
-from usaspending_api.awards.models import Award
 from usaspending_api.references.models import Agency
 from usaspending_api.common.exceptions import InvalidParameterException
 from usaspending_api.references.constants import DOD_ARMED_FORCES_CGAC, DOD_CGAC
@@ -113,8 +112,7 @@ def spending_filter(alt_set, queryset, filters, _type):
             # recipient
             elif key == 'recipient':
                 and_queryset = queryset.\
-                    filter(treasury_account__in=alt_set.
-                             filter(award__recipient__recipient_name=value).
+                    filter(treasury_account__in=alt_set.filter(award__recipient__recipient_name=value).
                            values_list('treasury_account_id', flat=True))
                 queryset &= and_queryset
 
