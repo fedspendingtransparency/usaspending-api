696a068fa7d1d678db32c54a650a4bfe7d9e5949
TheRJB <bodine_russell@bah.com>
2018-03-01 17:58:48 -0500

    spelling

--- usaspending_api/awards/v2/urls_award_spending.py
+++ usaspending_api/awards/v2/urls_award_spending.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from usaspending_api.awards.v2.views import award_spending as views
 
-# map reqest types to viewset method; replace this with a router
+# map request types to viewset method; replace this with a router
 award_category = views.AwardTypeAwardSpendingViewSet.as_view({'get': 'list'})
 recipient = views.RecipientAwardSpendingViewSet.as_view({'get': 'list'})
 
