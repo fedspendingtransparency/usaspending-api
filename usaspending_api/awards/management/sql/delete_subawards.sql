c72f6b12a290d8a8d4778de1a366c96593be99d4
Daniel Boos <boos_daniel@bah.com>
2018-03-13 12:18:36 -0400

    Re-ordering and using matviews to properly delete subaward data

--- usaspending_api/awards/management/sql/delete_subawards.sql
+++ usaspending_api/awards/management/sql/delete_subawards.sql
@@ -1,5 +1,5 @@
--- Remove the locations associated with the subawards
-WITH le_subaward AS (
+-- Get the legal entities associated with the subawards
+CREATE MATERIALIZED VIEW le_subaward AS (
     SELECT le.legal_entity_id, le.location_id
     FROM legal_entity AS le
     WHERE EXISTS (
@@ -7,8 +7,9 @@ WITH le_subaward AS (
         FROM awards_subaward AS asub
         WHERE asub.recipient_id = le.legal_entity_id
     )
-),
-sub_location AS (
+);
+-- Get the locations associated with the subawards
+CREATE MATERIALIZED VIEW sub_location AS (
     SELECT tmp.location_id
     FROM (
         (
@@ -31,24 +32,20 @@ sub_location AS (
             )
         )
     ) AS tmp
-)
-DELETE FROM references_location as loc
-USING sub_location
-WHERE sub_location.location_id = loc.location_id;
+);
+
+-- Remove the subawards
+DELETE FROM awards_subaward;
 
 -- Remove the legal entities
-WITH le_subaward AS (
-    SELECT le.legal_entity_id, le.location_id
-    FROM legal_entity AS le
-    WHERE EXISTS (
-        SELECT 1
-        FROM awards_subaward AS asub
-        WHERE asub.recipient_id = le.legal_entity_id
-    )
-)
 DELETE FROM legal_entity AS le
 USING le_subaward
 WHERE le_subaward.legal_entity_id = le.legal_entity_id;
 
--- Remove the subawards
-DELETE FROM awards_subaward;
+-- Delete teh subaward locations
+DELETE FROM references_location as loc
+USING sub_location
+WHERE sub_location.location_id = loc.location_id;
+
+DROP MATERIALIZED VIEW sub_location;
+DROP MATERIALIZED VIEW le_subaward;
