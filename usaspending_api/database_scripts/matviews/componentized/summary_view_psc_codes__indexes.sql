2b3678a9889003129d1a267985fbd100c9427236
Nipun Monga <nipunmnga@gmail.com>
2018-03-19 10:27:30 -0400

    Adding support for "total_loan_value" field in the awards table

--- usaspending_api/database_scripts/matviews/componentized/summary_view_psc_codes__indexes.sql
+++ usaspending_api/database_scripts/matviews/componentized/summary_view_psc_codes__indexes.sql
@@ -5,7 +5,7 @@
 --                                                    --
 --         !!DO NOT DIRECTLY EDIT THIS FILE!!         --
 --------------------------------------------------------
-CREATE UNIQUE INDEX idx_1b698194$c4b_unique_pk_temp ON summary_view_psc_codes_temp USING BTREE("pk") WITH (fillfactor = 97);
-CREATE INDEX idx_1b698194$c4b_action_date_temp ON summary_view_psc_codes_temp USING BTREE("action_date" DESC NULLS LAST) WITH (fillfactor = 97);
-CREATE INDEX idx_1b698194$c4b_type_temp ON summary_view_psc_codes_temp USING BTREE("type") WITH (fillfactor = 97);
-CREATE INDEX idx_1b698194$c4b_pulled_from_temp ON summary_view_psc_codes_temp USING BTREE("pulled_from") WITH (fillfactor = 97) WHERE "pulled_from" IS NOT NULL;
+CREATE UNIQUE INDEX idx_1b698194$77d_unique_pk_temp ON summary_view_psc_codes_temp USING BTREE("pk") WITH (fillfactor = 97);
+CREATE INDEX idx_1b698194$77d_action_date_temp ON summary_view_psc_codes_temp USING BTREE("action_date" DESC NULLS LAST) WITH (fillfactor = 97);
+CREATE INDEX idx_1b698194$77d_type_temp ON summary_view_psc_codes_temp USING BTREE("type") WITH (fillfactor = 97);
+CREATE INDEX idx_1b698194$77d_pulled_from_temp ON summary_view_psc_codes_temp USING BTREE("pulled_from") WITH (fillfactor = 97) WHERE "pulled_from" IS NOT NULL;
