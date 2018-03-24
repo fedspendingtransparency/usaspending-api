7ff026c3f731705799354cb32db417d41d9ae023
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-03-23 20:13:57 -0400

    Removed all generated matview scripts from repo
    SQL will now be generated on demand by the Jenkins CI pipeline

--- usaspending_api/database_scripts/matviews/componentized/summary_view__mods.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
--- Created using matview_sql_generator.py             --
---    The SQL definition is stored in a json file     --
---    Look in matview_generator for the code.         --
---                                                    --
---         !!DO NOT DIRECTLY EDIT THIS FILE!!         --
---------------------------------------------------------
-ANALYZE VERBOSE summary_view;
-GRANT SELECT ON summary_view TO readonly;
