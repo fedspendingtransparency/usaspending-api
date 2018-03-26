e2b91904dd7100c258429d22eb108f73946c9937
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-03-26 12:26:18 -0400

    Changed function to get git commit in case spaces are in filepath

--- usaspending_api/database_scripts/matview_generator/matview_sql_generator.py
+++ usaspending_api/database_scripts/matview_generator/matview_sql_generator.py
@@ -98,9 +98,10 @@ def generate_uid(characters=8, filename=None):
 
 def get_git_commit(characters=8, filename=None):
     cmd = 'git log -n 1 --pretty=format:"%H"'
-    if filename and os.path.isfile(filename):
-        cmd += ' -- {}'.format(filename)
     args = cmd.split(' ')
+    if filename and os.path.isfile(filename):
+        args.append(filename)
+
     shell = subprocess.run(args, stdout=subprocess.PIPE, check=True)
     if shell.stdout:
         # First character is a '#' so skip it
