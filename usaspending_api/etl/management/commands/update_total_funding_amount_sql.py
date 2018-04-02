298e44fdbd9e3bc5ab31e4f24ba5fdb91b91ff88
TheRJB <bodine_russell@bah.com>
2018-04-02 13:07:26 -0400

    update models

--- usaspending_api/etl/management/commands/update_total_funding_amount_sql.py
+++ usaspending_api/etl/management/commands/update_total_funding_amount_sql.py
@@ -29,26 +29,24 @@ class Command(BaseCommand):
             logger.info('New sum for total_funding_amount. {} successful updates'.format(curs.rowcount))
 
     TRANSACTION_FABS_TFA = """
-        update transaction_fabs as tf
-        set funding_amount= federal_action_obligation + non_federal_funding_amount 
+        update transaction_normalized as tn
+        set non_federal_funding_amount=tf.non_federal_funding_amount
+        from transaction_fabs as tf
+            where tf.transaction_id=tn.id;
     """
-
 
     TRANSACTION_NORMALIZED_TFA = """
-    update transaction_normalized as tn
-    set funding_amount=tf.funding_amount
-    from transaction_fabs as tf
-    where tf.transaction_id=tn.id
+        update transaction_normalized as tn
+        set funding_amount= federal_action_obligation + non_federal_funding_amount::decimal;
     """
 
-
     SUM_TRANSACTION_TFA = """
-    With sum_table as (
-    SELECT award_id, SUM(NULLIF(tn.funding_amount, '')::float) sum_val 
-    FROM transaction_normalized as tn join awards a on tn.award_id = a.id WHERE a.id = tn.award_id GROUP BY award_id
-    )
-    update awards as a
-    set total_funding_amount=sum_table.sum_val
-    from sum_table
-    where a.id = sum_table.award_id;
+        With sum_table as (
+        SELECT award_id, SUM(NULLIF(tn.funding_amount, '')::float) sum_val 
+        FROM transaction_normalized as tn join awards a on tn.award_id = a.id WHERE a.id = tn.award_id GROUP BY award_id
+        )
+        update awards as a
+        set total_funding_amount=sum_table.sum_val
+        from sum_table
+        where a.id = sum_table.award_id;
     """
