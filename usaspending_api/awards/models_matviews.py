f1f3186546abbc085d7e096f92079151396e82f9
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-05-03 13:40:20 -0400

    created django model for subaward matview

--- usaspending_api/awards/models_matviews.py
+++ usaspending_api/awards/models_matviews.py
@@ -708,3 +708,61 @@ class TransactionMatview(models.Model):
     class Meta:
         managed = False
         db_table = 'transaction_matview'
+
+
+class SubawardView(models.Model):
+    id = models.IntegerField(primary_key=True)
+    keyword_ts_vector = SearchVectorField()
+    award_ts_vector = SearchVectorField()
+    recipient_name_ts_vector = SearchVectorField()
+    subaward_number = models.TextField()
+    federal_action_obligation = models.DecimalField(
+        max_digits=20, decimal_places=2, blank=True,
+        null=True)
+    description = models.TextField()
+    fiscal_year = models.IntegerField()
+    action_date = models.DateField()
+    award_report_fy_month = models.IntegerField()
+    award_report_fy_year = models.IntegerField()
+
+    award_id = models.IntegerField()
+    awarding_agency_id = models.IntegerField()
+    funding_agency_id = models.IntegerField()
+    awarding_toptier_agency_name = models.TextField()
+    awarding_subtier_agency_name = models.TextField()
+
+    place_of_performance_id = models.IntegerField()
+    recipient_id = models.IntegerField()
+    type = models.TextField()
+
+    cfda_id = models.IntegerField()
+    piid = models.TextField()
+    fain = models.TextField()
+
+    recipient_name = models.TextField()
+    prime_recipient_name = models.TextField()
+    recipient_unique_id = models.TextField()
+    parent_recipient_unique_id = models.TextField()
+    product_or_service_code = models.TextField()
+    product_or_service_description = models.TextField()
+
+    recipient_location_country_code = models.TextField()
+    recipient_location_country_name = models.TextField()
+    recipient_location_state_code = models.TextField()
+    recipient_location_county_code = models.TextField()
+    recipient_location_county_name = models.TextField()
+    recipient_location_zip5 = models.TextField()
+    recipient_location_congressional_code = models.TextField()
+
+    pop_country_code = models.TextField()
+    pop_country_name = models.TextField()
+    pop_state_code = models.TextField()
+    pop_county_code = models.TextField()
+    pop_county_name = models.TextField()
+    pop_city_code = models.TextField()
+    pop_zip5 = models.TextField()
+    pop_congressional_code = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'subaward_view'
