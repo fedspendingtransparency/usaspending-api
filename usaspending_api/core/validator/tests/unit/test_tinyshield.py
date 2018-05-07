f55e6df02a8c2a6d013f3db8c013c378884d9748
Isaac Ray <isaacmray1984@gmail.com>
2018-05-07 12:05:00 -0400

    adding unit tests

---
+++ usaspending_api/core/validator/tests/unit/test_tinyshield.py
@@ -0,0 +1,172 @@
+import copy
+
+from usaspending_api.core.validator.award_filter import AWARD_FILTER
+from usaspending_api.core.validator.helpers import validate_array
+from usaspending_api.core.validator.helpers import validate_boolean
+from usaspending_api.core.validator.helpers import validate_datetime
+from usaspending_api.core.validator.helpers import validate_enum
+from usaspending_api.core.validator.helpers import validate_float
+from usaspending_api.core.validator.helpers import validate_integer
+from usaspending_api.core.validator.helpers import validate_object
+from usaspending_api.core.validator.helpers import validate_text
+from usaspending_api.core.validator.tinyshield import TinyShield
+
+
+ARRAY_RULE = {'name': 'test', 'type': 'array', 'key': 'filters|test',
+              'array_type': 'integer', 'optional': True, 'min': 1, 'value': [1, 2, 3]}
+BOOLEAN_RULE = {'name': 'test', 'type': 'boolean', 'key': 'filters|test', 'optional': True, 'value': True}
+DATETIME_RULE = {'name': 'test', 'type': 'datetime', 'key': 'filters|test', 'optional': True,
+                 'value': '1984-09-16T4:05:00'}
+ENUM_RULE = {'name': 'test', 'type': 'enum', 'key': 'filters|test',
+             'enum_values': ['foo', 'bar'], 'optional': True, 'value': "foo"}
+FLOAT_RULE = {'name': 'test', 'type': 'float', 'key': 'filters|test',
+              'optional': True, 'value': 3.14, 'min': 2, 'max': 4}
+TEXT_RULE = {'name': 'test', 'type': 'text', 'key': 'filters|test',
+             'optional': True, 'value': "hello world", "text_type": "search"}
+INTEGER_RULE = {'name': 'test', 'type': 'float', 'key': 'filters|test',
+                'optional': True, 'value': 3, 'min': 2, 'max': 4}
+OBJECT_RULE = {'name': 'test', 'type': 'object', 'key': 'filters|test',
+               'object_keys': {
+                    'foo': {'type': 'string', 'optional': False},
+                    'hello': {'type': 'integer', 'optional': False}
+                },
+               'value': {'foo': 'bar', 'hello': 1}}
+
+FILTER_OBJ = {
+        "filters": {
+            "keyword": "money",
+            "award_type_codes": [
+                "A",
+                "B",
+                "C",
+                "D"
+            ],
+            "time_period": [
+                {
+                    "start_date": "2001-01-01",
+                    "end_date": "2001-01-31"
+                }
+            ],
+            "place_of_performance_scope": "domestic",
+            "place_of_performance_locations": [
+                {
+                    "country": "USA",
+                    "state": "VA",
+                    "county": "059"
+                }
+            ],
+            "agencies": [
+                {
+                    "type": "funding",
+                    "tier": "toptier",
+                    "name": "Office of Pizza"
+                },
+                {
+                    "type": "awarding",
+                    "tier": "subtier",
+                    "name": "Personal Pizza"
+                }
+            ],
+            "recipient_search_text": ["D12345678"],
+            "recipient_scope": "domestic",
+            "recipient_locations": [
+                {
+                    "country": "USA",
+                    "state": "VA",
+                    "county": "059"
+                }
+            ],
+            "recipient_type_names": [
+                "Small Business",
+                "Alaskan Native Owned Business"
+            ],
+            "award_ids": ["1605SS17F00018"],
+            "award_amounts": [
+                  {
+                    "lower_bound": 1000000.00,
+                    "upper_bound": 25000000.00
+                  },
+                  {
+                    "upper_bound": 1000000.00
+                  },
+                  {
+                    "lower_bound": 500000000.00
+                  }
+            ],
+            "program_numbers": ["10.553"],
+            "naics_codes": ["336411"],
+            "psc_codes": ["1510"],
+            "contract_pricing_type_codes": ["SAMPLECODE123"],
+            "set_aside_type_codes": ["SAMPLECODE123"],
+            "extent_competed_type_codes": ["SAMPLECODE123"]
+
+        }
+    }
+
+
+TS = None
+
+'''
+Beacuse these functions all raise Exceptions on failure, all we need to do to write the unit tests is call the function.
+If an exception is raised, the test will fail
+'''
+
+
+def test_validate_array():
+    validate_array(ARRAY_RULE)
+
+
+def test_validate_boolean():
+    validate_boolean(BOOLEAN_RULE)
+
+
+def test_validate_datetime():
+    validate_datetime(DATETIME_RULE)
+
+
+def test_validate_enum():
+    validate_enum(ENUM_RULE)
+
+
+def test_validate_float():
+    validate_float(FLOAT_RULE)
+
+
+def test_validate_integer():
+    validate_integer(INTEGER_RULE)
+
+
+def test_validate_text():
+    validate_text(TEXT_RULE)
+
+
+def test_validate_object():
+    validate_object(OBJECT_RULE)
+
+
+def test_check_models():
+    '''We want this test to fail if either AWARD_FILTERS has an invalid model,
+    OR if the logic of the check_models function has been corrupted.
+    It will fail if an exception is raised. Otherwise it will define the global TS object
+    so we can use it in the remaining tests.'''
+    global TS
+    TS = TinyShield(copy.deepcopy(AWARD_FILTER))
+
+
+def test_recurse_append():
+    mydict = {}
+    struct = ['level1', 'level2']
+    data = "foobar"
+    TS.recurse_append(struct, mydict, data)
+    assert mydict == {"level1": {"level2": "foobar"}}
+
+
+def test_parse_request():
+    request = FILTER_OBJ
+    TS.parse_request(request)
+    assert all("value" in item for item in TS.rules)
+
+
+def test_enforce_rules():
+    TS.enforce_rules()
+    assert TS.data == FILTER_OBJ
