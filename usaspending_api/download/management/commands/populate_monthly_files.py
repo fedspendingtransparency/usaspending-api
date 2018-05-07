4fe05812ce4ba1aa6de73bf6b90560a0ebe7d59e
Michael Hess <hess_michael@bah.com>
2018-05-07 10:38:33 -0400

    Revert unnecessary change to old script

--- usaspending_api/download/management/commands/populate_monthly_files.py
+++ usaspending_api/download/management/commands/populate_monthly_files.py
@@ -189,7 +189,8 @@ class Command(BaseCommand):
         toptier_agencies = ToptierAgency.objects.all()
         include_all = True
         if use_modified_list:
-            toptier_agencies = ToptierAgency.objects.filter(cgac_code__in=set(pull_modified_agencies_cgacs()))
+            used_cgacs = set(self.pull_modified_agencies_cgacs())
+            toptier_agencies = ToptierAgency.objects.filter(cgac_code__in=used_cgacs)
         if agencies:
             if 'all' in agencies:
                 agencies.remove('all')
