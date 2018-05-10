329791f273e0eb13219206e25ed1d50fc0487ea2
Daniel Boos <boos_daniel@bah.com>
2018-05-10 12:05:54 -0400

    Making trailing slash required

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from usaspending_api.recipient.v2.views import StateMetaDataViewSet
 
 urlpatterns = [
-    url(r'^state/(?P<fips>[0-9]{2})/?$', StateMetaDataViewSet.as_view())
+    url(r'^state/(?P<fips>[0-9]{2})/$', StateMetaDataViewSet.as_view())
 ]
