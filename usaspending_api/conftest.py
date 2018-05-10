1659c467545b58222986f3323adb793d979c6a85
Isaac Ray <isaacmray1984@gmail.com>
2018-05-10 12:55:00 -0400

    changing conftest to supposrt matviews

--- usaspending_api/conftest.py
+++ usaspending_api/conftest.py
@@ -1,5 +1,20 @@
 from django.conf import settings
+import subprocess
+import pytest
+from django.db import connection
 
+ENUM_FILE = ['database_scripts/matviews/functions_and_enums.sql']
+
+TEMP_SQL_FILES = ['../matviews/universal_transaction_matview.sql',
+        '../matviews/universal_award_matview.sql',
+        '../matviews/summary_transaction_view.sql',
+        '../matviews/summary_transaction_month_view.sql',
+        '../matviews/summary_transaction_geo_view.sql',
+        '../matviews/summary_award_view.sql',
+        '../matviews/summary_view_cfda_number.sql', 
+        '../matviews/summary_view_naics_codes.sql',
+        '../matviews/summary_view_psc_codes.sql',
+        '../matviews/summary_view.sql']
 
 def pytest_configure():
     # To make sure the test setup process doesn't try
@@ -10,3 +25,23 @@ def pytest_configure():
     settings.DATABASES['default'] = test_db
     # Also remove any database routers
     settings.DATABASE_ROUTERS.clear()
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_blocker):
+    with django_db_blocker.unblock():
+        with connection.cursor() as c:
+            #c.execute(get_sql(ENUM_FILE)[0])
+            x = subprocess.call("python database_scripts/matview_generator/matview_sql_generator.py ", shell=True)
+            for file in get_sql(TEMP_SQL_FILES):
+                c.execute(file)
+
+
+
+def get_sql(sql_files):
+    data = []
+    for file in sql_files:    
+        with open(file, 'r') as myfile:
+            data.append(myfile.read())
+    return data     
+    
