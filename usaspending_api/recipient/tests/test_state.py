41ee38761e405b726d6c4d326bc533a5aa0e89e9
Daniel Boos <boos_daniel@bah.com>
2018-05-11 19:22:35 -0400

    Splitting tests by aspects

--- usaspending_api/recipient/tests/test_state.py
+++ usaspending_api/recipient/tests/test_state.py
@@ -11,6 +11,42 @@ import pytest
 # Imports from your apps
 from usaspending_api.common.helpers import generate_fiscal_year
 
+EXPECTED_STATE = {
+        'name': 'Test State',
+        'code': 'TS',
+        'fips': '01',
+        'type': 'state',
+        'population': 100000,
+        'pop_year': 2017,
+        'pop_source': 'Census 2010 Pop',
+        'median_household_income': 50000,
+        'mhi_year': 2016,
+        'mhi_source': 'Census 2010 MHI',
+        'total_prime_amount': 0,
+        'total_prime_awards': 0,
+        'award_amount_per_capita': 0.00
+    }
+EXPECTED_DISTRICT = EXPECTED_STATE.copy()
+EXPECTED_DISTRICT.update({
+    'name': 'Test District',
+    'code': 'TD',
+    'type': 'district',
+    'pop_year': 2016,
+    'median_household_income': 20000,
+    'fips': '02',
+    'population': 5000
+})
+EXPECTED_TERRITORY = EXPECTED_STATE.copy()
+EXPECTED_TERRITORY.update({
+    'name': 'Test Territory',
+    'code': 'TT',
+    'type': 'territory',
+    'pop_year': 2016,
+    'median_household_income': 10000,
+    'fips': '03',
+    'population': 5000
+})
+
 
 def state_metadata_endpoint(fips, year=None):
     url = '/api/v2/recipient/state/{}/'.format(fips)
@@ -117,58 +153,24 @@ def state_data(db):
 @pytest.mark.django_db
 def test_state_metadata_success(client, state_data, refresh_matviews):
     # test small request - state
-    expected_state = {
-        'name': 'Test State',
-        'code': 'TS',
-        'fips': '01',
-        'type': 'state',
-        'population': 100000,
-        'pop_year': 2017,
-        'pop_source': 'Census 2010 Pop',
-        'median_household_income': 50000,
-        'mhi_year': 2016,
-        'mhi_source': 'Census 2010 MHI',
-        'total_prime_amount': 0,
-        'total_prime_awards': 0,
-        'award_amount_per_capita': 0.00
-    }
-    expected_district = expected_state.copy()
-    expected_district.update({
-        'name': 'Test District',
-        'code': 'TD',
-        'type': 'district',
-        'pop_year': 2016,
-        'median_household_income': 20000,
-        'fips': '02',
-        'population': 5000
-    })
-    expected_territory = expected_state.copy()
-    expected_territory.update({
-        'name': 'Test Territory',
-        'code': 'TT',
-        'type': 'territory',
-        'pop_year': 2016,
-        'median_household_income': 10000,
-        'fips': '03',
-        'population': 5000
-    })
-
     resp = client.get(state_metadata_endpoint('01'))
     assert resp.status_code == status.HTTP_200_OK
-    assert resp.data == expected_state
+    assert resp.data == EXPECTED_STATE
 
     # test small request - district, testing 1 digit FIPS
     resp = client.get(state_metadata_endpoint('2'))
     assert resp.status_code == status.HTTP_200_OK
-    assert resp.data == expected_district
+    assert resp.data == EXPECTED_DISTRICT
 
     # test small request - territory
     resp = client.get(state_metadata_endpoint('03'))
     assert resp.status_code == status.HTTP_200_OK
-    assert resp.data == expected_territory
+    assert resp.data == EXPECTED_TERRITORY
 
+@pytest.mark.django_db
+def test_state_amounts_success(client, state_data, refresh_matviews):
     # test year with amounts
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'population': 50000,
         'pop_year': 2016,
@@ -182,8 +184,10 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     assert resp.data == expected_response
     assert resp.data == expected_response
 
+@pytest.mark.django_db
+def test_state_years_success(client, state_data, refresh_matviews):
     # test future year
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'pop_year': 2017,
         'mhi_year': 2016
@@ -193,7 +197,7 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     assert resp.data == expected_response
 
     # test old year
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'pop_year': 2016,
         'mhi_year': 2016,
@@ -206,7 +210,7 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     # test latest year
     # Note: the state data will be based on 2016/2017 state metadata but amounts should be based on the last 12 months
     # Amounts aren't really testable as they're tied to the day its run
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'pop_year': 2017,
         'mhi_year': 2016
@@ -215,10 +219,12 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == expected_response
 
+@pytest.mark.django_db
+def test_state_current_all_years_success(client, state_data, refresh_matviews):
     # test all years
     # Note: the state data will be based on 2016/2017 state metadata but amounts should be based on all years
     # Amounts aren't really testable as they're tied to the day its run
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'pop_year': 2017,
         'mhi_year': 2016,
@@ -230,8 +236,10 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == expected_response
 
+@pytest.mark.django_db
+def test_state_current_fy_capita_success(client, state_data, refresh_matviews):
     # making sure amount per capita is null for current fiscal year
-    expected_response = expected_state.copy()
+    expected_response = EXPECTED_STATE.copy()
     expected_response.update({
         'award_amount_per_capita': None
     })
