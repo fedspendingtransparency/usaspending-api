efc03473899e2a6eb597cc485b8911dd6a582587
Daniel Boos <boos_daniel@bah.com>
2018-05-24 01:22:26 -0400

    Fixing tests to work with latest dates and fiscal years

--- usaspending_api/recipient/tests/integration/test_state.py
+++ usaspending_api/recipient/tests/integration/test_state.py
@@ -1,5 +1,6 @@
 # Stdlib imports
 import datetime
+import decimal
 
 # Core Django imports
 
@@ -12,6 +13,10 @@ import pytest
 from usaspending_api.common.helpers.generic_helper import generate_fiscal_year
 from usaspending_api.recipient.v2.views.states import obtain_state_totals
 
+# Getting relative dates as the 'latest'/default argument returns results relative to when it gets called
+TODAY = datetime.datetime.now()
+OUTSIDE_OF_LATEST = (TODAY - datetime.timedelta(365 + 2))
+CURRENT_FISCAL_YEAR = generate_fiscal_year(TODAY)
 
 EXPECTED_STATE = {
     'name': 'Test State',
@@ -19,34 +24,40 @@ EXPECTED_STATE = {
     'fips': '01',
     'type': 'state',
     'population': 100000,
-    'pop_year': 2017,
+    'pop_year': CURRENT_FISCAL_YEAR,
     'pop_source': 'Census 2010 Pop',
     'median_household_income': 50000,
-    'mhi_year': 2016,
+    'mhi_year': CURRENT_FISCAL_YEAR-1,
     'mhi_source': 'Census 2010 MHI',
-    'total_prime_amount': 0,
-    'total_prime_awards': 0,
-    'award_amount_per_capita': 0.00
+    'total_prime_amount': 100000,
+    'total_prime_awards': 1,
+    'award_amount_per_capita': 1
 }
 EXPECTED_DISTRICT = EXPECTED_STATE.copy()
 EXPECTED_DISTRICT.update({
     'name': 'Test District',
     'code': 'TD',
-    'type': 'district',
-    'pop_year': 2016,
-    'median_household_income': 20000,
     'fips': '02',
-    'population': 5000
+    'type': 'district',
+    'pop_year': CURRENT_FISCAL_YEAR-1,
+    'median_household_income': 20000,
+    'population': 5000,
+    'total_prime_amount': 1000,
+    'total_prime_awards': 1,
+    'award_amount_per_capita': round(decimal.Decimal(0.20), 2)
 })
 EXPECTED_TERRITORY = EXPECTED_STATE.copy()
 EXPECTED_TERRITORY.update({
     'name': 'Test Territory',
     'code': 'TT',
-    'type': 'territory',
-    'pop_year': 2016,
-    'median_household_income': 10000,
     'fips': '03',
-    'population': 5000
+    'type': 'territory',
+    'pop_year': CURRENT_FISCAL_YEAR-1,
+    'median_household_income': 10000,
+    'population': 5000,
+    'total_prime_amount': 1000,
+    'total_prime_awards': 1,
+    'award_amount_per_capita': round(decimal.Decimal(0.20), 2)
 })
 
 
@@ -60,6 +71,11 @@ def state_metadata_endpoint(fips, year=None):
 def sort_breakdown_response(response_list):
     """Sorting response since on Travis order of breakdown response list is different"""
     return sorted(response_list, key=lambda k: k['type'])
+
+
+def sort_states_response(response_list):
+    """Sorting response since on Travis order of breakdown response list is different"""
+    return sorted(response_list, key=lambda k: k['fips'])
 
 
 @pytest.fixture
@@ -83,34 +99,34 @@ def state_data(db):
         'awards.TransactionNormalized',
         place_of_performance=location_ts,
         federal_action_obligation=100000,
-        action_date='2016-01-01'
+        action_date=TODAY.strftime('%Y-%m-%d')
     )
     mommy.make(
         'awards.TransactionNormalized',
         place_of_performance=location_ts,
         federal_action_obligation=100000,
-        action_date='2016-01-01'
+        action_date=OUTSIDE_OF_LATEST.strftime('%Y-%m-%d')
     )
     mommy.make(
         'awards.TransactionNormalized',
         place_of_performance=location_td,
         federal_action_obligation=1000,
-        action_date='2016-01-01'
+        action_date=TODAY.strftime('%Y-%m-%d')
     )
     mommy.make(
         'awards.TransactionNormalized',
         place_of_performance=location_tt,
         federal_action_obligation=1000,
-        action_date='2016-01-01'
+        action_date=TODAY.strftime('%Y-%m-%d')
     )
     mommy.make(
         'recipient.StateData',
-        id='01-2016',
+        id='01-{}'.format(CURRENT_FISCAL_YEAR-1),
         fips='01',
         name='Test State',
         code='TS',
         type='state',
-        year=2016,
+        year=CURRENT_FISCAL_YEAR-1,
         population=50000,
         pop_source='Census 2010 Pop',
         median_household_income=50000,
@@ -118,12 +134,12 @@ def state_data(db):
     )
     mommy.make(
         'recipient.StateData',
-        id='01-2017',
+        id='01-{}'.format(CURRENT_FISCAL_YEAR),
         fips='01',
         name='Test State',
         code='TS',
         type='state',
-        year=2017,
+        year=CURRENT_FISCAL_YEAR,
         population=100000,
         pop_source='Census 2010 Pop',
         median_household_income=None,
@@ -131,12 +147,12 @@ def state_data(db):
     )
     mommy.make(
         'recipient.StateData',
-        id='02-2016',
+        id='02-{}'.format(CURRENT_FISCAL_YEAR-1),
         fips='02',
         name='Test District',
         code='TD',
         type='district',
-        year=2016,
+        year=CURRENT_FISCAL_YEAR-1,
         population=5000,
         pop_source='Census 2010 Pop',
         median_household_income=20000,
@@ -144,12 +160,12 @@ def state_data(db):
     )
     mommy.make(
         'recipient.StateData',
-        id='03-2016',
+        id='03-{}'.format(CURRENT_FISCAL_YEAR-1),
         fips='03',
         name='Test Territory',
         code='TT',
         type='territory',
-        year=2016,
+        year=CURRENT_FISCAL_YEAR-1,
         population=5000,
         pop_source='Census 2010 Pop',
         median_household_income=10000,
@@ -167,34 +183,34 @@ def state_view_data(db, monkeypatch):
         state_code='AB'
     )
 
-    award_2016 = mommy.make('awards.Award',
-                            type='A')
+    award_old = mommy.make('awards.Award',
+                           type='A')
 
-    award_2017 = mommy.make('awards.Award',
-                            type='B')
+    award_cur = mommy.make('awards.Award',
+                           type='B')
 
-    trans_2016 = mommy.make(
+    trans_old = mommy.make(
         'awards.TransactionNormalized',
-        award=award_2016,
+        award=award_old,
         type='A',
         place_of_performance=location,
         federal_action_obligation=10,
-        fiscal_year='2016',
-        action_date='2016-01-01'
+        fiscal_year=generate_fiscal_year(OUTSIDE_OF_LATEST),
+        action_date=OUTSIDE_OF_LATEST.strftime('%Y-%m-%d')
     )
 
-    trans_2017 = mommy.make(
+    trans_cur = mommy.make(
         'awards.TransactionNormalized',
-        award=award_2017,
+        award=award_cur,
         type='B',
         place_of_performance=location,
         federal_action_obligation=15,
-        fiscal_year='2017',
-        action_date='2017-01-01'
+        fiscal_year=generate_fiscal_year(TODAY),
+        action_date=TODAY.strftime('%Y-%m-%d')
     )
 
-    mommy.make('awards.TransactionFPDS', transaction=trans_2016)
-    mommy.make('awards.TransactionFPDS', transaction=trans_2017)
+    mommy.make('awards.TransactionFPDS', transaction=trans_old)
+    mommy.make('awards.TransactionFPDS', transaction=trans_cur)
 
 
 @pytest.fixture()
@@ -215,6 +231,11 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == EXPECTED_STATE
 
+    # test small request - district
+    resp = client.get(state_metadata_endpoint('02'))
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.data == EXPECTED_DISTRICT
+
     # test small request - territory
     resp = client.get(state_metadata_endpoint('03'))
     assert resp.status_code == status.HTTP_200_OK
@@ -222,30 +243,15 @@ def test_state_metadata_success(client, state_data, refresh_matviews):
 
 
 @pytest.mark.django_db
-def test_state_amounts_success(client, state_data, refresh_matviews):
-    # test year with amounts
-    expected_response = EXPECTED_STATE.copy()
-    expected_response.update({
-        'population': 50000,
-        'pop_year': 2016,
-        'median_household_income': 50000,
-        'mhi_year': 2016,
-        'total_prime_amount': 200000,
-        'total_prime_awards': 2,
-        'award_amount_per_capita': 4
-    })
-    resp = client.get(state_metadata_endpoint('01', '2016'))
-    assert resp.data == expected_response
-    assert resp.data == expected_response
-
-
-@pytest.mark.django_db
 def test_state_years_success(client, state_data, refresh_matviews):
     # test future year
     expected_response = EXPECTED_STATE.copy()
     expected_response.update({
-        'pop_year': 2017,
-        'mhi_year': 2016
+        'pop_year': CURRENT_FISCAL_YEAR,
+        'mhi_year': CURRENT_FISCAL_YEAR-1,
+        'total_prime_amount': 0,
+        'total_prime_awards': 0,
+        'award_amount_per_capita': 0
     })
     resp = client.get(state_metadata_endpoint('01', '3000'))
     assert resp.status_code == status.HTTP_200_OK
@@ -254,21 +260,34 @@ def test_state_years_success(client, state_data, refresh_matviews):
     # test old year
     expected_response = EXPECTED_STATE.copy()
     expected_response.update({
-        'pop_year': 2016,
-        'mhi_year': 2016,
-        'population': 50000
+        'pop_year': CURRENT_FISCAL_YEAR-1,
+        'mhi_year': CURRENT_FISCAL_YEAR-1,
+        'population': 50000,
+        'total_prime_amount': 0,
+        'total_prime_awards': 0,
+        'award_amount_per_capita': 0
     })
     resp = client.get(state_metadata_endpoint('01', '2000'))
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == expected_response
 
-    # test latest year
-    # Note: the state data will be based on 2016/2017 state metadata but amounts should be based on the last 12 months
-    # Amounts aren't really testable as they're tied to the day its run
+    # test older year
     expected_response = EXPECTED_STATE.copy()
     expected_response.update({
-        'pop_year': 2017,
-        'mhi_year': 2016
+        'pop_year': CURRENT_FISCAL_YEAR-1,
+        'mhi_year': CURRENT_FISCAL_YEAR-1,
+        'population': 50000,
+        'award_amount_per_capita': 2
+    })
+    resp = client.get(state_metadata_endpoint('01', CURRENT_FISCAL_YEAR-1))
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.data == expected_response
+
+    # test latest year
+    expected_response = EXPECTED_STATE.copy()
+    expected_response.update({
+        'pop_year': CURRENT_FISCAL_YEAR,
+        'mhi_year': CURRENT_FISCAL_YEAR-1,
     })
     resp = client.get(state_metadata_endpoint('01', 'latest'))
     assert resp.status_code == status.HTTP_200_OK
@@ -278,12 +297,10 @@ def test_state_years_success(client, state_data, refresh_matviews):
 @pytest.mark.django_db
 def test_state_current_all_years_success(client, state_data, refresh_matviews):
     # test all years
-    # Note: the state data will be based on 2016/2017 state metadata but amounts should be based on all years
-    # Amounts aren't really testable as they're tied to the day its run
     expected_response = EXPECTED_STATE.copy()
     expected_response.update({
-        'pop_year': 2017,
-        'mhi_year': 2016,
+        'pop_year': CURRENT_FISCAL_YEAR,
+        'mhi_year': CURRENT_FISCAL_YEAR-1,
         'award_amount_per_capita': None,
         'total_prime_awards': 2,
         'total_prime_amount': 200000
@@ -300,7 +317,7 @@ def test_state_current_fy_capita_success(client, state_data, refresh_matviews):
     expected_response.update({
         'award_amount_per_capita': None
     })
-    resp = client.get(state_metadata_endpoint('01', generate_fiscal_year(datetime.date.today())))
+    resp = client.get(state_metadata_endpoint('01', CURRENT_FISCAL_YEAR))
     assert resp.status_code == status.HTTP_200_OK
     assert resp.data == expected_response
 
@@ -324,7 +341,7 @@ def test_state_metadata_failure(client, state_data, refresh_matviews):
 
 @pytest.mark.django_db
 def test_obtain_state_totals(state_view_data, refresh_matviews):
-    result = obtain_state_totals('01', '2016', ['A'])
+    result = obtain_state_totals('01', str(generate_fiscal_year(OUTSIDE_OF_LATEST)), ['A'])
     expected = {'pop_state_code': 'AB', 'total': 10, 'count': 1}
     assert result == expected
 
@@ -340,7 +357,7 @@ def test_obtain_state_totals_none(state_view_data, refresh_matviews, monkeypatch
 
 @pytest.mark.django_db
 def test_state_breakdown_success_state(client, state_view_data, state_breakdown_result, refresh_matviews):
-    resp = client.get('/api/v2/recipient/state/awards/01/')
+    resp = client.get('/api/v2/recipient/state/awards/01/?year=all')
     sorted_resp = sort_breakdown_response(resp.data)
 
     expected = state_breakdown_result
@@ -352,7 +369,7 @@ def test_state_breakdown_success_state(client, state_view_data, state_breakdown_
 
 @pytest.mark.django_db
 def test_state_breakdown_success_year(client, state_view_data, state_breakdown_result, refresh_matviews):
-    resp = client.get('/api/v2/recipient/state/awards/01/?year=2017')
+    resp = client.get('/api/v2/recipient/state/awards/01/?year={}'.format(str(generate_fiscal_year(TODAY))))
     sorted_resp = sort_breakdown_response(resp.data)
 
     expected = state_breakdown_result
@@ -364,7 +381,7 @@ def test_state_breakdown_success_year(client, state_view_data, state_breakdown_r
 
 @pytest.mark.django_db
 def test_state_breakdown_success_no_data(client, state_view_data, state_breakdown_result, refresh_matviews):
-    resp = client.get('/api/v2/recipient/state/awards/01/?year=2015')
+    resp = client.get('/api/v2/recipient/state/awards/01/?year={}'.format(CURRENT_FISCAL_YEAR-2))
     sorted_resp = sort_breakdown_response(resp.data)
 
     expected = state_breakdown_result
@@ -378,3 +395,18 @@ def test_state_breakdown_failure(client, state_view_data, refresh_matviews):
     resp = client.get('/api/v2/recipient/state/awards/05/')
 
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_state_list_success_state(client, state_data, refresh_matviews):
+    resp = client.get('/api/v2/recipient/state/')
+    sorted_resp = sort_states_response(resp.data)
+
+    expected = [
+        {'name': 'Test State', 'code': 'TS', 'fips': '01', 'amount': 100000.00, 'count': 1},
+        {'name': 'Test District', 'code': 'TD', 'fips': '02', 'amount': 1000.00, 'count': 1},
+        {'name': 'Test Territory', 'code': 'TT', 'fips': '03', 'amount': 1000.00, 'count': 1}
+    ]
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert sorted_resp == expected
