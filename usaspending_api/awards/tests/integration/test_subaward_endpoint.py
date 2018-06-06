10c09ad5cfef70196419a82dd5086f09596a078c
Isaac Ray <isaacmray1984@gmail.com>
2018-06-06 12:38:58 -0400

    query integration tests

--- usaspending_api/awards/tests/integration/test_subaward_endpoint.py
+++ usaspending_api/awards/tests/integration/test_subaward_endpoint.py
@@ -1,7 +1,14 @@
 import json
-
 import pytest
 from rest_framework import status
+
+# Core Django imports
+from django_mock_queries.query import MockModel
+
+
+# Imports from your apps
+from usaspending_api.common.helpers.unit_test_helper import add_to_mock_objects
+from usaspending_api.awards.tests.unit.test_subawards import subaward_1, subaward_2, subaward_3, subaward_12
 
 
 @pytest.mark.django_db
@@ -12,8 +19,8 @@ def test_subaward_success(client, refresh_matviews):
         content_type='application/json',
         data=json.dumps(
             {
-            "order": "desc", 
-            "limit": 100 
+                "order": "desc",
+                "limit": 100
             })
     )
     assert resp.status_code == status.HTTP_200_OK
@@ -27,10 +34,47 @@ def test_subaward_failure(client, refresh_matviews):
         content_type='application/json',
         data=json.dumps(
             {
-            "order": "desc", 
-            "limit": 100,
-            "award_id":"not an integer", 
+                "order": "desc",
+                "limit": 100,
+                "award_id": "not an integer",
             })
     )
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
 
+
+@pytest.mark.django_db
+def test_subaward_query_1(client, refresh_matviews, mock_matviews_qs):
+    mock_model_1 = MockModel(**subaward_1)
+    mock_model_2 = MockModel(**subaward_2)
+    mock_model_3 = MockModel(**subaward_3)
+
+    add_to_mock_objects(mock_matviews_qs, [mock_model_1, mock_model_2, mock_model_3])
+    resp = client.post(
+        '/api/v2/subawards/',
+        content_type='application/json',
+        data=json.dumps(
+            {
+                "order": "desc",
+                "limit": 100,
+                "award_id": 99,
+            })
+    )
+    assert len(json.loads(resp.content)['results']) == 3
+
+
+@pytest.mark.django_db
+def test_subaward_query_2(client, refresh_matviews, mock_matviews_qs):
+    mock_model_4 = MockModel(**subaward_12)
+
+    add_to_mock_objects(mock_matviews_qs, [mock_model_4])
+    resp = client.post(
+        '/api/v2/subawards/',
+        content_type='application/json',
+        data=json.dumps(
+            {
+                "order": "desc",
+                "limit": 100,
+                "award_id": 88,
+            })
+    )
+    assert json.loads(resp.content)['results'][0]['id'] == 12
