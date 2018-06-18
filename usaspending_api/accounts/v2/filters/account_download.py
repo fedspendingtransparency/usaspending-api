abca2c2b85197ae62811792cf7615d3e6ff5157a
Michael Hess <hess_michael@bah.com>
2018-06-18 17:11:37 -0400

    Group by the rest of the fields in the TAS file and include them in the FA file

--- usaspending_api/accounts/v2/filters/account_download.py
+++ usaspending_api/accounts/v2/filters/account_download.py
@@ -113,20 +113,46 @@ def generate_treasury_account_query(queryset, account_type, tas_id):
 
 def generate_federal_account_query(queryset, account_type, tas_id):
     """ Group by federal account (and budget function/subfunction) and SUM all other fields """
-    # Derive the federal_account_symbol
-    queryset = queryset.annotate(
-        federal_account_symbol=Concat('{}__federal_account__agency_identifier'.format(tas_id), Value('-'),
-                                      '{}__federal_account__main_account_code'.format(tas_id)))
+    # Derive the federal_account_symbol and agency_name
+    agency_name_subquery = ToptierAgency.objects.filter(
+        cgac_code=OuterRef('{}__federal_account__agency_identifier'.format(tas_id)))
+    derived_fields = {
+        # federal_account_symbol: fed_acct_AID-fed_acct_MAC
+        'federal_account_symbol': Concat('{}__federal_account__agency_identifier'.format(tas_id), Value('-'),
+                                         '{}__federal_account__main_account_code'.format(tas_id)),
 
-    # Group by federal_account_symbol, budget_function, budget_subfunction
-    group_vals = ['federal_account_symbol', '{}__federal_account__account_title'.format(tas_id),
-                  '{}__budget_function_title'.format(tas_id), '{}__budget_subfunction_title'.format(tas_id)]
-    queryset = queryset.values(*group_vals)
+        # agency_name: name of the ToptierAgency with CGAC matching federal_account__agency_identifier
+        'agency_name': Subquery(agency_name_subquery.values('name')[:1])
+    }
 
-    # Sum all fields excluding the ones we're grouping by
-    q_path = query_paths[account_type]
-    summed_cols = {i: Sum(q_path['treasury_account'][i]) for i in q_path['federal_account']
-                   if q_path['treasury_account'][i] not in group_vals}
+    # Derive recipient_parent_name for award_financial downloads
+    if account_type == 'award_financial':
+        derived_fields['recipient_parent_name'] = Case(
+            When(award__latest_transaction__type__in=list(contract_type_mapping.keys()),
+                 then='award__latest_transaction__contract_data__ultimate_parent_legal_enti'),
+            default='award__latest_transaction__assistance_data__ultimate_parent_legal_enti',
+            output_field=CharField())
+
+    queryset = queryset.annotate(**derived_fields)
+
+    # List of all columns that may appear in A, B, or C files that can be summed
+    all_summed_cols = [
+        'budget_authority_unobligated_balance_brought_forward', 'adjustments_to_unobligated_balance_brought_forward',
+        'budget_authority_appropriated_amount', 'borrowing_authority_amount', 'contract_authority_amount',
+        'spending_authority_from_offsetting_collections_amount', 'total_other_budgetary_resources_amount',
+        'total_budgetary_resources', 'obligations_incurred', 'deobligations_or_recoveries_or_refunds_from_prior_year',
+        'unobligated_balance', 'gross_outlay_amount', 'status_of_budgetary_resources_total',
+        'transaction_obligated_amount_']  # TODO: remove trailing  "_" from transaction_obligated_amount_ after data fix
+
+    # Group by all columns within the file that can't be summed
+    fed_acct_values_dict = query_paths[account_type]['federal_account']
+    grouped_cols = [fed_acct_values_dict[val] for val in fed_acct_values_dict if val not in all_summed_cols]
+    queryset = queryset.values(*grouped_cols)
+
+    # Sum all fields from all_summed_cols that appear in this file
+    values_dict = query_paths[account_type]
+    summed_cols = {val: Sum(values_dict['treasury_account'].get(val, None)) for val in values_dict['federal_account']
+                   if val in all_summed_cols}
 
     # Account for NaN bug in award_financial data
     # TODO: Fix the data and get rid of this code
