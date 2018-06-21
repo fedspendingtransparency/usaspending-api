0bae98fa685e2226ac5663faea4b72fa6d4368c5
Michael Hess <hess_michael@bah.com>
2018-06-21 13:05:18 -0400

    Update tests and fix issues found by tests

--- usaspending_api/download/tests/integration/test_account_download.py
+++ usaspending_api/download/tests/integration/test_account_download.py
@@ -11,29 +11,14 @@ from usaspending_api.download.lookups import JOB_STATUS
 
 
 @pytest.fixture
-def account_data(db):
+def base_job_data(db):
     # Populate job status lookup table
     for js in JOB_STATUS:
         mommy.make('download.JobStatus', job_status_id=js.id, name=js.name, description=js.desc)
 
-    # Create TreasuryAppropriationAccount models
-    tas1 = mommy.make('accounts.TreasuryAppropriationAccount', agency_id='-01')
-    tas2 = mommy.make('accounts.TreasuryAppropriationAccount', agency_id='-01')
-    tas3 = mommy.make('accounts.TreasuryAppropriationAccount', agency_id='-02')
-
-    # Create AppropriationAccountBalances models
-    mommy.make('accounts.AppropriationAccountBalances', treasury_account_identifier=tas1)
-    mommy.make('accounts.AppropriationAccountBalances', treasury_account_identifier=tas2)
-    mommy.make('accounts.AppropriationAccountBalances', treasury_account_identifier=tas3)
-
-    # Create FinancialAccountsByProgramActivityObjectClass models
-    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', treasury_account=tas1)
-    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', treasury_account=tas2)
-    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass', treasury_account=tas3)
-
 
 @pytest.mark.django_db
-def test_tas_a_defaults_success(client, account_data):
+def test_tas_a_defaults_success(client, base_job_data):
     """ Test the accounts endpoint using the default filters for an account_balances file"""
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
@@ -54,7 +39,7 @@ def test_tas_a_defaults_success(client, account_data):
 
 
 @pytest.mark.django_db
-def test_tas_b_defaults_success(client, account_data):
+def test_tas_b_defaults_success(client, base_job_data):
     """ Test the accounts endpoint using the default filters for an object_class_program_activity file"""
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
@@ -75,7 +60,7 @@ def test_tas_b_defaults_success(client, account_data):
 
 
 @pytest.mark.django_db
-def test_tas_c_defaults_success(client, account_data):
+def test_tas_c_defaults_success(client, base_job_data):
     """ Test the accounts endpoint using the default filters for an award_financial file"""
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
@@ -96,7 +81,158 @@ def test_tas_c_defaults_success(client, account_data):
 
 
 @pytest.mark.django_db
-def test_account_level_failure(client, account_data):
+def test_federal_account_a_defaults_success(client, base_job_data):
+    """ Test the accounts endpoint using the default filters for an account_balances file"""
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "federal_account",
+            "filters": {
+                "submission_type": "account_balances",
+                "fy": "2017",
+                "quarter": "3"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert '.zip' in resp.json()['url']
+
+
+@pytest.mark.django_db
+def test_federal_account_b_defaults_success(client, base_job_data):
+    """ Test the accounts endpoint using the default filters for an object_class_program_activity file"""
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "federal_account",
+            "filters": {
+                "submission_type": "object_class_program_activity",
+                "fy": "2018",
+                "quarter": "1"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert '.zip' in resp.json()['url']
+
+
+@pytest.mark.django_db
+def test_federal_account_c_defaults_success(client, base_job_data):
+    """ Test the accounts endpoint using the default filters for an award_financial file"""
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "federal_account",
+            "filters": {
+                "submission_type": "award_financial",
+                "fy": "2016",
+                "quarter": "4"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert '.zip' in resp.json()['url']
+
+
+@pytest.mark.django_db
+def test_agency_filter_success(client, base_job_data):
+    """ Test the accounts endpoint with a wrong account_level """
+    mommy.make('references.ToptierAgency', toptier_agency_id=-1, cgac_code='-01')
+
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "federal_account",
+            "filters": {
+                "submission_type": "account_balances",
+                "fy": "2017",
+                "quarter": "4",
+                "agency": "-1"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_agency_filter_failure(client, base_job_data):
+    """ Test the accounts endpoint with a wrong account_level """
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "treasury_account",
+            "filters": {
+                "submission_type": "object_class_program_activity",
+                "fy": "2017",
+                "quarter": "4",
+                "agency": "-2"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_federal_account_filter_success(client, base_job_data):
+    """ Test the accounts endpoint with a wrong account_level """
+    mommy.make('accounts.FederalAccount', id=-1)
+
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "treasury_account",
+            "filters": {
+                "submission_type": "award_financial",
+                "fy": "2017",
+                "quarter": "4",
+                "federal_account": "-1"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_federal_account_filter_failure(client, base_job_data):
+    """ Test the accounts endpoint with a wrong account_level """
+    csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
+    resp = client.post(
+        '/api/v2/download/accounts',
+        content_type='application/json',
+        data=json.dumps({
+            "account_level": "federal_account",
+            "filters": {
+                "submission_type": "account_balances",
+                "fy": "2017",
+                "quarter": "4",
+                "federal_account": "-2"
+            },
+            "file_format": "csv"
+        }))
+
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_account_level_failure(client, base_job_data):
     """ Test the accounts endpoint with a wrong account_level """
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
@@ -116,7 +252,7 @@ def test_account_level_failure(client, account_data):
 
 
 @pytest.mark.django_db
-def test_submission_type_failure(client, account_data):
+def test_submission_type_failure(client, base_job_data):
     """ Test the accounts endpoint with a wrong submission_type """
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
@@ -136,14 +272,14 @@ def test_submission_type_failure(client, account_data):
 
 
 @pytest.mark.django_db
-def test_fy_failure(client, account_data):
+def test_fy_failure(client, base_job_data):
     """ Test the accounts endpoint with a wrong fiscal year (FY) """
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
         '/api/v2/download/accounts',
         content_type='application/json',
         data=json.dumps({
-            "account_level": "treasury_account",
+            "account_level": "federal_account",
             "filters": {
                 "submission_type": "award_financial",
                 "fy": "string_not_int",
@@ -156,7 +292,7 @@ def test_fy_failure(client, account_data):
 
 
 @pytest.mark.django_db
-def test_quarter_failure(client, account_data):
+def test_quarter_failure(client, base_job_data):
     """ Test the accounts endpoint with a wrong quarter """
     csv_generation.retrieve_db_string = Mock(return_value=generate_test_db_connection_string())
     resp = client.post(
