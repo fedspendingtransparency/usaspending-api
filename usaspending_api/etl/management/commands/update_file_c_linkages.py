9f2afd58dc675706fbbf70a903673fcce6a3aa70
Nipun Monga <nipunmnga@gmail.com>
2018-07-17 10:53:01 -0400

    Adding necessary indexes to help with performance

--- usaspending_api/etl/management/commands/update_file_c_linkages.py
+++ usaspending_api/etl/management/commands/update_file_c_linkages.py
@@ -1,15 +1,28 @@
 from django.core.management.base import BaseCommand
-from django.db import transaction
+from django.db import connection, transaction
 
 from usaspending_api.common.helpers.etl_helpers import update_c_to_d_linkages
+from usaspending_api.common.helpers.sql_helpers import read_sql_file
 
 
 class Command(BaseCommand):
 
     LINKAGE_TYPES = ['contract', 'assistance']
+    ETL_SQL_FILE_PATH = 'usaspending_api/etl/management/sql/'
 
     def handle(self, *args, **options):
 
         with transaction.atomic():
+
+            sql_commands = read_sql_file(file_path=self.ETL_SQL_FILE_PATH + 'create_linkage_indexes.sql')
+            for command in sql_commands:
+                with connection.cursor() as cursor:
+                    cursor.execute(command)
+
             for link_type in self.LINKAGE_TYPES:
                 update_c_to_d_linkages(type=link_type)
+
+            sql_commands = read_sql_file(file_path=self.ETL_SQL_FILE_PATH + 'drop_linkage_indexes.sql')
+            for command in sql_commands:
+                with connection.cursor() as cursor:
+                    cursor.execute(command)
---
+++ usaspending_api/etl/management/sql/drop_linkage_indexes.sql
@@ -0,0 +1,5 @@
+DROP index tmp_aw_uri;
+DROP index tmp_faba_fain_uri;
+DROP index tmp_faba_fain;
+DROP index tmp_faba_uri;
+DROP index tmp_faba_piid;
