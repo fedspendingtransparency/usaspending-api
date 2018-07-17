327a6a745c4d8953fc72b80e53dd376f5d0d7b95
Nipun Monga <nipunmnga@gmail.com>
2018-07-17 11:34:32 -0400

    removing unnecessary indexes

--- usaspending_api/etl/management/sql/create_linkage_indexes.sql
+++ usaspending_api/etl/management/sql/create_linkage_indexes.sql
@@ -1,4 +1,3 @@
-CREATE index IF NOT EXISTS tmp_aw_uri ON awards(uri);
 CREATE index IF NOT EXISTS tmp_faba_fain_uri ON financial_accounts_by_awards(fain, uri, award_id) WHERE fain IS NOT NULL AND uri IS NOT NULL AND award_id IS NULL;
 CREATE index IF NOT EXISTS tmp_faba_fain ON financial_accounts_by_awards(fain, uri, award_id) WHERE fain IS NOT NULL AND uri IS NULL AND award_id IS NULL;
 CREATE index IF NOT EXISTS tmp_faba_uri ON financial_accounts_by_awards(fain, uri, award_id) WHERE fain IS NULL AND uri IS NOT NULL AND award_id IS NULL;
