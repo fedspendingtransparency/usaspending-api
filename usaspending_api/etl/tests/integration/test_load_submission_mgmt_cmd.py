d187476ca8320dafc48ebf577ad5e32eaa4e7cc5
Michael Hess <hess_michael@bah.com>
2018-07-18 12:29:22 -0400

    Create failing test

--- usaspending_api/etl/tests/integration/test_load_submission_mgmt_cmd.py
+++ usaspending_api/etl/tests/integration/test_load_submission_mgmt_cmd.py
@@ -321,3 +321,29 @@ def test_load_submission_file_c_fain_and_uri(mock_db_cursor):
     }
 
     assert expected_results == actual_results
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('mock_db_cursor', [DB_CURSOR_PARAMS.copy()], indirect=True)
+def test_load_submission_transaction_obligated_amount(mock_db_cursor):
+    """ Test load submission management command for File C transaction_obligated_amount NaNs """
+    mommy.make(
+        TreasuryAppropriationAccount,
+        **{
+            'treasury_account_identifier': -1111,
+            'allocation_transfer_agency_id': '999',
+            'agency_id': '999',
+            'beginning_period_of_availability': '1700-01-01',
+            'ending_period_of_availability': '1700-12-31',
+            'availability_type_code': '000',
+            'main_account_code': '0000',
+            'sub_account_code': '0000'
+        }
+    )
+    call_command('load_submission', '--noclean', '--nosubawards', '-9999')
+
+    expected_results = 0
+    actual_results = FinancialAccountsByAwards.objects.\
+        filter(Q(transaction_obligated_amount='NaN') | Q(transaction_obligated_amount=None)).count()
+
+    assert expected_results == actual_results   
