4c9c58ea0a9a50c491e213b31496c947bd580c67
Daniel Boos <boos_daniel@bah.com>
2018-07-19 10:52:48 -0400

    Starting to prep recipient profile endpoints for recipient profile matview

--- usaspending_api/recipient/v2/views/recipients.py
+++ usaspending_api/recipient/v2/views/recipients.py
@@ -2,7 +2,7 @@ import logging
 import copy
 
 from rest_framework.response import Response
-from django.db.models import Q, Sum, Count
+from django.db.models import Q, F, Sum, Count
 
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.exceptions import InvalidParameterException
@@ -11,9 +11,10 @@ from usaspending_api.common.views import APIDocumentationView
 from usaspending_api.core.validator.pagination import PAGINATION
 from usaspending_api.core.validator.tinyshield import TinyShield
 from usaspending_api.awards.v2.lookups.lookups import all_award_types_mappings
-from usaspending_api.awards.v2.filters.matview_filters import matview_search_filter
-from usaspending_api.awards.models_matviews import SummaryTransactionRecipientView
+from usaspending_api.awards.models_matviews import SummaryTransactionView, UniversalTransactionView
 from usaspending_api.references.models import RecipientLookup
+from usaspending_api.recipient.models_matviews import RecipientProfile
+from usaspending_api.awards.v2.filters.matview_filters import matview_search_filter
 
 from usaspending_api.recipient.models import DUNS
 from usaspending_api.recipient.v2.helpers import validate_year, reshape_filters
@@ -31,76 +32,71 @@ def validate_duns(duns):
     elif not DUNS.objects.filter(awardee_or_recipient_unique=duns).count() > 0:
         raise InvalidParameterException('DUNS not found: {}.'.format(duns))
 
-def obtain_recipient_totals(recipient_hash, year=None, award_type_codes=None, subawards=False):
-    filters = reshape_filters(duns=recipient_hash, year=year, award_type_codes=award_type_codes)
+def get_recipients(year=None, award_type_codes=None, subawards=False, duns=None, parent_duns=None, sort='desc', page=1,
+                   limit=None):
+    duns_list = []
+    if duns:
+        duns_list.append()
+    if parent_duns:
+        # TODO: Generate list of children via recipient profile table
+        duns_list = []
 
-    if not subawards:
-        queryset = matview_search_filter(filters, SummaryTransactionRecipientView)
-        queryset = None
+    if year == 'latest' or year == None:
+        # Use the Recipient Profile View
+        filters = Q() # recipient_profile_filters()
+        total_field = 'last_12_months' if year == 'latest' else 'all_fiscal_years'
+        queryset = RecipientProfile.objects.filter(filters).annotate(total=F(total_field)) \
+            .values('recipient_level', 'recipient_hash', 'recipient_unique_id', 'recipient_name', 'total')
+    else:
+        # Use the Universal Transaction Matview for specific years
+        filters = reshape_filters(year=year, subawards=subawards)
+        queryset = matview_search_filter(filters, UniversalTransactionView) \
+            .values('recipient_level', 'recipient_hash').annotate(total=Sum('generated_pragmatic_obligation'), count=Count()) \
+            .values('recipient_level', 'recipient_hash', 'recipient_unique_id', 'recipient_name', 'total')
 
-    try:
-        row = list(queryset)[0]
-        result = {
-            'duns': row['awardee_or_recipient_uniqu'],
-            'total': row['total'],
-            'count': len(set(row['distinct_awards'].split(','))),
-        }
-        return result
-    except IndexError:
-        # would prefer to catch an index error gracefully if the SQL query produces 0 rows
-        logger.warn('No results found for DUNS {} with filters: {}'.format(duns, filters))
-    return {'count': 0, 'duns': None, 'total': 0}
+    queryset = queryset.order_by('-total' if sort == 'desc' else 'total')
+    total_count = queryset.count()
+    if limit:
+        queryset = queryset[(page-1)*limit: page*limit]
 
+    results = []
+    for row in list(queryset):
+        results.append(
+            {
+                'id': '{}-{}'.format(row['recipient_hash'], row['recipient_level']),
+                'duns': row['recipient_unique_id'],
+                'name': row['recipient_name'],
+                'recipient_level': row['recipient_level'],
+                'total': row['total']
+            }
+        )
 
-def get_all_recipients(year=None, award_type_codes=None, subawards=False, parent_duns=None, sort='desc', page=1,
-                       limit=None):
-    filters = reshape_filters(year=year, award_type_codes=award_type_codes)
-
-    if not subawards:
-        order_by = '-total' if sort == 'desc' else 'total'
-        queryset = matview_search_filter(filters, SummaryTransactionRecipientView)\
-            .values('recipient_hash', 'recipient_unique_id', 'recipient_name')\
-            .annotate(total=Sum('generated_pragmatic_obligation'), count=Count('recipient_hash')).order_by(order_by)
-        total_count = queryset.count()
-        if limit:
-            queryset = queryset[(page-1)*limit: page*limit]
-
-
-        results = []
-        for row in list(queryset):
-            recipient_level = 'blah'
-            results.append(
-                {
-                    'hash': '{}-{}'.format(row['recipient_hash'], recipient_level),
-                    'duns': row['recipient_unique_id'],
-                    'name': row['recipient_name'],
-                    'recipient_level': recipient_level,
-                    # 'state_province': row['legal_entity_state_code'],
-                    'total': row['total'],
-                    'count': row['count'],
-                }
-            )
-
-        page_metadata = get_simple_pagination_metadata(total_count, limit, page)
+    page_metadata = get_simple_pagination_metadata(total_count, limit, page)
     return results, page_metadata
 
 
-def extract_location(duns):
+def extract_location(recipient_hash):
+    duns = RecipientLookup.objects.filter(recipient_hash=recipient_hash).values('duns').one()
+    duns_obj = DUNS.objects.filter(awardee_or_recipient_uniqu=duns)
+
     return {
-        'address_line1': '',
-        'address_line2': '',
-        'address_line3': '',
-        'foreign_province': '',
-        'city_name': '',
-        'county_name': '',
-        'state_code': '',
-        'zip': '',
-        'zip4': '',
-        'foreign_postal_code': '',
-        'country_name': '',
-        'country_code': '',
-        'congressional_code': ''
+        'address_line1': duns_obj.address_line_1,
+        'address_line2': duns_obj.address_line_2,
+        'address_line3': None,
+        'foreign_province': None,
+        'city_name': duns_obj.city,
+        'county_name': None,
+        'state_code': duns_obj.state,
+        'zip': duns_obj.zip,
+        'zip4': duns_obj.zip4,
+        'foreign_postal_code': None,
+        'country_name': None,
+        'country_code': duns_obj.country_code,
+        'congressional_code': duns_obj.congressional_district
     }
+
+def extract_business_types(recipient_hash):
+    return SummaryTransactionView.objects.filter(recipient_hash=recipient_hash).values('business_categories')
 
 
 # class ListRecipients(APIDocumentationView):
@@ -109,7 +105,8 @@ def extract_location(duns):
 #     def post(self, request):
 #         models = [
 #             {'name': 'keyword', 'key': 'keyword', 'type': 'text', 'text_type': 'search'},
-#             {'name': 'award_type', 'key': 'award_type', 'type': 'text', 'text_type': 'search', 'default': 'all'},
+#             {'name': 'award_type', 'key': 'award_type', 'type': 'enum',
+#              'enum_values': list(all_award_types_mappings.keys()) + ['all'],'default': 'all'},
 #         ]
 #         models.extend(copy.deepcopy(PAGINATION))
 #         validated_payload = TinyShield(models).block(request.data)
@@ -120,18 +117,16 @@ def extract_location(duns):
 #             award_type_codes = all_award_types_mappings[validated_payload['award_type']]
 #
 #         results = []
-#         recipients, page_metadata = get_all_recipients(year='latest', sort=validated_payload['order'],
-#                                                        award_type_codes=award_type_codes,
-#                                                        page=validated_payload['page'],
-#                                                        limit=validated_payload['limit'])
+#         recipients, page_metadata = get_recipients(year='latest', sort=validated_payload['order'],
+#                                                    award_type_codes=award_type_codes, page=validated_payload['page'],
+#                                                    limit=validated_payload['limit'])
 #         for item in recipients:
 #             results.append({
-#                 'id': item['hash'],
-#                 'name': item['name'],
+#                 'id': item['id'],
 #                 'duns': item['duns'],
+#                 'name': item['name'],
 #                 'recipient_level': item['recipient_level'],
-#                 'amount': item['total'],
-#                 'count': item['count']
+#                 'amount': item['total']
 #             })
 #
 #         return Response({'page_metadata': page_metadata, 'results': results})
@@ -145,19 +140,26 @@ class RecipientOverView(APIDocumentationView):
         year = validate_year(get_request.get('year', 'latest'))
         recipient_hash = validate_hash(recipient_hash)
 
-        # Gather totals
-        recipient_totals = obtain_recipient_totals(recipient_hash, year=year)
-        # recipient_sub_totals = obtain_recipient_totals(duns, year=year, subawards=True)
+        # Gather DUNS object via the hash
+        location = extract_location(recipient_hash)
+        business_types = extract_business_types() # TODO: CONVERT CODES TO READABLE NAMES
 
+        # Gather totals
+        recipients, page_metadata = get_recipients(recipient_hash, year=year)
+        # sub_recipients, page_metadata = get_recipients(recipient_hash, year=year, subawards=True)
+
+        item = recipients[0]
         result = {
-            # 'name': duns_obj.legal_business_name,
-            # 'duns': duns,
-            'recipient_level': '',
-            # 'parent_name': duns_obj.ultimate_parent_legal_enti,
-            # 'parent_duns': duns_obj.ultimate_parent_unique_ide,
-            # 'location': location,
+            'name': item['name'],
+            'duns': item['recipient_unique_id'],
+            'id': item['id'],
+            'recipient_level': item['recipient_level'],
+            'parent_name': duns_obj.ultimate_parent_legal_enti,
+            'parent_duns': duns_obj.ultimate_parent_unique_ide,
+            'business_types': business_types,
+            'location': location,
             'business_types': '',
-            'total_prime_amount': recipient_totals['total'],
+            'total_prime_amount': item['total'],
             'total_prime_awards': recipient_totals['count'],
             # 'total_sub_amount': recipient_sub_totals['total'],
             # 'total_sub_awards': recipient_sub_totals['count']
@@ -175,14 +177,12 @@ class RecipientOverView(APIDocumentationView):
 #         duns = validate_duns(duns)
 #
 #         results = []
-#         recipients, page_metadata = get_all_recipients(year=year, parent_duns=duns)
+#         recipients, page_metadata = get_recipients(year=year, parent_duns=duns)
 #         for item in recipients:
 #             results.append({
 #                 'id': item['hash'],
 #                 'name': item['name'],
 #                 'duns': item['duns'],
-#                 'state_province': item['state_province'],
 #                 'amount': item['amount'],
 #             })
 #         return Response(results)
-
