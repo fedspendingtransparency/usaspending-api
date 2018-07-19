46740222dfbcc6ccfed77b29ced0d3af4aeb949a
Michael Hess <hess_michael@bah.com>
2018-07-19 13:45:47 -0400

    Add budget_function and budget_subfunction Custom Account filter

--- usaspending_api/accounts/tests/unit/test_account_download_filter.py
+++ usaspending_api/accounts/tests/unit/test_account_download_filter.py
@@ -54,6 +54,50 @@ def test_federal_account_filter():
 
 
 @pytest.mark.django_db
+def test_budget_function_filter():
+    """ Ensure the Budget Function filter is working """
+    # Create TAS models
+    tas1 = mommy.make('accounts.TreasuryAppropriationAccount', budget_function_code='BUD')
+    tas2 = mommy.make('accounts.TreasuryAppropriationAccount', budget_function_code='NOT')
+
+    # Create file B models
+    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass',
+               treasury_account_id=tas1.treasury_account_identifier, reporting_period_start='1699-10-01',
+               reporting_period_end='1699-12-31')
+    mommy.make('financial_activities.FinancialAccountsByProgramActivityObjectClass',
+               treasury_account_id=tas2.treasury_account_identifier, reporting_period_start='1699-10-01',
+               reporting_period_end='1699-12-31')
+
+    queryset = account_download_filter('program_activity_object_class', FinancialAccountsByProgramActivityObjectClass, {
+        'budget_function': 'BUD',
+        'fy': 1700,
+        'quarter': 1
+    })
+    assert queryset.count() == 1
+
+
+@pytest.mark.django_db
+def test_budget_function_filter():
+    """ Ensure the Budget Subfunction filter is working """
+    # Create TAS models
+    tas1 = mommy.make('accounts.TreasuryAppropriationAccount', budget_subfunction_code='SUB')
+    tas2 = mommy.make('accounts.TreasuryAppropriationAccount', budget_subfunction_code='NOT')
+
+    # Create file C models
+    mommy.make('awards.FinancialAccountsByAwards', treasury_account_id=tas1.treasury_account_identifier,
+               reporting_period_start='1699-10-01', reporting_period_end='1699-12-31')
+    mommy.make('awards.FinancialAccountsByAwards', treasury_account_id=tas2.treasury_account_identifier,
+               reporting_period_start='1699-10-01', reporting_period_end='1699-12-31')
+
+    queryset = account_download_filter('award_financial', FinancialAccountsByAwards, {
+        'budget_subfunction': 'SUB',
+        'fy': 1700,
+        'quarter': 1
+    })
+    assert queryset.count() == 1
+
+
+@pytest.mark.django_db
 def test_cgac_agency_filter():
     """ Ensure the CGAC agency filter is working """
     # Create TAS models
