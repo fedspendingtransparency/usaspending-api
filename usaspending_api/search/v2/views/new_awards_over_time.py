617cb9eda8bfadae1c1697eb737afaa90b0b9af2
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-07-24 15:18:51 -0400

    added documentation, removed some temp code

--- usaspending_api/search/v2/views/new_awards_over_time.py
+++ usaspending_api/search/v2/views/new_awards_over_time.py
@@ -6,9 +6,9 @@ from django.db.models import Func, IntegerField, Sum
 from django.db.models.aggregates import Aggregate
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from usaspending_api.awards.models_matviews import SummaryAwardRecipientView
 from usaspending_api.awards.v2.filters.filter_helpers import combine_date_range_queryset
-from usaspending_api.common.api_versioning import api_transformations, API_TRANSFORM_FUNCTIONS
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.exceptions import InvalidParameterException
 from usaspending_api.core.validator.award_filter import AWARD_FILTER
@@ -48,16 +48,13 @@ class FiscalYear(Func):
     output_field = IntegerField()
 
 
-@api_transformations(api_version=API_VERSION, function_list=API_TRANSFORM_FUNCTIONS)
 class NewAwardsOverTimeVisualizationViewSet(APIView):
     """
-
-    endpoint_doc: /advanced_award_search/spending_over_time.md
+    endpoint_doc: /advanced_award_search/new_awards_over_time.md
     """
 
     @cache_response()
     def post(self, request):
-        """Return all budget function/subfunction titles matching the provided search text"""
         groupings = {
             "quarter": "quarter",
             "q": "quarter",
@@ -67,13 +64,12 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
             "m": "month",
         }
         models = [
-            {"name": "subawards", "key": "subawards", "type": "boolean", "default": False},
             {"name": "group", "key": "group", "type": "enum", "enum_values": list(groupings.keys()), "default": "fy"},
         ]
         advanced_search_filters = [
             model
             for model in copy.deepcopy(AWARD_FILTER)
-            if model["name"] in ("time_period", "award_type_codes", "recipient_id")
+            if model["name"] in ("time_period", "recipient_id")
         ]
         models.extend(advanced_search_filters)
         json_request = TinyShield(models).block(request.data)
@@ -81,9 +77,6 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
 
         if filters is None:
             raise InvalidParameterException("Missing request parameters: filters")
-
-        if json_request["subawards"]:
-            raise NotImplementedError("subawards are not implemented yet")
 
         recipient_hash = filters["recipient_id"][:-2]
         is_parent = True if filters["recipient_id"][-1] == "P" else False
