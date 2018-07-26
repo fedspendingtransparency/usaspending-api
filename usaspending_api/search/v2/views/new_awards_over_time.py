2d591a016541baf9de884d833e50c1a788c481e1
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-07-26 12:24:06 -0400

    Refactored post() into several functions

--- usaspending_api/search/v2/views/new_awards_over_time.py
+++ usaspending_api/search/v2/views/new_awards_over_time.py
@@ -43,9 +43,8 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
     endpoint_doc: /advanced_award_search/new_awards_over_time.md
     """
 
-    @cache_response()
-    def post(self, request):
-        groupings = {
+    def validate_api_request(self, json_payload):
+        self.groupings = {
             "quarter": "quarter",
             "q": "quarter",
             "fiscal_year": "fiscal_year",
@@ -54,7 +53,13 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
             "m": "month",
         }
         models = [
-            {"name": "group", "key": "group", "type": "enum", "enum_values": list(groupings.keys()), "default": "fy"},
+            {
+                "name": "group",
+                "key": "group",
+                "type": "enum",
+                "enum_values": list(self.groupings.keys()),
+                "default": "fy",
+            }
         ]
         advanced_search_filters = [
             model for model in copy.deepcopy(AWARD_FILTER) if model["name"] in ("time_period", "recipient_id")
@@ -64,15 +69,12 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
             if model["name"] in ("time_period", "recipient_id"):
                 model["optional"] = False
         models.extend(advanced_search_filters)
-        json_request = TinyShield(models).block(request.data)
-        filters = json_request.get("filters", None)
+        return TinyShield(models).block(json_payload)
 
-        if filters is None:
-            raise InvalidParameterException("Missing request parameters: filters")
-
-        recipient_hash = filters["recipient_id"][:-2]
+    def database_data_layer(self):
+        recipient_hash = self.filters["recipient_id"][:-2]
         time_ranges = []
-        for t in filters["time_period"]:
+        for t in self.filters["time_period"]:
             t["date_type"] = "date_signed"
             time_ranges.append(t)
         queryset = SummaryAwardRecipientView.objects.filter()
@@ -80,7 +82,7 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
             time_ranges, SummaryAwardRecipientView, API_SEARCH_MIN_DATE, API_MAX_DATE
         )
 
-        if filters["recipient_id"][-1] == "P":
+        if self.filters["recipient_id"][-1] == "P":
             # there *should* only one record with that hash and recipient_level = 'P'
             parent_duns_rows = RecipientProfile.objects.filter(
                 recipient_hash=recipient_hash, recipient_level="P"
@@ -89,36 +91,47 @@ class NewAwardsOverTimeVisualizationViewSet(APIView):
                 raise InvalidParameterException("Provided recipient_id has no parent records")
             parent_duns = parent_duns_rows[0]["recipient_unique_id"]
             queryset = queryset.filter(parent_recipient_unique_id=parent_duns)
-        elif filters["recipient_id"][-1] == "R":
+        elif self.filters["recipient_id"][-1] == "R":
             queryset = queryset.filter(recipient_hash=recipient_hash, parent_recipient_unique_id__isnull=True)
         else:
             queryset = queryset.filter(recipient_hash=recipient_hash, parent_recipient_unique_id__isnull=False)
 
-        values = ["year"]
-        if groupings[json_request["group"]] == "month":
-            queryset = queryset.annotate(month=FiscalMonth("date_signed"), year=FiscalYear("date_signed"))
+        values = ["fy"]
+        if self.groupings[self.json_request["group"]] == "month":
+            queryset = queryset.annotate(month=FiscalMonth("date_signed"), fy=FiscalYear("date_signed"))
             values.append("month")
 
-        elif groupings[json_request["group"]] == "quarter":
-            queryset = queryset.annotate(quarter=FiscalQuarter("date_signed"), year=FiscalYear("date_signed"))
+        elif self.groupings[self.json_request["group"]] == "quarter":
+            queryset = queryset.annotate(quarter=FiscalQuarter("date_signed"), fy=FiscalYear("date_signed"))
             values.append("quarter")
 
-        elif groupings[json_request["group"]] == "fiscal_year":
-            queryset = queryset.annotate(year=FiscalYear("date_signed"))
+        elif self.groupings[self.json_request["group"]] == "fiscal_year":
+            queryset = queryset.annotate(fy=FiscalYear("date_signed"))
 
         queryset = (
             queryset.values(*values).annotate(count=Sum("counts")).order_by(*["-{}".format(value) for value in values])
         )
+        return queryset, values
+
+    @cache_response()
+    def post(self, request):
+        self.json_request = self.validate_api_request(request.data)
+        self.filters = self.json_request.get("filters", None)
+
+        if self.filters is None:
+            raise InvalidParameterException("Missing request parameters: filters")
+
+        queryset, values = self.database_data_layer()
 
         results = []
         for row in queryset:
             result = {"time_period": {}}
             for period in values:
-                result["time_period"]["fiscal_{}".format(period)] = row[period]
+                result["time_period"][self.groupings[period]] = row[period]
             result["new_award_count_in_period"] = row["count"]
 
             results.append(result)
 
-        response_dict = {"group": groupings[json_request["group"]], "results": results}
+        response_dict = {"group": self.groupings[self.json_request["group"]], "results": results}
 
         return Response(response_dict)
