85c74a9eb626578815f076e8e857ac0f815fc217
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-07-31 17:34:55 -0400

    added more tests to flesh out test cases

--- usaspending_api/search/tests/unit/test_new_awards_over_time.py
+++ usaspending_api/search/tests/unit/test_new_awards_over_time.py
@@ -12,7 +12,9 @@ from django_mock_queries.query import MockModel
 from usaspending_api.common.exceptions import InvalidParameterException
 from usaspending_api.common.exceptions import UnprocessableEntityException
 from usaspending_api.common.helpers.unit_test_helper import add_to_mock_objects
-from usaspending_api.search.v2.views.new_awards_over_time import NewAwardsOverTimeVisualizationViewSet
+from usaspending_api.search.v2.views.new_awards_over_time import (
+    NewAwardsOverTimeVisualizationViewSet
+)
 
 
 def get_new_awards_over_time_url():
@@ -41,52 +43,284 @@ def catch_filter_errors(viewset, filters, expected_exception):
 
 @pytest.fixture
 def add_award_recipients(mock_matviews_qs, refresh_matviews):
-    mock_awards_1 = MockModel(
-        recipient_hash="oiwehfwefw",
+    mock_awards_1_0 = MockModel(
+        recipient_hash="21a1b0df-e7cd-349b-b948-60ed0ac1e6a0",
         parent_recipient_unique_id=None,
         counts=12,
         date_signed=datetime(2009, 5, 30),
         action_date="2009-05-30",
     )
-    mock_awards_2 = MockModel(
-        recipient_hash="uwqhewneuwe",
+    mock_awards_1_1 = MockModel(
+        recipient_hash="21a1b0df-e7cd-349b-b948-60ed0ac1e6a0",
+        parent_recipient_unique_id=None,
+        counts=3,
+        date_signed=datetime(2009, 5, 1),
+        action_date="2009-05-01",
+    )
+    mock_awards_1_2 = MockModel(
+        recipient_hash="21a1b0df-e7cd-349b-b948-60ed0ac1e6a0",
+        parent_recipient_unique_id=None,
+        counts=1,
+        date_signed=datetime(2009, 7, 2),
+        action_date="2009-07-02",
+    )
+    mock_awards_1_3 = MockModel(
+        recipient_hash="21a1b0df-e7cd-349b-b948-60ed0ac1e6a0",
+        parent_recipient_unique_id=None,
+        counts=2,
+        date_signed=datetime(2008, 1, 10),
+        action_date="2008-01-10",
+    )
+
+    mock_awards_2_0 = MockModel(
+        recipient_hash="4e418651-4b83-8722-ab4e-e68d80bfb3b3",
         parent_recipient_unique_id=None,
         counts=6,
         date_signed=datetime(2009, 7, 30),
         action_date="2009-05-30",
     )
-    add_to_mock_objects(mock_matviews_qs, [mock_awards_1, mock_awards_2])
+    add_to_mock_objects(
+        mock_matviews_qs,
+        [
+            mock_awards_1_0,
+            mock_awards_1_1,
+            mock_awards_1_2,
+            mock_awards_1_3,
+            mock_awards_2_0,
+        ],
+    )
 
 
 @pytest.mark.django_db
-def test_new_awards_simple_case(add_award_recipients, client):
+def test_new_awards_month(add_award_recipients, client):
     test_payload = {
-        "group": "quarter",
+        "group": "month",
         "filters": {
-            "time_period": [{"start_date": "2008-10-01", "end_date": "2010-09-30"}],
-            "recipient_id": "oiwehfwefw-R",
+            "time_period": [
+                {"start_date": "2008-10-01", "end_date": "2010-09-30"}
+            ],
+            "recipient_id": "21a1b0df-e7cd-349b-b948-60ed0ac1e6a0-R",
         },
     }
-    resp = client.post(get_new_awards_over_time_url(), content_type="application/json", data=json.dumps(test_payload))
-    assert resp.status_code == 200
-
     expected_response = {
-        "group": "quarter",
-        "results": [{"time_period": {"fiscal_year": 2009, "quarter": 3}, "new_award_count_in_period": 12}],
+        "group": "month",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009, "month": 10},
+                "new_award_count_in_period": 1,
+            },
+            {
+                "time_period": {"fiscal_year": 2009, "month": 8},
+                "new_award_count_in_period": 15,
+            },
+        ],
     }
 
-    assert resp.data["group"] == "quarter"
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+    assert resp.data["group"] == "month"
+    assert expected_response == resp.data
+
+    test_payload["filters"]["time_period"] = [
+        {"start_date": "2007-10-01", "end_date": "2010-09-30"}
+    ]
+
+    expected_response = {
+        "group": "month",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009, "month": 10},
+                "new_award_count_in_period": 1,
+            },
+            {
+                "time_period": {"fiscal_year": 2009, "month": 8},
+                "new_award_count_in_period": 15,
+            },
+            {
+                "time_period": {"fiscal_year": 2008, "month": 4},
+                "new_award_count_in_period": 2,
+            },
+        ],
+    }
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+    assert resp.data["group"] == "month"
     assert expected_response == resp.data
 
 
 @pytest.mark.django_db
-def test_new_awards():
+def test_new_awards_quarter(add_award_recipients, client):
+    test_payload = {
+        "group": "quarter",
+        "filters": {
+            "time_period": [
+                {"start_date": "2008-10-01", "end_date": "2010-09-30"}
+            ],
+            "recipient_id": "21a1b0df-e7cd-349b-b948-60ed0ac1e6a0-R",
+        },
+    }
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+
+    expected_response = {
+        "group": "quarter",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009, "quarter": 4},
+                "new_award_count_in_period": 1,
+            },
+            {
+                "time_period": {"fiscal_year": 2009, "quarter": 3},
+                "new_award_count_in_period": 15,
+            },
+        ],
+    }
+    assert resp.data["group"] == "quarter"
+    assert expected_response == resp.data
+
+    expected_response = {
+        "group": "quarter",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009, "quarter": 4},
+                "new_award_count_in_period": 1,
+            },
+            {
+                "time_period": {"fiscal_year": 2009, "quarter": 3},
+                "new_award_count_in_period": 15,
+            },
+            {
+                "time_period": {"fiscal_year": 2008, "quarter": 2},
+                "new_award_count_in_period": 2,
+            },
+        ],
+    }
+
+    test_payload["filters"]["time_period"] = [
+        {"start_date": "2007-10-01", "end_date": "2010-09-30"}
+    ]
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+    assert expected_response == resp.data
+
+
+@pytest.mark.django_db
+def test_new_awards_fiscal_year(add_award_recipients, client):
+    test_payload = {
+        "group": "fiscal_year",
+        "filters": {
+            "time_period": [
+                {"start_date": "2008-10-01", "end_date": "2010-09-30"}
+            ],
+            "recipient_id": "21a1b0df-e7cd-349b-b948-60ed0ac1e6a0-R",
+        },
+    }
+    expected_response = {
+        "group": "fiscal_year",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009},
+                "new_award_count_in_period": 16,
+            }
+        ],
+    }
+
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+    assert resp.data["group"] == "fiscal_year"
+    assert expected_response == resp.data
+
+    test_payload["filters"]["time_period"] = [
+        {"start_date": "2007-10-01", "end_date": "2010-09-30"}
+    ]
+
+    expected_response = {
+        "group": "fiscal_year",
+        "results": [
+            {
+                "time_period": {"fiscal_year": 2009},
+                "new_award_count_in_period": 16,
+            },
+            {
+                "time_period": {"fiscal_year": 2008},
+                "new_award_count_in_period": 2,
+            },
+        ],
+    }
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 200
+    assert resp.data["group"] == "fiscal_year"
+    assert expected_response == resp.data
+
+
+@pytest.mark.django_db
+def test_new_awards_failures(add_award_recipients, client):
+    test_payload = {
+        "group": "quarter",
+        "filters": {
+            "time_period": [
+                {"start_date": "2008-10-01", "end_date": "2010-09-30"}
+            ],
+            "recipient_id": "21a1b0df-e7cd-349b-b948-60ed0ac1e6a0-P",
+        },
+    }
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 400  # No parent records found, return 400
+
+    test_payload["filters"]["recipient_hash"] = "enriwerniewrn"
+    resp = client.post(
+        get_new_awards_over_time_url(),
+        content_type="application/json",
+        data=json.dumps(test_payload),
+    )
+    assert resp.status_code == 400  # might reject text as non UUID in future?
+
+
+@pytest.mark.django_db
+def test_new_awards_filter_errors():
     A = NewAwardsOverTimeVisualizationViewSet()
     filters = {"group": "baseball"}
     catch_filter_errors(A, filters, "UnprocessableEntityException")
-    filters = {"group": "baseball", "filters": {"recipient_id": "", "time_period": []}}
+    filters = {
+        "group": "baseball",
+        "filters": {"recipient_id": "", "time_period": []},
+    }
     catch_filter_errors(A, filters, "InvalidParameterException")
-    filters = {"group": "month", "filters": {"recipient_id": "", "time_period": []}}
+    filters = {
+        "group": "month",
+        "filters": {"recipient_id": "", "time_period": []},
+    }
     catch_filter_errors(A, filters, "UnprocessableEntityException")
-    filters = {"group": "month", "filters": {"recipient_id": "", "time_period": [{"start_date": ""}]}}
+    filters = {
+        "group": "month",
+        "filters": {"recipient_id": "", "time_period": [{"start_date": ""}]},
+    }
     catch_filter_errors(A, filters, "UnprocessableEntityException")
