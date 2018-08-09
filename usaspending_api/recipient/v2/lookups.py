561671afa9b75022c76b25c25a43a4ddce3445b0
Daniel Boos <boos_daniel@bah.com>
2018-08-09 14:45:49 -0400

    Adding SAM functional data dictionary csv, keeping mapping in memory

--- usaspending_api/recipient/v2/lookups.py
+++ usaspending_api/recipient/v2/lookups.py
@@ -1,3 +1,7 @@
+import os
+import django
+import csv
+
 # Recipient Levels
 #   - P = Parent Recipient, There is at least one child recipient that lists this recipient as a parent
 #   - C = Child Recipient, References a parent recipient
@@ -12,3 +16,14 @@ SPECIAL_CASES = [
     'PRIVATE INDIVIDUAL',
     'INDIVIDUAL RECIPIENT'
 ]
+
+# Based on SAM Functional Data Dictionary
+SAM_FUNCTIONAL_DATA_DICTIONARY_CSV = os.path.join(django.conf.settings.BASE_DIR, 'usaspending_api', 'data',
+                                                  'sam_functional_data_dictionary.csv')
+DUNS_BUSINESS_TYPES_MAPPING = {}
+
+with open(SAM_FUNCTIONAL_DATA_DICTIONARY_CSV, 'r') as sam_data_dict_csv:
+    reader = csv.DictReader(sam_data_dict_csv, delimiter=',')
+    rows = list(reader)
+    for index, row in enumerate(rows, 1):
+        DUNS_BUSINESS_TYPES_MAPPING[row['code']]= row['terse_label']
