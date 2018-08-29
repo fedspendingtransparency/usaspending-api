3c046258f00adef9f9070d35093cff4698c85900
Isaac Ray <isaacmray1984@gmail.com>
2018-08-29 11:58:24 -0400

    fixing tests

--- usaspending_api/awards/tests/test_awards_v2.py
+++ usaspending_api/awards/tests/test_awards_v2.py
@@ -188,204 +188,221 @@ def test_award_endpoint(client):
 
 
 expected_response_asst = {
-      "period_of_performance": {
-        "period_of_performance_start_date": "2004-02-04",
-        "period_of_performance_current_end_date": "2005-02-04"
-      },
-      "executive_details": {
-        "officers": []
-      },
-      "cfda_objectives": None,
-      "cfda_title": "Shazam",
-      "cfda_number": "1234",
-      "type": "11",
-      "type_description": "OTHER FINANCIAL ASSISTANCE",
-      "category": "grant",
-      "piid": "1234",
-      "description": "lorem ipsum",
-      "total_subaward_amount": "12345.00",
-      "subaward_count": 10,
-      "awarding_agency": {
-        "office_agency_name": "office_agency",
-        "toptier_agency": {
-          "abbreviation": "some other stuff",
-          "name": "agency name"
-        },
-        "subtier_agency": {
-          "abbreviation": "some other stuff",
-          "name": "agency name"
-        }
-      },
-      "funding_agency": {
-        "office_agency_name": "office_agency",
-        "toptier_agency": {
-          "abbreviation": "some other stuff",
-          "name": "agency name"
-        },
-        "subtier_agency": {
-          "abbreviation": "some other stuff",
-          "name": "agency name"
-        }
-      },
-      "recipient": {
-        "recipient_parent_name": "Sams Club",
-        "parent_recipient_unique_id": "123",
-        "recipient_name": "John's Pizza",
-        "business_categories": [
-          "small_business"
-        ],
-        "recipient_unique_id": "456",
-        "location": {
-          "location_country_code": "USA",
-          "country_name": "UNITED STATES",
-          "state_code": "NC",
-          "city_name": "Charlotte",
-          "county_name": "BUNCOMBE",
-          "address_line1": "123 main st",
-          "address_line2": None,
-          "address_line3": None,
-          "zip4": "122045312",
-          "congressional_code": "90",
-          "zip5": "12204",
-          "foreign_postal_code": None,
-          "foreign_province": None
-        }
-      },
-      "place_of_performance": {
-        "location_country_code": "USA",
-        "country_name": "UNITED STATES",
-        "state_code": "NC",
-        "city_name": "Charlotte",
-        "county_name": "BUNCOMBE",
-        "address_line1": "123 main st",
-        "address_line2": None,
-        "address_line3": None,
-        "zip4": "122045312",
-        "congressional_code": "90",
-        "zip5": "12204",
-        "foreign_postal_code": None,
-        "foreign_province": None
-      }
+  "id": 1,
+  "type": "11",
+  "category": "grant",
+  "type_description": "OTHER FINANCIAL ASSISTANCE",
+  "piid": "1234",
+  "description": "lorem ipsum",
+  "cfda_objectives": None,
+  "cfda_number": "1234",
+  "cfda_title": "Shazam",
+  "awarding_agency": {
+    "toptier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "subtier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "office_agency_name": "office_agency",
+    "office_agency": {
+      "aac_code": None,
+      "name": "office_agency"
     }
-
-expected_response_cont = {
-  "executive_details": {
-    "officers": []
   },
+  "funding_agency": {
+    "toptier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "subtier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "office_agency_name": "office_agency",
+    "office_agency": {
+      "aac_code": None,
+      "name": "office_agency"
+    }
+  },
+  "recipient": {
+    "recipient_name": "John's Pizza",
+    "recipient_unique_id": "456",
+    "parent_recipient_unique_id": "123",
+    "business_categories": [
+      "small_business"
+    ],
+    "location": {
+      "address_line1": "123 main st",
+      "address_line2": None,
+      "address_line3": None,
+      "foreign_province": None,
+      "city_name": "Charlotte",
+      "county_name": "BUNCOMBE",
+      "state_code": "NC",
+      "zip5": "12204",
+      "zip4": "122045312",
+      "foreign_postal_code": None,
+      "country_name": "UNITED STATES",
+      "location_country_code": "USA",
+      "congressional_code": "90"
+    },
+    "parent_recipient_name": None
+  },
+  "subaward_count": 10,
+  "total_subaward_amount": "12345.00",
   "period_of_performance": {
     "period_of_performance_start_date": "2004-02-04",
     "period_of_performance_current_end_date": "2005-02-04"
   },
-  "latest_transaction_contract_data": {
-    "type_of_contract_pric_desc": "FIRM FIXED PRICE",
-    "naics": "333911",
-    "naics_description": "PUMP AND PUMPING EQUIPMENT MANUFACTURING",
-    "referenced_idv_agency_iden": "9700",
-    "idv_type_description": None,
-    "multiple_or_single_aw_desc": None,
-    "type_of_idc_description": None,
-    "dod_claimant_program_code": "C9E",
-    "clinger_cohen_act_pla_desc": "NO",
-    "commercial_item_acquisitio": "A",
-    "commercial_item_test_desc": "NO",
-    "consolidated_contract_desc": "NOT CONSOLIDATED",
-    "cost_or_pricing_data_desc": "NO",
-    "construction_wage_rat_desc": "NO",
-    "evaluated_preference_desc": "NO PREFERENCE USED",
-    "extent_competed": "D",
-    "fed_biz_opps_description": "YES",
-    "foreign_funding_desc": "NOT APPLICABLE",
-    "information_technolog_desc": "NOT IT PRODUCTS OR SERVICES",
-    "interagency_contract_desc": "NOT APPLICABLE",
-    "major_program": None,
-    "purchase_card_as_paym_desc": "NO",
-    "multi_year_contract_desc": "NO",
-    "number_of_offers_received": None,
-    "price_evaluation_adjustmen": None,
-    "product_or_service_code": "4730",
-    "program_acronym": None,
-    "other_than_full_and_o_desc": None,
-    "sea_transportation_desc": "NO",
-    "labor_standards_descrip": "NO",
-    "small_business_competitive": "False",
-    "solicitation_identifier": None,
-    "solicitation_procedures": "NP",
-    "fair_opportunity_limi_desc": None,
-    "subcontracting_plan": "B",
-    "program_system_or_equipmen": "000",
-    "type_set_aside_description": None,
-    "materials_supplies_descrip": "NO",
-    "domestic_or_foreign_e_desc": "U.S. OWNED BUSINESS"
-  },
-  "type": "A",
-  "type_description": "DEFINITIVE CONTRACT",
-  "category": "contract",
-  "piid": "5678",
-  "total_obligation": "1000.00",
-  "description": "lorem ipsum",
-  "base_and_all_options_value": "2000.00",
-  "parent_award_piid": None,
-  "total_subaward_amount": "12345.00",
-  "subaward_count": 10,
-  "awarding_agency": {
-    "office_agency_name": "office_agency",
-    "toptier_agency": {
-      "abbreviation": "some other stuff",
-      "name": "agency name"
-    },
-    "subtier_agency": {
-      "abbreviation": "some other stuff",
-      "name": "agency name"
-    }
-  },
-  "funding_agency": {
-    "office_agency_name": "office_agency",
-    "toptier_agency": {
-      "abbreviation": "some other stuff",
-      "name": "agency name"
-    },
-    "subtier_agency": {
-      "abbreviation": "some other stuff",
-      "name": "agency name"
-    }
-  },
-  "recipient": {
-    "recipient_parent_name": "Sams Club",
-    "parent_recipient_unique_id": "123",
-    "recipient_name": "John's Pizza",
-    "business_categories": [
-      "small_business"
-    ],
-    "recipient_unique_id": "456",
-    "location": {
-      "location_country_code": "USA",
-      "country_name": "UNITED STATES",
-      "state_code": "NC",
-      "city_name": "Charlotte",
-      "county_name": "BUNCOMBE",
-      "address_line1": "123 main st",
-      "address_line2": None,
-      "address_line3": None,
-      "zip4": "122045312",
-      "congressional_code": "90",
-      "zip5": "12204",
-      "foreign_postal_code": None,
-      "foreign_province": None
-    }
-  },
   "place_of_performance": {
-    "location_country_code": "USA",
-    "country_name": "UNITED STATES",
-    "state_code": "NC",
-    "city_name": "Charlotte",
-    "county_name": "BUNCOMBE",
     "address_line1": "123 main st",
     "address_line2": None,
     "address_line3": None,
-    "zip4": "122045312",
-    "congressional_code": "90",
+    "foreign_province": None,
+    "city_name": "Charlotte",
+    "county_name": "BUNCOMBE",
+    "state_code": "NC",
     "zip5": "12204",
+    "zip4": "122045312",
     "foreign_postal_code": None,
-    "foreign_province": None
+    "country_name": "UNITED STATES",
+    "location_country_code": "USA",
+    "congressional_code": "90"
+  },
+  "executive_details": {
+    "officers": []
+  }
+}
+
+expected_response_cont = {
+  "id": 2,
+  "type": "A",
+  "category": "contract",
+  "type_description": "DEFINITIVE CONTRACT",
+  "piid": "5678",
+  "parent_award_piid": None,
+  "description": "lorem ipsum",
+  "awarding_agency": {
+    "toptier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "subtier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "office_agency_name": "office_agency",
+    "office_agency": {
+      "aac_code": None,
+      "name": "office_agency"
+    }
+  },
+  "funding_agency": {
+    "toptier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "subtier_agency": {
+      "name": "agency name",
+      "abbreviation": "some other stuff"
+    },
+    "office_agency_name": "office_agency",
+    "office_agency": {
+      "aac_code": None,
+      "name": "office_agency"
+    }
+  },
+  "recipient": {
+    "recipient_name": "John's Pizza",
+    "recipient_unique_id": "456",
+    "parent_recipient_unique_id": "123",
+    "business_categories": [
+      "small_business"
+    ],
+    "location": {
+      "address_line1": "123 main st",
+      "address_line2": None,
+      "address_line3": None,
+      "foreign_province": None,
+      "city_name": "Charlotte",
+      "county_name": "BUNCOMBE",
+      "state_code": "NC",
+      "zip5": "12204",
+      "zip4": "122045312",
+      "foreign_postal_code": None,
+      "country_name": "UNITED STATES",
+      "location_country_code": "USA",
+      "congressional_code": "90"
+    },
+    "parent_recipient_name": None
+  },
+  "total_obligation": "1000.00",
+  "base_and_all_options_value": "2000.00",
+  "period_of_performance": {
+    "period_of_performance_start_date": "2004-02-04",
+    "period_of_performance_current_end_date": "2005-02-04"
+  },
+  "place_of_performance": {
+    "address_line1": "123 main st",
+    "address_line2": None,
+    "address_line3": None,
+    "foreign_province": None,
+    "city_name": "Charlotte",
+    "county_name": "BUNCOMBE",
+    "state_code": "NC",
+    "zip5": "12204",
+    "zip4": "122045312",
+    "foreign_postal_code": None,
+    "country_name": "UNITED STATES",
+    "location_country_code": "USA",
+    "congressional_code": "90"
+  },
+  "latest_transaction_contract_data": {
+    "idv_type_description": None,
+    "type_of_idc_description": None,
+    "referenced_idv_agency_iden": "9700",
+    "multiple_or_single_aw_desc": None,
+    "solicitation_identifier": None,
+    "solicitation_procedures": "NP",
+    "number_of_offers_received": None,
+    "extent_competed": "D",
+    "other_than_full_and_o_desc": None,
+    "type_set_aside_description": None,
+    "commercial_item_acquisitio": "A",
+    "commercial_item_test_desc": "NO",
+    "evaluated_preference_desc": "NO PREFERENCE USED",
+    "fed_biz_opps_description": "YES",
+    "small_business_competitive": "False",
+    "fair_opportunity_limi_desc": None,
+    "product_or_service_code": "4730",
+    "naics": "333911",
+    "dod_claimant_program_code": "C9E",
+    "program_system_or_equipmen": "000",
+    "information_technolog_desc": "NOT IT PRODUCTS OR SERVICES",
+    "sea_transportation_desc": "NO",
+    "clinger_cohen_act_pla_desc": "NO",
+    "construction_wage_rat_desc": "NO",
+    "labor_standards_descrip": "NO",
+    "materials_supplies_descrip": "NO",
+    "cost_or_pricing_data_desc": "NO",
+    "domestic_or_foreign_e_desc": "U.S. OWNED BUSINESS",
+    "foreign_funding_desc": "NOT APPLICABLE",
+    "interagency_contract_desc": "NOT APPLICABLE",
+    "major_program": None,
+    "price_evaluation_adjustmen": None,
+    "program_acronym": None,
+    "subcontracting_plan": "B",
+    "multi_year_contract_desc": "NO",
+    "purchase_card_as_paym_desc": "NO",
+    "consolidated_contract_desc": "NOT CONSOLIDATED",
+    "type_of_contract_pric_desc": "FIRM FIXED PRICE"
+  },
+  "subaward_count": 10,
+  "total_subaward_amount": "12345.00",
+  "executive_details": {
+    "officers": []
   }
 }
