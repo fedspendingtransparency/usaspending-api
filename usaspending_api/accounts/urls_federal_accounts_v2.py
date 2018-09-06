5da1427c5b2b63d4aa5b18fe57d6b89fbb6cbf38
Michael Hess <hess_michael@bah.com>
2018-09-06 13:04:57 -0400

    Create v2 Federal Account endpoint that uses the federal_account_code instead of database ID

--- usaspending_api/accounts/urls_federal_accounts_v2.py
+++ usaspending_api/accounts/urls_federal_accounts_v2.py
@@ -7,6 +7,7 @@ object_class_federal_accounts = views.ObjectClassFederalAccountsViewSet.as_view(
 fiscal_year_snapshot_federal_accounts = views.FiscalYearSnapshotFederalAccountsViewSet.as_view()
 spending_over_time_federal_accounts = views.SpendingOverTimeFederalAccountsViewSet.as_view()
 spending_by_category_federal_accounts = views.SpendingByCategoryFederalAccountsViewSet.as_view()
+federal_account = views.FederalAccountViewSet.as_view()
 federal_accounts = views.FederalAccountsViewSet.as_view()
 
 urlpatterns = [
@@ -15,5 +16,6 @@ urlpatterns = [
     url(r'(?P<pk>[0-9]+)/fiscal_year_snapshot$', fiscal_year_snapshot_federal_accounts),
     # url(r'(?P<pk>[0-9]+)/spending_over_time$', spending_over_time_federal_accounts),
     # url(r'(?P<pk>[0-9]+)/spending_by_category$', spending_by_category_federal_accounts),
+    url(r'(?P<fed_acct_code>[0-9]{3}[\-][0-9]{4})/$', federal_account),
     url(r'$', federal_accounts),
 ]
