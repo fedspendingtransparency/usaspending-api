da75f6161736820fabf1aa8296c53809de4faef0
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-09-14 15:12:51 -0400

    Adding more tests to check "no intersection"

--- usaspending_api/search/tests/test_spending_by_award.py
+++ usaspending_api/search/tests/test_spending_by_award.py
@@ -1,8 +1,16 @@
-import json
-
+# Stdlib imports
 import pytest
+import json
+from time import perf_counter
 from rest_framework import status
 
+# Core Django imports
+
+# Third-party app imports
+from django_mock_queries.query import MockModel
+
+# Imports from your apps
+from usaspending_api.common.helpers.unit_test_helper import add_to_mock_objects
 from usaspending_api.search.tests.test_mock_data_search import all_filters
 
 
@@ -10,14 +18,12 @@ from usaspending_api.search.tests.test_mock_data_search import all_filters
 def test_spending_by_award_subaward_success(client, refresh_matviews):
 
     resp = client.post(
-        '/api/v2/search/spending_by_award',
-        content_type='application/json',
-        data=json.dumps({
-            "subawards": True,
-            "fields": ["Sub-Award ID"],
-            "sort": "Sub-Award ID",
-            "filters": all_filters()
-        }))
+        "/api/v2/search/spending_by_award",
+        content_type="application/json",
+        data=json.dumps(
+            {"subawards": True, "fields": ["Sub-Award ID"], "sort": "Sub-Award ID", "filters": all_filters()}
+        ),
+    )
     assert resp.status_code == status.HTTP_200_OK
 
 
@@ -25,12 +31,48 @@ def test_spending_by_award_subaward_success(client, refresh_matviews):
 def test_spending_by_award_success(client, refresh_matviews):
 
     resp = client.post(
-        '/api/v2/search/spending_by_award',
-        content_type='application/json',
-        data=json.dumps({
-            "subawards": False,
-            "fields": ["Award ID"],
-            "sort": "Award ID",
-            "filters": all_filters()
-        }))
+        "/api/v2/search/spending_by_award",
+        content_type="application/json",
+        data=json.dumps({"subawards": False, "fields": ["Award ID"], "sort": "Award ID", "filters": all_filters()}),
+    )
     assert resp.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_no_intersection(client, mock_matviews_qs):
+    mock_model_1 = MockModel(
+        award_ts_vector="",
+        type="A",
+        type_of_contract_pricing="",
+        naics_code="98374",
+        cfda_number="987.0",
+        pulled_from=None,
+        uri=None,
+        piid="djsd",
+        fain=None,
+        award_id=90,
+        awarding_toptier_agency_name="Department of Pizza",
+        awarding_toptier_agency_abbreviation="DOP",
+        generated_pragmatic_obligation=10,
+    )
+
+    add_to_mock_objects(mock_matviews_qs, [mock_model_1])
+
+    request = {
+        "subawards": False,
+        "fields": ["Award ID"],
+        "sort": "Award ID",
+        "filters": {"award_type_codes": ["A", "B", "C", "D"]},
+    }
+
+    resp = client.post("/api/v2/search/spending_by_award", content_type="application/json", data=json.dumps(request))
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(resp.data["results"]) == 1
+
+    request["filters"]["award_type_codes"].append("no intersection")
+    api_start = perf_counter()
+    resp = client.post("/api/v2/search/spending_by_award", content_type="application/json", data=json.dumps(request))
+    api_end = perf_counter()
+    assert resp.status_code == status.HTTP_200_OK
+    assert api_end - api_start < 0.5, "Response took over 0.5s! Investigate why"
+    assert len(resp.data["results"]) == 0, "Results returned, there should be 0"
