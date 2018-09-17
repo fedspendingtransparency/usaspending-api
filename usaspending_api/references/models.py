48829c492e055c7944759c7d447c2c02112e70b2
Justin Le <justinle9@gmail.com>
2018-09-17 11:57:08 -0400

    added migration file

--- usaspending_api/references/models.py
+++ usaspending_api/references/models.py
@@ -770,7 +770,7 @@ class NAICS(models.Model):
     """Based on United States Census Bureau"""
     code = models.TextField(primary_key=True)
     description = models.TextField(null=False)
-    year = models.IntegerField()
+    year = models.IntegerField(default=0)
 
     class Meta:
         managed = True
