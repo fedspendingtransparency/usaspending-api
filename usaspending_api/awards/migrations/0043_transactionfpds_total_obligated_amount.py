921af33e2b1dd6996e2acc01a92ce8d41e393210
Daniel Boos <boos_daniel@bah.com>
2018-09-17 11:26:50 -0400

    Updating migration to avoid migration conflicts

--- usaspending_api/awards/migrations/0043_transactionfpds_total_obligated_amount.py
+++ usaspending_api/awards/migrations/0043_transactionfpds_total_obligated_amount.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('awards', '0042_updated_at_subawards'),
+        ('awards', '0043_summarytransactionfedacctview'),
     ]
 
     operations = [
