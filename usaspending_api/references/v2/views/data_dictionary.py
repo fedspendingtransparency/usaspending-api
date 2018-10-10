4bdb6df10102bec4ce54e5dd69d01a08e8da5f0b
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-10-10 12:14:51 -0400

    Removed API caching for data_dictionary

--- usaspending_api/references/v2/views/data_dictionary.py
+++ usaspending_api/references/v2/views/data_dictionary.py
@@ -2,7 +2,7 @@ import logging
 
 from rest_framework.response import Response
 
-from usaspending_api.common.cache_decorator import cache_response
+# from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.exceptions import NoDataFoundException
 from usaspending_api.common.views import APIDocumentationView
 from usaspending_api.references.models import Rosetta
@@ -16,7 +16,6 @@ class DataDictionaryViewSet(APIDocumentationView):
     endpoint_doc: /references/data_dictionary.md
     """
 
-    @cache_response()
     def get(self, request, format=None):
         try:
             api_response = Rosetta.objects.filter(document_name="api_response").values("document")[0]
