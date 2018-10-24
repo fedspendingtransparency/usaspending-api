09f59827332380edba5a4381a29bbcefc1df2cdd
Daniel Boos <boos_daniel@bah.com>
2018-10-24 10:07:28 -0400

    Revert "Fixing final_of_fy for financial_accounts_by_program_activity_object_class"
    This reverts commit 0d819e32e2cc6edd8f753d70b0846eed81b5cbff.

--- usaspending_api/financial_activities/models.py
+++ usaspending_api/financial_activities/models.py
@@ -77,11 +77,10 @@ class FinancialAccountsByProgramActivityObjectClass(DataSourceTrackedModel):
 
     FINAL_OF_FY_SQL = """
         UPDATE financial_accounts_by_program_activity_object_class
-        SET final_of_fy = (treasury_account_id, submission_id) in
+        SET final_of_fy = submission_id in
         ( SELECT DISTINCT ON
             (fabpaoc.treasury_account_id,
              FY(s.reporting_period_start))
-          fabpaoc.treasury_account_id,
           s.submission_id
           FROM submission_attributes s
           JOIN financial_accounts_by_program_activity_object_class fabpaoc
