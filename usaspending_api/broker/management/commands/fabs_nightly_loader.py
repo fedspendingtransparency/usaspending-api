fadb7d2b251db8d8e94cd2dea430696a73cbd718
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-10-25 15:50:58 -0400

    altering non_federal_funding_amount linkage for downloads

--- usaspending_api/broker/management/commands/fabs_nightly_loader.py
+++ usaspending_api/broker/management/commands/fabs_nightly_loader.py
@@ -245,7 +245,6 @@ class Command(BaseCommand):
                 "type_description": row['assistance_type_desc'],
                 "transaction_unique_id": row['afa_generated_unique'],
                 "generated_unique_award_id": generated_unique_id,
-                "non_federal_funding_amount": row["non_federal_funding_amount"],
             }
 
             fad_field_map = {
