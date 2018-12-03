e47087e09895996a315ee5d73b20c4130654a676
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2018-12-03 14:46:03 -0500

    pausing for a few minutes

--- usaspending_api/awards/v2/views/awards.py
+++ usaspending_api/awards/v2/views/awards.py
@@ -7,6 +7,7 @@ from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.views import APIDocumentationView
 from usaspending_api.core.validator.tinyshield import TinyShield
 from usaspending_api.awards.serializers_v2.serializers import AwardContractSerializerV2, AwardMiscSerializerV2
+from usaspending_api.common.exceptions import InvalidParameterException
 
 
 class AwardLastUpdatedViewSet(APIDocumentationView):
@@ -31,18 +32,25 @@ class AwardRetrieveViewSet(APIDocumentationView):
     endpoint_doc: /awards.md
     """
 
-    def _parse_and_validate_request(self, generated_unique_award_id) -> dict:
+    def _parse_and_validate_request(self, provided_award_id: str) -> dict:
+        models = [
+            {"key": "generated_unique_award_id", "name": "generated_unique_award_id", "type": "text", "text_type": "search", "optional": True},
+            {"key": "id", "name": "id", "type": "int", "optional": True},
+        ]
 
-        # TODO:  May be better to accept either generated_unique_award_id or
-        #       internal pk to ease transition from v1 to v2
+        try:
+            award_pk = int(provided_award_id)
+        except Exception:
+            award_pk = None
 
-        models = [{"key": "generated_unique_award_id", "name": "generated_unique_award_id", "type": "text",
-                   "text_type": "search", "optional": False}]
-        request_dict = {"generated_unique_award_id": generated_unique_award_id}
+        if award_pk:
+            request_dict = {"id": award_pk}
+        else:
+            request_dict = {"generated_unique_award_id": provided_award_id}
         validated_request_data = TinyShield(models).block(request_dict)
         return validated_request_data
 
-    def _business_logic(self, generated_unique_award_id: str) -> list:
+    def _business_logic(self, generated_unique_award_id: dict) -> list:
         try:
             award = Award.objects.get(generated_unique_award_id=generated_unique_award_id)
             if award.category == 'contract':
@@ -59,7 +67,9 @@ class AwardRetrieveViewSet(APIDocumentationView):
             return {"message": "No award found with this id"}
 
     @cache_response()
-    def get(self, request: Request, generated_unique_award_id: str) -> Response:
-        request_data = self._parse_and_validate_request(generated_unique_award_id)
-        response = self._business_logic(request_data['generated_unique_award_id'])
+    def get(self, request: Request, provided_award_id: str) -> Response:
+        if not provided_award_id:
+            raise InvalidParameterException("Missing Award ID")
+        request_data = self._parse_and_validate_request(provided_award_id)
+        response = self._business_logic(request_data)
         return Response(response)
