d37a187ffb25a5e27dc580770c54d053ba080e75
Kirk Barden <barden_kirk@bah.com>
2018-12-17 10:51:11 -0500

    fix migration hierarchy

--- usaspending_api/awards/migrations/0048_auto_20181126_1528.py
+++ usaspending_api/awards/migrations/0048_auto_20181126_1528.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('awards', '0046_merge_20181026_2142'),
+        ('awards', '0047_auto_20181207_0143'),
     ]
 
     operations = [
