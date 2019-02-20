1ed6db806586547d5c4503f932aaab5df35d9ce5
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-02-20 17:33:23 -0500

    Removed unnecessary null field from ordering

--- usaspending_api/recipient/management/sql/restock_recipient_lookup.sql
+++ usaspending_api/recipient/management/sql/restock_recipient_lookup.sql
@@ -60,7 +60,7 @@ VACUUM ANALYZE public.temporary_transaction_recipients_view;
 
 
 --------------------------------------------------------------------------------
--- Step 2a, Upsert rows with DUNS + Recipient Names from SAM
+-- Step 2a, Create rows with DUNS + Recipient Names from SAM
 --------------------------------------------------------------------------------
 DO $$ BEGIN RAISE NOTICE 'Step 2a: Adding Recipient records from SAM'; END $$;
 
@@ -219,7 +219,7 @@ INSERT INTO public.temporary_restock_recipient_lookup (
   congressional_district, business_types_codes
   )
   SELECT
-    DISTINCT ON (awardee_or_recipient_uniqu, legal_business_name)
+    DISTINCT ON (awardee_or_recipient_uniqu)
     MD5(UPPER(CONCAT(awardee_or_recipient_uniqu, legal_business_name)))::uuid AS recipient_hash,
     UPPER(legal_business_name) AS legal_business_name,
     awardee_or_recipient_uniqu AS duns,
@@ -236,7 +236,7 @@ INSERT INTO public.temporary_restock_recipient_lookup (
     business_types_codes
   FROM duns
   WHERE awardee_or_recipient_uniqu IS NOT NULL AND legal_business_name IS NULL
-  ORDER BY awardee_or_recipient_uniqu, legal_business_name, update_date DESC
+  ORDER BY awardee_or_recipient_uniqu, update_date DESC
 ON CONFLICT (duns) DO NOTHING;
 
 
