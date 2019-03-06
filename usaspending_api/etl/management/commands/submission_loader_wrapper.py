b42bcddbf79bed3db0a1d736545b845010a716ef
Isaac Ray <isaacmray1984@gmail.com>
2019-03-05 21:20:17 -0500

    submission loader wrapper complete

--- usaspending_api/etl/management/commands/submission_loader_wrapper.py
+++ usaspending_api/etl/management/commands/submission_loader_wrapper.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import argparse
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
@@ -15,9 +16,6 @@ exception_logger = logging.getLogger("exceptions")
 class Command(BaseCommand):
 
     # Give it a fiscal year and a quarter. Will list missing subs/agencies and their recent certified dates.
-    def add_arguments(self, parser):
-        parser.add_argument('all_time', nargs=1, help='all quarters and fiscal years.', type=strtobool)
-        parser.add_argument('ids' nargs=1, help='comma-separated list of ids to load', typ=str)
 
     def strtobool(arg):
         try:
@@ -25,27 +23,32 @@ class Command(BaseCommand):
         except InvalidParameterException:
             raise argparse.ArgumentTypeError('Boolean value expected.')
 
+    def add_arguments(self, parser):
+        parser.add_argument('--all_time', action="store_true", help='all quarters and fiscal years.')
+        parser.add_argument('--ids', nargs=1, help='comma-separated list of ids to load', type=str)
+
     def handle(self, *args, **options):
 
-        if options['all_time'][0]:
+        if options['all_time']:
             now = datetime.datetime.now()
-            start = datetime.datetime(1,1,2017,0,0,0)
+            start = datetime.datetime(2017,1,1,0,0,0)
             time_periods = create_full_time_periods(start,now,"q",{})
             all_periods = [(period['time_period']['fy'], period['time_period']['q']) for period in time_periods]
             for idx, val in enumerate(all_periods):
                 fy = val[0]
                 q = val[1]
                 try:
-                    logger.info('Running submission load for fy {}, quarter {}...'.format)(fy, q)
-                    call_command('load_multiple_submissions',"{} {}".format(fy, q))
-                except CommandError:
+                    logger.info('Running submission load for fy {}, quarter {}...'.format(fy, q))
+                    call_command('load_multiple_submissions', int(fy), int(q))
+                except CommandError as e:
                     logger.info('Error with fy/q combination')
+                    logger.info(str(e))
                     continue
-        else if options['ids'][0]:
+        elif options['ids'][0]:
             ids = options['ids'][0].split(",")
             logger.info("Running submission load for submitted ids...")
             for idx in ids:
-                 try:
+                try:
                     call_command('load_submission', '--noclean', '--nosubawards', idx)
                 except CommandError:
                     logger.info('Skipping submission ID {} due to CommandError (bad ID)'.format(idx))
