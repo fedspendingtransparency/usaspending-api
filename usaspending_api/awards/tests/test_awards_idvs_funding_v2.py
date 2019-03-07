163ee6947df7c9603d4036ad55fe27401c1046a8
Kirk Barden <barden_kirk@bah.com>
2019-03-07 11:32:08 -0500

    update tests

--- usaspending_api/awards/tests/test_awards_idvs_funding_v2.py
+++ usaspending_api/awards/tests/test_awards_idvs_funding_v2.py
@@ -39,6 +39,11 @@ class IDVFundingTestCase(TestCase):
             _sid = str(_id).zfill(3)
 
             mommy.make(
+                'references.Agency',
+                id=9000 + _id
+            )
+
+            mommy.make(
                 'awards.Award',
                 id=_id,
                 generated_unique_award_id='GENERATED_UNIQUE_AWARD_ID_%s' % _sid,
@@ -46,7 +51,8 @@ class IDVFundingTestCase(TestCase):
                 piid='piid_%s' % _sid,
                 fpds_agency_id='fpds_agency_id_%s' % _sid,
                 parent_award_piid='piid_%s' % _spid if _spid else None,
-                fpds_parent_agency_id='fpds_agency_id_%s' % _spid if _spid else None
+                fpds_parent_agency_id='fpds_agency_id_%s' % _spid if _spid else None,
+                funding_agency_id=9000 + _id
             )
 
             if _id in IDVS:
@@ -120,6 +126,7 @@ class IDVFundingTestCase(TestCase):
                 'reporting_fiscal_year': 2000 + _id,
                 'reporting_fiscal_quarter': _id % 4 + 1,
                 'piid': 'piid_%s' % _sid,
+                'funding_agency_id': 9000 + _id,
                 'reporting_agency_id': _sid.zfill(3),
                 'reporting_agency_name': 'reporting agency name %s' % _sid,
                 'agency_id': _sid.zfill(3),
