012ba74a9983a083fe07be5cdd41f8e3887e60d1
Isaac Ray <isaacmray1984@gmail.com>
2019-03-15 14:33:21 -0400

    functional rollup endpoint

--- usaspending_api/awards/v2/urls_idv_awards.py
+++ usaspending_api/awards/v2/urls_idv_awards.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from usaspending_api.awards.v2.views.idvs.amounts import IDVAmountsViewSet
 from usaspending_api.awards.v2.views.idvs.awards import IDVAwardsViewSet
 from usaspending_api.awards.v2.views.idvs.funding import IDVFundingViewSet
+from usaspending_api.awards.v2.views.idvs.funding_rollup import IDVFundingRollupViewSet
 
 urlpatterns = [
     url(r'^amounts/(?P<requested_award>[A-Za-z0-9_. -]+)/$', IDVAmountsViewSet.as_view()),
     url(r'^awards/$', IDVAwardsViewSet.as_view()),
     url(r'^funding/$', IDVFundingViewSet.as_view()),
+    url(r'^funding-rollup/$', IDVFundingRollupViewSet.as_view()),
 ]
---
+++ usaspending_api/awards/v2/views/idvs/funding_rollup.py
@@ -0,0 +1,78 @@
+from collections import OrderedDict
+from copy import deepcopy
+
+from psycopg2.sql import Identifier, Literal, SQL
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from usaspending_api.common.cache_decorator import cache_response
+from usaspending_api.common.helpers.sql_helpers import execute_sql_to_ordered_dictionary
+from usaspending_api.common.views import APIDocumentationView
+from usaspending_api.common.validator.award import get_internal_or_generated_award_id_model
+from usaspending_api.common.validator.tinyshield import TinyShield
+
+
+
+
+FUNDING_ROLLUP_SQL = SQL("""
+    with cte as (
+        select    award_id
+        from      parent_award
+        where     {award_id_column} = {award_id}
+        union all
+        select    cpa.award_id
+        from      parent_award ppa
+                  inner join parent_award cpa on cpa.parent_award_id = ppa.award_id
+        where     ppa.{award_id_column} = {award_id}
+    )
+    select
+        sum(nullif(faba.transaction_obligated_amount, 'NaN')) total_transaction_obligated_amount,
+        count(distinct ca.awarding_agency_id) awarding_agency_count,
+        count(distinct taa.agency_id || '-' || taa.main_account_code) federal_account_count
+    from
+        cte
+        inner join awards pa on
+            pa.id = cte.award_id
+        inner join awards ca on
+            ca.parent_award_piid = pa.piid and
+            ca.fpds_parent_agency_id = pa.fpds_agency_id and
+            ca.type not like 'IDV\_%'
+        inner join financial_accounts_by_awards faba on
+            faba.award_id = ca.id
+        left outer join treasury_appropriation_account taa on
+            taa.treasury_account_identifier = faba.treasury_account_id;""")
+
+
+
+
+class IDVFundingRollupViewSet(APIDocumentationView):
+    """Returns File C funding totals associated with an IDV's children."""
+
+    @staticmethod
+    def _parse_and_validate_request(request: Request) -> dict:
+        return TinyShield(deepcopy([get_internal_or_generated_award_id_model()])).block(request)
+
+    @staticmethod
+    def _business_logic(request_data: dict) -> list:
+        # By this point, our award_id has been validated and cleaned up by
+        # TinyShield.  We will either have an internal award id that is an
+        # integer or a generated award id that is a string.
+        award_id = request_data['award_id']
+        award_id_column = 'award_id' if type(award_id) is int else 'generated_unique_award_id'
+
+        sql = FUNDING_ROLLUP_SQL.format(
+            award_id_column=Identifier(award_id_column),
+            award_id=Literal(award_id),
+        )
+        return execute_sql_to_ordered_dictionary(sql)
+
+    @cache_response()
+    def post(self, request: Request) -> Response:
+        request_data = self._parse_and_validate_request(request.data)
+        results = self._business_logic(request_data)
+
+        response = OrderedDict((
+            ('results', results),
+        ))
+
+        return Response(response)
