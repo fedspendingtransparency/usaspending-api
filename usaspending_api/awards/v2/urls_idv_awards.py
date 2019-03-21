525222bcf3d1b21093f26c4c89820ae678651454
Isaac Ray <isaacmray1984@gmail.com>
2019-03-21 16:21:34 -0400

    fixing url

--- usaspending_api/awards/v2/urls_idv_awards.py
+++ usaspending_api/awards/v2/urls_idv_awards.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^amounts/(?P<requested_award>[A-Za-z0-9_. -]+)/$', IDVAmountsViewSet.as_view()),
     url(r'^awards/$', IDVAwardsViewSet.as_view()),
     url(r'^funding/$', IDVFundingViewSet.as_view()),
-    url(r'^funding-rollup/$', IDVFundingRollupViewSet.as_view()),
+    url(r'^funding_rollup/$', IDVFundingRollupViewSet.as_view()),
     # This is a stub for the tree map endpoint fo DEV-2237
     # url(r'^accounts/$', IDVFundingTreemapViewSet.as_view()),
 ]
