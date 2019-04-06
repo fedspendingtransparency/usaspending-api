9d8c54891516db3e205bafbd21fa3c020b50bfc4
Isaac Ray <isaacmray1984@gmail.com>
2019-04-05 22:30:59 -0400

    functional glossary list endpoint

--- usaspending_api/references/v2/urls.py
+++ usaspending_api/references/v2/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from usaspending_api.references.v1 import views
-from usaspending_api.references.v2.views import agency, toptier_agencies, data_dictionary
+from usaspending_api.references.v2.views import agency, toptier_agencies, data_dictionary, glossary
 
+
+# This does not appear to be used and looks like its an artifact from a copy-pasta of v1 urls. Can probably delete?
 glossary_router = DefaultRouter()
 glossary_router.register('glossary', views.GlossaryViewSet)
 
@@ -15,4 +17,5 @@ urlpatterns = [
     url(r'^agency/(?P<pk>[0-9]+)/$', agency.AgencyViewSet.as_view()),
     url(r'^toptier_agencies/$', toptier_agencies.ToptierAgenciesViewSet.as_view()),
     url(r'^data_dictionary/$', data_dictionary.DataDictionaryViewSet.as_view()),
+    url(r'^glossary/$', glossary.GlossaryViewSet.as_view()),
 ]
--- usaspending_api/references/v2/views/glossary.py
+++ usaspending_api/references/v2/views/glossary.py
@@ -1,5 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-
+from usaspending_api.common.helpers.generic_helper import get_pagination
+from usaspending_api.common.cache_decorator import cache_response
+from usaspending_api.common.views import APIDocumentationView
+from usaspending_api.common.serializers import LimitableSerializer
+from usaspending_api.references.models import Definition
 
 
 class DefinitionSerializer(LimitableSerializer):
@@ -8,3 +14,38 @@ class DefinitionSerializer(LimitableSerializer):
 
         model = Definition
         fields = ['term', 'slug', 'data_act_term', 'plain', 'official', 'resources']
+
+
+class GlossaryViewSet(APIDocumentationView):
+    """
+    This view returns paginated glossary terms in a list
+    """
+
+    @cache_response()
+    def get(self, request: Request) -> Response:
+        """
+        Accepts only pagination-related query parameters
+        """
+
+        get_request = request.query_params
+        limit = get_request.get('limit', 500)
+        page = get_request.get('page', 1)
+
+        queryset = Definition.objects.all()
+        queryset, pagination = get_pagination(queryset, int(limit), int(page))
+
+        serializer = DefinitionSerializer(queryset, many=True)
+        response = {
+            "page_metadata":pagination,
+            "results":serializer.data
+        }
+
+        return Response(response)
+
+
+
+
+
+
+
+
