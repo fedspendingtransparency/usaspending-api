ca32b8dad8a04c706796b7a21e0a09c421b53143
Isaac Ray <isaacmray1984@gmail.com>
2019-04-17 16:45:32 -0400

    finally got ordering to work

--- usaspending_api/awards/v2/urls_idv_awards.py
+++ usaspending_api/awards/v2/urls_idv_awards.py
@@ -2,13 +2,12 @@ from django.conf.urls import url
 from usaspending_api.awards.v2.views.idvs.amounts import IDVAmountsViewSet
 from usaspending_api.awards.v2.views.idvs.awards import IDVAwardsViewSet
 from usaspending_api.awards.v2.views.idvs.funding import IDVFundingViewSet
-from usaspending_api.awards.v2.views.idvs.funding_treemap import IDVFundingRollupViewSet  # , IDVFundingTreemapViewSet
+from usaspending_api.awards.v2.views.idvs.funding_treemap import IDVFundingRollupViewSet, IDVFundingTreemapViewSet
 
 urlpatterns = [
     url(r'^amounts/(?P<requested_award>[A-Za-z0-9_. -]+)/$', IDVAmountsViewSet.as_view()),
     url(r'^awards/$', IDVAwardsViewSet.as_view()),
     url(r'^funding/$', IDVFundingViewSet.as_view()),
     url(r'^funding_rollup/$', IDVFundingRollupViewSet.as_view()),
-    # This is a stub for the tree map endpoint fo DEV-2237
-    # url(r'^accounts/$', IDVFundingTreemapViewSet.as_view()),
+    url(r'^accounts/$', IDVFundingTreemapViewSet.as_view()),
 ]
--- usaspending_api/awards/v2/views/idvs/funding_treemap.py
+++ usaspending_api/awards/v2/views/idvs/funding_treemap.py
@@ -1,16 +1,28 @@
 from collections import OrderedDict
 
-from psycopg2.sql import Identifier, Literal, SQL
+from psycopg2.sql import Identifier, Literal, SQL, Composed
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from usaspending_api.common.cache_decorator import cache_response
-from usaspending_api.common.helpers.sql_helpers import execute_sql_to_ordered_dictionary
+from usaspending_api.common.helpers.sql_helpers import execute_sql_to_ordered_dictionary, build_composable_order_by
 from usaspending_api.common.views import APIDocumentationView
 from usaspending_api.common.validator.award import get_internal_or_generated_award_id_model
 from usaspending_api.common.helpers.generic_helper import get_simple_pagination_metadata
+from usaspending_api.common.validator.pagination import customize_pagination_with_sort_columns
 from usaspending_api.common.validator.tinyshield import validate_post_request
 
+
+SORTABLE_COLUMNS = {
+    'federal_account':'taa.main_account_code',
+    'total_transaction_obligated_amount':'total_transaction_obligated_amount',
+    'agency': 'taa.agency_id'
+}
+
+'''for k, v in SORTABLE_COLUMNS.items():
+    v.append('faba.financial_accounts_by_awards_id')'''
+
+DEFAULT_SORT_COLUMN = 'federal_account'
 
 FUNDING_TREEMAP_SQL = SQL("""
     with cte as (
@@ -36,7 +48,11 @@ FUNDING_TREEMAP_SQL = SQL("""
         inner join financial_accounts_by_awards faba on
             faba.award_id = ca.id
         left outer join treasury_appropriation_account taa on
-            taa.treasury_account_identifier = faba.treasury_account_id {group_by};""")
+            taa.treasury_account_identifier = faba.treasury_account_id 
+        {group_by} 
+        {order_by} 
+        {limit}
+         {offset};""")
 
 
 class IDVFundingBaseViewSet(APIDocumentationView):
@@ -47,7 +63,7 @@ class IDVFundingBaseViewSet(APIDocumentationView):
     """
 
     @staticmethod
-    def _business_logic(request_data: dict, columns: str, group_by: str) -> list:
+    def _business_logic(request_data: dict, columns: object, group_by: object, limit: object, order_by: object, offset: object) -> list:
         # By this point, our award_id has been validated and cleaned up by
         # TinyShield.  We will either have an internal award id that is an
         # integer or a generated award id that is a string.
@@ -55,15 +71,18 @@ class IDVFundingBaseViewSet(APIDocumentationView):
         award_id_column = 'award_id' if type(award_id) is int else 'generated_unique_award_id'
 
         sql = FUNDING_TREEMAP_SQL.format(
-            columns=SQL(columns),
+            columns=columns,
             award_id_column=Identifier(award_id_column),
             award_id=Literal(award_id),
-            group_by=SQL(group_by),
+            group_by=group_by,
+            order_by=order_by,
+            limit=limit,
+            offset=offset,
         )
         return execute_sql_to_ordered_dictionary(sql)
 
 
-@validate_post_request([get_internal_or_generated_award_id_model()])
+@validate_post_request([get_internal_or_generated_award_id_model(),])
 class IDVFundingRollupViewSet(IDVFundingBaseViewSet):
 
     """
@@ -72,31 +91,62 @@ class IDVFundingRollupViewSet(IDVFundingBaseViewSet):
 
     @cache_response()
     def post(self, request: Request) -> Response:
-        columns = """coalesce(sum(nullif(faba.transaction_obligated_amount, 'NaN')), 0.0) total_transaction_obligated_amount,
+        limit = SQL("")
+        order_by = SQL("")
+        offset = SQL("")
+        columns = SQL("""coalesce(sum(nullif(faba.transaction_obligated_amount, 'NaN')), 0.0) total_transaction_obligated_amount,
                      count(distinct ca.awarding_agency_id) awarding_agency_count,
-                     count(distinct taa.agency_id || '-' || taa.main_account_code) federal_account_count"""
-        group_by = ""
-        results = self._business_logic(request.data, columns, group_by)
+                     count(distinct taa.agency_id || '-' || taa.main_account_code) federal_account_count""")
+        group_by = SQL("")
+        results = self._business_logic(request.data, columns, group_by, limit, order_by, offset)
         return Response(results[0])
 
 
-# THIS IS A STUB FOR DEV-2237
+TREEMAP_MODELS = customize_pagination_with_sort_columns(list(SORTABLE_COLUMNS.keys()), DEFAULT_SORT_COLUMN)
+TREEMAP_MODELS.append(get_internal_or_generated_award_id_model())
 
+@validate_post_request(TREEMAP_MODELS)
 class IDVFundingTreemapViewSet(IDVFundingBaseViewSet):
 
     @cache_response()
     def post(self, request: Request) -> Response:
-
-        group_by = "group by ca.awarding_agency_id, taa.agency_id || '-' || taa.main_account_code"
-        columns = """sum(nullif(faba.transaction_obligated_amount, 'NaN')) total_transaction_obligated_amount,
-                      taa.agency_id || '-' || taa.main_account_code federal_account,
-                      ca.awarding_agency_id agency_id"""
-        request_data = self._parse_and_validate_request(request.data)
-        results = self._business_logic(request_data, columns, group_by)
-        page_metadata = get_simple_pagination_metadata(len(results), request_data['limit'], request_data['page'])
+        limit = SQL("limit {}". format(request.data['limit'] + 1))
+        offset = SQL("offset {}".format((request.data['page'] - 1) * request.data['limit']))
+        order_by = SQL("order by {} {}".format(SORTABLE_COLUMNS[request.data['sort']], request.data['order']))
+        group_by = SQL("group by taa.main_account_code")
+        columns = SQL("""sum(nullif(faba.transaction_obligated_amount, 'NaN')) total_transaction_obligated_amount,
+                      taa.main_account_code federal_account""")
+        
+        results = self._business_logic(request.data, columns, group_by, limit, order_by, offset)
+        page_metadata = get_simple_pagination_metadata(len(results), request.data['limit'], request.data['page'])
 
         response = OrderedDict((
-            ('results', results[:request_data['limit']]),
+            ('results', results[:request.data['limit']]),
+            ('page_metadata', page_metadata)
+        ))
+
+        return Response(response)
+
+
+# STUB FOR DEV-2238
+
+#@validate_post_request(TREEMAP_MODELS.extend([{'key': 'account', 'name': 'account', 'optional': False, 'type': 'text', 'text_type': 'search'}]))
+class IDVFundingTreemapDrillDownViewSet(IDVFundingBaseViewSet):
+
+    @cache_response()
+    def post(self, request: Request) -> Response:
+        limit = SQL("limit {}".format(request.data['limit'] + 1))
+        offset = SQL("offset {}".format((request.data['page'] - 1) * request.data['limit']))
+        group_by = SQL("group by ca.awarding_agency_id, taa.agency_id || '-' || taa.main_account_code")
+        order_by = SQL("order by {} {}".format(SORTABLE_COLUMNS[request.data['sort']], request.data['order']))
+        columns = SQL("""sum(nullif(faba.transaction_obligated_amount, 'NaN')) total_transaction_obligated_amount,
+                      taa.agency_id || '-' || taa.main_account_code federal_account,""")
+        
+        results = self._business_logic(request.data, columns, group_by, limit, order_by, offset)
+        page_metadata = get_simple_pagination_metadata(len(results), request.data['limit'], request.data['page'])
+
+        response = OrderedDict((
+            ('results', results[:request.data['limit']]),
             ('page_metadata', page_metadata)
         ))
 
