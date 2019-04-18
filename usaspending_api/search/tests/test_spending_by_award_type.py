66099b142d5100799fc90706c1bd2cfec953429c
Emily Brents <brents_emily@bah.com>
2019-04-18 08:13:53 -0400

    Update test_spending_by_award_type.py

--- usaspending_api/search/tests/test_spending_by_award_type.py
+++ usaspending_api/search/tests/test_spending_by_award_type.py
@@ -305,4 +305,76 @@ def test_spending_by_award_foreign_filter(client, mock_matviews_qs):
         }))
     # One result is returned when searching for "Foreign" recipients
     assert len(resp.data['results']) == 1
+
+    
+@pytest.mark.django_db
 def test_spending_by_award_idv_subawards_success(client, refresh_matviews): 
+    # test idv subawards
+    resp = client.post(
+        '/api/v2/search/spending_by_award',
+        content_type='application/json',
+        data=json.dumps({
+            "fields": ["Sub-Award ID"],
+            "filters": {
+                "award_type_codes": ["IDV_A", "IDV_B", "IDV_B_A", "IDV_B_B", "IDV_B_C", "IDV_C", "IDV_D", "IDV_E"]
+            },
+            "subawards": True
+        }))
+    assert resp.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+def test_spending_by_award_subawards_fail(client, refresh_matviews): 
+    # test idv subawards error message
+    resp = client.post(
+        '/api/v2/search/spending_by_award',
+        content_type='application/json',
+        data=json.dumps({
+            "fields": ["Sub-Award ID"],
+            "filters": {
+                "award_type_codes": ["06"]
+            },
+            "subawards": True
+        }))
+    assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+@pytest.mark.django_db
+def test_spending_by_award_subawards(client, mock_matviews_qs): 
+
+    mock_model_0 = MockModel(fain="", prime_award_type="IDV_B", award_ts_vector="", award_id=121658, subaward_number="EP-W-13-028-3", award_type="procurement", recipient_name="WESTON SOLUTIONS HOLDINGS, INC.", action_date="2013-10-01", amount=125000, awarding_toptier_agency_name="Environmental Protection Agency", awarding_subtier_agency_name="Environmental Protection Agency", piid="EPW13028", prime_recipient_name="TECHLAW, INC.")
+    mock_model_1 = MockModel(fain="", prime_award_type="IDV_A", award_ts_vector="", award_id=121659, subaward_number="EP-W-13-028-1", award_type="procurement", recipient_name="ENVIRONMENTAL COMPLIANCE CONSULTANTS, INC", action_date="2013-09-01", amount=102432, awarding_toptier_agency_name="Environmental Protection Agency", awarding_subtier_agency_name="Environmental Protection Agency", piid="EPW13028", prime_recipient_name="TECHLAW, INC.")
+    add_to_mock_objects(mock_matviews_qs, [mock_model_0, mock_model_1])
+    resp = client.post(
+        '/api/v2/search/spending_by_award',
+        content_type='application/json',
+        data=json.dumps({
+            "filters": {
+                "award_type_codes": ["IDV_A", "IDV_B", "IDV_B_A", "IDV_B_B", "IDV_B_C", "IDV_C", "IDV_D", "IDV_E"]},
+	        "fields":["Sub-Award ID"],
+	        "subawards": True
+        }))
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(resp.data['results']) == 2
+
+    resp = client.post(
+        '/api/v2/search/spending_by_award',
+        content_type='application/json',
+        data=json.dumps({
+            "filters": {
+                "award_type_codes": ["IDV_A"]},
+	        "fields":["Sub-Award ID"],
+	        "subawards": True
+        }))
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(resp.data['results']) == 1
+
+    resp = client.post(
+        '/api/v2/search/spending_by_award',
+        content_type='application/json',
+        data=json.dumps({
+            "filters": {
+                "award_type_codes": ["IDV_B"]},
+	        "fields":["Sub-Award ID"],
+	        "subawards": True
+        }))
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(resp.data['results']) == 1
