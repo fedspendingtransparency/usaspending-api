28591572e87307b20c003d23f375768f9128c55e
Brian Zito <zito_brian@bah.com>
2019-04-30 13:40:52 -0400

    added date based test. Doesn't currently work

--- usaspending_api/etl/tests/unit/test_store_value.py
+++ usaspending_api/etl/tests/unit/test_store_value.py
@@ -5,3 +5,10 @@ def test_store_basic_values():
     store_value(testDict,'x',2)
     assert testDict['x'] == 2
     assert testDict['y'] == 6
+
+def test_store_valid_dates():
+    testDict = {'test_date': 3, 'y': 6}
+    goodDate = '2005-08-31 00:00:00'
+    store_value(testDict, 'test_date', goodDate)
+    assert testDict['x'] == '2005-08-31'
+    assert testDict['y'] == 6
