98837c2b49f3289681a76b843a52aa81eede61b6
Brian Zito <zito_brian@bah.com>
2019-05-01 07:27:05 -0400

    linter appeasement

--- usaspending_api/etl/tests/unit/test_store_value.py
+++ usaspending_api/etl/tests/unit/test_store_value.py
@@ -1,17 +1,20 @@
 from usaspending_api.etl.management.load_base import store_value
 import re
 
+
 def test_store_basic_values():
-    test_dict = {'x':3,'y':6}
-    store_value(test_dict,'x',2)
+    test_dict = {'x': 3, 'y': 6}
+    store_value(test_dict, 'x', 2)
     assert test_dict['x'] == 2
     assert test_dict['y'] == 6
+
 
 def test_store_none():
     test_dict = {'x': 3, 'y': 6}
     store_value(test_dict, 'x', None)
     assert test_dict['x'] is None
     assert test_dict['y'] == 6
+
 
 def test_store_valid_dates():
     test_dict = {'test_date': 3, 'y': 6}
@@ -20,6 +23,7 @@ def test_store_valid_dates():
     assert str(test_dict['test_date']) == '2005-08-31'
     assert test_dict['y'] == 6
 
+
 def test_store_unlabeled_dates():
     test_dict = {'test_date_not': 3, 'y': 6}
     good_date = '2005-08-31 00:00:00'
@@ -27,9 +31,10 @@ def test_store_unlabeled_dates():
     assert test_dict['test_date_not'] == '2005-08-31 00:00:00'
     assert test_dict['y'] == 6
 
+
 def test_store_bad_dates():
     test_dict = {'test_bad_date': 3, 'y': 6}
     bad_date = [4,7]
     store_value(test_dict, 'test_date_not', bad_date)
-    assert test_dict['test_date_not'] == [4,7]
+    assert test_dict['test_date_not'] == [4, 7]
     assert test_dict['y'] == 6
