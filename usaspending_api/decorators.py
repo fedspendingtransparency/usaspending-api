6fa09bccb91dc5b1564ab6dcd7d6fd6f1da932bd
Emily Brents <brents_emily@bah.com>
2019-05-03 11:13:47 -0400

    Update decorators.py

--- usaspending_api/decorators.py
+++ usaspending_api/decorators.py
@@ -1,7 +1,14 @@
+import logging
+
+
+logger = logging.getLogger('console')
+
+
 def deprecated(function):
     """Add deprecation warning to endpoint"""
     def wrap(request, *args, **kwargs):
         response = function(request, *args, **kwargs)
+        logger.warning('Endpoint "{}" is deprecated. Please move to v2 endpoint.'.format(request.path))
         response["X-API-Warn"] = "Deprecated API"
         return response
     return wrap
