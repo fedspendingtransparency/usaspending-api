26f27f336538abccda4a000e3210d7c3ce3a8d63
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-05-14 15:39:07 -0400

    blacked the file

--- usaspending_api/references/v2/views/city.py
+++ usaspending_api/references/v2/views/city.py
@@ -18,46 +18,38 @@ INDEX = "{}*".format(settings.TRANSACTIONS_INDEX_ROOT)
 
 models = [
     {
-        'name': 'filter|country_code',
-        'key': 'filter|country_code',
-        'type': 'text',
-        'text_type': 'search',
-        'optional': False,
-    }, {
-        'key': 'filter|state_code',
-        'name': 'fitler|state_code',
-        'type': 'text',
-        'text_type': 'search',
-        'optional': True,
-        'default': None,
-        'allow_nulls': True,
-    }, {
-        'key': 'filter|scope',
-        'name': 'filter|scope',
-        'type': 'enum',
-        'enum_values': ("recipient_location", "primary_place_of_performance"),
-        'optional': False,
-    }, {
-        "key": "search_text",
-        "name": "search_text",
+        "name": "filter|country_code",
+        "key": "filter|country_code",
         "type": "text",
         "text_type": "search",
         "optional": False,
-    }, {
+    },
+    {
+        "key": "filter|state_code",
+        "name": "fitler|state_code",
+        "type": "text",
+        "text_type": "search",
+        "optional": True,
+        "default": None,
+        "allow_nulls": True,
+    },
+    {
+        "key": "filter|scope",
+        "name": "filter|scope",
+        "type": "enum",
+        "enum_values": ("recipient_location", "primary_place_of_performance"),
+        "optional": False,
+    },
+    {"key": "search_text", "name": "search_text", "type": "text", "text_type": "search", "optional": False},
+    {
         "key": "method",
         "name": "method",
         "type": "enum",
-        'enum_values': ('wildcard', 'fuzzy'),
+        "enum_values": ("wildcard", "fuzzy"),
         "optional": True,
         "default": "wildcard",
-    }, {
-        "key": "limit",
-        "name": "limit",
-        "type": "integer",
-        "max": 500,
-        "optional": True,
-        "default": 10,
-    }
+    },
+    {"key": "limit", "name": "limit", "type": "integer", "max": 500, "optional": True, "default": 10},
 ]
 
 
@@ -71,11 +63,11 @@ class CityAutocompleteViewSet(APIDocumentationView):
     def post(self, request, format=None):
 
         search_text = preprocess(request.data["search_text"])
-        country = request.data['filter']['country_code']
-        state = request.data['filter']['state_code']
-        scope = 'recipient_location' if request.data['filter']['scope'] == 'recipient_location' else 'pop'
-        method = request.data['method']
-        limit = request.data['limit']
+        country = request.data["filter"]["country_code"]
+        state = request.data["filter"]["state_code"]
+        scope = "recipient_location" if request.data["filter"]["scope"] == "recipient_location" else "pop"
+        method = request.data["method"]
+        limit = request.data["limit"]
         return_fields = ["{}_city_name".format(scope), "{}_state_code".format(scope)]
         if state:
             start_string = "({scope}_country_code:USA) AND ({scope}_state_code:{state}) AND"
@@ -87,33 +79,19 @@ class CityAutocompleteViewSet(APIDocumentationView):
 
         method_char = "*" if method == "wildcard" else "~"
 
-        query_string += "({scope}_city_name:{city_partial}{method_char})".format(scope=scope,
-                                                                                 city_partial=search_text,
-                                                                                 method_char=method_char)
+        query_string += "({scope}_city_name:{city_partial}{method_char})".format(
+            scope=scope, city_partial=search_text, method_char=method_char
+        )
         query = {
             "_source": return_fields,
             "size": 0,
-            "query": {
-                "query_string": {
-                    "query": query_string
-                }
-            },
+            "query": {"query_string": {"query": query_string}},
             "aggs": {
                 "cities": {
-                    "terms": {
-                        "field": "{}.keyword".format(return_fields[0]),
-                        "size": 50000
-                    },
-                    "aggs": {
-                        "states": {
-                            "terms": {
-                                "field": return_fields[1],
-                                "size": 50000
-                            }
-                        }
-                    }
+                    "terms": {"field": "{}.keyword".format(return_fields[0]), "size": 50000},
+                    "aggs": {"states": {"terms": {"field": return_fields[1], "size": 50000}}},
                 }
-            }
+            },
         }
 
         if method == "fuzzy":
@@ -128,10 +106,5 @@ class CityAutocompleteViewSet(APIDocumentationView):
 
         sorted_results = sorted(results, key=lambda x: (x["city_name"], x["state_code"]))
 
-        response = OrderedDict(
-            [
-                ("count", len(sorted_results)),
-                ("results", sorted_results[:limit]),
-            ]
-        )
+        response = OrderedDict([("count", len(sorted_results)), ("results", sorted_results[:limit])])
         return Response(response)
