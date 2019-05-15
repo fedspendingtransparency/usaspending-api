bb123490b07d82b77822718abfc41b204a1dd21c
Emily Brents <brents_emily@bah.com>
2019-05-15 15:16:34 -0400

    Update test_es_sanitize.py

--- usaspending_api/search/tests/unit/test_es_sanitize.py
+++ usaspending_api/search/tests/unit/test_es_sanitize.py
@@ -1,9 +1,5 @@
-# Stdlib imports
-from datetime import datetime
-import json
-import pytest
-# Imports from your apps
 from usaspending_api.search.v2.elasticsearch_helper import es_sanitize
+
 
 def test_sanitizer():
     test_string = '+-&|!()[]{}^~*?:"/<>\\'
