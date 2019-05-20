e022115062a3a5b72d0b0f183d5521a3d7f30b7d
Kirk Barden <barden_kirk@bah.com>
2019-05-20 08:56:25 -0400

    another test

--- usaspending_api/search/tests/test_spending_by_geography_city.py
+++ usaspending_api/search/tests/test_spending_by_geography_city.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 
+from django.db import connection
 from model_mommy import mommy
 from rest_framework import status
 
@@ -31,11 +32,26 @@ def award_data_fixture(db):
         "awards.Award", id=1, is_fpds=True, latest_transaction_id=1, piid="piiiiid", recipient_id=1, type="A",
         place_of_performance_id=1
     )
+    mommy.make(
+        "awards.Subaward",
+        id=1,
+        award_id=1,
+        amount=123.45,
+        pop_country_code="USA",
+        pop_state_code="TX",
+        recipient_location_country_code="USA",
+        recipient_location_state_code="CA"
+    )
 
 
-def test_geocode_filter_locations(client, award_data_fixture, elasticsearch_transaction_index):
+def test_geocode_filter_by_city(client, award_data_fixture, elasticsearch_transaction_index):
     elasticsearch_transaction_index.update_index()
 
+    # We need to refresh this materialized view in order for mocked data to show up.
+    with connection.cursor() as cursor:
+        cursor.execute("refresh materialized view subaward_view;")
+
+    # Place of performance that does exist.
     resp = client.post(
         '/api/v2/search/spending_by_geography',
         content_type='application/json',
@@ -49,6 +65,7 @@ def test_geocode_filter_locations(client, award_data_fixture, elasticsearch_tran
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.data["results"]) == 1
 
+    # Place of performance that doesn't exist.
     resp = client.post(
         '/api/v2/search/spending_by_geography',
         content_type='application/json',
@@ -62,6 +79,7 @@ def test_geocode_filter_locations(client, award_data_fixture, elasticsearch_tran
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.data["results"]) == 0
 
+    # Recipient location that does exist.
     resp = client.post(
         '/api/v2/search/spending_by_geography',
         content_type='application/json',
@@ -75,6 +93,7 @@ def test_geocode_filter_locations(client, award_data_fixture, elasticsearch_tran
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.data["results"]) == 1
 
+    # Recipient location that doesn't exist.
     resp = client.post(
         '/api/v2/search/spending_by_geography',
         content_type='application/json',
@@ -87,3 +106,19 @@ def test_geocode_filter_locations(client, award_data_fixture, elasticsearch_tran
         }))
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.data["results"]) == 0
+
+    # Filter subawards.
+    resp = client.post(
+        '/api/v2/search/spending_by_geography',
+        content_type='application/json',
+        data=json.dumps({
+            "scope": "place_of_performance",
+            "geo_layer": "state",
+            "subawards": True,
+            "filters": {
+                'place_of_performance_locations': [{'country': 'USA', 'state': 'TX', 'city': 'Houston'}]
+            }
+        }))
+    assert resp.status_code == status.HTTP_200_OK
+    print(resp.data)
+    assert len(resp.data["results"]) == 1
