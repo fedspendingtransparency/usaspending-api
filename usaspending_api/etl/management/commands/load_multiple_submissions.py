81f726f7e8408cc980f3821340519abc774523e9
Brian Zito <zito_brian@bah.com>
2019-07-12 09:49:23 -0400

    removed unused import

--- usaspending_api/etl/management/commands/load_multiple_submissions.py
+++ usaspending_api/etl/management/commands/load_multiple_submissions.py
@@ -1,7 +1,7 @@
 import logging
 import pytz
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.db import connections
 from datetime import datetime
