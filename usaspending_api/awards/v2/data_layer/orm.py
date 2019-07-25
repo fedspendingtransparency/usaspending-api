7f75ed9ccf329dcfb6f749514657a3fc0ea63e6b
Brian Zito <zito_brian@bah.com>
2019-07-25 10:32:52 -0400

    removed unnessicary gate

--- usaspending_api/awards/v2/data_layer/orm.py
+++ usaspending_api/awards/v2/data_layer/orm.py
@@ -245,14 +245,13 @@ def create_officers_object(award,mapper, transaction_type):
 
     officers = []
 
-    if transaction is not None:
-        for officer_num in range(1, 6):
-            officer_name_key = "_officer_{}_name".format(officer_num)
-            officer_amount_key = "_officer_{}_amount".format(officer_num)
-            officer_name = transaction.get(officer_name_key)
-            officer_amount = transaction.get(officer_amount_key)
-            if officer_name or officer_amount:
-                officers.append({"name": officer_name, "amount": officer_amount})
+    for officer_num in range(1, 6):
+        officer_name_key = "_officer_{}_name".format(officer_num)
+        officer_amount_key = "_officer_{}_amount".format(officer_num)
+        officer_name = transaction.get(officer_name_key)
+        officer_amount = transaction.get(officer_amount_key)
+        if officer_name or officer_amount:
+            officers.append({"name": officer_name, "amount": officer_amount})
 
     return {"officers": officers}
 
