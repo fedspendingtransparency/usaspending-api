e2583de9ec416bc9f714e35f22645bcf78050d52
sethstoudenmier <stoudenmier_seth@bah.com>
2019-08-05 11:02:25 -0700

    Mod/Dev-2973: Add financial accounts to download

---
+++ usaspending_api/download/helpers/download_annotations_helper.py
@@ -0,0 +1,37 @@
+from django.contrib.postgres.aggregates import StringAgg
+from django.db.models.functions import Concat
+from django.db.models import Value
+
+
+def universal_transaction_matview_annotations():
+    # transaction__award__financial_set__treasury_account__federal_account__agency_identifier
+    federal_account_query_path = "transaction__award__financial_set__treasury_account__federal_account"
+    annotation_fields = {
+        "federal_accounts_funding_this_award": StringAgg(
+            Concat(
+                "{}__agency_identifier".format(federal_account_query_path),
+                Value("-"),
+                "{}__main_account_code".format(federal_account_query_path),
+            ),
+            ";",
+            distinct=True,
+        )
+    }
+    return annotation_fields
+
+
+def universal_award_matview_annotations():
+    # transaction__award__financial_set__treasury_account__federal_account__agency_identifier
+    federal_account_query_path = "award__financial_set__treasury_account__federal_account"
+    annotation_fields = {
+        "federal_accounts_funding_this_award": StringAgg(
+            Concat(
+                "{}__agency_identifier".format(federal_account_query_path),
+                Value("-"),
+                "{}__main_account_code".format(federal_account_query_path),
+            ),
+            ";",
+            distinct=True,
+        )
+    }
+    return annotation_fields
