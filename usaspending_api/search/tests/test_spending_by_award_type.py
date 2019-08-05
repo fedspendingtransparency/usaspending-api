9cf5754b4cd38e51b165e414a0d5aef000443d6b
Emily Brents <brents_emily@bah.com>
2019-08-05 13:38:14 -0400

    fixed flake8, tests

--- usaspending_api/search/tests/test_spending_by_award_type.py
+++ usaspending_api/search/tests/test_spending_by_award_type.py
@@ -447,7 +447,7 @@ def test_spending_by_award_foreign_filter(client, mock_matviews_qs):
     # One result is returned when searching for "Foreign" recipients
     assert len(resp.data["results"]) == 1
 
-#test subaward types
+# test subaward types
 @pytest.mark.django_db
 def test_spending_by_subaward_type_success(client, refresh_matviews):
     resp = client.post(
@@ -457,7 +457,7 @@ def test_spending_by_subaward_type_success(client, refresh_matviews):
             {
                 "fields": ["Sub-Award ID"],
                 "filters": {
-                    "award_type_codes": ["10","06","07","08","09","11"]
+                    "award_type_codes": ["10", "06", "07", "08", "09", "11"]
                 },
                 "subawards": True
             }
