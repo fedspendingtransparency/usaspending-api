1b502201e3953eaa3ee8f9f4d6cf270b5f95d205
Brian Zito <zito_brian@bah.com>
2019-08-06 07:51:07 -0400

    spoofing linter

--- usaspending_api/download/helpers/download_annotation_functions.py
+++ usaspending_api/download/helpers/download_annotation_functions.py
@@ -22,13 +22,13 @@ def universal_transaction_matview_annotations():
 
 
 def universal_award_matview_annotations():
-    federal_account_query_path = "award__financial_set__treasury_account__federal_account"
+    federal_account_query = "award__financial_set__treasury_account__federal_account"
     annotation_fields = {
         "federal_accounts_funding_this_award": StringAgg(
             Concat(
-                "{}__agency_identifier".format(federal_account_query_path),
+                "{}__agency_identifier".format(federal_account_query),
                 Value("-"),
-                "{}__main_account_code".format(federal_account_query_path),
+                "{}__main_account_code".format(federal_account_query),
             ),
             ";",
             distinct=True,
