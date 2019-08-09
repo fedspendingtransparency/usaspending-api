17cb56820aee3d07bdcb1edf7bb49ab5c22aef9a
Kirk Barden <barden_kirk@bah.com>
2019-08-09 17:38:54 -0400

    fixes

--- usaspending_api/search/helpers.py
+++ usaspending_api/search/helpers.py
@@ -21,25 +21,22 @@ def build_tas_codes_filter(queryset, model, tas_filters):
         # Build a full text OR query (any one of the TASes can match).  This is
         # done by concatenating the TASes together using pipes.
         tas_query = "|".join(TASAutocompleteMatview.objects.filter(where).values_list("tas_rendering_label", flat=True))
+        tas_queryset = TASSearchMatview.objects.extra(
+            where=['"{}"."tas_ts_vector" @@ %s::tsquery'.format(TASSearchMatview._meta.db_table)],
+            params=[tas_query]
+        )
 
-        if tas_query:
-
-            tas_queryset = TASSearchMatview.objects.extra(
-                where=['"{}"."tas_ts_vector" @@ %s::tsquery'.format(model._meta.db_table)],
-                params=[tas_query]
-            )
-
-            # Now that we've built the actual queryset filter, let's turn it into SQL
-            # that we can provide to the queryset.extra method.  We do this by converting
-            # the queryset to raw SQL and nabbing the where clause.
-            where_sql, where_params = generate_where_clause(tas_queryset)
-            return queryset.extra(
-                tables=[TASSearchMatview._meta.db_table],
-                where=[
-                    '"{}".award_id = "{}".award_id'.format(TASSearchMatview._meta.db_table, model._meta.db_table),
-                    where_sql,
-                ],
-                params=where_params
-            )
+        # Now that we've built the actual queryset filter, let's turn it into SQL
+        # that we can provide to the queryset.extra method.  We do this by converting
+        # the queryset to raw SQL and nabbing the where clause.
+        where_sql, where_params = generate_where_clause(tas_queryset)
+        return queryset.extra(
+            tables=[TASSearchMatview._meta.db_table],
+            where=[
+                '"{}".award_id = "{}".award_id'.format(TASSearchMatview._meta.db_table, model._meta.db_table),
+                where_sql,
+            ],
+            params=where_params
+        )
 
     return queryset
