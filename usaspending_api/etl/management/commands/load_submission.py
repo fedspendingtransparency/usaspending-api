79213a8bec3016247e01aa36b56c47544dca6173
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-08-16 17:19:28 -0600

    Update load_submission.py

--- usaspending_api/etl/management/commands/load_submission.py
+++ usaspending_api/etl/management/commands/load_submission.py
@@ -314,6 +314,17 @@ def get_submission_attributes(broker_submission_id, submission_data):
         submission_data["reporting_fiscal_period"],
     )
 
+    # if another submission lists the previous submission as it's previous submission, set to null and update later
+    potential_conflicts = []
+    if previous_submission:
+        potential_conflicts = SubmissionAttributes.objects.filter(previous_submission=previous_submission)
+        if potential_conflicts:
+            logger.info("==== ATTENTION! Previous Submission ID Conflict Detected ====")
+            for conflict in potential_conflicts:
+                logger.info("Temporarily setting {}'s Previous Submission ID from {} to Null".format(conflict, previous_submission.submission_id))
+                conflict.previous_submission = None
+                conflict.save()
+
     # Update and save submission attributes
     field_map = {
         "reporting_period_start": "reporting_start_date",
@@ -332,9 +343,24 @@ def get_submission_attributes(broker_submission_id, submission_data):
         else None,
     }
 
-    return load_data_into_model(
+    new_submission = load_data_into_model(
         submission_attributes, submission_data, field_map=field_map, value_map=value_map, save=True
     )
+
+    # If there were any submissions which were temporarily modified, reassign the submission
+    for conflict in potential_conflicts:
+        remapped_previous = get_previous_submission(
+            conflict.cgac_code, conflict.reporting_fiscal_year, conflict.reporting_fiscal_period
+        )
+        logger.info(
+            "New Previous Submission ID for Submission ID {} was mapped to {} ".format(
+                conflict.submission_id, remapped_previous
+            )
+        )
+        conflict.previous_submission = None if remapped_previous is None else remapped_previous
+        conflict.save()
+
+    return new_submission
 
 
 def load_file_a(submission_attributes, appropriation_data, db_cursor):
