0bb5d1154b75a7f9037c5164ce18d868a6964ba9
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-08-16 01:46:08 -0600

    Adding additional logging and cleaning existing logging

--- usaspending_api/etl/management/commands/load_multiple_submissions.py
+++ usaspending_api/etl/management/commands/load_multiple_submissions.py
@@ -40,6 +40,8 @@ class Command(BaseCommand):
         # Convert fiscal quarter to starting month of calendar quarter
         quarter = int(quarter) * 3
 
+        logger.info("Querying Broker DB for Submissions")
+
         broker_cursor.execute(
             "SELECT submission.submission_id, MAX(certify_history.created_at) AS certified_at, \
                                   submission.cgac_code, submission.frec_code \
@@ -66,9 +68,7 @@ class Command(BaseCommand):
                 frec = next_broker_sub[3]
 
                 api_cursor.execute(
-                    "SELECT update_date \
-                                FROM submission_attributes \
-                                WHERE broker_submission_id = {}".format(
+                    "SELECT update_date FROM submission_attributes WHERE broker_submission_id = {}".format(
                         submission_id
                     )
                 )
@@ -87,34 +87,40 @@ class Command(BaseCommand):
 
                 if certify_date > most_recently_loaded_date:
                     missing_submissions.append((submission_id, agency_name, certify_date, most_recently_loaded_date))
-            except Exception as error:
-                logger.exception("Submission {} failed in pull from broker: {}".format(submission_id, error))
+            except Exception:
+                logger.exception("Submission ID {} failed in pull from broker".format(submission_id))
                 failed_submissions.append(str(submission_id))
-        logger.info("Total missing submissions: {}".format(len(missing_submissions)))
-        logger.info("-----------------------------------")
-        for next_missing_sub in missing_submissions:
-            logger.info(
-                "Submission ID {} ({})\tCertified: {}".format(
-                    next_missing_sub[0], next_missing_sub[1], next_missing_sub[2].date()
+
+        if len(missing_submissions):
+            logger.info("Total missing submissions: {}".format(len(missing_submissions)))
+            logger.info("-----------------------------------")
+            for submission in missing_submissions:
+                logger.info(
+                    "Submission ID {} ({})\tCertified: {}".format(submission[0], submission[1], submission[2].date())
                 )
-            )
-        logger.info("-----------------------------------")
+            logger.info("-----------------------------------")
+        else:
+            logger.info("No DABS to load")
 
         # Data modification happens here, if you don't flag '--safe'
-        # The submission loader is atomic, so one of these failing should not affect subsequent submissions
-        if not options["safe"]:
-            for next_missing_sub in missing_submissions:
-                submission_id = next_missing_sub[0]
-                try:
-                    call_command("load_submission", "--noclean", submission_id)
-                except SystemExit:
-                    logger.info("Submission {} failed to load: {}".format(submission_id))
-                    failed_submissions.append(str(submission_id))
-                except Exception as error:
-                    logger.exception("Submission {} failed to load: {}".format(submission_id, error))
-                    failed_submissions.append(str(submission_id))
+        # The submission loader is atomic, so one of these failing will not affect subsequent submissions
+        if options["safe"]:
+            logger.info("Exiting script before data load occurs in accordance with the --safe flag.")
+            return
 
-        # If there were any failures, display them
+        for submission in missing_submissions:
+            submission_id = submission[0]
+            try:
+                call_command("load_submission", "--noclean", submission_id)
+            except SystemExit:
+                logger.info("Submission {} failed to load: {}".format(submission_id))
+                failed_submissions.append(str(submission_id))
+            except Exception:
+                logger.exception("Submission {} failed to load".format(submission_id))
+                failed_submissions.append(str(submission_id))
+
         if failed_submissions:
-            logger.error("The following submissions failed: {}. Script Complete.".format(", ".join(failed_submissions)))
+            logger.error("Script completed with the following submissions failures: {}".format(", ".join(failed_submissions)))
             raise SystemExit(3)
+        else:
+            logger.info("Script completed with no failures")
