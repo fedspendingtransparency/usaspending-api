bb466f02b26a46903ba189ad64ab7b903637922f
Brian Zito <zito_brian@bah.com>
2019-08-22 15:57:16 -0400

    added support for awarding and funding agency

--- usaspending_api/data_load/fpds_loader.py
+++ usaspending_api/data_load/fpds_loader.py
@@ -7,8 +7,23 @@ from usaspending_api.data_load.field_mappings_fpds import transaction_fpds_colum
 USASPENDING_CONNECTION_STRING = environ["DATABASE_URL"]
 BROKER_CONNECTION_STRING = environ["DATA_BROKER_DATABASE_URL"]
 
+subtier_agency_list = {}
+
+def load_reference_data():
+    with psycopg2.connect(dsn=USASPENDING_CONNECTION_STRING) as connection:
+        with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            sql = "SELECT * FROM subtier_agency " \
+                  "JOIN agency " \
+                  "ON subtier_agency.subtier_agency_id = agency.subtier_agency_id"
+
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            for result in results:
+                subtier_agency_list[result["subtier_code"]] = result
 
 def run_fpds_load(id_list):
+    load_reference_data()
+
     broker_transactions = fetch_broker_objects(id_list)
 
     load_objects = generate_load_objects(broker_transactions)
@@ -40,20 +55,35 @@ def generate_load_objects(broker_objects):
     for broker_object in broker_objects:
         connected_objects = {}
 
+        # transaction_fpds values
         transaction_fpds = {}
         for key in transaction_fpds_columns:
             transaction_fpds[key] = broker_object[key]
         connected_objects["transaction_fpds"] = transaction_fpds
 
+        # transaction_normalized values
         transaction_normalized = {"is_fpds": True}
         for key in transaction_normalized_columns:
             transaction_normalized[transaction_normalized_columns[key]] = broker_object[key]
+
         for key in transaction_normalized_functions:
             transaction_normalized[key] = transaction_normalized_functions[key](broker_object)
+
+        awarding_agency = subtier_agency_list.get(broker_object["awarding_sub_tier_agency_c"], None)
+        if awarding_agency is not None:
+            transaction_normalized["awarding_agency_id"] = awarding_agency["id"]
+        else:
+            transaction_normalized["awarding_agency_id"] = None
+
+        funding_agency = subtier_agency_list.get(broker_object["funding_sub_tier_agency_co"], None)
+        if funding_agency is not None:
+            transaction_normalized["funding_agency_id"] = funding_agency["id"]
+        else:
+            transaction_normalized["funding_agency_id"] = None
+
         connected_objects["transaction_normalized"] = transaction_normalized
 
         retval.append(connected_objects)
-
     return retval
 
 
@@ -61,4 +91,4 @@ def load_transactions(load_objects):
     with psycopg2.connect(dsn=USASPENDING_CONNECTION_STRING) as connection:
         with connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
             for load_object in load_objects:
-                print(load_object)
+                print(load_object["transaction_normalized"]["awarding_agency_id"])
