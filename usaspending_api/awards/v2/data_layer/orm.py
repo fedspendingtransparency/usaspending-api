bbe97b0a89c5330efc70686d750b92a2624237dd
Brian <zito_brian@bah.com>
2019-08-22 19:32:18 -0400

    removed unneeded get

--- usaspending_api/awards/v2/data_layer/orm.py
+++ usaspending_api/awards/v2/data_layer/orm.py
@@ -227,7 +227,7 @@ def create_place_of_performance_object(db_row_dict):
             ("county_name", db_row_dict["_pop_county_name"]),
             ("city_name", db_row_dict["_pop_city_name"]),
             ("state_code", db_row_dict["_pop_state_code"] if db_row_dict["_pop_state_code"]
-                else state_code_from_name(db_row_dict.get("_pop_state_name", None))),
+                else state_code_from_name(db_row_dict["_pop_state_name"])),
             ("state_name", db_row_dict["_pop_state_name"] if db_row_dict.get("_pop_state_name", None)
                 else state_name_from_code(db_row_dict.get("_pop_state_code", None))),
             ("congressional_code", db_row_dict["_pop_congressional_code"]),
