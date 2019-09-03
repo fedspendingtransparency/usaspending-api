57176211c8802a7dd37b4e3f8ccda31e5fcc4363
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-09-03 11:25:07 -0600

    [DEV-3233] removed partial index definition and cleaned up code

--- usaspending_api/search/helpers.py
+++ usaspending_api/search/helpers.py
@@ -4,34 +4,30 @@ from usaspending_api.accounts.models import TreasuryAppropriationAccount
 
 
 def build_tas_codes_filter(queryset, model, tas_filters):
-    """
-    We're using a GIN indexed array of treasury_appropriation_account.treasury_account_identifiers
-    to filter awards/transactions by TAS/FA.
+    """Append a performant predicate using Django's extra to a queryset
 
-        Step one)  Get the list of all treasury_account_identifiers that match the filters provided.
-        Step bee)  Assemble an integer array overlap (&&) query to search for those integers.
+    The materialized views contain a GIN indexed integer array of
+    treasury_appropriation_account.treasury_account_identifiers.
+
+    Step 1)  Get the list of all treasury_account_identifiers that match the filters provided.
+    Step 2)  Assemble an integer array overlap (&&) query to search for those integers.
     """
 
-    # Build a queryset filter that contains all of the TAS request filters.
-    where = Q()
+    tas_qs = Q()
     for tas_filter in tas_filters:
-        where |= Q(**{TAS_COMPONENT_TO_FIELD_MAPPING[k]: v for k, v in tas_filter.items()})
+        tas_qs |= Q(**{TAS_COMPONENT_TO_FIELD_MAPPING[k]: v for k, v in tas_filter.items()})
 
     # No sense continuing if this resulted in an empty Q statement for whatever reason.
-    if where:
+    if not tas_qs:
+        return queryset
 
-        # Concatenate the list of integer treasury_account_identifiers into a string suitable for
-        # stuffing into a PostgreSQL integer array.
-        where = ",".join([
-            str(i) for i in
-            TreasuryAppropriationAccount.objects.filter(where).values_list("treasury_account_identifier", flat=True)
-        ])
+    # Concatenate the list of integer treasury_account_identifiers into a string
+    # suitable for a PostgreSQL integer array.
+    tas_ids = TreasuryAppropriationAccount.objects.filter(tas_qs).values_list("treasury_account_identifier", flat=True)
+    tas_sql_array = "ARRAY[{}]::integer[]".format(",".join([str(i) for i in tas_ids]))
 
-        # Craft a where clause that performs an array overlap search (&&) on the
-        # treasury_account_identifiers column.
-        where = '"%s"."treasury_account_identifiers" && ARRAY[%s]::integer[]' % (model._meta.db_table, where)
+    # Craft a where clause that performs an array overlap search (&&) on the
+    # treasury_account_identifiers column.
+    where = '"{}"."treasury_account_identifiers" && {}'.format(model._meta.db_table, tas_sql_array)
 
-        # Finally, let's graft all of this nonsense into the provided queryset.
-        return queryset.extra(where=[where])
-
-    return queryset
+    return queryset.extra(where=[where])
