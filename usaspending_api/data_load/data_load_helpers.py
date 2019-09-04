4ec26fc6e2b56b8a78ece19616fb0b64343c49df
Brian Zito <zito_brian@bah.com>
2019-09-04 14:23:57 -0400

    Added space

--- usaspending_api/data_load/data_load_helpers.py
+++ usaspending_api/data_load/data_load_helpers.py
@@ -6,7 +6,7 @@ subtier_agency_list = {}  # global variable, populated by fpds_loader
 # TODO: replace this with cursor.morgify() in some way that doesn't need a live connection passed around everywhere
 def format_value_for_sql(val):
     if isinstance(val, str):
-        return "\'{}\'".format(val.replace("'", "''").replace("\"","\"\""))
+        return "\'{}\'".format(val.replace("'", "''").replace("\"", "\"\""))
     elif val is None:
         return "null"
     elif isinstance(val, list):
