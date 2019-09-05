ed207b84bae8fba8cc551244a8288c54baa9dce3
Kirk Barden <barden_kirk@bah.com>
2019-09-05 06:54:48 -0400

    [DEV-3233] build tas code filter cleanup

--- usaspending_api/search/helpers.py
+++ usaspending_api/search/helpers.py
@@ -3,14 +3,13 @@ from usaspending_api.accounts.helpers import TAS_COMPONENT_TO_FIELD_MAPPING
 from usaspending_api.accounts.models import TreasuryAppropriationAccount
 
 
-def build_tas_codes_filter(queryset, model, tas_filters):
-    """Append a performant predicate using Django's extra to a queryset
+def build_tas_codes_filter(queryset, tas_filters):
+    """
+    Filter the queryset to take advantage of the GIN indexed integer array of
+    treasury_appropriation_account treasury_account_identifiers by:
 
-    The materialized views contain a GIN indexed integer array of
-    treasury_appropriation_account.treasury_account_identifiers.
-
-    Step 1)  Get the list of all treasury_account_identifiers that match the filters provided.
-    Step 2)  Assemble an integer array overlap (&&) query to search for those integers.
+        - Grabbing the list of all treasury_account_identifiers that match the filters provided.
+        - Assembling an integer array overlap (&&) query to search for those integers.
     """
 
     tas_qs = Q()
@@ -21,13 +20,11 @@ def build_tas_codes_filter(queryset, model, tas_filters):
     if not tas_qs:
         return queryset
 
-    # Concatenate the list of integer treasury_account_identifiers into a string
-    # suitable for a PostgreSQL integer array.
-    tas_ids = TreasuryAppropriationAccount.objects.filter(tas_qs).values_list("treasury_account_identifier", flat=True)
-    tas_sql_array = "ARRAY[{}]::integer[]".format(",".join([str(i) for i in tas_ids]))
-
-    # Craft a where clause that performs an array overlap search (&&) on the
-    # treasury_account_identifiers column.
-    where = '"{}"."treasury_account_identifiers" && {}'.format(model._meta.db_table, tas_sql_array)
-
-    return queryset.extra(where=[where])
+    return queryset.filter(
+        treasury_account_identifiers__overlap=list(
+            TreasuryAppropriationAccount
+            .objects
+            .filter(tas_qs)
+            .values_list("treasury_account_identifier", flat=True)
+        )
+    )
