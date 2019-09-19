fbf33c8a9d2cfd3f9b007a0d8ab7b3627087dd5f
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-09-18 21:51:30 -0600

    [DEV-3443] switching  to log output and added args

--- usaspending_api/database_scripts/job_archive/recompute_all_awards.py
+++ usaspending_api/database_scripts/job_archive/recompute_all_awards.py
@@ -13,22 +13,25 @@ Purpose:
     Updates award records which need to be updated using the new award_helper logic
 
 """
-import asyncpg
+import argparse
 import asyncio
-import re
+import asyncpg
+import logging
 import math
 import psycopg2
+import re
 import time
-# import argparse
 
 from os import environ
 
 
-# DEFINE THIS ENVIRONMENT VARIABLE BEFORE RUNNING!
-CONNECTION_STRING = environ["DATABASE_URL"]
-CHUNK_SIZE = 10000
-DEBUG = False
-earliest_transaction_cte = (
+try:
+    CONNECTION_STRING = environ["DATABASE_URL"]
+except Exception:
+    print("SET env var DATABASE_URL!!!\nTerminating script")
+    raise SystemExit(1)
+
+_earliest_transaction_cte = str(
     "txn_earliest AS ( "
     "SELECT DISTINCT ON (award_id) "
     "award_id, "
@@ -41,7 +44,7 @@ earliest_transaction_cte = (
     "ORDER BY award_id, action_date ASC, modification_number ASC "
     ")"
 )
-latest_transaction_cte = (
+_latest_transaction_cte = str(
     "txn_latest AS ( "
     "SELECT DISTINCT ON (award_id) "
     "award_id, "
@@ -68,7 +71,7 @@ latest_transaction_cte = (
     "ORDER BY award_id, action_date DESC, modification_number DESC "
     ")"
 )
-aggregate_transaction_cte = (
+_aggregate_transaction_cte = str(
     "txn_totals AS ( "
     "SELECT award_id, SUM(federal_action_obligation) AS total_obligation, "
     "SUM(original_loan_subsidy_cost) AS total_subsidy_cost, "
@@ -80,7 +83,7 @@ aggregate_transaction_cte = (
     " GROUP BY award_id "
     ")"
 )
-executive_comp_cte = (
+_executive_comp_cte = str(
     "executive_comp AS ( "
     "SELECT DISTINCT ON (tn.award_id) "
     "tn.award_id, "
@@ -102,7 +105,7 @@ executive_comp_cte = (
     ") "
 )
 
-UPDATE_AWARDS_SQL = (
+UPDATE_AWARDS_SQL = str(
     "WITH {}, {}, {}, {} "
     "UPDATE awards a "
     "SET "
@@ -178,18 +181,24 @@ UPDATE_AWARDS_SQL = (
     "    OR a.officer_5_name IS DISTINCT FROM ec.officer_5_name "
     "  )"
 )
-UPDATE_AWARDS_SQL = str(UPDATE_AWARDS_SQL).format(earliest_transaction_cte, latest_transaction_cte, aggregate_transaction_cte, executive_comp_cte)
+UPDATE_AWARDS_SQL = UPDATE_AWARDS_SQL.format(
+    _earliest_transaction_cte, _latest_transaction_cte, _aggregate_transaction_cte, _executive_comp_cte
+)
 
-
-GET_MIN_MAX_SQL = "SELECT MIN(id), MAX(id) FROM awards"
-GET_FPDS_AWARDS = "SELECT id FROM awards where is_fpds = TRUE AND id BETWEEN {minid} AND {maxid}"
+DEBUG = False
 GET_FABS_AWARDS = "SELECT id FROM awards where is_fpds = FALSE AND id BETWEEN {minid} AND {maxid}"
+GET_FPDS_AWARDS = "SELECT id FROM awards where is_fpds = TRUE AND id BETWEEN {minid} AND {maxid}"
+GET_MIN_MAX_SQL = "SELECT MIN(id), MAX(id) FROM awards"
+MAX_ID, MIN_ID = None, None
+TOTAL_UPDATES, CHUNK_SIZE = 0, 20000
 
 
 class Timer:
     def __init__(self, msg, pipe_output=print):
-        self.print_func = pipe_output
+        self.elapsed = None
+        self.end = None
         self.msg = msg
+        self.print_func = pipe_output
 
     def __enter__(self):
         self.start = time.perf_counter()
@@ -214,14 +223,14 @@ async def async_run_create(sql):
     try:
         insert_msg = await conn.execute(sql)
         await conn.close()
-    except Exception as e:
-        print("========= ERROR: {}\n".format(e))
-        print("{}\n=========".format(sql))
+    except Exception:
+        logging.exception("=== ERROR ===")
+        logging.error("{}\n=============".format(sql))
         raise SystemExit
 
     if DEBUG:
-        print(sql)
-        print(insert_msg)
+        logging.info(sql)
+        logging.info(insert_msg)
     count = re.findall(r"\d+", insert_msg)  # Example msg: UPDATE 10
     return int(count[0])
 
@@ -230,10 +239,10 @@ def run_update_query(fabs_awards, fpds_awards):
     loop = asyncio.new_event_loop()
     statements = []
     if fabs_awards:
-        fabs_sql = UPDATE_AWARDS_SQL.format(award_ids=', '.join(fabs_awards), transaction_table="transaction_fabs")
+        fabs_sql = UPDATE_AWARDS_SQL.format(award_ids=", ".join(fabs_awards), transaction_table="transaction_fabs")
         statements.append(asyncio.ensure_future(async_run_create(fabs_sql), loop=loop))
     if fpds_awards:
-        fpds_sql = UPDATE_AWARDS_SQL.format(award_ids=', '.join(fpds_awards), transaction_table="transaction_fpds")
+        fpds_sql = UPDATE_AWARDS_SQL.format(award_ids=", ".join(fpds_awards), transaction_table="transaction_fpds")
         statements.append(asyncio.ensure_future(async_run_create(fpds_sql), loop=loop))
 
     all_statements = asyncio.gather(*statements)
@@ -243,24 +252,36 @@ def run_update_query(fabs_awards, fpds_awards):
 
 
 def main():
+    global TOTAL_UPDATES
     with psycopg2.connect(dsn=CONNECTION_STRING) as connection:
         connection.autocommit = True
         connection.readonly = True
 
-        with connection.cursor() as cursor:
-            print("Finding min/max IDs...")
-            cursor.execute(GET_MIN_MAX_SQL)
-            results = cursor.fetchall()
-            min_id, max_id = results[0]
+        if MIN_ID is None or MAX_ID is None:
+            with connection.cursor() as cursor:
+                logging.info("Finding min/max IDs from awards table...")
+                cursor.execute(GET_MIN_MAX_SQL)
+                results = cursor.fetchall()
+                min_id, max_id = results[0]
 
-        print("Min ID: {:,}".format(min_id))
-        print("Max ID: {:,}".format(max_id))
-        print("Total in range: {:,}".format(max_id - min_id + 1), flush=True)
+        if MAX_ID is not None:
+            logging.info("Using provided MAX ID {}".format(MAX_ID))
+            max_id = MAX_ID
+        if MIN_ID is not None:
+            logging.info("Using provided MIN ID {}".format(MIN_ID))
+            min_id = MIN_ID
+
+        if min_id >= max_id:
+            raise RuntimeError("MAX ID ({}) must be greater than MIN ID ({})".format(MAX_ID, MIN_ID))
+
+        logging.info("Min ID: {:,}".format(min_id))
+        logging.info("Max ID: {:,}".format(max_id))
+        logging.info("Total in range: {:,}".format(max_id - min_id + 1))
 
         batch_min = min_id
         while batch_min <= max_id:
             batch_max = min(batch_min + CHUNK_SIZE - 1, max_id)
-            with Timer("[Awards {:,} - {:,}]".format(batch_min, batch_max)):
+            with Timer("[Awards {:,} - {:,}]".format(batch_min, batch_max), pipe_output=logging.info):
                 with connection.cursor() as cursor:
                     cursor.execute(GET_FABS_AWARDS.format(minid=batch_min, maxid=batch_max))
                     fabs = [str(row[0]) for row in cursor.fetchall()]
@@ -269,9 +290,10 @@ def main():
 
                 if fabs or fpds:
                     row_count = run_update_query(fabs_awards=fabs, fpds_awards=fpds)
-                    print("UPDATED {:,} records".format(row_count), flush=True)
+                    logging.info("UPDATED {:,} records".format(row_count))
+                    TOTAL_UPDATES += row_count
                 else:
-                    print("#### No awards to update in range ###", flush=True)
+                    logging.info("#### No awards to update in range ###")
             batch_min = batch_max + 1
 
 
@@ -279,28 +301,44 @@ def setup():
     with psycopg2.connect(dsn=CONNECTION_STRING) as connection:
         connection.autocommit = True
         with connection.cursor() as cursor:
-            print("### Disabling autovacuum ###")
+            logging.info("### Disabling autovacuum ###")
             cursor.execute("ALTER TABLE awards SET (autovacuum_enabled = false, toast.autovacuum_enabled = false)")
-            print(cursor.statusmessage)
+            logging.info(cursor.statusmessage)
 
 
 def teardown(successful_run=False):
     with psycopg2.connect(dsn=CONNECTION_STRING) as connection:
         connection.autocommit = True
         with connection.cursor() as cursor:
-            with Timer("teardown"):
-                if successful_run:
-                    print("### running full vacuum ###")
+            with Timer("teardown", pipe_output=logging.info):
+                if successful_run and TOTAL_UPDATES > 0:
+                    logging.info("### running full vacuum ###")
                     cursor.execute("VACUUM (FULL, ANALYZE, VERBOSE) awards")
-                    print(cursor.statusmessage)
-                print("### Resetting autovacuum ###")
+                    logging.info(cursor.statusmessage)
+                logging.info("### Resetting autovacuum ###")
                 cursor.execute("ALTER TABLE awards SET (autovacuum_enabled = true, toast.autovacuum_enabled = true)")
-                print(cursor.statusmessage)
+                logging.info(cursor.statusmessage)
 
 
 if __name__ == "__main__":
+    log_format = "[%(asctime)s] [%(levelname)s] - %(message)s"
+    logging.getLogger()
+    logging.basicConfig(level=logging.INFO, format=log_format, datefmt="%Y/%m/%d %H:%M:%S (%Z)")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--chunk-size", type=int, default=CHUNK_SIZE)
+    parser.add_argument("--max-id", type=int)
+    parser.add_argument("--min-id", type=int)
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    CHUNK_SIZE = args.chunk_size
+    MAX_ID = args.max_id
+    MIN_ID = args.min_id
+    DEBUG = args.debug
+
     successful_run = False
-    with Timer("recompute_all_awards"):
+    with Timer("recompute_all_awards", pipe_output=logging.info):
         setup()
         try:
             main()
@@ -308,10 +346,10 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             pass
         except Exception:
-            print("ERROR ENCOUNTERED!!!!!")
+            logging.exception("ERROR ENCOUNTERED!!!")
             raise SystemExit(1)
         finally:
-            print("Cleaning up table and restoring autovacuum")
+            logging.info("Cleaning up table and restoring autovacuum")
             teardown(successful_run)
 
-    print("Finished.")
+    logging.info("Finished. Updated {:,} total award records".format(TOTAL_UPDATES))
