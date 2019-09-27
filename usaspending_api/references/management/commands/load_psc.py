f99c985f216aefa447868870290924c5bfbe93ff
Emily Brents <brents_emily@bah.com>
2019-09-27 14:49:24 -0400

    Update load_psc.py

--- usaspending_api/references/management/commands/load_psc.py
+++ usaspending_api/references/management/commands/load_psc.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
 
 def load_psc(fullpath):
     """
-    Create CFDA Program records from a Excel doc of historical data.
+    Create/Update Product or Service Code records from a Excel doc of historical data.
     """
     try:
         logger = logging.getLogger("console")
@@ -28,41 +28,50 @@ def load_psc(fullpath):
         ws = wb.active
 
         for current_row, row in enumerate(ws.rows):
-            if not row[0].value or not row[2].value or row[0].value == "PSC CODE":
+            if not row[0].value or row[0].value == "PSC CODE":
                 continue  # skip lines without codes
             psc_code = str(row[0].value)
-            psc_length = len(psc_code)
-            psc_description = row[2].value.strip()
+            psc_length = len(psc_code.strip())
+            psc_description = row[2].value
             psc_start_date = row[3].value
             psc_end_date = row[4].value
             psc_full_name = row[5].value
             psc_excludes = row[6].value
             psc_notes = row[7].value
             psc_includes = row[8].value
-            if psc_end_date:
-                try: # if we have an end date we are either 1) updated a code that already has an end date (unlikely)
-                    psc = PSC.objects.get(code=psc_code, start_date=psc_start_date,
-                                          end_date=psc_end_date)
-                except PSC.DoesNotExist:
-                    try: #or still unlikely, updating a code that has a start date but no end date
-                        psc = PSC.objects.get(code=psc_code, start_date=psc_start_date, end_date__isnull=True)
-                    except PSC.DoesNotExist: #but most likely we are adding an already expired code
-                        psc, created = PSC.objects.get_or_create(code=psc_code, end_date=psc_end_date)
-            elif psc_start_date:
-                try: #if we have only a start date we don't want to overwrite data that has a different start date
-                    psc = PSC.objects.get(code=psc_code, start_date=psc_start_date)
-                except PSC.DoesNotExist:
-                    try: # but we do want to overwrite data that has blank dates
-                        psc = PSC.objects.get(code=psc_code, start_date__isnull=True, end_date__isnull=True)
-                    except PSC.DoesNotExist:
-                        psc, created = PSC.objects.get_or_create(code=psc_code)
-            else:
-                psc, created = PSC.objects.get_or_create(code=psc_code)
+            print(psc_code)
+            try:
+                psc = PSC.objects.get(code=psc_code)
+                if psc_end_date:
+                    if psc.end_date and psc_end_date > psc.end_date:
+                        psc.end_date = psc_end_date
+                if psc_start_date:
+                    if psc.start_date and psc_start_date > psc.start_date:
+                        psc.start_date = psc_start_date
+            except PSC.DoesNotExist:
+                psc = PSC(code=psc_code)
+
+            # if psc_end_date:
+            #     try:  # if we have an end date we are either 1) updated a code that already has an end date (unlikely)
+            #         psc = PSC.objects.get(code=psc_code, start_date=psc_start_date,
+            #                               end_date=psc_end_date)
+            #     except PSC.DoesNotExist:
+            #         try:  # or still unlikely, updating a code that has a start date but no end date
+            #             psc = PSC.objects.get(code=psc_code, start_date=psc_start_date, end_date__isnull=True)
+            #         except PSC.DoesNotExist:  # but most likely we are adding an already expired code
+            #             psc, created = PSC.objects.get_or_create(code=psc_code, start_date__isnull=True, end_date__isnull=True)
+            # elif psc_start_date:
+            #     try:  # if we have only a start date we don't want to overwrite data that has a different start date
+            #         psc = PSC.objects.get(code=psc_code, start_date=psc_start_date)
+            #     except PSC.DoesNotExist:
+            #         psc, created = PSC.objects.get_or_create(code=psc_code, start_date__isnull=True, end_date__isnull=True)
+            # else:
+            #     psc, created = PSC.objects.get_or_create(code=psc_code, start_date__isnull=True, end_date__isnull=True)
 
             psc.description = psc_description
             psc.length = psc_length
-            psc.start_date = psc_start_date
-            psc.end_date = psc_end_date
+            # psc.start_date = psc_start_date
+
             psc.full_name = psc_full_name
             psc.excludes = psc_excludes
             psc.notes = psc_notes
