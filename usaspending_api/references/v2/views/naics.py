ba8600ef666c6228f12776982ebd373dbc2ce140
Emily Brents <brents_emily@bah.com>
2019-10-01 14:06:38 -0400

    [DEV-3527] fighting with filtering

--- usaspending_api/references/v2/views/naics.py
+++ usaspending_api/references/v2/views/naics.py
@@ -23,14 +23,15 @@ class NAICSViewSet(APIView):
 
     def _fetch_children(self, naics_code) -> list:
         results = []
-        naics = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naics_code, text_len=len(naics_code) + 2).exclude(code=naics_code)
+        naics = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naics_code,
+                                                                       text_len=len(naics_code) + 2)
         for naic in naics:
             if len(naic.code) < 6:
                 result = OrderedDict()
                 result["naics"] = naic.code
                 result["naics_description"] = naic.description
-                result["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naics_code, text_len=6).count()
-                result["children"] = self._fetch_children(naic.code)
+                result["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naic.code,
+                                                                                         text_len=6).count()
             else:
                 result = OrderedDict()
                 result["naics"] = naic.code
@@ -39,34 +40,76 @@ class NAICSViewSet(APIView):
             results.append(result)
         return results
 
-    def _fetch_parents(self, naics_code) -> list:
-        if len(naics_code) == 6:
-            codes = [naics_code[:2], naics_code[:4]]
-        elif len(naics_code = 4):
-            codes = [naics_code[:2]]
-        parents = 
-        return []
+    def _fetch_parent(self, naics_code) -> dict:
+        parent = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naics_code[:2],
+                                                                        text_len=2).first()
+        parent2 = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naics_code[:4],
+                                                                         text_len=4).first()
+        results = OrderedDict()
+        results["naics"] = parent.code
+        results["description"] = parent.description
+        results["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=parent.code,
+                                                                                  text_len=6).count()
+        results2 = OrderedDict()
+        results2["naics"] = parent2.code
+        results2["description"] = parent2.description
+        results2["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=parent2.code,
+                                                                                   text_len=6).count()
+        results2["children"] = []
+        results["children"] = [results2]
+        return results
 
     def _business_logic(self, request_data: dict) -> list:
         naics_filter = {}
-        if request_data.get("code"):
+        code = request_data.get("code")
+        description = request_data.get("description")
+
+        if not code and not description:
+            naics = NAICS.objects.annotate(text_len=Length("code")).filter(text_len=2)
+            results = []
+            for naic in naics:
+                result = OrderedDict()
+                result["naics"] = naic.code
+                result["naics_description"] = naic.description
+                result["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naic.code,
+                                                                                         text_len=6).count()
+                results.append(result)
+            response_content = OrderedDict({"results": results})
+            return response_content
+
+        filtered = False
+        if code:
             naics_filter.update({"code": request_data.get("code")})
-        if request_data.get("description"):
+        if description:
+            filtered = True
             naics_filter.update({"description__icontains": request_data.get("description")})
-        naics = NAICS.objects.filter(**naics_filter)
+
+        if filtered:
+            naics = NAICS.objects.annotate(text_len=Length("code")).filter(**naics_filter, text_len=6)
+        else:
+            naics = NAICS.objects.filter(**naics_filter)
         results = []
         for naic in naics:
             if len(naic.code) < 6:
                 result = OrderedDict()
                 result["naics"] = naic.code
                 result["naics_description"] = naic.description
-                result["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naic.code, text_len=6).count()
+                result["count"] = NAICS.objects.annotate(text_len=Length("code")).filter(code__startswith=naic.code,
+                                                                                         text_len=6).count()
                 result["children"] = self._fetch_children(naic.code)
             else:
                 result = OrderedDict()
-                result["naics"] = naic.code
-                result["naics_description"] = naic.description
-                result["count"] = 0
+                if filtered:
+                    parent = self._fetch_parent(naic.code)
+                    result["naics"] = naic.code
+                    result["naics_description"] = naic.description
+                    result["count"] = 0
+                    parent["children"][0]["children"].append(result)
+                    result = parent
+                else:
+                    result["naics"] = naic.code
+                    result["naics_description"] = naic.description
+                    result["count"] = 0
             results.append(result)
         response_content = OrderedDict({"results": results})
         return response_content
