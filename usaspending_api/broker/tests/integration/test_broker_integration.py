0513c8df54602fc7554c06f18a75ae449ceaf190
Keith Hickey <hickey_keith@bah.com>
2019-10-02 16:45:53 -0400

    [DEV-1526] One-lining the env var substitution, and adding a test for dblink integration

--- usaspending_api/broker/tests/integration/test_broker_integration.py
+++ usaspending_api/broker/tests/integration/test_broker_integration.py
@@ -1,6 +1,8 @@
+from unittest import SkipTest
+
 import psycopg2
 
-from usaspending_api.common.helpers.sql_helpers import get_broker_dsn_string
+from usaspending_api.common.helpers.sql_helpers import get_broker_dsn_string, get_database_dsn_string
 
 
 def test_can_connect_to_broker():
@@ -11,3 +13,22 @@ def test_can_connect_to_broker():
             results = cursor.fetchall()
     assert results is not None
     assert len(str(results[0][0])) > 0
+
+
+def test_can_connect_to_broker_by_dblink():
+    """Simple 'integration test' that checks the USAspending to Broker dblink works
+
+    It will be skipped if the `broker_server` server is not installed in the USAspending database under test
+    """
+    with psycopg2.connect(dsn=get_database_dsn_string()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("select srvname from pg_foreign_server where srvname = 'broker_server'")
+            results = cursor.fetchall()
+            if not results or not results[0][0] == 'broker_server':
+                raise SkipTest("No foreign server named 'broker_server' has been setup on this USAspending database. "
+                               "Skipping the test of integration with that server via dblink")
+            cursor.execute("SELECT * FROM dblink('broker_server','SELECT now()') AS broker_time(the_now datetime)")
+            results = cursor.fetchall()
+    assert results is not None
+    assert len(str(results[0][0])) > 0
+
