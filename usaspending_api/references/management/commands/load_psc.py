c5a9e6f7647cf1320e1b31bf2864a3f786871d7e
Emily Brents <brents_emily@bah.com>
2019-10-04 11:28:39 -0400

    [DEV-3558] date comparison

--- usaspending_api/references/management/commands/load_psc.py
+++ usaspending_api/references/management/commands/load_psc.py
@@ -3,23 +3,30 @@ from usaspending_api.references.models import PSC
 import os
 import logging
 import csv
-
+from datetime import datetime
 
 class Command(BaseCommand):
     help = "Loads program information obtained from Excel file on https://www.acquisition.gov/PSC_Manual"
 
     logger = logging.getLogger("console")
+    default_directory = os.path.normpath("usaspending_api/references/management/commands/")
+    default_filepath = os.path.join(default_directory, "filtered_csv_data.csv")
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-p", "--path", help="the path to the spreadsheets to load", default=self.default_filepath
+        )
+        parser.add_argument("-u", "--update", help="Updates the lengths of any codes that were not in the file.",
+                            action="store_true")
 
     def handle(self, *args, **options):
-        default_directory = os.path.normpath("usaspending_api/references/management/commands/")
-        # default_filepath = os.path.join(default_directory, "PSC_Data_June_2019_Edition_FINAL_6-20-19+DRW.xlsx")
-        default_filepath = os.path.join(default_directory, "filtered_csv_data.csv")
-        load_psc(default_filepath)
-        self.logger.log(20, "Loaded PSC codes successfully.")
-        update_lengths()
-        self.logger.log(20, "Updated PSC codes.")
 
-def load_psc(fullpath):
+        load_psc(fullpath=options["path"], update=options["update"])
+        self.logger.log(20, "Loaded PSC codes successfully.")
+
+
+
+def load_psc(fullpath, update):
     """
     Create/Update Product or Service Code records from a Excel doc of historical data.
     """
@@ -43,19 +50,27 @@ def load_psc(fullpath):
                 psc, created = PSC.objects.get_or_create(code=psc_code)
                 psc.description = psc_description
                 psc.length = psc_length
-                if psc_start_date: psc.start_date = psc_start_date
-                if psc_end_date: psc.end_date = psc_end_date
+                if psc_start_date:
+                    if psc.start_date and datetime.strptime(psc_start_date, '%Y-%m-%d').date() >= psc.start_date:
+                        psc.start_date = psc_start_date
+                if psc_end_date:
+                    if psc.end_date and datetime.strptime(psc_end_date, '%Y-%m-%d').date() >= psc.end_date:
+                        psc.end_date = psc_end_date
                 psc.full_name = psc_full_name
                 psc.excludes = psc_excludes
                 psc.notes = psc_notes
                 psc.includes = psc_includes
                 psc.save()
+            if update:
+                update_lengths()
+                logger.log(20, "Updated PSC codes.")
     except IOError:
         logger.error("Could not open file {}".format(fullpath))
+
 
 def update_lengths():
     unupdated_pscs = PSC.objects.filter(length=0)
     for psc in unupdated_pscs:
         length = len(psc.code)
-        psc.length=length
+        psc.length = length
         psc.save()
