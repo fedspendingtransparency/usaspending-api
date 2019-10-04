d210a47798a61ca24c173afad0870cf3bdd1447d
Emily Brents <brents_emily@bah.com>
2019-10-04 13:17:16 -0400

    [DEV-3558] - fixed excel load

--- usaspending_api/references/management/commands/load_psc.py
+++ usaspending_api/references/management/commands/load_psc.py
@@ -3,27 +3,27 @@ from usaspending_api.references.models import PSC
 import os
 import logging
 import csv
+from openpyxl import load_workbook
 from datetime import datetime
+
 
 class Command(BaseCommand):
     help = "Loads program information obtained from Excel file on https://www.acquisition.gov/PSC_Manual"
 
     logger = logging.getLogger("console")
     default_directory = os.path.normpath("usaspending_api/references/management/commands/")
-    default_filepath = os.path.join(default_directory, "filtered_csv_data.csv")
+    default_filepath = os.path.join(default_directory, "PSC_Data_June_2019_Edition_FINAL_6-20-19+DRW.xlsx")
 
     def add_arguments(self, parser):
+        parser.add_argument("-p", "--path", help="the path to the spreadsheets to load", default=self.default_filepath)
         parser.add_argument(
-            "-p", "--path", help="the path to the spreadsheets to load", default=self.default_filepath
+            "-u", "--update", help="Updates the lengths of any codes that were not in the file.", action="store_true"
         )
-        parser.add_argument("-u", "--update", help="Updates the lengths of any codes that were not in the file.",
-                            action="store_true")
 
     def handle(self, *args, **options):
 
         load_psc(fullpath=options["path"], update=options["update"])
         self.logger.log(20, "Loaded PSC codes successfully.")
-
 
 
 def load_psc(fullpath, update):
@@ -32,38 +32,39 @@ def load_psc(fullpath, update):
     """
     try:
         logger = logging.getLogger("console")
-        with open(fullpath, errors="backslashreplace", encoding="utf-8-sig") as csvfile:
+        wb = load_workbook(filename=fullpath, data_only=True)
+        ws = wb.active
+        for current_row, row in enumerate(ws.rows):
+            if not row[0].value or row[0].value == "PSC CODE" or ws.row_dimensions[row[0].row].hidden:
+                continue  # skip lines without codes and hidden rows
+            psc_code = row[0].value
+            psc_length = row[1].value
+            psc_description = row[2].value
 
-            reader = csv.DictReader(csvfile, delimiter=",", quotechar='"', skipinitialspace="true")
+            psc_start_date = row[3].value
+            psc_end_date = row[4].value
+            psc_full_name = row[5].value
+            psc_excludes = row[6].value
+            psc_notes = row[7].value
+            psc_includes = row[8].value
 
-            for row in reader:
-                psc_code = row["PSC CODE"]
-                psc_description = row["PRODUCT AND SERVICE CODE NAME"]
-                psc_length = len(psc_code)
-                psc_start_date = row["START DATE"]
-                psc_end_date = row["END DATE"]
-                psc_full_name = row["PRODUCT AND SERVICE CODE FULL NAME"]
-                psc_excludes = row["PRODUCT AND SERVICE CODE EXCLUDES"]
-                psc_notes = row["PRODUCT AND SERVICE CODE NOTES"]
-                psc_includes = row["PRODUCT AND SERVICE CODE INCLUDES"]
-
-                psc, created = PSC.objects.get_or_create(code=psc_code)
-                psc.description = psc_description
-                psc.length = psc_length
-                if psc_start_date:
-                    if psc.start_date and datetime.strptime(psc_start_date, '%Y-%m-%d').date() >= psc.start_date:
-                        psc.start_date = psc_start_date
-                if psc_end_date:
-                    if psc.end_date and datetime.strptime(psc_end_date, '%Y-%m-%d').date() >= psc.end_date:
-                        psc.end_date = psc_end_date
-                psc.full_name = psc_full_name
-                psc.excludes = psc_excludes
-                psc.notes = psc_notes
-                psc.includes = psc_includes
-                psc.save()
-            if update:
-                update_lengths()
-                logger.log(20, "Updated PSC codes.")
+            psc, created = PSC.objects.get_or_create(code=psc_code)
+            psc.description = psc_description
+            psc.length = psc_length
+            if psc_start_date:
+                if psc.start_date and psc_start_date.date() >= psc.start_date:
+                    psc.start_date = psc_start_date
+            if psc_end_date:
+                if psc.end_date and psc_end_date.date() >= psc.end_date:
+                    psc.end_date = psc_end_date
+            psc.full_name = psc_full_name
+            psc.excludes = psc_excludes
+            psc.notes = psc_notes
+            psc.includes = psc_includes
+            psc.save()
+        if update:
+            update_lengths()
+            logger.log(20, "Updated PSC codes.")
     except IOError:
         logger.error("Could not open file {}".format(fullpath))
 
