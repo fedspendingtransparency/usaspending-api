8abd9d59e4f411dd63198b5228b1961b8434e1e1
Kirk Barden <barden_kirk@bah.com>
2019-10-04 17:44:05 -0400

    dev-3527: added some more fringe tests

--- usaspending_api/references/tests/test_naics.py
+++ usaspending_api/references/tests/test_naics.py
@@ -6,13 +6,13 @@ import json
 @pytest.fixture()
 def naics_test_data():
     mommy.make("references.NAICS", code="11", description="Agriculture, Forestry, Fishing and Hunting")
-    mommy.make("references.NAICS", code="21", description="Mining, Quarrying, and Oil and Gas Extraction")
-    mommy.make("references.NAICS", code="22", description="Utilities")
     mommy.make("references.NAICS", code="1111", description="Oilseed and Grain Farming")
-    mommy.make("references.NAICS", code="1112", description="Vegetable and Melon Farming")
-    mommy.make("references.NAICS", code="111211", description="Potato Farming")
     mommy.make("references.NAICS", code="111110", description="Soybean Farming")
     mommy.make("references.NAICS", code="111120", description="Oilseed (except Soybean) Farming")
+    mommy.make("references.NAICS", code="1112", description="Vegetable and Melon Farming")
+    mommy.make("references.NAICS", code="111211", description="Potato Farming")
+    mommy.make("references.NAICS", code="21", description="Mining, Quarrying, and Oil and Gas Extraction")
+    mommy.make("references.NAICS", code="22", description="Utilities")
 
 
 @pytest.mark.django_db
@@ -47,9 +47,57 @@ def test_with_id(client, naics_test_data):
     ]
     assert resp.data["results"] == expected_data
 
+    # mommy.make("references.NAICS", code="11", description="Agriculture, Forestry, Fishing and Hunting")
+    # mommy.make("references.NAICS", code="1111", description="Oilseed and Grain Farming")
+    # mommy.make("references.NAICS", code="111110", description="Soybean Farming")
+    # mommy.make("references.NAICS", code="111120", description="Oilseed (except Soybean) Farming")
+    # mommy.make("references.NAICS", code="1112", description="Vegetable and Melon Farming")
+    # mommy.make("references.NAICS", code="111211", description="Potato Farming")
+    # mommy.make("references.NAICS", code="21", description="Mining, Quarrying, and Oil and Gas Extraction")
+    # mommy.make("references.NAICS", code="22", description="Utilities")
+
+    resp = client.get("/api/v2/references/naics/1111/")
+    assert resp.status_code == 200
+    expected_data = [
+        {
+            "naics": "1111",
+            "naics_description": "Oilseed and Grain Farming",
+            "count": 2,
+            "children": [
+                {"naics": "111110", "naics_description": "Soybean Farming", "count": 1},
+                {"naics": "111120", "naics_description": "Oilseed (except Soybean) Farming", "count": 1},
+            ],
+        }
+    ]
+    assert resp.data["results"] == expected_data
+
+    resp = client.get("/api/v2/references/naics/111120/")
+    assert resp.status_code == 200
+    expected_data = [{"naics": "111120", "naics_description": "Oilseed (except Soybean) Farming", "count": 1}]
+    assert resp.data["results"] == expected_data
+
+    resp = client.get("/api/v2/references/naics/1/")
+    assert resp.status_code == 200
+    expected_data = []
+    assert resp.data["results"] == expected_data
+
 
 @pytest.mark.django_db
 def test_with_filter(client, naics_test_data):
+
+    resp = client.get("/api/v2/references/naics/?filter=fish")
+    assert resp.status_code == 200
+    expected_data = {
+        "results": [
+            {
+                "naics": "11",
+                "naics_description": "Agriculture, Forestry, Fishing and Hunting",
+                "count": 3,
+                "children": [],
+            }
+        ]
+    }
+    assert json.loads(resp.content.decode("utf-8")) == expected_data
 
     resp = client.get("/api/v2/references/naics/?filter=grain")
     assert resp.status_code == 200
@@ -85,6 +133,61 @@ def test_with_filter(client, naics_test_data):
                             {"naics": "111120", "naics_description": "Oilseed (except Soybean) Farming", "count": 1},
                         ],
                     }
+                ],
+            }
+        ]
+    }
+    assert json.loads(resp.content.decode("utf-8")) == expected_data
+
+    resp = client.get("/api/v2/references/naics/?filter=farming")
+    assert resp.status_code == 200
+    expected_data = {
+        "results": [
+            {
+                "naics": "11",
+                "naics_description": "Agriculture, Forestry, Fishing and Hunting",
+                "count": 3,
+                "children": [
+                    {
+                        "naics": "1111",
+                        "naics_description": "Oilseed and Grain Farming",
+                        "count": 2,
+                        "children": [
+                            {"naics": "111110", "naics_description": "Soybean Farming", "count": 1},
+                            {"naics": "111120", "naics_description": "Oilseed (except Soybean) Farming", "count": 1},
+                        ],
+                    },
+                    {
+                        "naics": "1112",
+                        "naics_description": "Vegetable and Melon Farming",
+                        "count": 1,
+                        "children": [{"naics": "111211", "naics_description": "Potato Farming", "count": 1}],
+                    },
+                ],
+            }
+        ]
+    }
+    assert json.loads(resp.content.decode("utf-8")) == expected_data
+
+    resp = client.get("/api/v2/references/naics/?filter=b")
+    assert resp.status_code == 200
+    expected_data = {
+        "results": [
+            {
+                "naics": "11",
+                "naics_description": "Agriculture, Forestry, Fishing and Hunting",
+                "count": 3,
+                "children": [
+                    {
+                        "naics": "1111",
+                        "naics_description": "Oilseed and Grain Farming",
+                        "count": 2,
+                        "children": [
+                            {"naics": "111110", "naics_description": "Soybean Farming", "count": 1},
+                            {"naics": "111120", "naics_description": "Oilseed (except Soybean) Farming", "count": 1},
+                        ],
+                    },
+                    {"naics": "1112", "naics_description": "Vegetable and Melon Farming", "count": 1, "children": []},
                 ],
             }
         ]
