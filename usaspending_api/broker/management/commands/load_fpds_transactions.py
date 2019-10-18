0f54ffe07c3bf003d5b0ce72810c70af586ebfbd
Brian Zito <zito_brian@bah.com>
2019-10-18 10:41:33 -0400

    dev-3634 stylistic changes to make delete functionality more clear

--- usaspending_api/broker/management/commands/load_fpds_transactions.py
+++ usaspending_api/broker/management/commands/load_fpds_transactions.py
@@ -41,12 +41,18 @@ class Command(BaseCommand):
             db_cursor.execute(db_query)
         return db_cursor
 
-    def load_fpds_from_date(self, date):
+    def load_fpds_incrementally(self, date):
+        """
+        Handles "increment" loads, which includes deleting transactions so long as a date is provided
+        :param date: if provided, loader will load and delete all transactions starting from this date
+        :return: nothing
+        """
         if date is None:
             logger.info("fetching all fpds transactions...")
         else:
             logger.info("fetching fpds transactions since {}...".format(str(date)))
 
+        # First clear any transactions marked for deletion. If the transactions have since been re-added, they will be back in the broker DB and will be re-inserted in the next step
         stale_awards = delete_stale_fpds(date.date())
         self.modified_award_ids.extend(stale_awards)
 
@@ -74,6 +80,11 @@ class Command(BaseCommand):
             yield lines
 
     def load_fpds_from_file(self, file_path):
+        """
+        Loads arbitrary set of ids, WITHOUT checking for deletes.
+        :param file_path:
+        :return:
+        """
         with RetrieveFileFromUri(file_path).get_file_object() as file:
             for next_batch in self.next_file_batch_generator(file):
                 id_list = [int(re.search(r"\d+", x).group()) for x in next_batch]
@@ -114,7 +125,7 @@ class Command(BaseCommand):
         mutually_exclusive_group.add_argument(
             "--reload-all",
             action="store_true",
-            help="Script will load or reload all FPDS records in broker database, from all time",
+            help="Script will load or reload all FPDS records in broker database, from all time. This does NOT clear the USASpending database first",
         )
 
     def handle(self, *args, **options):
@@ -123,10 +134,10 @@ class Command(BaseCommand):
         update_time = datetime.now(timezone.utc)
 
         if options["reload_all"]:
-            self.load_fpds_from_date(None)
+            self.load_fpds_incrementally(None)
 
         elif options["date"]:
-            self.load_fpds_from_date(options["date"])
+            self.load_fpds_incrementally(options["date"])
 
         elif options["ids"]:
             self.modified_award_ids.extend(load_ids(options["ids"]))
@@ -138,7 +149,7 @@ class Command(BaseCommand):
             last_load = get_last_load_date("fpds")
             if not last_load:
                 raise ValueError("No last load date for FPDS stored in the database")
-            self.load_fpds_from_date(last_load)
+            self.load_fpds_incrementally(last_load)
 
         if self.modified_award_ids:
             logger.info("cleaning orphaned metadata")
