bbcda6459c8cea93c6d11f02bbbe075c4e82421a
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2019-10-22 21:15:17 -0600

    [DEV-1666] new source assistance model

--- usaspending_api/transactions/models/__init__.py
+++ usaspending_api/transactions/models/__init__.py
@@ -1,6 +1,8 @@
+from usaspending_api.transactions.models.source_assistance_transaction import SourceAssistanceTransaction
 from usaspending_api.transactions.models.source_procurement_transaction import SourceProcurmentTransaction
 
 
 __all__ = [
+    "SourceAssistanceTransaction",
     "SourceProcurmentTransaction",
 ]
