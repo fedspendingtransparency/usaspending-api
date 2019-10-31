69926580bba68fa6c7423914481d19563759ac5d
Emily Brents <brents_emily@bah.com>
2019-10-31 13:21:15 -0400

    Update urls_awards.py

--- usaspending_api/awards/v2/urls_awards.py
+++ usaspending_api/awards/v2/urls_awards.py
@@ -8,22 +8,15 @@ from usaspending_api.awards.v2.views.count.transaction_count import TransactionC
 from usaspending_api.awards.v2.views.count.subaward_count import SubawardCountRetrieveViewSet
 from usaspending_api.awards.v2.views.count.federal_accounts_count import FederalAccountCountRetrieveViewSet
 
+award_id_regex = "(?P<requested_award>(((CONT|ASST)_(AWD|IDV|NON|AGG)_.+)|([0-9]+)))"
+
 urlpatterns = [
     url(r"^accounts/$", AwardAccountsViewSet.as_view()),
     url(r"^funding/$", AwardFundingViewSet.as_view()),
     url(r"^funding_rollup/$", AwardFundingRollupViewSet.as_view()),
     url(r"^last_updated", AwardLastUpdatedViewSet.as_view()),
-    url(
-        "^count/transaction/(?P<requested_award>(((CONT|ASST)_(AWD|IDV|NON|AGG)_.+)|([0-9]+)))/$",
-        TransactionCountRetrieveViewSet.as_view(),
-    ),
-    url(
-        "^count/subaward/(?P<requested_award>(((CONT|ASST)_(AWD|IDV|NON|AGG)_.+)|([0-9]+)))/$",
-        SubawardCountRetrieveViewSet.as_view(),
-    ),
-    url(
-        "^count/federal_account/(?P<requested_award>(((CONT|ASST)_(AWD|IDV|NON|AGG)_.+)|([0-9]+)))/$",
-        FederalAccountCountRetrieveViewSet.as_view(),
-    ),
-    url("^(?P<requested_award>(((CONT|ASST)_(AWD|IDV|NON|AGG)_.+)|([0-9]+)))/$", AwardRetrieveViewSet.as_view()),
+    url(r"^count/transaction/{}/$".format(award_id_regex), TransactionCountRetrieveViewSet.as_view()),
+    url(r"^count/subaward/{}/$".format(award_id_regex), SubawardCountRetrieveViewSet.as_view()),
+    url(r"^count/federal_account/{}/$".format(award_id_regex), FederalAccountCountRetrieveViewSet.as_view()),
+    url(r"^{}/$".format(award_id_regex), AwardRetrieveViewSet.as_view()),
 ]
