d52c59f5bea70ce65c94c9b0f1c9a773d6eab63b
Brian Zito <zito_brian@bah.com>
2019-11-08 10:22:53 -0500

    added early termination in the event of failure

--- usaspending_api/broker/management/commands/load_fpds_transactions.py
+++ usaspending_api/broker/management/commands/load_fpds_transactions.py
@@ -172,6 +172,7 @@ class Command(BaseCommand):
                     ["{},".format(failure) for failure in failed_ids]
                 )
             )
+            SystemExit(1)
 
         if options["reload_all"] or options["since_last_load"]:
             # we wait until after the load finishes to update the load date because if this crashes we'll need to load again
