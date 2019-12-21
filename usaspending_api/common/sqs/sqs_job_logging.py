cd35cd082e21625a4cd317ccc0004da9a19906d8
Keith Hickey <hickey_keith@bah.com>
2019-12-21 09:06:47 -0500

    DEV-2895 usaspending only picks up logs for 'console' as the logger name. Also adding message_type back in addition too keeping job_type

--- usaspending_api/common/sqs/sqs_job_logging.py
+++ usaspending_api/common/sqs/sqs_job_logging.py
@@ -10,15 +10,18 @@ def log_job_message(
     is_warning=False,
     is_error=False,
     is_exception=False,
-    other_params={},
+    other_params=None,
 ):
     """Handles logging a message about a Bulk Download job, with additional job metadata"""
+    if not other_params:
+        other_params = {}
+
     log_dict = {
-        "message": message,
         "job_type": job_type,
         "job_id": job_id,
         "proc_id": os.getpid(),
         "parent_proc_id": os.getppid(),
+        "message": message,
     }
 
     for param in other_params:
@@ -26,17 +29,17 @@ def log_job_message(
             log_dict[param] = other_params[param]
 
     if is_exception:  # use this when handling an exception to include exception details in log output
-        log_dict["job_type"] = f"{job_type}Error"
+        log_dict["message_type"] = f"{job_type}Error"
         logger.exception(log_dict)
     elif is_error:
-        log_dict["job_type"] = f"{job_type}Error"
+        log_dict["message_type"] = f"{job_type}Error"
         logger.error(log_dict)
     elif is_warning:
-        log_dict["job_type"] = f"{job_type}Warning"
+        log_dict["message_type"] = f"{job_type}Warning"
         logger.warning(log_dict)
     elif is_debug:
-        log_dict["job_type"] = f"{job_type}Debug"
+        log_dict["message_type"] = f"{job_type}Debug"
         logger.debug(log_dict)
     else:
-        log_dict["job_type"] = f"{job_type}Info"
+        log_dict["message_type"] = f"{job_type}Info"
         logger.info(log_dict)
