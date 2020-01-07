23ba0fbaa41b88c8c1087ccb9d43dc33907d4ad6
Keith Hickey <hickey_keith@bah.com>
2020-01-07 15:38:05 -0500

    DEV-2896 Removing use of write-mode to create a zip file as our usage makes multiple calls to append files to the same zip. Adding zip-removal at the start of download generation as a precaution.

--- usaspending_api/download/filestreaming/zip_file.py
+++ usaspending_api/download/filestreaming/zip_file.py
@@ -10,19 +10,10 @@ def append_files_to_zip_file(file_paths, zip_file_path):
     NOTE: If a zip file already exists at zip_file_path, the given files will be added in addition to the ones
     already in the zip when using append (`a`) mode. If that zip contains a file with the same name as one provided,
     it will throw a UserWarning and duplicate the file.
+    Use caution in this case by removing the zip in the finally of an exception and also checking for and removing
+    the zip if it exists before you begin to create it from scratch
     """
     with zipfile.ZipFile(zip_file_path, "a", compression=zipfile.ZIP_DEFLATED, allowZip64=True) as zip_file:
-        for file_path in file_paths:
-            archive_name = os.path.basename(file_path)
-            zip_file.write(file_path, archive_name)
-
-
-def write_files_to_zip_file(file_paths, zip_file_path):
-    """(Re)create a zip archive at the specified zip_file_path, and add all the files at provided file_paths to it.
-
-    NOTE: If a (zip) file already exists at zip_file_path, it will be overwritten in write (`w`) mode.
-    """
-    with zipfile.ZipFile(zip_file_path, "w", compression=zipfile.ZIP_DEFLATED, allowZip64=True) as zip_file:
         for file_path in file_paths:
             archive_name = os.path.basename(file_path)
             zip_file.write(file_path, archive_name)
