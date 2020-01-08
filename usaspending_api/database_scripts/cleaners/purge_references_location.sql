7f9e8fcd7d10336d414f9a785f1934a239d4f0f8
Brian Zito <zito_brian@bah.com>
2020-01-08 09:19:38 -0500

    dev-1669 remove last AND

--- usaspending_api/database_scripts/cleaners/purge_references_location.sql
+++ usaspending_api/database_scripts/cleaners/purge_references_location.sql
@@ -7,7 +7,7 @@ DELETE
 FROM references_location rl
 WHERE (
     NOT EXISTS (SELECT 1 FROM transaction_normalized WHERE place_of_performance_id = rl.location_id) AND
-    NOT EXISTS (SELECT 1 FROM awards WHERE place_of_performance_id = rl.location_id) AND
+    NOT EXISTS (SELECT 1 FROM awards WHERE place_of_performance_id = rl.location_id)
 );
 
 COMMIT;
