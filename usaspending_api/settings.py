3fa623a9390159f1744ad8c357d56a4ba09a26f6
sethstoudenmier <stoudenmier_seth@bah.com>
2020-02-10 10:28:25 -0500

    Add info logging for usaspending-api modules

--- usaspending_api/settings.py
+++ usaspending_api/settings.py
@@ -318,6 +318,8 @@ LOGGING = {
     "loggers": {
         # The root logger; i.e. "all modules"
         "": {"handlers": ["console", "console_file"], "level": "WARNING", "propagate": False},
+        # The "root" logger for all usaspending_api modules
+        "usaspending_api": {"handlers": ["console", "console_file"], "level": "INFO", "propagate": False},
         # Logger for Django API requests via middleware. See logging.py
         "server": {"handlers": ["server"], "level": "INFO", "propagate": False},
         # Catch-all logger (over)used for non-Django-API commands that output to the console
