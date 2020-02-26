7b3cb3a363486371b7f35d87eb46014980ac1c08
Kirk Barden <barden_kirk@bah.com>
2020-02-26 16:26:03 -0500

    [DEV-3932] Revert testss

--- usaspending_api/bulk_download/tests/test_download.py
+++ usaspending_api/bulk_download/tests/test_download.py
@@ -1,8 +1,9 @@
 import json
-import pytest
 
 from model_mommy import mommy
+import pytest
 from rest_framework import status
+
 from usaspending_api.awards.models import TransactionNormalized, TransactionFABS, TransactionFPDS
 from usaspending_api.download.lookups import JOB_STATUS
 from usaspending_api.etl.award_helpers import update_awards
@@ -17,7 +18,6 @@ def award_data(db):
     # Create Awarding Top Agency
     ata1 = mommy.make(
         "references.ToptierAgency",
-        toptier_agency_id=1,
         name="Bureau of Things",
         toptier_code="100",
         website="http://test.com",
@@ -26,7 +26,6 @@ def award_data(db):
     )
     ata2 = mommy.make(
         "references.ToptierAgency",
-        toptier_agency_id=2,
         name="Bureau of Stuff",
         toptier_code="101",
         website="http://test.com",
@@ -35,16 +34,12 @@ def award_data(db):
     )
 
     # Create Awarding subs
-    asa1 = mommy.make("references.SubtierAgency", name="SubBureau of Things")
-    asa2 = mommy.make("references.SubtierAgency", name="SubBureau of Stuff")
+    asa1 = mommy.make("references.SubtierAgency", name="Bureau of Things")
+    asa2 = mommy.make("references.SubtierAgency", name="Bureau of Stuff")
 
     # Create Awarding Agencies
-    aa1 = mommy.make(
-        "references.Agency", toptier_agency=ata1, subtier_agency=asa1, toptier_flag=False, user_selectable=True
-    )
-    aa2 = mommy.make(
-        "references.Agency", toptier_agency=ata2, subtier_agency=asa2, toptier_flag=False, user_selectable=True
-    )
+    aa1 = mommy.make("references.Agency", toptier_agency=ata1, subtier_agency=asa1, toptier_flag=False)
+    aa2 = mommy.make("references.Agency", toptier_agency=ata2, subtier_agency=asa2, toptier_flag=False)
 
     # Create Funding Top Agency
     fta = mommy.make(
@@ -164,31 +159,59 @@ def test_download_status_nonexistent_file_404(client):
     assert resp.status_code == status.HTTP_404_NOT_FOUND
 
 
+def sort_function(agency):
+    return agency["toptier_code"]
+
+
+@pytest.mark.skip
 def test_list_agencies(client, award_data):
     """Test transaction list agencies endpoint"""
+    resp = client.post("/api/v2/bulk_download/list_agencies", content_type="application/json", data=json.dumps({}))
+
+    all_toptiers = [
+        {"name": "Bureau of Things", "toptier_code": "100"},
+        {"name": "Bureau of Stuff", "toptier_code": "101"},
+        {"name": "Bureau of Money", "toptier_code": "102"},
+    ]
+
+    index = 0
+    agency_ids = []
+    for toptier in sorted(resp.json()["agencies"]["other_agencies"], key=sort_function):
+        assert toptier["name"] == all_toptiers[index]["name"]
+        assert toptier["toptier_code"] == all_toptiers[index]["toptier_code"]
+        agency_ids.append(toptier["toptier_agency_id"])
+        index += 1
+    assert resp.json()["sub_agencies"] == []
+    assert resp.json()["federal_accounts"] == []
+
     resp = client.post(
         "/api/v2/bulk_download/list_agencies",
         content_type="application/json",
-        data=json.dumps({"type": "award_agencies"}),
+        data=json.dumps({"agency": agency_ids[0]}),
     )
 
-    assert resp.status_code == status.HTTP_200_OK
-    assert resp.data == {
-        "agencies": {
-            "cfo_agencies": [],
-            "other_agencies": [
-                {"name": "Bureau of Stuff", "toptier_agency_id": 2, "toptier_code": "101"},
-                {"name": "Bureau of Things", "toptier_agency_id": 1, "toptier_code": "100"},
-            ],
-        },
-        "sub_agencies": [],
-    }
+    assert resp.json()["agencies"] == []
+    assert resp.json()["sub_agencies"] == [{"subtier_agency_name": "Bureau of Things", "subtier_agency_id": 1}]
+    assert resp.json()["federal_accounts"] == []
 
     resp = client.post(
         "/api/v2/bulk_download/list_agencies",
         content_type="application/json",
-        data=json.dumps({"type": "award_agencies", "agency": 2}),
+        data=json.dumps({"agency": agency_ids[1]}),
     )
 
-    assert resp.status_code == status.HTTP_200_OK
-    assert resp.data == {"agencies": [], "sub_agencies": [{"subtier_agency_name": "SubBureau of Stuff"}]}
+    assert resp.json()["agencies"] == []
+    assert resp.json()["sub_agencies"] == [{"subtier_agency_name": "Bureau of Stuff", "subtier_agency_id": 2}]
+    assert resp.json()["federal_accounts"] == []
+
+    resp = client.post(
+        "/api/v2/bulk_download/list_agencies",
+        content_type="application/json",
+        data=json.dumps({"agency": agency_ids[2]}),
+    )
+
+    assert resp.json()["agencies"] == []
+    assert resp.json()["sub_agencies"] == [{"subtier_agency_name": "Bureau of Things", "subtier_agency_id": 3}]
+    assert resp.json()["federal_accounts"] == [
+        {"federal_account_name": "Compensation to Accounts", "federal_account_id": 1}
+    ]
