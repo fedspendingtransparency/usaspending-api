40f433e7fbb6e7e4b3a6b043a90e473f920c1d2d
Kirk Barden <barden_kirk@bah.com>
2020-02-27 09:33:01 -0500

    [DEV-3932] Fixed problematic test

--- usaspending_api/database_scripts/tests/test_custom_sql_functions.py
+++ usaspending_api/database_scripts/tests/test_custom_sql_functions.py
@@ -10,45 +10,51 @@ URLENCODE_FUNCTION_NAME = "urlencode"
 
 @pytest.fixture()
 def add_fun_awards(db):
-    mommy.make("awards.award", generated_unique_award_id="CONT_IDV_ABCDEFG_0123456")
-    mommy.make("awards.award", generated_unique_award_id="CONT_IDV_abcdefg_9876543")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_._.._..._....")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_-_--_---_----")
-    mommy.make("awards.award", generated_unique_award_id="ASST_AGG_1008DRCATTHP  01^~@01906470531201403_7022")
-    mommy.make("awards.award", generated_unique_award_id="ASST_AGG_12C30000000000006122970000  121/21000_12C3")
-    mommy.make("awards.award", generated_unique_award_id="ASST_AGG_17.302-MARYLAND-PRINCE GEORGE'S-20081231-10_1635")
-    mommy.make("awards.award", generated_unique_award_id="ASST_NON_30180J015 MOD#2_1448")
-    mommy.make("awards.award", generated_unique_award_id="ASST_NON_5% RECAP_8630")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_GS30FY0027QP0019405Â_4732_GS30FY0027_4732")
-    mommy.make("awards.award", generated_unique_award_id="ASST_NON_R!D1102A37    10_12E2")
-    mommy.make("awards.award", generated_unique_award_id="CONT_IDV_[_]_test")
-    mommy.make("awards.award", generated_unique_award_id="CONT_IDV_(_)_test")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_(())_[[]]_test")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_==_++_test")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_?_??_test")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_^_^^_^^^")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_::_;;_:::;;;")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_,_,,_,,,")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_$_$$_$$$")
-    mommy.make("awards.award", generated_unique_award_id="CONT_AWD_%_%%_%%%%")
-    mommy.make("awards.award", generated_unique_award_id="☰☱☳☲☶☴ ൠൠൠ ☴☶☲☳☱☰")
-    mommy.make("awards.award", generated_unique_award_id="❋❋❋ ALL YOUR BASE ARE BELONG TO US ❋❋❋")
-    mommy.make("awards.award", generated_unique_award_id="⎺╲_❪ツ❫_╱⎺")
-    mommy.make("awards.award", generated_unique_award_id="питон е јазик на компјутер и змија")
-    mommy.make("awards.award", generated_unique_award_id="如果科羅拉多被鋪平會比得克薩斯州大")
-    mommy.make("awards.award", generated_unique_award_id="епстеин се није убио")
-    mommy.make("awards.award", generated_unique_award_id="kjo frazë nuk bën mirëkuptim")
-    mommy.make("awards.award", generated_unique_award_id="何者なにものかによって、爆発物ばくはつぶつが仕掛しかけられたようです。")
+    generated_unique_award_ids = [
+        "CONT_IDV_ABCDEFG_0123456",
+        "CONT_IDV_abcdefg_9876543",
+        "CONT_AWD_._.._..._....",
+        "CONT_AWD_-_--_---_----",
+        "ASST_AGG_1008DRCATTHP  01^~@01906470531201403_7022",
+        "ASST_AGG_12C30000000000006122970000  121/21000_12C3",
+        "ASST_AGG_17.302-MARYLAND-PRINCE GEORGE'S-20081231-10_1635",
+        "ASST_NON_30180J015 MOD#2_1448",
+        "ASST_NON_5% RECAP_8630",
+        "CONT_AWD_GS30FY0027QP0019405Â_4732_GS30FY0027_4732",
+        "ASST_NON_R!D1102A37    10_12E2",
+        "CONT_IDV_[_]_test",
+        "CONT_IDV_(_)_test",
+        "CONT_AWD_(())_[[]]_test",
+        "CONT_AWD_==_++_test",
+        "CONT_AWD_?_??_test",
+        "CONT_AWD_^_^^_^^^",
+        "CONT_AWD_::_;;_:::;;;",
+        "CONT_AWD_,_,,_,,,",
+        "CONT_AWD_$_$$_$$$",
+        "CONT_AWD_%_%%_%%%%",
+        "☰☱☳☲☶☴ ൠൠൠ ☴☶☲☳☱☰",
+        "❋❋❋ ALL YOUR BASE ARE BELONG TO US ❋❋❋",
+        "⎺╲_❪ツ❫_╱⎺",
+        "питон е јазик на компјутер и змија",
+        "如果科羅拉多被鋪平會比得克薩斯州大",
+        "епстеин се није убио",
+        "kjo frazë nuk bën mirëkuptim",
+        "何者なにものかによって、爆発物ばくはつぶつが仕掛しかけられたようです。",
+    ]
+    for id_, generated_unique_award_id in enumerate(generated_unique_award_ids):
+        mommy.make("awards.award", id=id_, generated_unique_award_id=generated_unique_award_id)
 
 
 def test_urlencoding_no_change(add_fun_awards):
-    test_sql = f"SELECT generated_unique_award_id, {URLENCODE_FUNCTION_NAME}(generated_unique_award_id) from awards"
+    test_sql = f"""
+        SELECT      generated_unique_award_id,
+                    {URLENCODE_FUNCTION_NAME}(generated_unique_award_id)
+        from        awards
+        order by    id
+    """
     with connection.cursor() as cursor:
         cursor.execute(test_sql)
         results = cursor.fetchall()
-
-    for i in range(4):
-        print(results[i][0], results[i][1])
 
     for i in range(4):
         assert results[i][0] == results[i][1], "Safe ASCII characters were incorrectly modified!"
