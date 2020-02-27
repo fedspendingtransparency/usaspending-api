a34bf274c9846cd8c26feb3bdf8e877fb4e445f3
Emily Brents <brents_emily@bah.com>
2020-02-27 14:01:44 -0500

    [DEV-4531] add checks

--- usaspending_api/database_scripts/job_archive/dev-4531-fix-hashes.sql
+++
@@ -1,8 +0,0 @@
--- Jira Ticket Number: DEV-4531
--- Expected CLI: psql -v ON_ERROR_STOP=1 -c '\timing' -f dev-4531-fix-hashes.sql $DATABASE_URL
---
--- Purpose: Remove the "b''" byte literal from hashes in the filter_hash table
-
-DELETE FROM filter_hash where id in (SELECT b.id FROM filter_hash a INNER JOIN filter_hash b ON a.hash = trim(both '\''' from trim(leading 'b' from b.hash)) WHERE b.hash LIKE 'b\''%\''');
-
-UPDATE filter_hash SET hash = TRIM(both '\''' from trim(leading 'b' from hash)) WHERE hash LIKE 'b\''%\'''; 
