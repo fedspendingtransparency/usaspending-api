7ab43646499412e5b3b5a7b06f2dbd992b0dc716
Brian Zito <zito_brian@bah.com>
2020-02-28 12:00:24 -0500

    dev-4275 filter tree prints in json

--- usaspending_api/references/v2/views/filter_trees/filter_tree.py
+++ usaspending_api/references/v2/views/filter_trees/filter_tree.py
@@ -10,17 +10,26 @@ class Node:
     count: int
     children: list
 
+    def toJSON(self):
+        return {
+            "name": self.name,
+            "ancestors": self.ancestors,
+            "description": self.description,
+            "count": self.count,
+            "children": self.children,
+        }
+
 
 class FilterTree(metaclass=ABCMeta):
     def basic_search(self, tier1, tier2, tier3) -> list:
         if tier3:
-            return self.tier_three_search(tier3)
+            return [self.construct_node_from_raw(3, data) for data in self.tier_three_search(tier3)]
         elif tier2:
-            return self.tier_two_search(tier2)
+            return [self.construct_node_from_raw(2, data) for data in self.tier_two_search(tier2)]
         elif tier1:
-            return self.tier_one_search(tier1)
+            return [self.construct_node_from_raw(1, data) for data in self.tier_one_search(tier1)]
         else:
-            return self.toptier_search()
+            return [self.construct_node_from_raw(1, data) for data in self.toptier_search()]
 
     @abstractmethod
     def toptier_search(self):
@@ -36,4 +45,8 @@ class FilterTree(metaclass=ABCMeta):
 
     @abstractmethod
     def tier_three_search(self, key):
+        pass
+
+    @abstractmethod
+    def construct_node_from_raw(self, tier: int, data) -> Node:
         pass
--- usaspending_api/references/v2/views/filter_trees/tas.py
+++ usaspending_api/references/v2/views/filter_trees/tas.py
@@ -6,7 +6,7 @@ from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.validator.tinyshield import TinyShield
 
 from usaspending_api.accounts.models import TreasuryAppropriationAccount, FederalAccount
-from usaspending_api.references.v2.views.filter_trees.filter_tree import FilterTree
+from usaspending_api.references.v2.views.filter_trees.filter_tree import Node, FilterTree
 
 
 class TASViewSet(APIView):
@@ -19,16 +19,7 @@ class TASViewSet(APIView):
     @cache_response()
     def get(self, request: Request, tier1: str = None, tier2: str = None, tier3: str = None) -> Response:
         filter_tree = TASFilterTree()
-        if tier3:
-            return Response(f"{filter_tree.basic_search(tier1,tier2,tier3).account_title}")
-        elif tier2:
-            return Response(f"{[tas.tas_rendering_label for tas in filter_tree.basic_search(tier1,tier2,tier3)]}")
-        elif tier1:
-            return Response(f"{[fa.federal_account_code for fa in filter_tree.basic_search(tier1,tier2,tier3)]}")
-        else:
-            return Response(
-                f"{[agency['agency_identifier'] for agency in filter_tree.basic_search(tier1,tier2,tier3)]}"
-            )
+        return Response([elem.toJSON() for elem in filter_tree.basic_search(tier1, tier2, tier3)])
 
 
 class TASFilterTree(FilterTree):
@@ -42,4 +33,20 @@ class TASFilterTree(FilterTree):
         return TreasuryAppropriationAccount.objects.filter(federal_account__federal_account_code=fed_account)
 
     def tier_three_search(self, tas_code):
-        return TreasuryAppropriationAccount.objects.filter(tas_rendering_label=tas_code).first()
+        return TreasuryAppropriationAccount.objects.filter(tas_rendering_label=tas_code)
+
+    def construct_node_from_raw(self, tier: int, data) -> Node:
+        if tier == 0:  # A tier zero search is returning an agency code
+            return Node(name=data["agency_identifier"], ancestors=[], description="dummy", count=-1, children=[])
+        if tier == 1:  # A tier one search is returning a FederalAccount object
+            return Node(
+                name=data.federal_account_code, ancestors=[], description=data.account_title, count=-1, children=[]
+            )
+        if tier == 2:  # A tier two search will be returning a TreasuryAppropriationAccount object
+            return Node(
+                name=data.tas_rendering_label, ancestors=[], description=data.account_title, count=-1, children=[]
+            )
+        if tier == 3:  # A tier three search will be returning a TreasuryAppropriationAccount object
+            return Node(
+                name=data.tas_rendering_label, ancestors=[], description=data.account_title, count=-1, children=[]
+            )
