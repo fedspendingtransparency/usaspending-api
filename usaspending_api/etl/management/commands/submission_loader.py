3ea60a282773a5ca919c454c109442875f8fd970
Kirk Barden <barden_kirk@bah.com>
2020-03-02 08:12:37 -0500

    [UNTICKETED] Add Timer convenience functions

--- usaspending_api/etl/management/commands/submission_loader.py
+++ usaspending_api/etl/management/commands/submission_loader.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 
 from usaspending_api.common.helpers.generic_helper import create_full_time_periods
-from usaspending_api.common.helpers.timing_helpers import Timer
+from usaspending_api.common.helpers.timing_helpers import ConsoleTimer as Timer
 
 logger = logging.getLogger("console")
 
