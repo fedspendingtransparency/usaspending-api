ef1249475bf95dcac6bd1bc89d5fdc21fc70f7b5
Emily Brents <brents_emily@bah.com>
2020-03-06 13:34:10 -0500

    [DEV-4208] add test to check logging

--- usaspending_api/search/tests/integration/spending_by_category/test_elasticsearch_header.py
+++ usaspending_api/search/tests/integration/spending_by_category/test_elasticsearch_header.py
@@ -72,3 +72,47 @@ def elasticsearch_http_header_helper(client, monkeypatch, elasticsearch_transact
     )
     assert resp.status_code == status.HTTP_200_OK
     assert len(logging_statements) == 0, "Expected zero logging statements for Sub Awards without the Header"
+
+
+def test_mirror_requests(client, monkeypatch, elasticsearch_award_index):
+    logging_statements = []
+    monkeypatch.setattr(
+        "usaspending_api.search.v2.views.spending_by_award.logger.info",
+        lambda message: logging_statements.append(message),
+    )
+    monkeypatch.setattr(
+        "usaspending_api.common.experimental_api_flags.logger.warning",
+        lambda message: logging_statements.append(message),
+    )
+    monkeypatch.setattr(
+        "usaspending_api.common.elasticsearch.search_wrappers.AwardSearch._index_name",
+        settings.ES_AWARDS_QUERY_ALIAS_PREFIX,
+    )
+
+    elasticsearch_award_index.update_index()
+
+    # Logging statement is triggered for Prime Awards when Header is present
+    resp = client.post(
+        f"/api/v2/search/spending_by_award/",
+        content_type="application/json",
+        data=json.dumps(
+            {
+                "filters": {
+                    "time_period": [{"start_date": "2007-10-01", "end_date": "2020-09-30"}],
+                    "award_type_codes": ["A", "B", "C", "D"]
+                },
+                "fields": ["Award ID"],
+                "page": 1,
+                "limit": 60,
+                "sort": "Award ID",
+                "order": "desc",
+                "subawards": False
+            }
+        ),
+    )
+    # we should get the ES logging message, and the mirroring logging messages
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(logging_statements) == 1, "Expected one logging statement"
+    assert (
+        logging_statements[0] == """Mirroring inbound request with elasticsearch experimental header.\n\tOriginial Request details: <WSGIRequest: POST '/api/v2/search/spending_by_award/'>\n\tMirrored Request details: \n\t\turl = http://testserver/api/v2/search/spending_by_award/, \n\t\tdata = {"filters": {"time_period": [{"start_date": "2007-10-01", "end_date": "2020-09-30"}], "award_type_codes": ["A", "B", "C", "D"]}, "fields": ["Award ID"], "page": 1, "limit": 60, "sort": "Award ID", "order": "desc", "subawards": false}, \n\t\theaders = {'Cookie': '', 'Content-Length': '233', 'Content-Type': 'application/json', 'X-Experimental-Api': 'elasticsearch'}"""
+    ), "Expected a different logging statement"
