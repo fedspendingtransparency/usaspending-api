c397ec5459300b45f7a49043a8c7fbf1adfacac9
Emily Brents <brents_emily@bah.com>
2020-03-19 09:36:11 -0400

    [DEV-4693]

--- usaspending_api/common/cache_decorator.py
+++ usaspending_api/common/cache_decorator.py
@@ -2,22 +2,27 @@
 import logging
 from rest_framework_extensions.cache.decorators import CacheResponse
 
+from usaspending_api.common.experimental_api_flags import is_experimental_elasticsearch_api
+
 logger = logging.getLogger("console")
 
 
 class CustomCacheResponse(CacheResponse):
     def process_cache_response(self, view_instance, view_method, request, args, kwargs):
+        if is_experimental_elasticsearch_api(request):
+            # bypass cache altogether
+            response = view_method(view_instance, request, *args, **kwargs)
+            response = view_instance.finalize_response(request, response, *args, **kwargs)
+            return response
         key = self.calculate_key(
             view_instance=view_instance, view_method=view_method, request=request, args=args, kwargs=kwargs
         )
         response = None
-        elasticsearch = request.headers.get("X-Experimental-Api") == "elasticsearch"
-        if not elasticsearch:
-            try:
-                response = self.cache.get(key)
-            except Exception:
-                msg = "Problem while retrieving key [{k}] from cache for path:'{p}'"
-                logger.exception(msg.format(k=key, p=str(request.path)))
+        try:
+            response = self.cache.get(key)
+        except Exception:
+            msg = "Problem while retrieving key [{k}] from cache for path:'{p}'"
+            logger.exception(msg.format(k=key, p=str(request.path)))
 
         if not response:
             response = view_method(view_instance, request, *args, **kwargs)
@@ -28,13 +33,12 @@ class CustomCacheResponse(CacheResponse):
             if not response.status_code >= 400 or self.cache_errors:
                 if self.cache_errors:
                     logger.error(self.cache_errors)
-                if not elasticsearch:
-                    try:
-                        self.cache.set(key, response, self.timeout)
-                        response["Cache-Trace"] = "set-cache"
-                    except Exception:
-                        msg = "Problem while writing to cache: path:'{p}' data:'{d}'"
-                        logger.exception(msg.format(p=str(request.path), d=str(request.data)))
+                try:
+                    self.cache.set(key, response, self.timeout)
+                    response["Cache-Trace"] = "set-cache"
+                except Exception:
+                    msg = "Problem while writing to cache: path:'{p}' data:'{d}'"
+                    logger.exception(msg.format(p=str(request.path), d=str(request.data)))
         else:
             response["Cache-Trace"] = "hit-cache"
 
