e6c9646f6f6c1708fba86bb41b30a508a545d2b7
Brian Zito <zito_brian@bah.com>
2020-03-26 15:25:16 -0400

    dev-4624 removed bucketing function

--- usaspending_api/common/helpers/agency_logic_helpers.py
+++ usaspending_api/common/helpers/agency_logic_helpers.py
@@ -1,41 +1,6 @@
 from usaspending_api.download.lookups import CFO_CGACS
-from usaspending_api.references.constants import (
-    DOD_SUBSUMED_CGAC,
-    DOD_CGAC,
-    DOD_ARMED_FORCES_TAS_CGAC_FREC,
-    DHS_CGAC,
-    DHS_SUBSUMED_CGAC,
-)
-from usaspending_api.references.models import ToptierAgency
 
 TOPTIER_FREC_CACHE = None
-
-
-def agency_from_identifiers(cgac, frec):
-    """
-    General logic for what agency to include a given CGAC/FREC combination under, accounting for all rollups.
-    :param cgac:
-    :param frec:
-    :return: string, which is the toptier_code for the correct agency to count this combination under
-    """
-    global TOPTIER_FREC_CACHE
-    # If this function hasn't been called, populate the cache for future reference
-    if not TOPTIER_FREC_CACHE:
-        TOPTIER_FREC_CACHE = [elem["toptier_code"] for elem in ToptierAgency.objects.values("toptier_code")]
-
-    if (cgac, frec) in DOD_ARMED_FORCES_TAS_CGAC_FREC:
-        return DOD_CGAC
-
-    if cgac in DOD_SUBSUMED_CGAC:
-        return DOD_CGAC
-
-    if cgac in DHS_SUBSUMED_CGAC:
-        return DHS_CGAC
-
-    if frec and frec in TOPTIER_FREC_CACHE:
-        return frec
-    else:
-        return cgac
 
 
 def cfo_presentation_order(agency_list):
