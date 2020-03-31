7e3467037fa9a937173fe168dcee9a771c201edf
Brian Zito <zito_brian@bah.com>
2020-03-31 11:07:05 -0400

    added tests

--- usaspending_api/references/tests/integration/filter_tree/conftest.py
+++ usaspending_api/references/tests/integration/filter_tree/conftest.py
@@ -1,4 +1,9 @@
-from usaspending_api.references.tests.integration.filter_tree.tas.tas_data_fixtures import basic_agency, cfo_agencies
+from usaspending_api.references.tests.integration.filter_tree.tas.tas_data_fixtures import (
+    basic_agency,
+    cfo_agencies,
+    non_cfo_agencies,
+    unsupported_agencies,
+)
 
 
-__all__ = ["basic_agency", "cfo_agencies"]
+__all__ = ["basic_agency", "cfo_agencies", "non_cfo_agencies", "unsupported_agencies"]
--- usaspending_api/references/tests/integration/filter_tree/tas/tas_data_fixtures.py
+++ usaspending_api/references/tests/integration/filter_tree/tas/tas_data_fixtures.py
@@ -1,4 +1,5 @@
 from model_mommy import mommy
+from datetime import datetime
 import pytest
 
 from usaspending_api.download.lookups import CFO_CGACS
@@ -17,9 +18,27 @@ def cfo_agencies(db):
     [_complete_agency(int(CFO_CGACS[id])) for id in aribitrary_cfo_cgac_sample]
 
 
+@pytest.fixture
+def non_cfo_agencies(db):
+    for i in range(1, 100):
+        if str(i).zfill(3) not in CFO_CGACS:
+            _complete_agency(i)
+
+
+@pytest.fixture
+def unsupported_agencies(db):
+    for i in range(101, 200):
+        if str(i).zfill(3) not in CFO_CGACS:
+            _unsupported_agency(i)
+
+
 def _complete_agency(id):
     _setup_agency(id)
     _complete_fa(id)
+
+
+def _unsupported_agency(id):
+    _setup_agency(id)
 
 
 def _complete_fa(id):
@@ -38,6 +57,7 @@ def _setup_agency(id):
         toptier_agency_id=id,
         name=f"Agency {str(id).zfill(3)}",
         toptier_code=str(id).zfill(3),
+        create_date=datetime(2010, 1, 1, 12, 0, 0),
     )
 
 
