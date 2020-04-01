64d21adb7c7ed5759c79a05583553722540324b9
sethstoudenmier <stoudenmier_seth@bah.com>
2020-04-01 14:07:33 -0700

    [DEV-4073] Fix a test case

--- usaspending_api/search/tests/integration/spending_by_category/test_spending_by_recipient.py
+++ usaspending_api/search/tests/integration/spending_by_category/test_spending_by_recipient.py
@@ -242,7 +242,12 @@ def test_correct_response(client, monkeypatch, elasticsearch_transaction_index, 
         "limit": 10,
         "page_metadata": {"page": 1, "next": None, "previous": None, "hasNext": False, "hasPrevious": False},
         "results": [
-            {"amount": 5000000.0, "code": None, "name": "MULTIPLE RECIPIENTS", "recipient_id": None},
+            {
+                "amount": 5000000.0,
+                "code": "DUNS Number not provided",
+                "name": "MULTIPLE RECIPIENTS",
+                "recipient_id": None,
+            },
             {"amount": 550000.0, "code": "123456789", "name": None, "recipient_id": None},
             {"amount": 5000.0, "code": "096354360", "name": "MULTIPLE RECIPIENTS", "recipient_id": None},
             {
