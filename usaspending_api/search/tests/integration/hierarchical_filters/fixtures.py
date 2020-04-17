38f45b783b45098ff0efc84b162f67335690b009
Brian Zito <zito_brian@bah.com>
2020-04-17 12:40:28 -0400

    dev-4825 added  more tests

--- usaspending_api/search/tests/integration/hierarchical_filters/fixtures.py
+++ usaspending_api/search/tests/integration/hierarchical_filters/fixtures.py
@@ -2,11 +2,19 @@ import pytest
 from model_mommy import mommy
 from datetime import datetime
 
+from usaspending_api.accounts.models import TreasuryAppropriationAccount
+
+# ensures that tests aren't failing for having the wrong TAS. We trust functionality of tas_rendering_label_to_component_dictionary because it is tested elsewhere
+TAS_STRINGS = ["000-X-0126-000", "010-024-X-8445-002", "012-2000/2000-1231-000"]
+TAS_DICTIONARIES = [
+    TreasuryAppropriationAccount.tas_rendering_label_to_component_dictionary(tas) for tas in TAS_STRINGS
+]
+
 
 @pytest.fixture
 def award_with_tas(db):
     award(db, 1)
-    tas(db, 1)
+    tas(db, 1, 0)
 
 
 def award(db, id):
@@ -26,15 +34,15 @@ def award(db, id):
     mommy.make("awards.TransactionNormalized", id=1000 + 1, award=award, action_date=datetime(2017, 12, 1))
 
 
-def tas(db, award_id):
+def tas(db, award_id, index):
     mommy.make("accounts.FederalAccount", id=1)
     mommy.make(
         "accounts.TreasuryAppropriationAccount",
         treasury_account_identifier=1,
-        agency_id="123",
-        main_account_code="2345",
-        sub_account_code="000",
-        availability_type_code="X",
+        agency_id=TAS_DICTIONARIES[index]["aid"],
+        main_account_code=TAS_DICTIONARIES[index]["main"],
+        sub_account_code=TAS_DICTIONARIES[index]["sub"],
+        availability_type_code=TAS_DICTIONARIES[index]["a"],
         federal_account_id=1,
     )
     mommy.make("awards.FinancialAccountsByAwards", award_id=award_id, treasury_account_id=1)
--- usaspending_api/search/tests/integration/hierarchical_filters/test_tas_filter.py
+++ usaspending_api/search/tests/integration/hierarchical_filters/test_tas_filter.py
@@ -1,16 +1,75 @@
 import json
 import pytest
+from django.conf import settings
 
 from usaspending_api.search.elasticsearch.filters.tas import TasCodes
 from usaspending_api.common.experimental_api_flags import EXPERIMENTAL_API_HEADER, ELASTICSEARCH_HEADER_VALUE
+from usaspending_api.search.tests.integration.hierarchical_filters.fixtures import TAS_DICTIONARIES, TAS_STRINGS
 
 
 @pytest.mark.django_db
-def test_match_from_fa(client, elasticsearch_award_index, award_with_tas):
-    elasticsearch_award_index.update_index()
-    resp = _query_by_tas(client, {"require": [["123-2345"]]})
+def test_match_from_agency(client, monkeypatch, elasticsearch_award_index, award_with_tas):
+    _setup_es(client, monkeypatch, elasticsearch_award_index)
+    resp = _query_by_tas(client, {"require": [[TAS_DICTIONARIES[0]["aid"]]]})
 
     assert resp.json()["results"] == [{"internal_id": 1, "Award ID": "abcdefg", "generated_internal_id": "AWARD_{}"}]
+
+
+@pytest.mark.django_db
+def test_match_from_fa(client, monkeypatch, elasticsearch_award_index, award_with_tas):
+    _setup_es(client, monkeypatch, elasticsearch_award_index)
+    resp = _query_by_tas(
+        client,
+        {"require": [[TAS_DICTIONARIES[0]["aid"], f"{TAS_DICTIONARIES[0]['aid']}-{TAS_DICTIONARIES[0]['main']}"]]},
+    )
+
+    assert resp.json()["results"] == [{"internal_id": 1, "Award ID": "abcdefg", "generated_internal_id": "AWARD_{}"}]
+
+
+@pytest.mark.django_db
+def test_match_from_tas(client, monkeypatch, elasticsearch_award_index, award_with_tas):
+    _setup_es(client, monkeypatch, elasticsearch_award_index)
+    resp = _query_by_tas(
+        client,
+        {
+            "require": [
+                [
+                    TAS_DICTIONARIES[0]["aid"],
+                    f"{TAS_DICTIONARIES[0]['aid']}-{TAS_DICTIONARIES[0]['main']}",
+                    TAS_STRINGS[0],
+                ]
+            ]
+        },
+    )
+
+    assert resp.json()["results"] == [{"internal_id": 1, "Award ID": "abcdefg", "generated_internal_id": "AWARD_{}"}]
+
+
+@pytest.mark.django_db
+def test_non_match_from_tas(client, monkeypatch, elasticsearch_award_index, award_with_tas):
+    _setup_es(client, monkeypatch, elasticsearch_award_index)
+    resp = _query_by_tas(
+        client,
+        {
+            "require": [
+                [
+                    TAS_DICTIONARIES[0]["aid"],
+                    f"{TAS_DICTIONARIES[0]['aid']}-{TAS_DICTIONARIES[0]['main']}",
+                    TAS_STRINGS[0][:-3] + "898",
+                ]
+            ]
+        },
+    )
+
+    assert resp.json()["results"] == []
+
+
+def _setup_es(client, monkeypatch, elasticsearch_award_index):
+    monkeypatch.setattr(
+        "usaspending_api.common.elasticsearch.search_wrappers.AwardSearch._index_name",
+        settings.ES_AWARDS_QUERY_ALIAS_PREFIX,
+    )
+    elasticsearch_award_index.update_index()
 
 
 def _query_by_tas(client, tas):
