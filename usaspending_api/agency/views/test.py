7fc7094cb88b7ef3a712c713750f640f3b77d391
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2020-04-23 10:21:03 -0600

    Moved models

--- usaspending_api/agency/views/test.py
+++ usaspending_api/agency/views/test.py
@@ -3,5 +3,8 @@ from rest_framework.views import APIView
 
 
 class EndpointTest(APIView):
+
+    endpoint_doc = "usaspending_api/api_contracts/contracts/v2/agency/test.md"
+
     def get(self, request):
         return Response({"status": "up"})
