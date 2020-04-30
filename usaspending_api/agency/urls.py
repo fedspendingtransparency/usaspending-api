a42bb8ff11714408e1e3efd5c391dfeb1e3dfdeb
Emily Brents <brents_emily@bah.com>
2020-04-30 10:23:11 -0400

    [DEV-4929] federal account ocunt for agency page

--- usaspending_api/agency/urls.py
+++ usaspending_api/agency/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from usaspending_api.agency.views.program_activity_count import ProgramActivityCount
+from usaspending_api.agency.views.federal_account_count import FederalAccountCount
 
 urlpatterns = [
     url(r"^(?P<code>[0-9]{3,4})/program_activity/count/$", ProgramActivityCount.as_view()),
+    url(r"^(?P<code>[0-9]{3,4})/federal_account/count/$", FederalAccountCount.as_view()),
 ]
