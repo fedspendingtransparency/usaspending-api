def2e0d2e46a7a565fe6926e0eebf823cd94be52
Brian Zito <zito_brian@bah.com>
2020-04-30 21:31:36 -0400

    more test updates

--- usaspending_api/search/tests/integration/hierarchical_filters/test_tas_filter_irregular_formats.py
+++ usaspending_api/search/tests/integration/hierarchical_filters/test_tas_filter_irregular_formats.py
@@ -1,5 +1,4 @@
 import pytest
-import json
 from rest_framework import status
 
 from usaspending_api.search.tests.integration.hierarchical_filters.es_search_test_helpers import (
@@ -8,7 +7,6 @@ from usaspending_api.search.tests.integration.hierarchical_filters.es_search_tes
     query_by_treasury_account_components,
 )
 from usaspending_api.search.tests.integration.hierarchical_filters.tas_fixtures import TAS_DICTIONARIES
-from usaspending_api.search.elasticsearch.filters.tas import TasCodes
 
 
 @pytest.mark.django_db
@@ -65,29 +63,6 @@ def test_tas_filter_is_legacy(client, monkeypatch, elasticsearch_award_index, aw
     resp = query_by_tas(client, [{"main": TAS_DICTIONARIES[0]["main"], "aid": TAS_DICTIONARIES[0]["aid"]}])
 
     assert len(resp.json()["results"]) == 1
-
-
-@pytest.mark.django_db
-def test_not_using_es_header(client, monkeypatch, elasticsearch_award_index, award_with_tas):
-    _setup_es(client, monkeypatch, elasticsearch_award_index)
-    resp = client.post(
-        "/api/v2/search/spending_by_award",
-        content_type="application/json",
-        data=json.dumps(
-            {
-                "subawards": False,
-                "fields": ["Award ID"],
-                "sort": "Award ID",
-                "filters": {
-                    "award_type_codes": ["A", "B", "C", "D"],
-                    TasCodes.underscore_name: {"require": [["011"]]},
-                    "time_period": [{"start_date": "2007-10-01", "end_date": "2020-09-30"}],
-                },
-            }
-        ),
-    )
-
-    assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY, "Failed to return 422 Response"
 
 
 @pytest.mark.django_db
