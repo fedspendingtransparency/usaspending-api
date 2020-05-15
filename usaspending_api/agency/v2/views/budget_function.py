b0c056fc6af5e7604c33d54b31e0141ab0c9053c
Emily Brents <brents_emily@bah.com>
2020-05-15 18:32:00 -0400

    [DEV-4924] remove limit

--- usaspending_api/agency/v2/views/budget_function.py
+++ usaspending_api/agency/v2/views/budget_function.py
@@ -45,7 +45,6 @@ class BudgetFunctionList(ListMixin, AgencyBase):
             {
                 "toptier_code": self.toptier_code,
                 "fiscal_year": self.fiscal_year,
-                "limit": self.pagination.limit,
                 "results": results[self.pagination.lower_limit : self.pagination.upper_limit],
                 "messages": self.standard_response_messages,
                 "page_metadata": page_metadata,
