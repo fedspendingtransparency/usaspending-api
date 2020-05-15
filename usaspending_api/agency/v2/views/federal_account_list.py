77fd4040bbd35482e2781f3d97d393b3ed7f77d0
Emily Brents <brents_emily@bah.com>
2020-05-15 19:18:50 -0400

    Update federal_account_list.py

--- usaspending_api/agency/v2/views/federal_account_list.py
+++ usaspending_api/agency/v2/views/federal_account_list.py
@@ -59,7 +59,7 @@ class FederalAccountList(ListMixin, AgencyBase):
                 "toptier_code": self.toptier_code,
                 "fiscal_year": self.fiscal_year,
                 "page_metadata": page_metadata,
-                "results": results[self.pagination.lower_limit : self.pagination.upper_limit]
+                "results": results[self.pagination.lower_limit : self.pagination.upper_limit],
                 "messages": self.standard_response_messages,
             }
         )
