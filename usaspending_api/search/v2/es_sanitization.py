bcdff648dd511b33fb27da511ebed777e764ca6c
Emily Brents <brents_emily@bah.com>
2020-05-27 09:20:35 -0400

    [HOTFIX] adjust sanitization, change regex

--- usaspending_api/search/v2/es_sanitization.py
+++ usaspending_api/search/v2/es_sanitization.py
@@ -20,7 +20,7 @@ def concat_if_array(data):
 def es_sanitize(input_string):
     """ Escapes reserved elasticsearch characters and removes when necessary """
 
-    processed_string = re.sub(r'([-&!|{}()^~*?:\\/"+\[\]<>])', "", input_string)
+    processed_string = re.sub(r'([&|{}()*?:\\"+\[\]<>])', "", input_string)
     if len(processed_string) != len(input_string):
         msg = "Stripped characters from input string New: '{}' Original: '{}'"
         logger.info(msg.format(processed_string, input_string))
