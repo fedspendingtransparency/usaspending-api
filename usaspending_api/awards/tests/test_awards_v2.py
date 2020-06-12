61935d61833eb0e65ab45443e5cb5aa2407c4e53
Emily Brents <brents_emily@bah.com>
2020-06-12 17:49:21 -0400

    Update test_awards_v2.py

--- usaspending_api/awards/tests/test_awards_v2.py
+++ usaspending_api/awards/tests/test_awards_v2.py
@@ -1100,20 +1100,29 @@ def test_file_c_data(client, awards_and_transactions):
     assert json.loads(resp.content.decode("utf-8"))["account_outlays_by_defc"] == [{"code": "L", "amount": 100.0}]
     assert json.loads(resp.content.decode("utf-8"))["account_obligations_by_defc"] == [{"code": "L", "amount": 100.0}]
 
-    mommy.make("submissions.SubmissionAttributes", pk=2, reporting_fiscal_period=12, reporting_fiscal_year=2020)
+    mommy.make("submissions.SubmissionAttributes", pk=2, reporting_fiscal_period=12, reporting_fiscal_year=2018)
     mommy.make(
         "awards.FinancialAccountsByAwards",
         award_id=1,
-        transaction_obligated_amount=100,
-        gross_outlay_amount_by_award_cpe=100,
+        transaction_obligated_amount=10,
+        gross_outlay_amount_by_award_cpe=10,
         disaster_emergency_fund=defc,
         submission_id=2,
     )
+    mommy.make("submissions.SubmissionAttributes", pk=4, reporting_fiscal_period=10, reporting_fiscal_year=2018)
+    mommy.make(
+        "awards.FinancialAccountsByAwards",
+        award_id=1,
+        transaction_obligated_amount=10,
+        gross_outlay_amount_by_award_cpe=10,
+        disaster_emergency_fund=defc,
+        submission_id=4,
+    )
     assert resp.status_code == status.HTTP_200_OK
-    assert json.loads(resp.content.decode("utf-8"))["total_account_outlay"] == 200.0
-    assert json.loads(resp.content.decode("utf-8"))["total_account_obligation"] == 200.0
-    assert json.loads(resp.content.decode("utf-8"))["account_outlays_by_defc"] == [{"code": "L", "amount": 200.0}]
-    assert json.loads(resp.content.decode("utf-8"))["account_obligations_by_defc"] == [{"code": "L", "amount": 200.0}]
+    assert json.loads(resp.content.decode("utf-8"))["total_account_outlay"] == 110.0
+    assert json.loads(resp.content.decode("utf-8"))["total_account_obligation"] == 110.0
+    assert json.loads(resp.content.decode("utf-8"))["account_outlays_by_defc"] == [{"code": "L", "amount": 110.0}]
+    assert json.loads(resp.content.decode("utf-8"))["account_obligations_by_defc"] == [{"code": "L", "amount": 110.0}]
 
 
 def test_defc(client, awards_and_transactions):
