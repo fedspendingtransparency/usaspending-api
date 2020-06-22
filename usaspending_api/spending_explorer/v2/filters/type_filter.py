b372c3cfe519d810354808eb182abe90da0b2e42
Brian Zito <zito_brian@bah.com>
2020-06-22 16:11:33 -0400

    dev-5050 fix explorer query

--- usaspending_api/spending_explorer/v2/filters/type_filter.py
+++ usaspending_api/spending_explorer/v2/filters/type_filter.py
@@ -51,6 +51,7 @@ def get_unreported_data_obj(
             fiscal_period__gt=((fiscal_quarter - 1) * 3),
             fiscal_period__lte=(fiscal_quarter + 1) * 3,
         )
+        .order_by("fiscal_period")
         .values_list("obligations_incurred_total_cpe", flat=True)
         .first()
     )
