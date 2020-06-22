00e79f59bdf149b6c77938d3c7022c12c1b32c37
Wil Collins <collins_william@bah.com>
2020-06-22 13:22:22 -0400

    [DEV-5279] - Adds dabs_submission_window table

---
+++ usaspending_api/references/models/dabs_submission_window.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class DABSSubmissionWindow(models.Model):
+    id = models.AutoField(primary_key=True)
+    reporting_start = models.DateTimeField()
+    reporting_end = models.DateTimeField()
+    submission_start_date = models.DateTimeField()
+    submission_end_date = models.DateTimeField()
+    certification_end_date = models.DateTimeField()
+    submission_fiscal_year = models.IntegerField()
+    submission_fiscal_quarter = models.IntegerField()
+    submission_fiscal_period = models.IntegerField()
+    is_quarter = models.BooleanField()
+
+    class Meta:
+        managed = True
+        db_table = "dabs_submission_window"
+        unique_together = (
+            "submission_fiscal_year",
+            "submission_fiscal_quarter",
+            "submission_fiscal_period",
+            "is_quarter",
+        )
