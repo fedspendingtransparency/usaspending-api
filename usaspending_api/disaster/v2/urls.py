3d41ae82696dcc5821b3b6b57667985b83981bec
sethstoudenmier <stoudenmier_seth@bah.com>
2020-06-24 15:35:30 -0700

    [DEV-5430] Disaster DEF Code Count endpoint

--- usaspending_api/disaster/v2/urls.py
+++ usaspending_api/disaster/v2/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from usaspending_api.disaster.v2.views.agency.count import AgencyCountViewSet
+from usaspending_api.disaster.v2.views.def_code.count import DefCodeCountViewSet
 from usaspending_api.disaster.v2.views.federal_account.count import FederalAccountCountViewSet
 from usaspending_api.disaster.v2.views.spending import ExperimentalDisasterViewSet
 
 urlpatterns = [
     url(r"^agency/count/$", AgencyCountViewSet.as_view()),
+    url(r"^def_code/count/$", DefCodeCountViewSet.as_view()),
     url(r"^federal_account/count/$", FederalAccountCountViewSet.as_view()),
     url(r"^spending/$", ExperimentalDisasterViewSet.as_view()),
 ]
