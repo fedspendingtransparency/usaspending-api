31d1a2eccf7fdde93a773f66a16e885602f230cd
sethstoudenmier <stoudenmier_seth@bah.com>
2020-06-24 14:56:56 -0700

    [DEV-5429] Disaster Object Class count

--- usaspending_api/disaster/v2/urls.py
+++ usaspending_api/disaster/v2/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from usaspending_api.disaster.v2.views.federal_account.count import FederalAccountCountViewSet
+from usaspending_api.disaster.v2.views.object_class.count import ObjectClassCountViewSet
 from usaspending_api.disaster.v2.views.spending import ExperimentalDisasterViewSet
 
 urlpatterns = [
     url(r"^federal_account/count/$", FederalAccountCountViewSet.as_view()),
+    url(r"^object_class/count/$", ObjectClassCountViewSet.as_view()),
     url(r"^spending/$", ExperimentalDisasterViewSet.as_view()),
 ]
