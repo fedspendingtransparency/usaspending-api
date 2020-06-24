f946e3d22aad9c6795ba2df7dc6748143242a6d6
sethstoudenmier <stoudenmier_seth@bah.com>
2020-06-24 15:39:39 -0700

    [DEV-5429] Update test name

--- usaspending_api/disaster/tests/integration/test_disaster_object_class_count.py
+++ usaspending_api/disaster/tests/integration/test_disaster_object_class_count.py
@@ -6,7 +6,7 @@ url = "/api/v2/disaster/object_class/count/"
 
 
 @pytest.mark.django_db
-def test_federal_account_count_success(client, monkeypatch, disaster_account_data, helpers):
+def test_object_class_count_success(client, monkeypatch, disaster_account_data, helpers):
     helpers.patch_datetime_now(monkeypatch, 2022, 12, 31)
 
     resp = helpers.post_for_count_endpoint(client, url, ["L", "M", "N", "O", "P"])
@@ -27,21 +27,21 @@ def test_federal_account_count_success(client, monkeypatch, disaster_account_dat
 
 
 @pytest.mark.django_db
-def test_federal_account_count_invalid_defc(client, disaster_account_data, helpers):
+def test_object_class_count_invalid_defc(client, disaster_account_data, helpers):
     resp = helpers.post_for_count_endpoint(client, url, ["ZZ"])
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
     assert resp.data["detail"] == "Field 'filter|def_codes' is outside valid values ['L', 'M', 'N', 'O', 'P', '9']"
 
 
 @pytest.mark.django_db
-def test_federal_account_count_invalid_defc_type(client, disaster_account_data, helpers):
+def test_object_class_count_invalid_defc_type(client, disaster_account_data, helpers):
     resp = helpers.post_for_count_endpoint(client, url, "100")
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
     assert resp.data["detail"] == "Invalid value in 'filter|def_codes'. '100' is not a valid type (array)"
 
 
 @pytest.mark.django_db
-def test_federal_account_count_missing_defc(client, disaster_account_data, helpers):
+def test_object_class_count_missing_defc(client, disaster_account_data, helpers):
     resp = helpers.post_for_count_endpoint(client, url)
     assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert resp.data["detail"] == "Missing value: 'filter|def_codes' is a required field"
