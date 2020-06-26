003cd3c5a3dfc881f8a71fce9977ad3c4e68d70f
Emily Brents <brents_emily@bah.com>
2020-06-26 15:58:27 -0400

    [DEV-5136] update closed period sql

--- usaspending_api/awards/v2/data_layer/sql.py
+++ usaspending_api/awards/v2/data_layer/sql.py
@@ -12,13 +12,14 @@ defc_sql = """
         where {award_id_sql} and faba.disaster_emergency_fund_code is not null
     ),
     closed_periods as (
-        select
-            distinct concat(w.submission_fiscal_year::text, lpad(w.submission_fiscal_month::text, 2, '0')) AS fyp,
-            w.submission_fiscal_year,
-            w.submission_fiscal_month
-        from dabs_submission_window_schedule w
-        where w.submission_reveal_date <= now() -- change "period end" with "window close"
-        order by w.submission_fiscal_year desc, w.submission_fiscal_month desc
+    select 
+        w.submission_fiscal_year,
+        w.submission_fiscal_month,
+        w.is_quarter
+    from dabs_submission_window_schedule w
+    where
+        w.submission_reveal_date <= now() -- change "period end" with "window close"
+    order by w.submission_fiscal_year desc, w.submission_fiscal_month desc
     ),
     fy_final_outlay_balances as (
         -- Rule: If a balance is not zero at the end of the year, it must be reported in the
@@ -34,26 +35,33 @@ defc_sql = """
     ),
     current_fy_outlay_balance as (
         select
+            faba.disaster_emergency_fund_code,
             faba.reporting_fiscal_year,
             faba.reporting_fiscal_period,
-            faba.disaster_emergency_fund_code,
             sum(faba.gross_outlay_amount_by_award_cpe) as current_fy_outlay
         from eligible_file_c_records faba
+        where
+            faba.reporting_fiscal_period != 12 -- don't duplicate the year-end period's value if in unclosed period 01 or 02 (since there is no P01 submission)
+            and (faba.reporting_fiscal_year, faba.reporting_fiscal_period) in (
+            -- Most recent closed period
+                select c.submission_fiscal_year, c.submission_fiscal_month 
+                from closed_periods c
+                order by c.submission_fiscal_year desc, c.submission_fiscal_month desc
+                limit 1
+            )
+            and (faba.gross_outlay_amount_by_award_cpe is not null and faba.gross_outlay_amount_by_award_cpe != 0) -- negative or positive balance
         group by
+            faba.disaster_emergency_fund_code,
             faba.reporting_fiscal_year,
-            faba.reporting_fiscal_period,
-            faba.disaster_emergency_fund_code
-        having concat(faba.reporting_fiscal_year::text, lpad(faba.reporting_fiscal_period::text, 2, '0')) in
-        (select max(fyp) from closed_periods) and sum(faba.gross_outlay_amount_by_award_cpe) > 0
+            faba.reporting_fiscal_period
     )
     select
         faba.disaster_emergency_fund_code,
-        coalesce(ffy.prior_fys_outlay, 0) + coalesce(cfy.current_fy_outlay, 0) as total_outlay,
-        coalesce(sum(faba.transaction_obligated_amount), 0) as obligated_amount
+        coalesce(sum(faba.transaction_obligated_amount), 0) as obligated_amount,
+        coalesce(sum(ffy.prior_fys_outlay), 0) + coalesce(sum(cfy.current_fy_outlay), 0) as total_outlay
     from eligible_file_c_records faba
     left join fy_final_outlay_balances ffy on ffy.disaster_emergency_fund_code = faba.disaster_emergency_fund_code
-    left join current_fy_outlay_balance cfy
-        on cfy.reporting_fiscal_period != 12 -- don't duplicate the year-end period's value if in unclosed period 01
-        and cfy.disaster_emergency_fund_code = faba.disaster_emergency_fund_code
-    group by faba.disaster_emergency_fund_code, total_outlay;
+    left join current_fy_outlay_balance cfy on cfy.disaster_emergency_fund_code = faba.disaster_emergency_fund_code
+    group by faba.disaster_emergency_fund_code
+    order by obligated_amount desc
     """
