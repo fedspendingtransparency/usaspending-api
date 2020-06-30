e0afabaff366a408b7e7130aadd82e5eeca091e8
sethstoudenmier <stoudenmier_seth@bah.com>
2020-06-30 13:22:16 -0700

    Fix Disaster Endpoints to Accomodate Data

--- usaspending_api/disaster/tests/integration/test_disaster_agency_count.py
+++ usaspending_api/disaster/tests/integration/test_disaster_agency_count.py
@@ -27,21 +27,24 @@ def test_agency_count_success(client, monkeypatch, disaster_account_data, helper
 
 
 @pytest.mark.django_db
-def test_agency_count_invalid_defc(client, disaster_account_data, helpers):
+def test_agency_count_invalid_defc(client, monkeypatch, disaster_account_data, helpers):
+    helpers.patch_datetime_now(monkeypatch, 2022, 12, 31)
     resp = helpers.post_for_count_endpoint(client, url, ["ZZ"])
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
     assert resp.data["detail"] == "Field 'filter|def_codes' is outside valid values ['9', 'L', 'M', 'N', 'O', 'P']"
 
 
 @pytest.mark.django_db
-def test_agency_count_invalid_defc_type(client, disaster_account_data, helpers):
+def test_agency_count_invalid_defc_type(client, monkeypatch, disaster_account_data, helpers):
+    helpers.patch_datetime_now(monkeypatch, 2022, 12, 31)
     resp = helpers.post_for_count_endpoint(client, url, "100")
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
     assert resp.data["detail"] == "Invalid value in 'filter|def_codes'. '100' is not a valid type (array)"
 
 
 @pytest.mark.django_db
-def test_agency_count_missing_defc(client, disaster_account_data, helpers):
+def test_agency_count_missing_defc(client, monkeypatch, disaster_account_data, helpers):
+    helpers.patch_datetime_now(monkeypatch, 2022, 12, 31)
     resp = helpers.post_for_count_endpoint(client, url)
     assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert resp.data["detail"] == "Missing value: 'filter|def_codes' is a required field"
