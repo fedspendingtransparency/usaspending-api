5a78721a6304fbfcc6e12fa401730752fac2d0c4
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2020-07-02 08:52:19 -0600

    [DEV-5297] cleaned up some view logic to make room for ES

--- usaspending_api/disaster/v2/views/spending_by_geography.py
+++ usaspending_api/disaster/v2/views/spending_by_geography.py
@@ -1,35 +1,14 @@
-from typing import Optional, List, Dict
-from django.db.models import Q, Sum, Count, F, Value, DecimalField, Case, When, OuterRef, Exists
-from django.db.models.functions import Coalesce
-from rest_framework.response import Response
-from rest_framework.request import Request
-from enum import Enum
 from decimal import Decimal
+from enum import Enum
+from rest_framework.request import Request
+from rest_framework.response import Response
+from typing import Optional, List, Dict
 
-from usaspending_api.references.models import PopCounty, PopCongressionalDistrict
-from usaspending_api.references.abbreviations import code_to_state, fips_to_code, pad_codes
 from usaspending_api.common.cache_decorator import cache_response
-from usaspending_api.disaster.v2.views.disaster_base import DisasterBase
-from usaspending_api.search.models import LoanAwardSearchMatview
 from usaspending_api.common.validator import TinyShield
-from usaspending_api.awards.models import FinancialAccountsByAwards
-from usaspending_api.awards.v2.filters.location_filter_geocode import geocode_filter_locations
-
-
-def submission_window_cutoff(min_date, monthly_sub, quarterly_sub):
-    return [
-        Q(submission__reporting_period_start__gte=min_date),
-        Q(
-            Q(
-                Q(submission__quarter_format_flag=False)
-                & Q(submission__reporting_period_end__lte=monthly_sub["submission_reveal_date"])
-            )
-            | Q(
-                Q(submission__quarter_format_flag=True)
-                & Q(submission__reporting_period_end__lte=quarterly_sub["submission_reveal_date"])
-            )
-        ),
-    ]
+from usaspending_api.disaster.v2.views.disaster_base import DisasterBase
+from usaspending_api.references.abbreviations import code_to_state, fips_to_code, pad_codes
+from usaspending_api.references.models import PopCounty, PopCongressionalDistrict
 
 
 class GeoLayer(Enum):
@@ -57,6 +36,7 @@ class GeoLayer(Enum):
 #     def __init__(self, type: GeoLayer):
 #         if self.type == GeoLayer.STATE:
 #             pass
+
 
 
 class SpendingByGeographyViewSet(DisasterBase):
@@ -98,21 +78,37 @@ class SpendingByGeographyViewSet(DisasterBase):
 
         self.filters.update(TinyShield(models).block(self.request.data))
 
-        geo_filters = Q()
+        # ======================================================================
+        # Advanced Search's Spending By Geography
+        #     usaspending-api/usaspending_api/search/v2/views/spending_by_geography.py
+        # Elasticsearch helpers
+        #     usaspending-api/usaspending_api/common/query_with_filters.py
 
+        # self.source_fields contains which ES document fields are necessary for the API response
+
+        # ======================================================================
+
+        # Set which field will be the aggregation amount
+        if self.filters["spending_type"] == "face_value_of_loan":
+            pass
+        elif self.filters["spending_type"] == "obligation":
+            pass
+        else:  # spending_type == "outlay"
+            pass
+
+        return Response({"results": self.assemble_results()})
+
+    @property
+    def source_fields(self):
         if self.filters["geo_layer"] == GeoLayer.STATE.value:
             if self.filters["geo_layer_filters"]:
-                geo_filters = Q(recipient_location_state_code__in=self.filters["geo_layer_filters"])
-            values = ["recipient_location_state_code"]
+                pass
+            fields = ["recipient_location_state_code"]
 
         elif self.filters["geo_layer"] == GeoLayer.COUNTY.value:
             if self.filters["geo_layer_filters"]:
-                geo_layers_list = [
-                    {"state": fips_to_code.get(x[:2]), self.filters["geo_layer"]: x[2:], "country": "USA"}
-                    for x in self.filters["geo_layer_filters"]
-                ]
-            geo_filters = geocode_filter_locations("recipient_location", geo_layers_list)
-            values = [
+                pass
+            fields = [
                 "recipient_location_state_code",
                 "recipient_location_county_code",
                 "recipient_location_county_name",
@@ -120,23 +116,12 @@ class SpendingByGeographyViewSet(DisasterBase):
 
         else:  # GeoLayer.DISTRICT.value
             if self.filters["geo_layer_filters"]:
-                geo_layers_list = [
-                    {"state": fips_to_code.get(x[:2]), self.filters["geo_layer"]: x[2:], "country": "USA"}
-                    for x in self.filters["geo_layer_filters"]
-                ]
-                geo_filters = geocode_filter_locations("recipient_location", geo_layers_list)
-            values = ["recipient_location_state_code", "recipient_location_congressional_code"]
+                pass
+            fields = ["recipient_location_state_code", "recipient_location_congressional_code"]
 
-        if self.filters["spending_type"] == "loan":
-            self.queryset = self.loan_queryset.filter(geo_filters).values(*values, "amount")
-        else:
-            raise NotImplementedError
+        return fields
 
-        from usaspending_api.common.helpers.orm_helpers import generate_raw_quoted_query
-
-        print(f"=======================================\nQueryset: {generate_raw_quoted_query(self.queryset)}")
-        self.results = list(self.queryset)
-
+    def assemble_results(self):
         if self.filters["geo_layer"] == GeoLayer.STATE.value:
             results = self.state_results("recipient_location_state_code")
         elif self.filters["geo_layer"] == GeoLayer.COUNTY.value:
@@ -144,7 +129,7 @@ class SpendingByGeographyViewSet(DisasterBase):
         else:
             results = self.district_results("recipient_location_state_code")
 
-        return Response({"results": results})
+        return results
 
     def state_results(self, loc_lookup: str) -> List[dict]:
         state_pop_rows = PopCounty.objects.filter(county_number="000").values()
@@ -230,46 +215,3 @@ class SpendingByGeographyViewSet(DisasterBase):
             )
 
         return results
-
-    @property
-    def loan_queryset(self):
-        filters = [
-            Q(award_id=OuterRef("award_id")),
-            Q(disaster_emergency_fund__in=self.def_codes),
-            Q(award_id__isnull=False),
-        ]
-        filters.extend(
-            submission_window_cutoff(
-                self.reporting_period_min,
-                self.last_closed_monthly_submission_dates,
-                self.last_closed_quarterly_submission_dates,
-            )
-        )
-
-        annotations = {
-            "amount": Coalesce(Sum("face_value_loan_guarantee"), 0),
-            "district": F("recipient_location_congressional_code"),
-            "county": F("recipient_location_county_code"),
-            "state": F("recipient_location_state_code"),
-        }
-        # fields = [
-        #     "recipient_location_congressional_code",
-        #     "recipient_location_country_code",
-        #     "recipient_location_state_code",
-        #     "recipient_location_county_code",
-        #     "recipient_location_county_name",
-        # ]
-
-        return (
-            LoanAwardSearchMatview.objects.annotate(
-                include=Exists(FinancialAccountsByAwards.objects.filter(*filters).values("award_id"))
-            )
-            .filter(include=True, recipient_location_country_code="USA")
-            .values(
-                "recipient_location_congressional_code",
-                "recipient_location_county_code",
-                "recipient_location_state_code",
-            )
-            .annotate(**annotations)
-            .values(*annotations.keys())
-        )
