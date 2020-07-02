f243b032cb530c8993c96de24cc2be471ef9863e
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2020-07-01 21:37:22 -0600

    [DEV-5546] added and cleaned up tests

--- usaspending_api/disaster/tests/conftest.py
+++ usaspending_api/disaster/tests/conftest.py
@@ -1,4 +1,5 @@
 from usaspending_api.disaster.tests.fixtures.disaster_account_data import disaster_account_data
+from usaspending_api.disaster.tests.fixtures.federal_account_data import account_data
 from usaspending_api.disaster.tests.fixtures.helpers import helpers
 from usaspending_api.disaster.tests.fixtures.overview_data import (
     basic_ref_data,
@@ -22,23 +23,24 @@ from usaspending_api.disaster.tests.fixtures.overview_data import (
 
 
 __all__ = [
+    "account_data",
+    "basic_faba",
     "basic_ref_data",
-    "partially_completed_year",
-    "disaster_account_data",
-    "helpers",
     "defc_codes",
-    "late_gtas",
-    "quarterly_gtas",
+    "disaster_account_data",
     "early_gtas",
+    "faba_with_non_covid_values",
+    "faba_with_values",
+    "helpers",
+    "late_gtas",
+    "multi_period_faba",
+    "multi_year_faba",
     "non_covid_gtas",
-    "unobligated_balance_gtas",
     "other_budget_authority_gtas",
+    "partially_completed_year",
+    "quarterly_gtas",
+    "unobligated_balance_gtas",
     "year_2_gtas_covid",
     "year_2_gtas_covid_2",
     "year_2_gtas_non_covid",
-    "basic_faba",
-    "faba_with_values",
-    "faba_with_non_covid_values",
-    "multi_year_faba",
-    "multi_period_faba",
 ]
