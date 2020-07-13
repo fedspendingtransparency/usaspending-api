bb11df2527dbd7099b83fc5328341622642c9993
sethstoudenmier <stoudenmier_seth@bah.com>
2020-07-12 20:49:18 -0700

    [DEV-5301] Copy-paste cleanup

--- usaspending_api/disaster/tests/integration/test_recipient_spending.py
+++ usaspending_api/disaster/tests/integration/test_recipient_spending.py
@@ -181,7 +181,7 @@ def test_correct_response_with_award_type_codes(
 
 
 @pytest.mark.django_db
-def test_recipient_loans_invalid_defc(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
+def test_invalid_defc(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
     setup_elasticsearch_test(monkeypatch, elasticsearch_award_index)
 
     resp = helpers.post_for_spending_endpoint(client, url, def_codes=["ZZ"])
@@ -190,9 +190,7 @@ def test_recipient_loans_invalid_defc(client, monkeypatch, helpers, elasticsearc
 
 
 @pytest.mark.django_db
-def test_recipient_loans_invalid_defc_type(
-    client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions
-):
+def test_invalid_defc_type(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
     setup_elasticsearch_test(monkeypatch, elasticsearch_award_index)
 
     resp = helpers.post_for_spending_endpoint(client, url, def_codes="100")
@@ -201,7 +199,7 @@ def test_recipient_loans_invalid_defc_type(
 
 
 @pytest.mark.django_db
-def test_recipient_loans_missing_defc(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
+def test_missing_defc(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
     setup_elasticsearch_test(monkeypatch, elasticsearch_award_index)
 
     resp = helpers.post_for_spending_endpoint(client, url)
