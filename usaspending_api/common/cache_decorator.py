b88bbef3ef3b893ab56c799ff12f04f9abef6e0c
Kirk Barden <barden_kirk@bah.com>
2020-07-24 13:11:42 -0400

    [UNTICKETED] Band-Aid (tm) for caching issue

--- usaspending_api/common/cache_decorator.py
+++ usaspending_api/common/cache_decorator.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
-from rest_framework_extensions.cache.decorators import CacheResponse
 
+from django.conf import settings
+from django.db.models import QuerySet
+from rest_framework_extensions.cache.decorators import CacheResponse
 from usaspending_api.common.experimental_api_flags import is_experimental_elasticsearch_api
 
 logger = logging.getLogger("console")
@@ -28,8 +30,33 @@ class CustomCacheResponse(CacheResponse):
         if not response:
             response = view_method(view_instance, request, *args, **kwargs)
             response = view_instance.finalize_response(request, response, *args, **kwargs)
+
+            # THIS IS A STOPGAP UNTIL WE GET A CHANCE TO FIX ALL OF THE OFFENDING ENDPOINTS.
+            # Subqueries with OuterRefs cannot be pickled currently.  Rather than attempt to locate
+            # every single instance of a response meeting this criteria we will force QuerySet
+            # materialization by simply replacing {"results": QuerySet} with {"results": List}.
+            # This will not solve every case, but should catch the bulk of them.  Any stragglers
+            # will need to be fixed by hand...
+            if (
+                response
+                and not response.is_rendered
+                and response.data
+                and isinstance(response.data, dict)
+                and "results" in response.data
+                and isinstance(response.data["results"], QuerySet)
+            ):
+                if settings.IS_LOCAL:
+                    raise RuntimeError(
+                        "Your view is returning a QuerySet.  QuerySets are not really designed to "
+                        "be pickled and can cause caching issues.  Please materialize the QuerySet "
+                        "using a List or some other more primitive data structure.  Thank you, have "
+                        "a lovely day, and don't forget to wash your hands regularly!"
+                    )
+                else:
+                    response.data["results"] = list(response.data["results"])
+
+            response.render()  # should be rendered, before pickling while storing to cache
             response["Cache-Trace"] = "no-cache"
-            response.render()  # should be rendered, before picklining while storing to cache
 
             if not response.status_code >= 400 or self.cache_errors:
                 if self.cache_errors:
