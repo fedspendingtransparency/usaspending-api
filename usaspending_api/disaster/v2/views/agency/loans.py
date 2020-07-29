c50ba8815fc478d61535f80b7695f64070a4c976
Kirk Barden <barden_kirk@bah.com>
2020-07-29 11:33:36 -0400

    [DEV-5746] Fix loan calculation

--- usaspending_api/disaster/v2/views/agency/loans.py
+++ usaspending_api/disaster/v2/views/agency/loans.py
@@ -1,13 +1,14 @@
 import json
 import logging
-from decimal import Decimal
-from typing import List
 
+from decimal import Decimal
 from django.contrib.postgres.fields import ArrayField
 from django.db.models import Q, Sum, F, Value, Case, When, IntegerField, Subquery, OuterRef, Count
 from django.db.models.functions import Coalesce
 from django.views.decorators.csrf import csrf_exempt
+from django_cte import With
 from rest_framework.response import Response
+from typing import List
 from usaspending_api.awards.models import FinancialAccountsByAwards
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.helpers.generic_helper import get_pagination_metadata
@@ -16,12 +17,13 @@ from usaspending_api.disaster.v2.views.disaster_base import (
     LoansPaginationMixin,
     LoansMixin,
     FabaOutlayMixin,
+    final_submissions_for_all_fy,
 )
 from usaspending_api.disaster.v2.views.elasticsearch_base import (
     ElasticsearchDisasterBase,
     ElasticsearchLoansPaginationMixin,
 )
-from usaspending_api.references.models import Agency
+from usaspending_api.references.models import Agency, ToptierAgency
 
 logger = logging.getLogger(__name__)
 
@@ -71,47 +73,94 @@ class LoansByAgencyViewSet(LoansPaginationMixin, LoansMixin, FabaOutlayMixin, Di
 
     @property
     def queryset(self):
-        filters = [
-            Q(treasury_account__funding_toptier_agency__isnull=False),
-            Q(treasury_account__isnull=False),
-            self.all_closed_defc_submissions,
-            self.is_in_provided_def_codes,
-            self.is_loan_award,
-        ]
+
+        base_values = With(
+            FinancialAccountsByAwards.objects.filter(
+                Q(treasury_account__isnull=False),
+                self.all_closed_defc_submissions,
+                self.is_in_provided_def_codes,
+                self.is_loan_award,
+            )
+            .annotate(
+                funding_toptier_agency_id=F("treasury_account__funding_toptier_agency_id"),
+                total_loan_value=F("award__total_loan_value"),
+                reporting_fiscal_year=F("submission__reporting_fiscal_year"),
+                reporting_fiscal_period=F("submission__reporting_fiscal_period"),
+                quarter_format_flag=F("submission__quarter_format_flag"),
+            )
+            .values(
+                "funding_toptier_agency_id",
+                "financial_accounts_by_awards_id",
+                "award_id",
+                "transaction_obligated_amount",
+                "gross_outlay_amount_by_award_cpe",
+                "reporting_fiscal_year",
+                "reporting_fiscal_period",
+                "quarter_format_flag",
+                "total_loan_value",
+            ),
+            "base_values",
+        )
+
+        q = Q()
+        for sub in final_submissions_for_all_fy():
+            q |= (
+                Q(reporting_fiscal_year=sub.fiscal_year)
+                & Q(quarter_format_flag=sub.is_quarter)
+                & Q(reporting_fiscal_period=sub.fiscal_period)
+            )
+
+        aggregate_faba = With(
+            base_values.queryset()
+            .values("funding_toptier_agency_id")
+            .annotate(
+                obligation=Coalesce(Sum("transaction_obligated_amount"), 0),
+                outlay=Coalesce(Sum(Case(When(q, then=F("gross_outlay_amount_by_award_cpe")), default=Value(0),)), 0,),
+            )
+            .values("funding_toptier_agency_id", "obligation", "outlay"),
+            "aggregate_faba",
+        )
+
+        distinct_awards = With(
+            base_values.queryset().values("funding_toptier_agency_id", "award_id", "total_loan_value").distinct(),
+            "distinct_awards",
+        )
+
+        aggregate_awards = With(
+            distinct_awards.queryset()
+            .values("funding_toptier_agency_id")
+            .annotate(award_count=Count("award_id"), face_value_of_loan=Coalesce(Sum("total_loan_value"), 0))
+            .values("funding_toptier_agency_id", "award_count", "face_value_of_loan"),
+            "aggregate_awards",
+        )
 
         annotations = {
             "id": Subquery(
-                Agency.objects.filter(toptier_agency=OuterRef("treasury_account__funding_toptier_agency"))
+                Agency.objects.filter(toptier_agency=OuterRef("toptier_agency_id"))
                 .order_by("-toptier_flag", "id")
                 .values("id")[:1]
             ),
-            "code": F("treasury_account__funding_toptier_agency__toptier_code"),
-            "description": F("treasury_account__funding_toptier_agency__name"),
-            # Currently, this endpoint can never have children.
+            "code": F("toptier_code"),
+            "description": F("name"),
+            # Currently, this flavor of the endpoint can never have children
             "children": Value([], output_field=ArrayField(IntegerField())),
-            "award_count": Count("award_id", distinct=True),
-            "obligation": Coalesce(Sum("transaction_obligated_amount"), 0),
-            "outlay": Coalesce(
-                Sum(
-                    Case(
-                        When(self.final_period_submission_query_filters, then=F("gross_outlay_amount_by_award_cpe")),
-                        default=Value(0),
-                    )
-                ),
-                0,
-            ),
-            "face_value_of_loan": Coalesce(Sum("award__total_loan_value"), 0),
+            "award_count": aggregate_awards.col.award_count,
+            "obligation": aggregate_faba.col.obligation,
+            "outlay": aggregate_faba.col.outlay,
+            "face_value_of_loan": aggregate_awards.col.face_value_of_loan,
         }
 
         return (
-            FinancialAccountsByAwards.objects.filter(*filters)
-            .values(
-                "treasury_account__funding_toptier_agency",
-                "treasury_account__funding_toptier_agency__toptier_code",
-                "treasury_account__funding_toptier_agency__name",
+            aggregate_awards.join(
+                aggregate_faba.join(ToptierAgency, toptier_agency_id=aggregate_faba.col.funding_toptier_agency_id),
+                toptier_agency_id=aggregate_awards.col.funding_toptier_agency_id,
             )
+            .with_cte(base_values)
+            .with_cte(aggregate_faba)
+            .with_cte(distinct_awards)
+            .with_cte(aggregate_awards)
             .annotate(**annotations)
-            .values(*annotations.keys())
+            .values(*annotations)
         )
 
 
