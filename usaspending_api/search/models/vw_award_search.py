1b907fdeb92d5a23a8dbf43b170cfb9b893691a6
sethstoudenmier <stoudenmier_seth@bah.com>
2020-08-06 15:23:21 -0700

    [DEV-5821] Refactor Recipient Count

--- usaspending_api/search/models/vw_award_search.py
+++ usaspending_api/search/models/vw_award_search.py
@@ -1,7 +1,11 @@
+from django_cte import CTEManager
+
 from usaspending_api.search.models.base_award_search import BaseAwardSearchModel
 
 
 class AwardSearchView(BaseAwardSearchModel):
+    objects = CTEManager()
+
     class Meta:
         managed = False
         db_table = "vw_award_search"
