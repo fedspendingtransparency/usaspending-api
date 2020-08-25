5a6d3f67360dc3600162cdac648b5b85de4268f0
Ben Minahan <64608730+bminahankcfrb@users.noreply.github.com>
2020-08-25 13:17:18 -0500

    checking existence of span

--- usaspending_api/common/datadog.py
+++ usaspending_api/common/datadog.py
@@ -4,8 +4,9 @@ from ddtrace import tracer
 def add_headers(get_response):
     def middleware(request):
         span = tracer.current_root_span()
-        for header in request.headers:
-            span.set_tag("http.headers.%s" % header, request.headers[header])
+        if span:
+            for header in request.headers:
+                span.set_tag("http.headers.%s" % header, request.headers[header])
         response = get_response(request)
         return response
 
