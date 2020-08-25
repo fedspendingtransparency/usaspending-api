eaf85eaace25605c877677a306d2c978882d765d
Wil Collins <collins_william@bah.com>
2020-08-25 18:52:46 -0400

    [DEV-5881] Simplifies sql to search for covid awards needing to 'zero-out' by using new 'is_final_balances_for_fy' column

--- usaspending_api/etl/management/commands/zero_missing_award_outlays.py
+++ usaspending_api/etl/management/commands/zero_missing_award_outlays.py
@@ -10,107 +10,23 @@ from usaspending_api.common.elasticsearch.client import instantiate_elasticsearc
 logger = logging.getLogger("script")
 
 MISSING_COVID_AWARD_SQL = """
--- Find ALL/ANY award_id of awards linked to File C records since COVID began (2020-04-01)
--- Of those, keep the ones that are NOT linked to any File C record in the latest closed periods
--- ... so that we can explicitly update their Outlay to $0 in Elasticsearch
-WITH closed_covid_periods AS (
-    SELECT submission_fiscal_year, submission_fiscal_month, is_quarter
-    FROM dabs_submission_window_schedule
-    WHERE period_start_date >= '2020-04-01'
-    AND submission_reveal_date < now()
-),
-quarterlies_from_monthly_submitters AS (
-    SELECT q.submission_id
-    FROM submission_attributes q
-    WHERE q.reporting_period_start >= '2020-04-01'
-    AND q.quarter_format_flag = TRUE
-    AND q.toptier_code IN (
-        -- Any Agency with a quarterly COVID submission that (later) submitted a MONTHLY submissions for closed COVID periods
-        SELECT toptier_code
-        FROM submission_attributes
-        INNER JOIN closed_covid_periods
-            ON closed_covid_periods.submission_fiscal_year = reporting_fiscal_year
-            AND closed_covid_periods.submission_fiscal_month = reporting_fiscal_period
-            AND closed_covid_periods.is_quarter = quarter_format_flag
-        WHERE quarter_format_flag = FALSE
-    )
-),
-latest_closed_covid_periods AS (
-    -- Latest closed monthly period and quarterly period
-    SELECT DISTINCT ON (is_quarter) submission_fiscal_year, submission_fiscal_month, is_quarter
-    FROM closed_covid_periods
-    ORDER BY is_quarter, submission_fiscal_year DESC, submission_fiscal_month DESC
-),
-covid_submissions AS (
-    SELECT submission_id, reporting_fiscal_year, reporting_fiscal_period, quarter_format_flag, toptier_code, reporting_agency_name
-    FROM submission_attributes
-    INNER JOIN closed_covid_periods
-        ON closed_covid_periods.submission_fiscal_year = reporting_fiscal_year
-        AND closed_covid_periods.submission_fiscal_month = reporting_fiscal_period
-        AND closed_covid_periods.is_quarter = quarter_format_flag
-    WHERE reporting_period_start >= '2020-04-01'
-    AND submission_id NOT IN (SELECT submission_id from quarterlies_from_monthly_submitters)
-),
-latest_covid_submissions AS (
-    SELECT submission_id, reporting_fiscal_year, reporting_fiscal_period, quarter_format_flag, toptier_code, reporting_agency_name
-    FROM covid_submissions
-    INNER JOIN latest_closed_covid_periods
-        ON latest_closed_covid_periods.submission_fiscal_year = reporting_fiscal_year
-        AND latest_closed_covid_periods.submission_fiscal_month = reporting_fiscal_period
-        AND latest_closed_covid_periods.is_quarter = quarter_format_flag
-),
-covid_awards_from_covid_submissions AS (
-    SELECT
-        DISTINCT ON (faba.award_id) faba.award_id,
-        faba.submission_id,
-        covid_submissions.reporting_fiscal_year,
-        covid_submissions.reporting_fiscal_period,
-        covid_submissions.quarter_format_flag,
-        covid_submissions.toptier_code,
-        covid_submissions.reporting_agency_name
-    FROM financial_accounts_by_awards faba
-    INNER JOIN disaster_emergency_fund_code defc
-        ON defc.code = faba.disaster_emergency_fund_code
-        AND defc.group_name = 'covid_19'
-    INNER JOIN covid_submissions ON faba.submission_id = covid_submissions.submission_id
-    WHERE faba.award_id IS NOT NULL
-    ORDER BY faba.award_id, faba.submission_id DESC
-),
-covid_awards_from_latest_covid_submisions AS (
-    SELECT
-        DISTINCT ON (faba.award_id) faba.award_id,
-        faba.submission_id,
-        latest_covid_submissions.reporting_fiscal_year,
-        latest_covid_submissions.reporting_fiscal_period,
-        latest_covid_submissions.quarter_format_flag,
-        latest_covid_submissions.toptier_code,
-        latest_covid_submissions.reporting_agency_name
-    FROM financial_accounts_by_awards faba
-    INNER JOIN disaster_emergency_fund_code defc
-        ON defc.code = faba.disaster_emergency_fund_code
-        AND defc.group_name = 'covid_19'
-    INNER JOIN latest_covid_submissions ON faba.submission_id = latest_covid_submissions.submission_id
-    WHERE faba.award_id IS NOT NULL
-    ORDER BY faba.award_id, faba.submission_id DESC
-)
--- Awards linked in prior COVID submissions,
--- that are not linked in any Agencies' submissions for the latest closed periods (up to one monthly and one quarterly period)
 SELECT
-    awards.generated_unique_award_id,
-    dropped_awards.award_id,
-    dropped_awards.toptier_code,
-    dropped_awards.reporting_agency_name,
-    dropped_awards.submission_id as last_reported_submission_id,
-    dropped_awards.reporting_fiscal_year,
-    dropped_awards.reporting_fiscal_period,
-    dropped_awards.quarter_format_flag
-FROM covid_awards_from_covid_submissions dropped_awards
-INNER JOIN awards on awards.id = dropped_awards.award_id
-LEFT JOIN covid_awards_from_latest_covid_submisions
-    ON dropped_awards.award_id = covid_awards_from_latest_covid_submisions.award_id
-WHERE covid_awards_from_latest_covid_submisions.award_id IS NULL
-ORDER BY dropped_awards.toptier_code, dropped_awards.submission_id DESC
-;
+    DISTINCT ON
+    (faba.award_id) faba.award_id
+FROM
+    financial_accounts_by_awards faba
+INNER JOIN disaster_emergency_fund_code defc ON
+    defc.code = faba.disaster_emergency_fund_code
+    AND defc.group_name = 'covid_19'
+INNER JOIN submission_attributes sa ON
+    faba.submission_id = sa.submission_id
+WHERE
+    faba.award_id IS NOT NULL
+    AND sa.is_final_balances_for_fy = FALSE
+    AND sa.reporting_period_start >= '2020-04-01'
+ORDER BY
+    faba.award_id,
+    faba.submission_id DESC;
 """
 
 
@@ -139,7 +55,7 @@ class Command(BaseCommand):
 
             rows = cursor.fetchmany(self.FETCH_COUNT)
             while len(rows) > 0:
-                award_ids = [row[1] for row in rows]
+                award_ids = [row[0] for row in rows]
 
                 # Sets the outlays of these awards to zero in Elasticsearch
                 self.set_elasticsearch_covid_outlays_to_zero(es_client, award_ids)
