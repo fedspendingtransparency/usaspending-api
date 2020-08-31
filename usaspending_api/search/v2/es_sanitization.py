ccabee41cab95a0e896dd85007a6b9859f5534c5
Emily Brents <brents_emily@bah.com>
2020-08-31 15:06:51 -0400

    [DEV-5252] add &

--- usaspending_api/search/v2/es_sanitization.py
+++ usaspending_api/search/v2/es_sanitization.py
@@ -19,12 +19,13 @@ def concat_if_array(data):
 
 def es_sanitize(input_string):
     """ Escapes reserved elasticsearch characters and removes when necessary """
-    processed_string = re.sub(r'([&|{}()*?:\\"+\[\]<>])', "", input_string)
+    processed_string = re.sub(r'([|{}()*?:\\"+\[\]<>])', "", input_string)
     processed_string = re.sub(r"[\-]", r"\-", processed_string)
     processed_string = re.sub(r"[\^]", r"\^", processed_string)
     processed_string = re.sub(r"[~]", r"\~", processed_string)
     processed_string = re.sub(r"[/]", r"\/", processed_string)
     processed_string = re.sub(r"[!]", r"\!", processed_string)
+    processed_string = re.sub(r"[&]", r"\&", processed_string)
     if len(processed_string) != len(input_string):
         msg = "Stripped characters from input string New: '{}' Original: '{}'"
         logger.info(msg.format(processed_string, input_string))
@@ -40,6 +41,7 @@ def es_minimal_sanitize(keyword):
     processed_string = re.sub(r"[~]", r"\~", processed_string)
     processed_string = re.sub(r"[/]", r"\/", processed_string)
     processed_string = re.sub(r"[!]", r"\!", processed_string)
+    processed_string = re.sub(r"[&]", r"\&", processed_string)
     if len(processed_string) != len(keyword):
         msg = "Stripped characters from ES keyword search string New: '{}' Original: '{}'"
         logger.info(msg.format(processed_string, keyword))
