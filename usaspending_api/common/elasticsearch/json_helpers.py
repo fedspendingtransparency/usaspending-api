9494a79312c8edbc8d6af8df87ac08e7f7f78e32
sethstoudenmier <stoudenmier_seth@bah.com>
2020-09-01 09:44:24 -0700

    [DEV-6033] Regex to handle quotes in a json formatted string

--- usaspending_api/common/elasticsearch/json_helpers.py
+++ usaspending_api/common/elasticsearch/json_helpers.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def json_str_to_dict(string: str) -> dict:
@@ -12,5 +13,12 @@ def json_str_to_dict(string: str) -> dict:
 
     try:
         return json.loads(string.encode("unicode_escape"))
+    except json.decoder.JSONDecodeError:
+        pass  # One more attempt to Decode
+
+    try:
+        regex = r"\"([^\"]*)\"\s?:\s?\"([^\"]*(?:(?:\w|\s)?(?:\"|\')?(?:\w|\s)?)*[^\"]*)(?:(?:\"\,)|(?:\"\}))"
+        matches = re.findall(regex, string)
+        return {key: value for key, value in matches}
     except json.decoder.JSONDecodeError as e:
         raise e
