dd183509759fad0d1ea8eaa86704639489143713
sethstoudenmier <stoudenmier_seth@bah.com>
2020-09-01 10:14:26 -0700

    [DEV-6033] Add a check for the regex

--- usaspending_api/common/elasticsearch/json_helpers.py
+++ usaspending_api/common/elasticsearch/json_helpers.py
@@ -13,12 +13,21 @@ def json_str_to_dict(string: str) -> dict:
 
     try:
         return json.loads(string.encode("unicode_escape"))
-    except json.decoder.JSONDecodeError:
-        pass  # One more attempt to Decode
-
-    try:
-        regex = r"\"([^\"]*)\"\s?:\s?\"([^\"]*(?:(?:\w|\s)?(?:\"|\')?(?:\w|\s)?)*[^\"]*)(?:(?:\"\,)|(?:\"\}))"
-        matches = re.findall(regex, string)
-        return {key: value for key, value in matches}
     except json.decoder.JSONDecodeError as e:
-        raise e
+
+        # Try to parse the string with Regex before throwing error
+        key_count_regex = r"\"[^\"]*\"\s?:"
+        grouping_regex = r"\"([^\"]*)\"\s?:\s?\"([^\"]*(?:(?:\w|\s)?(?:\"|\')?(?:\w|\s)?)*[^\"]*)(?:(?:\"\,)|(?:\"\}))"
+
+        key_count_matches = re.findall(key_count_regex, string)
+        grouping_matches = re.findall(grouping_regex, string)
+
+        # Need to verify the correct number of elements in case grouping regex didn't work
+        if (
+            isinstance(key_count_matches, list)
+            and isinstance(grouping_matches, list)
+            and len(key_count_matches) == len(grouping_matches)
+        ):
+            return {key: value for key, value in grouping_matches}
+        else:
+            return e  # Return the JSONDecodeError since we couldn't salvage with regex
