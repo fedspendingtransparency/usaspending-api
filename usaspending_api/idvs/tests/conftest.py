90a928f7b4535e81b3956b8d4a56042b9f76a12b
Brian Zito <zito_brian@bah.com>
2020-09-17 15:18:18 -0400

    refactored idv rollup tests to use fixtures

---
+++ usaspending_api/idvs/tests/conftest.py
@@ -0,0 +1,4 @@
+from usaspending_api.idvs.tests.data.idv_rollup_data import defc_codes, basic_idvs
+from usaspending_api.disaster.tests.fixtures.helpers import helpers
+
+__all__ = ["defc_codes", "basic_idvs", "helpers"]
