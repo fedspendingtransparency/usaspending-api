a7893268f9d14b1b2a4971d8752f7f5433d7abfd
Brian Zito <zito_brian@bah.com>
2020-09-18 14:44:42 -0400

    refactored test to rely on new fixtures

--- usaspending_api/awards/tests/test_awards_accounts_v2.py
+++ usaspending_api/awards/tests/test_awards_accounts_v2.py
@@ -1,52 +1,55 @@
-from django.test import TestCase
+import pytest
 from rest_framework import status
 
-from usaspending_api.awards.tests.data.award_test_data import create_award_test_data, AWARD_COUNT
+from usaspending_api.awards.tests.data.award_test_data import AWARD_COUNT
 from usaspending_api.awards.v2.views.accounts import SORTABLE_COLUMNS
 
 
 FEDERAL_ACCOUNT_ENDPOINT = "/api/v2/awards/accounts/"
 
 
-class AwardAccountsTestCase(TestCase):
-    @classmethod
-    def setUp(cls):
-        create_award_test_data()
+def _test_post(client, request, expected_status_code=status.HTTP_200_OK):
+    """
+    Perform the actual request and interrogates the results.
 
-    def _test_post(self, request, expected_status_code=status.HTTP_200_OK):
-        """
-        Perform the actual request and interrogates the results.
+    request is the Python dictionary that will be posted to the endpoint.
+    expected_response_parameters are the values that you would normally
+        pass into _generate_expected_response but we're going to do that
+        for you so just pass the parameters as a tuple or list.
+    expected_status_code is the HTTP status we expect to be returned from
+        the call to the endpoint.
+    """
+    response = client.post(FEDERAL_ACCOUNT_ENDPOINT, request)
+    assert response.status_code == expected_status_code
 
-        request is the Python dictionary that will be posted to the endpoint.
-        expected_response_parameters are the values that you would normally
-            pass into _generate_expected_response but we're going to do that
-            for you so just pass the parameters as a tuple or list.
-        expected_status_code is the HTTP status we expect to be returned from
-            the call to the endpoint.
-        """
-        response = self.client.post(FEDERAL_ACCOUNT_ENDPOINT, request)
-        assert response.status_code == expected_status_code
 
-    def test_complete_queries(self):
-        for _id in range(1, AWARD_COUNT + 1):
-            self._test_post({"award_id": _id})
+@pytest.mark.django_db
+def test_complete_queries(client, create_award_test_data):
+    for _id in range(1, AWARD_COUNT + 1):
+        _test_post(client, {"award_id": _id})
 
-    def test_with_nonexistent_id(self):
 
-        self._test_post({"award_id": 0})
+@pytest.mark.django_db
+def test_with_nonexistent_id(client, create_award_test_data):
 
-        self._test_post({"award_id": "GENERATED_UNIQUE_AWARD_ID_000"})
+    _test_post(client, {"award_id": 0})
 
-    def test_with_bogus_id(self):
+    _test_post(client, {"award_id": "GENERATED_UNIQUE_AWARD_ID_000"})
 
-        self._test_post({"award_id": "BOGUS_ID"})
 
-    def test_sort_columns(self):
+@pytest.mark.django_db
+def test_with_bogus_id(client, create_award_test_data):
 
-        for sortable_column in SORTABLE_COLUMNS:
+    _test_post(client, {"award_id": "BOGUS_ID"})
 
-            self._test_post({"award_id": 2, "order": "desc", "sort": sortable_column})
 
-            self._test_post({"award_id": 2, "order": "asc", "sort": sortable_column})
+@pytest.mark.django_db
+def test_sort_columns(client, create_award_test_data):
 
-        self._test_post({"award_id": 2, "sort": "BOGUS FIELD"}, expected_status_code=status.HTTP_400_BAD_REQUEST)
+    for sortable_column in SORTABLE_COLUMNS:
+
+        _test_post(client, {"award_id": 2, "order": "desc", "sort": sortable_column})
+
+        _test_post(client, {"award_id": 2, "order": "asc", "sort": sortable_column})
+
+    _test_post(client, {"award_id": 2, "sort": "BOGUS FIELD"}, expected_status_code=status.HTTP_400_BAD_REQUEST)
