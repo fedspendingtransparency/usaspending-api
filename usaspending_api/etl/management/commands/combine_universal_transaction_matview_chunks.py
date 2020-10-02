6e7186d3b109947f00ef90f9bd9ac3cd29e35588
Wil Collins <collins_william@bah.com>
2020-10-02 13:30:39 -0400

    [DEV-5892] Adds table swapping to combine_utm_chunks command

--- usaspending_api/etl/management/commands/combine_universal_transaction_matview_chunks.py
+++ usaspending_api/etl/management/commands/combine_universal_transaction_matview_chunks.py
@@ -10,27 +10,67 @@ from usaspending_api.common.helpers.timing_helpers import ConsoleTimer as Timer
 logger = logging.getLogger("script")
 
 RECREATE_TABLE_SQL = """
-DROP TABLE IF EXISTS universal_transaction_table;
-CREATE TABLE universal_transaction_table AS TABLE universal_transaction_matview_0 WITH NO DATA;
+DROP TABLE IF EXISTS universal_transaction_table_temp;
+CREATE TABLE universal_transaction_table_temp AS TABLE universal_transaction_matview_0 WITH NO DATA;
 """
 
 INSERT_INTO_TABLE_SQL = """
-INSERT INTO universal_transaction_table SELECT * FROM universal_transaction_matview_{current_chunk}
+INSERT INTO universal_transaction_table_temp SELECT * FROM universal_transaction_matview_{current_chunk}
 """
 
 TABLE_INDEX_SQL = """
-CREATE UNIQUE INDEX idx_ut_transaction_id_table ON universal_transaction_table USING BTREE(transaction_id ASC NULLS LAST) WITH (fillfactor = 97);
-CREATE INDEX idx_ut_action_date_table ON universal_transaction_table USING BTREE(action_date DESC NULLS LAST) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
-CREATE INDEX idx_ut_last_modified_date_table ON universal_transaction_table USING BTREE(last_modified_date DESC NULLS LAST) WITH (fillfactor = 97);
-CREATE INDEX idx_ut_fiscal_year_table ON universal_transaction_table USING BTREE(fiscal_year DESC NULLS LAST) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
-CREATE INDEX idx_ut_type_table ON universal_transaction_table USING BTREE(type) WITH (fillfactor = 97) WHERE type IS NOT NULL AND action_date >= '2007-10-01';
-CREATE INDEX idx_ut_award_id_table ON universal_transaction_table USING BTREE(award_id) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
-CREATE INDEX idx_ut_pop_zip5_table ON universal_transaction_table USING BTREE(pop_zip5) WITH (fillfactor = 97) WHERE pop_zip5 IS NOT NULL AND action_date >= '2007-10-01';
-CREATE INDEX idx_ut_recipient_unique_id_table ON universal_transaction_table USING BTREE(recipient_unique_id) WITH (fillfactor = 97) WHERE recipient_unique_id IS NOT NULL AND action_date >= '2007-10-01';
-CREATE INDEX idx_ut_parent_recipient_unique_id_table ON universal_transaction_table USING BTREE(parent_recipient_unique_id) WITH (fillfactor = 97) WHERE parent_recipient_unique_id IS NOT NULL AND action_date >= '2007-10-01';
-CREATE INDEX idx_ut_simple_pop_geolocation_table ON universal_transaction_table USING BTREE(pop_state_code, action_date) WITH (fillfactor = 97) WHERE pop_country_code = 'USA' AND pop_state_code IS NOT NULL AND action_date >= '2007-10-01';
-CREATE INDEX idx_ut_recipient_hash_table ON universal_transaction_table USING BTREE(recipient_hash) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
-CREATE INDEX idx_ut_action_date_pre2008_table ON universal_transaction_table USING BTREE(action_date) WITH (fillfactor = 97) WHERE action_date < '2007-10-01';
+CREATE UNIQUE INDEX idx_ut_transaction_id_temp ON universal_transaction_table_temp USING BTREE(transaction_id ASC NULLS LAST) WITH (fillfactor = 97);
+CREATE INDEX idx_ut_action_date_temp ON universal_transaction_table_temp USING BTREE(action_date DESC NULLS LAST) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
+CREATE INDEX idx_ut_last_modified_date_temp ON universal_transaction_table_temp USING BTREE(last_modified_date DESC NULLS LAST) WITH (fillfactor = 97);
+CREATE INDEX idx_ut_fiscal_year_temp ON universal_transaction_table_temp USING BTREE(fiscal_year DESC NULLS LAST) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
+CREATE INDEX idx_ut_type_temp ON universal_transaction_table_temp USING BTREE(type) WITH (fillfactor = 97) WHERE type IS NOT NULL AND action_date >= '2007-10-01';
+CREATE INDEX idx_ut_award_id_temp ON universal_transaction_table_temp USING BTREE(award_id) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
+CREATE INDEX idx_ut_pop_zip5_temp ON universal_transaction_table_temp USING BTREE(pop_zip5) WITH (fillfactor = 97) WHERE pop_zip5 IS NOT NULL AND action_date >= '2007-10-01';
+CREATE INDEX idx_ut_recipient_unique_id_temp ON universal_transaction_table_temp USING BTREE(recipient_unique_id) WITH (fillfactor = 97) WHERE recipient_unique_id IS NOT NULL AND action_date >= '2007-10-01';
+CREATE INDEX idx_ut_parent_recipient_unique_id_temp ON universal_transaction_table_temp USING BTREE(parent_recipient_unique_id) WITH (fillfactor = 97) WHERE parent_recipient_unique_id IS NOT NULL AND action_date >= '2007-10-01';
+CREATE INDEX idx_ut_simple_pop_geolocation_temp ON universal_transaction_table_temp USING BTREE(pop_state_code, action_date) WITH (fillfactor = 97) WHERE pop_country_code = 'USA' AND pop_state_code IS NOT NULL AND action_date >= '2007-10-01';
+CREATE INDEX idx_ut_recipient_hash_temp ON universal_transaction_table_temp USING BTREE(recipient_hash) WITH (fillfactor = 97) WHERE action_date >= '2007-10-01';
+CREATE INDEX idx_ut_action_date_pre2008_temp ON universal_transaction_table_temp USING BTREE(action_date) WITH (fillfactor = 97) WHERE action_date < '2007-10-01';
+"""
+
+SWAP_TABLES_SQL = """
+ALTER TABLE IF EXISTS universal_transaction_table RENAME TO universal_transaction_table_old;
+
+ALTER INDEX IF EXISTS idx_ut_transaction_id RENAME TO idx_ut_transaction_id_old;
+ALTER INDEX IF EXISTS idx_ut_action_date RENAME TO idx_ut_action_date_old;
+ALTER INDEX IF EXISTS idx_ut_last_modified_date RENAME TO idx_ut_last_modified_date_old;
+ALTER INDEX IF EXISTS idx_ut_fiscal_year RENAME TO idx_ut_fiscal_year_old;
+ALTER INDEX IF EXISTS idx_ut_type RENAME TO idx_ut_type_old;
+ALTER INDEX IF EXISTS idx_ut_award_id RENAME TO idx_ut_award_id_old;
+ALTER INDEX IF EXISTS idx_ut_pop_zip5 RENAME TO idx_ut_pop_zip5_old;
+ALTER INDEX IF EXISTS idx_ut_recipient_unique_id RENAME TO idx_ut_recipient_unique_id_old;
+ALTER INDEX IF EXISTS idx_ut_parent_recipient_unique_id RENAME TO idx_ut_parent_recipient_unique_id_old;
+ALTER INDEX IF EXISTS idx_ut_simple_pop_geolocation RENAME TO idx_ut_simple_pop_geolocation_old;
+ALTER INDEX IF EXISTS idx_ut_recipient_hash RENAME TO idx_ut_recipient_hash_old;
+ALTER INDEX IF EXISTS idx_ut_action_date_pre2008 RENAME TO idx_ut_action_date_pre2008_old;
+
+ALTER TABLE universal_transaction_table_temp RENAME TO universal_transaction_table;
+
+ALTER INDEX idx_ut_transaction_id_temp RENAME TO idx_ut_transaction_id;
+ALTER INDEX idx_ut_action_date_temp RENAME TO idx_ut_action_date;
+ALTER INDEX idx_ut_last_modified_date_temp RENAME TO idx_ut_last_modified_date;
+ALTER INDEX idx_ut_fiscal_year_temp RENAME TO idx_ut_fiscal_year;
+ALTER INDEX idx_ut_type_temp RENAME TO idx_ut_type;
+ALTER INDEX idx_ut_award_id_temp RENAME TO idx_ut_award_id;
+ALTER INDEX idx_ut_pop_zip5_temp RENAME TO idx_ut_pop_zip5;
+ALTER INDEX idx_ut_recipient_unique_id_temp RENAME TO idx_ut_recipient_unique_id;
+ALTER INDEX idx_ut_parent_recipient_unique_id_temp RENAME TO idx_ut_parent_recipient_unique_id;
+ALTER INDEX idx_ut_simple_pop_geolocation_temp RENAME TO idx_ut_simple_pop_geolocation;
+ALTER INDEX idx_ut_recipient_hash_temp RENAME TO idx_ut_recipient_hash;
+ALTER INDEX idx_ut_action_date_pre2008_temp RENAME TO idx_ut_action_date_pre2008;
+"""
+
+ANALYZE_TABLE_SQL = """
+ANALYZE VERBOSE universal_transaction_table;
+"""
+
+TABLE_PERMISSION_SQL = """
+GRANT SELECT ON universal_transaction_table TO readonly;
 """
 
 
@@ -42,7 +82,8 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        parser.add_argument("--chunk-count", default=10, help="Broker submission_id to load", type=int)
+        parser.add_argument("--chunk-count", default=10, help="Number of chunked matviews to read from", type=int)
+        parser.add_argument("--analyze", action="store_true", help="Indicates whether table should be analyzed", type=int)
 
     def handle(self, *args, **options):
         chunk_count = options["chunk_count"]
@@ -57,6 +98,16 @@ class Command(BaseCommand):
 
         with Timer("Creating table indexes"):
             self.create_indexes()
+
+        with Timer("Swapping Tables/Indexes"):
+            self.swap_tables()
+
+        if options["analyze"]:
+            with Timer("Analyzing Table"):
+                self.analyze_table()
+
+        with Timer("Granting Table Permissions"):
+            self.grant_table_permissions()
 
     def recreate_table(self):
         with connection.cursor() as cursor:
@@ -97,3 +148,15 @@ class Command(BaseCommand):
 
         loop.run_until_complete(asyncio.gather(*tasks))
         loop.close()
+
+    def swap_tables(self):
+        with connection.cursor() as cursor:
+            cursor.execute(SWAP_TABLES_SQL)
+
+    def analyze_table(self):
+        with connection.cursor() as cursor:
+            cursor.execute(ANALYZE_TABLE_SQL)
+
+    def grant_table_permissions(self):
+        with connection.cursor() as cursor:
+            cursor.execute(TABLE_PERMISSION_SQL)
