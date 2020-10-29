8ee248199e24be1a510186da7fcf5205c5f24f95
Wil Collins <collins_william@bah.com>
2020-10-29 10:07:49 -0400

    [DEV-6053] Removes whitespaces from end of award delta view lines 2

--- usaspending_api/database_scripts/etl/award_delta_view.sql
+++ usaspending_api/database_scripts/etl/award_delta_view.sql
@@ -70,10 +70,10 @@ SELECT
   CASE
     WHEN vw_es_award_search.funding_toptier_agency_name IS NOT NULL
       THEN CONCAT(
-        '{"name":"', vw_es_award_search.funding_toptier_agency_name,	
+        '{"name":"', vw_es_award_search.funding_toptier_agency_name,
         '","code":"', vw_es_award_search.funding_toptier_agency_code,
         '","id":"', (SELECT a1.id FROM agency a1 WHERE a1.toptier_agency_id = (SELECT a2.toptier_agency_id FROM agency a2 WHERE a2.id = vw_es_award_search.funding_agency_id) ORDER BY a1.toptier_flag DESC, a1.id LIMIT 1), '"}'
-      )	
+      )
     ELSE NULL
   END AS funding_toptier_agency_agg_key,
   CASE
