55d759662fd77f4afc81d1ecf2affafda9325c04
Wil Collins <collins_william@bah.com>
2020-11-10 17:43:32 -0500

    [DEV-6068] Replaces 4 sql queries with unified query

--- usaspending_api/etl/management/commands/update_missing_covid_awards.py
+++ usaspending_api/etl/management/commands/update_missing_covid_awards.py
@@ -10,21 +10,27 @@ from usaspending_api.common.helpers.timing_helpers import ConsoleTimer as Timer
 logger = logging.getLogger("script")
 
 UPDATE_AWARDS_SQL = """
-WITH this_period AS (
+WITH recent_covid_awards AS (
     SELECT
-        DISTINCT award_id
+        DISTINCT ON
+        (faba.award_id) faba.award_id, sa.submission_id, sa.is_final_balances_for_fy, sa.reporting_fiscal_year, sa.reporting_fiscal_period, sa.quarter_format_flag
     FROM
         financial_accounts_by_awards faba
+    INNER JOIN disaster_emergency_fund_code defc ON
+        defc.code = faba.disaster_emergency_fund_code
+        AND defc.group_name = 'covid_19'
     INNER JOIN submission_attributes sa ON
-        faba.submission_id = sa.submission_id
+        sa.reporting_period_start >= '2020-04-01'
+        AND faba.submission_id = sa.submission_id
     INNER JOIN dabs_submission_window_schedule dabs ON
         dabs.id = sa.submission_window_id
+        AND dabs.submission_reveal_date <= now()
     WHERE
-        submission_fiscal_year = {this_year}
-        AND submission_fiscal_month = {this_month}
-        AND is_quarter = {this_is_quarter}
+        faba.award_id IS NOT NULL
+    ORDER BY
+        faba.award_id, submission_reveal_date DESC, is_quarter
 ),
-last_period AS (
+last_periods_covid_awards AS (
     SELECT
         DISTINCT award_id
     FROM
@@ -37,9 +43,16 @@ last_period AS (
         defc.code = faba.disaster_emergency_fund_code
         AND defc.group_name = 'covid_19'
     WHERE
-        submission_fiscal_year = {last_year}
-        AND submission_fiscal_month = {last_month}
-        AND is_quarter = {last_is_quarter}
+        (
+            submission_fiscal_year = {last_months_year}
+            AND submission_fiscal_month = {last_months_month}
+            AND is_quarter = FALSE
+        )
+        OR (
+            submission_fiscal_year = {last_quarters_year}
+            AND submission_fiscal_month = {last_quarters_month}
+            AND is_quarter = TRUE
+        )
 )
 UPDATE
     awards
@@ -50,13 +63,15 @@ WHERE
         SELECT
             *
         FROM
-            last_period
+            last_periods_covid_awards
     )
-    AND id NOT IN (
+    AND id IN (
         SELECT
-            *
+            award_id
         FROM
-            this_period
+            recent_covid_awards
+        WHERE
+            recent_covid_awards.is_final_balances_for_fy = FALSE
     )
     AND update_date < '{submission_reveal_date}'
 """
@@ -141,9 +156,6 @@ class Command(BaseCommand):
             logger.info("Broad flag provided. Updating all Covid awards not reported in the latest submission")
             self.update_awards_broad(recent_periods)
 
-    def read_period_fields(self, period):
-        return {"month": period[0], "year": period[1], "is_quarter": period[2], "submission_reveal_date": period[3]}
-
     def retreive_recent_periods(self):
         # Open connection to database
         with connection.cursor() as cursor:
@@ -159,57 +171,27 @@ class Command(BaseCommand):
         recent_periods = {
             "this_month": self.read_period_fields(recent_month_periods[0]),
             "last_month": self.read_period_fields(recent_month_periods[1]),
-            "this_quarter": self.read_period_fields(recent_quarter_periods[0]),
-            "last_quarter": self.read_period_fields(recent_quarter_periods[1]),
+            "last_quarter": self.read_period_fields(recent_quarter_periods[1])
         }
 
         return recent_periods
 
-    def format_update_sql(self, this_period, last_period, last_is_quarter="default"):
-        # Used for special Quarter -> Month case
-        if last_is_quarter == "default":
-            last_is_quarter = last_period["is_quarter"]
-
-        return UPDATE_AWARDS_SQL.format(
-            this_year=this_period["year"],
-            this_month=this_period["month"],
-            this_is_quarter=this_period["is_quarter"],
-            last_year=last_period["year"],
-            last_month=last_period["month"],
-            last_is_quarter=last_is_quarter,
-            submission_reveal_date=this_period["submission_reveal_date"],
-        )
+    def read_period_fields(self, period):
+        return {"month": period[0], "year": period[1], "submission_reveal_date": period[3]}
 
     def update_awards(self, periods):
+        submission_reveal_date = periods["this_month"]["submission_reveal_date"]
 
-        sql_statements = {}
-
-        sql_statements["Current:Month,   Last:Month"] = self.format_update_sql(
-            periods["this_month"], periods["last_month"]
-        )
-        sql_statements["Current:Quarter, Last:Quarter"] = self.format_update_sql(
-            periods["this_quarter"], periods["last_quarter"]
-        )
-
-        # Special case to compare the current quarter to the last month of the previous quarter
-        sql_statements["Current:Quarter, Last:Month"] = self.format_update_sql(
-            periods["this_quarter"], periods["this_quarter"], last_is_quarter=False
-        )
-
-        # Special case to only compare current month to last quarter if the this month is the first of a quarter
-        if periods["this_month"]["month"] in (4, 7, 10):
-            sql_statements["Current:Month,   Last:Quarter"] = self.format_update_sql(
-                periods["this_month"], periods["last_quarter"]
-            )
-
-        loop = asyncio.new_event_loop()
-        tasks = []
-
-        for description, sql in sql_statements.items():
-            tasks.append(asyncio.ensure_future(async_run_update(sql, wrapper=Timer(description),), loop=loop,))
-
-        loop.run_until_complete(asyncio.gather(*tasks))
-        loop.close()
+        # Open connection to database
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_AWARDS_SQL.format(                    
+                last_months_year=periods["last_month"]["year"],
+                last_months_month=periods["last_month"]["month"],
+                last_quarters_year=periods["last_quarter"]["year"],
+                last_quarters_month=periods["last_quarter"]["month"],
+                submission_reveal_date=submission_reveal_date
+            ))
+            logger.info(f"Update message (records updated): {cursor.statusmessage}")
 
     def update_awards_broad(self, periods):
         submission_reveal_date = periods["this_month"]["submission_reveal_date"]
