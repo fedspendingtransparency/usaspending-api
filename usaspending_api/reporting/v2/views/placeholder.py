aea9167d3115f06a0a874d23c150cb0168fde32a
Tony Sappe <tony.sappe@tuta.io>
2020-11-18 13:24:47 -0700

    Finalized dir structure and fixed api doc checks

--- usaspending_api/reporting/v2/views/placeholder.py
+++ usaspending_api/reporting/v2/views/placeholder.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 class Placeholder(APIView):
     """Placeholder"""
 
-    endpoint_doc = "usaspending_api/api_contracts/contracts/v2/recipient/duns.md"
+    endpoint_doc = "usaspending_api/api_contracts/contracts/v2/reporting/placeholder.md"
 
     def get(self, request):
         return Response({"status": "success"})
