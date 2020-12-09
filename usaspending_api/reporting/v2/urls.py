2e88dae7fb399c6410029ff9861b4c288c3230b7
Emily Brents <brents_emily@bah.com>
2020-12-09 14:39:22 -0500

    [DEV-6435] fix typo in endpoint doc

--- usaspending_api/reporting/v2/urls.py
+++ usaspending_api/reporting/v2/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url, include
-from usaspending_api.reporting.v2.views.placeholder import Placeholder
 
 urlpatterns = [
-    url(r"^placeholder/$", Placeholder.as_view()),
     url(r"^agencies/", include("usaspending_api.reporting.v2.views.agencies.urls")),
 ]
