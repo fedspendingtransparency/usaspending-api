4ab401a6bd7d8daa42dd8eb661034e37b855a661
brett-varney <brett.varney@kc.frb.org>
2020-12-09 10:39:06 -0600

    dev-6436 error test passes

--- usaspending_api/references/tests/test_total_budgetary_resources_v2.py
+++ usaspending_api/references/tests/test_total_budgetary_resources_v2.py
@@ -2,69 +2,71 @@ import pytest
 from model_mommy import mommy
 from rest_framework import status
 
+
 @pytest.fixture
 def create_agency_data():
-    # # Create agency - submission relationship
-    # # Create AGENCY AND TopTier AGENCY
-    # ttagency1 = mommy.make(
-    #     "references.ToptierAgency",
-    #     name="tta_name",
-    #     toptier_code="100",
-    #     abbreviation="tta_abrev",
-    #     justification="test.com/cj",
-    # )
-    # ttagency2 = mommy.make(
-    #     "references.ToptierAgency", name="tta_name_2", toptier_code="200", abbreviation="tta_abrev_2"
-    # )
+    # Create agency - submission relationship
+    # Create AGENCY AND TopTier AGENCY
+    ttagency1 = mommy.make(
+        "references.ToptierAgency",
+        name="tta_name",
+        toptier_code="100",
+        abbreviation="tta_abrev",
+        justification="test.com/cj",
+    )
+    ttagency2 = mommy.make(
+        "references.ToptierAgency", name="tta_name_2", toptier_code="200", abbreviation="tta_abrev_2"
+    )
 
-    # mommy.make("references.Agency", id=1, toptier_agency=ttagency1, toptier_flag=True)
-    # mommy.make("references.Agency", id=2, toptier_agency=ttagency2, toptier_flag=True)
+    mommy.make("references.Agency", id=1, toptier_agency=ttagency1, toptier_flag=True)
+    mommy.make("references.Agency", id=2, toptier_agency=ttagency2, toptier_flag=True)
 
-    # # create TAS
-    # tas = mommy.make("accounts.TreasuryAppropriationAccount", funding_toptier_agency=ttagency1)
-    # tas2 = mommy.make("accounts.TreasuryAppropriationAccount", funding_toptier_agency=ttagency2)
 
-    # # CREATE SUBMISSIONS
-    # # submission_3 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2015, toptier_code='100')
-    # submission_1 = mommy.make(
-    #     "submissions.SubmissionAttributes", reporting_fiscal_year=2017, reporting_fiscal_quarter=2, toptier_code="100"
-    # )
-    # # submission_2 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2016, toptier_code='100')
-    # submission_2 = mommy.make(
-    #     "submissions.SubmissionAttributes", reporting_fiscal_year=2017, reporting_fiscal_quarter=2, toptier_code="200"
-    # )
+# # create TAS
+# tas = mommy.make("accounts.TreasuryAppropriationAccount", funding_toptier_agency=ttagency1)
+# tas2 = mommy.make("accounts.TreasuryAppropriationAccount", funding_toptier_agency=ttagency2)
 
-    # # CREATE AppropriationAccountBalances
-    # mommy.make(
-    #     "accounts.AppropriationAccountBalances",
-    #     final_of_fy=True,
-    #     reporting_period_start="2017-1-1",
-    #     submission=submission_1,
-    #     total_budgetary_resources_amount_cpe=2,
-    #     obligations_incurred_total_by_tas_cpe=2,
-    #     gross_outlay_amount_by_tas_cpe=2,
-    #     treasury_account_identifier=tas,
-    # )
-    # mommy.make(
-    #     "accounts.AppropriationAccountBalances",
-    #     final_of_fy=True,
-    #     reporting_period_start="2017-1-1",
-    #     submission=submission_2,
-    #     total_budgetary_resources_amount_cpe=14,
-    #     obligations_incurred_total_by_tas_cpe=14,
-    #     gross_outlay_amount_by_tas_cpe=14,
-    #     treasury_account_identifier=tas2,
-    # )
+# # CREATE SUBMISSIONS
+# # submission_3 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2015, toptier_code='100')
+# submission_1 = mommy.make(
+#     "submissions.SubmissionAttributes", reporting_fiscal_year=2017, reporting_fiscal_quarter=2, toptier_code="100"
+# )
+# # submission_2 = mommy.make('submissions.SubmissionAttributes', reporting_fiscal_year=2016, toptier_code='100')
+# submission_2 = mommy.make(
+#     "submissions.SubmissionAttributes", reporting_fiscal_year=2017, reporting_fiscal_quarter=2, toptier_code="200"
+# )
 
-    # # CREATE OverallTotals
-    # ot = mommy.make('references.OverallTotals', fiscal_year=2017, total_budget_authority=3860000000.00)
+# # CREATE AppropriationAccountBalances
+# mommy.make(
+#     "accounts.AppropriationAccountBalances",
+#     final_of_fy=True,
+#     reporting_period_start="2017-1-1",
+#     submission=submission_1,
+#     total_budgetary_resources_amount_cpe=2,
+#     obligations_incurred_total_by_tas_cpe=2,
+#     gross_outlay_amount_by_tas_cpe=2,
+#     treasury_account_identifier=tas,
+# )
+# mommy.make(
+#     "accounts.AppropriationAccountBalances",
+#     final_of_fy=True,
+#     reporting_period_start="2017-1-1",
+#     submission=submission_2,
+#     total_budgetary_resources_amount_cpe=14,
+#     obligations_incurred_total_by_tas_cpe=14,
+#     gross_outlay_amount_by_tas_cpe=14,
+#     treasury_account_identifier=tas2,
+# )
+
+# # CREATE OverallTotals
+# ot = mommy.make('references.OverallTotals', fiscal_year=2017, total_budget_authority=3860000000.00)
 
 
 @pytest.mark.django_db
 def test_award_type_endpoint(client, create_agency_data):
 
     """Test the total_budgetary_resources endpoint with bad parameters."""
-    resp = client.get("/api/v2/references/total_budgetary_resources?fiscal_period=3")
+    resp = client.get("/api/v2/references/total_budgetary_resources/?fiscal_period=3")
     assert resp.status_code == status.HTTP_400_BAD_REQUEST
 
     # """Test the total_budgetary_resources endpoint with bad parameters."""
@@ -102,4 +104,3 @@ def test_award_type_endpoint(client, create_agency_data):
     #         },
     #     ]
     # }
-
--- usaspending_api/references/v2/views/total_budgetary_resources.py
+++ usaspending_api/references/v2/views/total_budgetary_resources.py
@@ -1,5 +1,11 @@
-class TotalBudgetaryResources(APIView):
+from usaspending_api.common.cache_decorator import cache_response
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from usaspending_api.common.exceptions import InvalidParameterException
+
+
+class TotalBudgetaryResources(APIView):
     @cache_response()
     def get(self, request, format=None):
         fiscal_year = request.query_params.get("fiscal_year")
@@ -7,5 +13,3 @@ class TotalBudgetaryResources(APIView):
 
         if fiscal_period and not fiscal_year:
             raise InvalidParameterException("fiscal_period was provided without any fiscal_year.")
-
-
