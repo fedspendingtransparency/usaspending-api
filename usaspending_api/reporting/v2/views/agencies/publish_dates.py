bb5aed892ceb2f106f7c1d0da2deb8a8a38c9753
Emily Brents <brents_emily@bah.com>
2021-01-06 11:35:34 -0500

    [DEV-6439] sorting by publication date
    yikes

--- usaspending_api/reporting/v2/views/agencies/publish_dates.py
+++ usaspending_api/reporting/v2/views/agencies/publish_dates.py
@@ -1,14 +1,19 @@
-from django.contrib.postgres.aggregates import ArrayAgg, JSONBAgg
-from django.contrib.postgres.fields import JSONField
-from django.db.models import OuterRef, Subquery, DateTimeField, TextField, F, Value, Func, DecimalField
-from django.db.models.functions import Concat, Cast
+from builtins import next, enumerate
+from copy import deepcopy
+
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import OuterRef, Subquery, TextField, F, Value, Func, DecimalField
+from django.db.models.functions import Cast
+from django.utils.functional import cached_property
 from jsonpickle import json
 from rest_framework.response import Response
 
 from usaspending_api.agency.v2.views.agency_base import AgencyBase, PaginationMixin
 
+from usaspending_api.common.data_classes import Pagination
 from usaspending_api.common.helpers.generic_helper import get_pagination_metadata
 from usaspending_api.common.helpers.orm_helpers import ConcatAll
+from usaspending_api.common.validator import customize_pagination_with_sort_columns, TinyShield
 from usaspending_api.references.models import ToptierAgency
 from usaspending_api.reporting.models import ReportingAgencyOverview
 from usaspending_api.submissions.models import SubmissionAttributes
@@ -19,24 +24,10 @@ class PublishDates(AgencyBase, PaginationMixin):
 
     endpoint_doc = "usaspending_api/api_contracts/contracts/v2/reporting/agencies/publish_dates.md"
 
-    # {
-    #     "agency_name": "Department of Health and Human Services",
-    #     "abbreviation": "DHHS",
-    #     "agency_code": "020",
-    #     "current_total_budget_authority_amount": 8361447130497.72,
-    #     "periods": [{
-    #         "period": 2,
-    #         "quarter": 1,
-    #         "submission_dates": {
-    #             "publication_date": "2020-01-20T11:59:21Z",
-    #             "certification_date": "2020-01-21T10:58:21Z"
-    #         },
-    #         "quarterly": false
-    #     }]
-    # },
     def get_agency_data(self):
         results = (
-            SubmissionAttributes.objects.filter(reporting_fiscal_year=self.fiscal_year).values("toptier_code")
+            SubmissionAttributes.objects.filter(reporting_fiscal_year=self.fiscal_year)
+            .values("toptier_code")
             .annotate(
                 agency_name=Subquery(
                     ToptierAgency.objects.filter(toptier_code=OuterRef("toptier_code")).values("name")
@@ -54,14 +45,14 @@ class PublishDates(AgencyBase, PaginationMixin):
                 ),
                 periods=ArrayAgg(
                     ConcatAll(
-                        Value('{"publication_date": "'),
-                        Cast("published_date", output_field=TextField()),
-                        Value('", "certification_date": "'),
-                        Cast("certified_date", output_field=TextField()),
-                        Value('", "submission_dates": {"reporting_fiscal_period": "'),
+                        Value('{"reporting_fiscal_period": "'),
                         Cast("reporting_fiscal_period", output_field=TextField()),
                         Value('", "reporting_fiscal_quarter": "'),
                         Cast("reporting_fiscal_quarter", output_field=TextField()),
+                        Value('", "submission_dates":{ "publication_date": "'),
+                        Cast("published_date", output_field=TextField()),
+                        Value('", "certification_date": "'),
+                        Cast("certified_date", output_field=TextField()),
                         Value('"}, "quarterly": "'),
                         Cast("quarter_format_flag", output_field=TextField()),
                         Value('"}'),
@@ -69,34 +60,7 @@ class PublishDates(AgencyBase, PaginationMixin):
                 ),
             )
             .values("agency_name", "abbreviation", "toptier_code", "current_total_budget_authority_amount", "periods")
-            .order_by(f"{'-' if self.pagination.sort_order == 'desc' else ''}{self.pagination.sort_key}")
-
         )
-        # ReportingAgencyOverview.objects.filter(fiscal_year=self.fiscal_year)
-        # .annotate(
-        #     agency_name=Subquery(
-        #         ToptierAgency.objects.filter(toptier_code=OuterRef("toptier_code")).values("name")
-        #     ),
-        #     abbreviation=Subquery(
-        #         ToptierAgency.objects.filter(toptier_code=OuterRef("toptier_code")).values("abbreviation")
-        #     ),
-        #     periods=Subquery(
-        #         SubmissionAttributes.objects.filter(
-        #             reporting_fiscal_year=OuterRef("fiscal_year"), toptier_code=OuterRef("toptier_code")
-        #         )
-        #         .annotate(
-        #             json=ConcatAll(
-        #                 Value('{"publication_date": "'), Cast("published_date", output_field=TextField()),
-        #                 Value('", "certification_date": "'), Cast("certified_date", output_field=TextField()),
-        #                 Value('", "reporting_fiscal_period": "'), Cast("reporting_fiscal_period", output_field=TextField()),
-        #                 Value('", "reporting_fiscal_quarter": "'), Cast("reporting_fiscal_quarter", output_field=TextField()),
-        #                 Value('", "quarter_format_flag": "'),  Cast("quarter_format_flag", output_field=TextField()),
-        #                 Value('"}')
-        #             )
-        #         )
-        #         .values("toptier_code").annotate(arr=ArrayAgg(F("json"))).values("arr"), output_field=TextField()
-        #     ),
-        # ).values("agency_name", "abbreviation", "toptier_code", "current_total_budget_authority_amount").order_by("toptier_code")
         return self.format_results(results)
 
     def format_results(self, result_list):
@@ -108,23 +72,67 @@ class PublishDates(AgencyBase, PaginationMixin):
                     "abbreviation": result["abbreviation"],
                     "agency_code": result["toptier_code"],
                     "current_total_budget_authority_amount": result["current_total_budget_authority_amount"],
-                    "periods": [json.loads(x) for x in result["periods"]] if result.get("periods") else None,
+                    "periods": sorted(
+                        [json.loads(x) for x in result["periods"]], key=lambda x: x["reporting_fiscal_period"]
+                    )
+                    if result.get("periods")
+                    else None,
                 }
             )
         return results
 
     def get(self, request):
-        self.sortable_columns = [
-            "agency_name",
-            "abbreviation",
-            "toptier_code",
-            "current_total_budget_authority_amount",
-            "publication_date",
-        ]
-        self.default_sort_column = "current_total_budget_authority_amount"
-        results = self.get_agency_data()
+
+        if "publication_date" in self.pagination.sort_key:
+            sort_key = deepcopy(self.pagination.sort_key)
+            pub_sort = sort_key.split(",")[1]
+            self.pagination.sort_key = "publication_date"
+            results = sorted(
+                self.get_agency_data(),
+                key=lambda x: x["periods"][
+                    next(
+                        (index for (index, d) in enumerate(x["periods"]) if d["reporting_fiscal_period"] == pub_sort), 0
+                    )
+                ]["submission_dates"]["publication_date"],
+                reverse=(self.pagination.sort_order == "desc"),
+            )
+        else:
+            results = sorted(
+                self.get_agency_data(),
+                key=lambda x: x[self.pagination.sort_key],
+                reverse=(self.pagination.sort_order == "desc"),
+            )
         page_metadata = get_pagination_metadata(len(results), self.pagination.limit, self.pagination.page)
         results = results[self.pagination.lower_limit : self.pagination.upper_limit]
         return Response(
             {"page_metadata": page_metadata, "results": results, "messages": self.standard_response_messages}
+        )
+
+    @cached_property
+    def pagination(self):
+        sortable_columns = [
+            "agency_name",
+            "abbreviation",
+            "agency_code",
+            "current_total_budget_authority_amount",
+            "publication_date",
+        ]
+        default_sort_column = "current_total_budget_authority_amount"
+        model = customize_pagination_with_sort_columns(sortable_columns, default_sort_column)
+
+        if self.request.query_params.get("sort") and "publication_date" in self.request.query_params.get("sort"):
+            modified_query_params = deepcopy(self.request.query_params)
+            modified_query_params.pop("sort")
+            request_data = TinyShield(model).block(modified_query_params)
+            request_data["sort"] = self.request.query_params.get("sort")
+        else:
+            request_data = TinyShield(model).block(self.request.query_params)
+        # since publication_date requires a variable that we can't check for using the enum check, we're doing it seperately
+        return Pagination(
+            page=request_data["page"],
+            limit=request_data["limit"],
+            lower_limit=(request_data["page"] - 1) * request_data["limit"],
+            upper_limit=(request_data["page"] * request_data["limit"]),
+            sort_key=request_data.get("sort", default_sort_column),
+            sort_order=request_data["order"],
         )
