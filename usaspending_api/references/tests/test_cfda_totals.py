f74579ed96123379b85301ed8d8e012c8a7d6c9b
brett-varney <brett.varney@kc.frb.org>
2021-01-19 10:29:45 -0600

    dev-6087 wip monkeypatch isn't working either

--- usaspending_api/references/tests/test_cfda_totals.py
+++ usaspending_api/references/tests/test_cfda_totals.py
@@ -1,32 +1,36 @@
 import pytest
-import requests
 from mock import patch, Mock
 from rest_framework import status
 
-# @patch("requests.post")
-# def mock503():
-#     return {"status_code": status.HTTP_503_SERVICE_UNAVAILABLE}
-
-
-class Mock503:
-    @staticmethod
-    def status_code():
-        return status.HTTP_503_SERVICE_UNAVAILABLE
-
-    @staticmethod
-    def json():
-        return {"cfdas": None}
-
+@patch("requests.post")
+def mock503():
+    return {"status_code": status.HTTP_503_SERVICE_UNAVAILABLE}
 
 @pytest.mark.django_db
-def test_service_unavailable(client, monkeypatch):
-    def mock_post(*args, **kwargs):
-        return Mock503()
-
-    monkeypatch.setattr(requests, "post", mock_post)
-
+def test_service_unavailable(client):
     with pytest.raises(Exception) as e_info:
         client.get("/api/v2/references/cfda/totals/")
     print(e_info)
     assert e_info == 0
 
+# class Mock503:
+#     @staticmethod
+#     def status_code():
+#         return status.HTTP_503_SERVICE_UNAVAILABLE
+
+#     @staticmethod
+#     def json():
+#         return {"cfdas": None}
+
+
+# @pytest.mark.django_db
+# def test_service_unavailable(client, monkeypatch):
+#     def mock_post(*args, **kwargs):
+#         return Mock503()
+
+#     monkeypatch.setattr(requests, "post", mock_post)
+
+#     with pytest.raises(Exception) as e_info:
+#         client.get("/api/v2/references/cfda/totals/")
+#     print(e_info)
+#     assert e_info == 0
