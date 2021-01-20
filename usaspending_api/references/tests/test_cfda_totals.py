bc30213f30c53692fb578a971f6db41a6325066f
brett-varney <brett.varney@kc.frb.org>
2021-01-20 08:14:58 -0600

    dev-6087 wip

--- usaspending_api/references/tests/test_cfda_totals.py
+++ usaspending_api/references/tests/test_cfda_totals.py
@@ -1,14 +1,30 @@
 import pytest
-import mock
+from mock import patch, MagicMock
 from rest_framework import status
-from usaspending_api.common.exceptions import NoDataFoundException, InternalServerError, ServiceUnavailable
 
 
-@mock.patch(
+@patch(
     "requests.post",
-    mock.MagicMock(return_value={"status_code": status.HTTP_503_SERVICE_UNAVAILABLE}),
+    MagicMock(return_value={"cfdas": []}),
 )
 @pytest.mark.django_db
 def test_service_unavailable(client):
     response = client.get("/api/v2/references/cfda/totals/")
-    assert response.json()["detail"] == "https://www.grants.gov/grantsws/rest/opportunities/search/cfda/totals not available (status 503)"
+    print(response.json())
+    assert (
+        response.json()["detail"]
+        == "https://www.grants.gov/grantsws/rest/opportunities/search/cfda/totals not available (status 503)"
+    )
+
+
+# @patch(
+#     "requests.post",
+#     MagicMock(return_value={"status_code": status.HTTP_503_SERVICE_UNAVAILABLE}),
+# )
+# @pytest.mark.django_db
+# def test_service_unavailable(client):
+#     response = client.get("/api/v2/references/cfda/totals/")
+#     assert (
+#         response.json()["detail"]
+#         == "https://www.grants.gov/grantsws/rest/opportunities/search/cfda/totals not available (status 503)"
+#     )
