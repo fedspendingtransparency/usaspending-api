e65d6d7d79fef58c236bc1d7a187aea7407ddcb6
sethstoudenmier <stoudenmier_seth@bah.com>
2021-02-05 09:33:03 -0800

    [DEV-6713] Update reporting agency SQL to only grab closed submissions

--- usaspending_api/reporting/management/sql/populate_reporting_agency_missing_tas.sql
+++ usaspending_api/reporting/management/sql/populate_reporting_agency_missing_tas.sql
@@ -11,17 +11,53 @@ INSERT INTO public.reporting_agency_missing_tas (
 
 WITH missing AS (
     SELECT
-        gtas.id
-    FROM appropriation_account_balances AS aab 
-    INNER JOIN submission_attributes AS sa
-        ON aab.submission_id = sa.submission_id
-    INNER JOIN dabs_submission_window_schedule AS dsws
-        ON sa.submission_window_id = dsws.id
-        AND dsws.submission_reveal_date <= now()
-    RIGHT OUTER JOIN gtas_sf133_balances AS gtas
-        ON sa.reporting_fiscal_period = gtas.fiscal_period
-        AND sa.reporting_fiscal_year = gtas.fiscal_year
-        AND aab.treasury_account_identifier = gtas.treasury_account_identifier
+        limited_gtas.id
+    FROM appropriation_account_balances AS aab
+    INNER JOIN (
+        SELECT
+            sa.submission_id,
+            sa.reporting_fiscal_period,
+            sa.reporting_fiscal_year
+        FROM
+            submission_attributes AS sa
+        INNER JOIN dabs_submission_window_schedule AS dsws
+            ON sa.submission_window_id = dsws.id
+            AND dsws.submission_reveal_date <= now()
+    ) AS limited_sa ON (aab.submission_id = limited_sa.submission_id)
+    RIGHT OUTER JOIN (
+        SELECT
+            gtas.id,
+            gtas.fiscal_year,
+            gtas.fiscal_period,
+            gtas.treasury_account_identifier
+        FROM
+            gtas_sf133_balances AS gtas
+        WHERE
+            /*
+                ----- For GTAS that are not associated to a submission. -----
+                Check that there is a submission_reveal_date that is prior to the current
+                date for the fiscal_year and fiscal_period on the GTAS record. Since GTAS
+                are submitted each period and we have no way to tie them back to a
+                submission window this is a work around to try and limit them to only
+                closed submissions.
+            */
+            EXISTS (
+                SELECT 1
+                FROM dabs_submission_window_schedule dsws
+                WHERE dsws.submission_reveal_date <= now()
+                    AND (
+                        gtas.fiscal_year < dsws.submission_fiscal_year
+                        OR (
+                            gtas.fiscal_year = dsws.submission_fiscal_year
+                            AND gtas.fiscal_period <= dsws.submission_fiscal_month
+                        )
+                    )
+            )
+    ) AS limited_gtas ON (
+        limited_sa.reporting_fiscal_period = limited_gtas.fiscal_period
+        AND limited_sa.reporting_fiscal_year = limited_gtas.fiscal_year
+        AND aab.treasury_account_identifier = limited_gtas.treasury_account_identifier
+    )
     WHERE
         aab.submission_id IS NULL
 )
--- usaspending_api/reporting/management/sql/populate_reporting_agency_tas.sql
+++ usaspending_api/reporting/management/sql/populate_reporting_agency_tas.sql
@@ -19,7 +19,7 @@ FROM (
             treasury_account_identifier AS tas_id,
             SUM(obligations_incurred_total_by_tas_cpe) AS appropriation_obligated_amount
         FROM appropriation_account_balances AS aab
-        JOIN submission_attributes AS sa
+        INNER JOIN submission_attributes AS sa
             ON sa.submission_id = aab.submission_id
         INNER JOIN dabs_submission_window_schedule AS dsws
             ON sa.submission_window_id = dsws.id
@@ -32,7 +32,7 @@ FROM (
             treasury_account_id AS tas_id,
             SUM(obligations_incurred_by_program_object_class_cpe) AS object_class_pa_obligated_amount
         FROM financial_accounts_by_program_activity_object_class AS fapaoc
-        JOIN submission_attributes AS sa
+        INNER JOIN submission_attributes AS sa
             ON sa.submission_id = fapaoc.submission_id
         INNER JOIN dabs_submission_window_schedule AS dsws
             ON sa.submission_window_id = dsws.id
@@ -47,12 +47,12 @@ FROM (
         object_class_pa_obligated_amount,
         (appropriation_obligated_amount - object_class_pa_obligated_amount) AS diff_approp_ocpa_obligated_amounts
     FROM summed_appropriation AS sa
-    JOIN summed_object_class_program_activity AS socpa
+    INNER JOIN summed_object_class_program_activity AS socpa
         ON socpa.tas_id = sa.tas_id
         AND socpa.fiscal_period = sa.fiscal_period
         AND socpa.fiscal_year = sa.fiscal_year
-    JOIN treasury_appropriation_account AS taa
+    INNER JOIN treasury_appropriation_account AS taa
         ON sa.tas_id = taa.treasury_account_identifier
-    JOIN toptier_agency AS ta
+    INNER JOIN toptier_agency AS ta
         ON taa.funding_toptier_agency_id = ta.toptier_agency_id
 ) AS reporting_agency_tas_content;
