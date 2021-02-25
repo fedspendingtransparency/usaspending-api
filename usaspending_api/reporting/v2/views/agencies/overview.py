e5a69831f8291b0b808f8736155270d562efb62b
Tony Sappe <tony.sappe@tuta.io>
2021-02-25 12:19:01 -0700

    [DEV-6948] assurance statement only generated when submission exists

--- usaspending_api/reporting/v2/views/agencies/overview.py
+++ usaspending_api/reporting/v2/views/agencies/overview.py
@@ -183,7 +183,9 @@ class AgenciesOverview(AgencyBase, PaginationMixin):
                 "obligation_difference": result["obligation_difference"],
                 "unlinked_contract_award_count": result["unlinked_contract_award_count"],
                 "unlinked_assistance_award_count": result["unlinked_assistance_award_count"],
-                "assurance_statement_url": self.create_assurance_statement_url(result),
+                "assurance_statement_url": self.create_assurance_statement_url(result)
+                if result["recent_publication_date"]
+                else None,
             }
             for result in result_list
         ]
