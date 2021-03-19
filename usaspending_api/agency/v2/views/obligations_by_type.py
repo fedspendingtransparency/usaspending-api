77b8989c6d3fa9929bd7fdec3fe7e16a9f8defe2
Brett Varney <brett.varney@kc.frb.org>
2021-03-19 14:27:35 -0500

    dev-6615 wip just starting

--- usaspending_api/agency/v2/views/obligations_by_type.py
+++ usaspending_api/agency/v2/views/obligations_by_type.py
@@ -1,34 +1,50 @@
-from django.db.models import Exists, OuterRef, Q
 from rest_framework.request import Request
 from rest_framework.response import Response
-from typing import Any
 from usaspending_api.agency.v2.views.agency_base import AgencyBase
+from usaspending_api.awards.models import TransactionNormalized
 from usaspending_api.common.cache_decorator import cache_response
-from usaspending_api.financial_activities.models import FinancialAccountsByProgramActivityObjectClass
-from usaspending_api.references.models import ObjectClass
+
+# from django.db.models import Exists, OuterRef, Q
+# from typing import Any
+# from usaspending_api.financial_activities.models import FinancialAccountsByProgramActivityObjectClass
+# from usaspending_api.references.models import ObjectClass
 
 
 class ObligationsByType(AgencyBase):
     """
-    Obtain the count of object classes for a specific agency in a single
-    fiscal year based on whether or not that object class has ever
-    been submitted in File B.
+    Returns a breakdown of obligations by award category (contracts, IDVs, grants, loans, direct payments, other) within the requested fiscal year (or current FY).
     """
 
-    endpoint_doc = "usaspending_api/api_contracts/contracts/v2/agency/toptier_code/object_class/count.md"
+    endpoint_doc = "usaspending_api/api_contracts/contracts/v2/agency/toptier_code/obligations_by_type.md"
 
     @cache_response()
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         return Response(
             {
-                "toptier_code": self.toptier_code,
-                "fiscal_year": self.fiscal_year,
-                "object_class_count": self.get_object_class_count(),
+                "total_aggregated_amount": self.total_amount,
+                "results": self.get_category_amounts(),
                 "messages": self.standard_response_messages,
             }
         )
 
-    def get_object_class_count(self):
+    def get_category_amounts(self):
+
+
+
+        queryset = TransactionNormalized.objects.filter(
+            federal_action_obligation__isnull=False, fiscal_year=fiscal_year, awarding_agency_id=awarding_agency_id
+        ).annotate(
+            award_category=F("award__category"),
+            recipient_name=Coalesce(
+                F("award__latest_transaction__assistance_data__awardee_or_recipient_legal"),
+                F("award__latest_transaction__contract_data__awardee_or_recipient_legal"),
+            ),
+        )
+
+
+
+
+
         filters = [
             Q(object_class_id=OuterRef("pk")),
             Q(final_of_fy=True),
