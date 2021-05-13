857aeb60d9e2c38c941bdb8c2110bf1bea311cf6
Emily Brents <brents_emily@bah.com>
2021-05-13 13:29:55 -0400

    Update spending_by_award.py

--- usaspending_api/search/v2/views/spending_by_award.py
+++ usaspending_api/search/v2/views/spending_by_award.py
@@ -363,6 +363,9 @@ class SpendingByAwardVisualizationViewSet(APIView):
                         for x in row.get("COVID-19 Outlays")
                     ]
                 )
+            if row.get("def_codes"):
+                if self.filters.get("def_codes"):
+                    row["def_codes"] = filter(lambda x: x in self.filters.get("def_codes"), row["def_codes"])
             row["generated_internal_id"] = hit["generated_unique_award_id"]
             row["recipient_id"] = hit.get("recipient_unique_id")
             row["parent_recipient_unique_id"] = hit.get("parent_recipient_unique_id")
