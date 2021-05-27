cea4a8539be07c4cd8404fd7fc1e51bc70a9dbac
Emily Brents <brents_emily@bah.com>
2021-05-27 10:08:53 -0400

    [DEV-6618] switch to recipient_agency table

--- usaspending_api/agency/v2/views/recipients.py
+++ usaspending_api/agency/v2/views/recipients.py
@@ -24,23 +24,15 @@ class RecipientList(AgencyBase):
 
     def get_recipients_queryset(self):
         SQL="""
-        with recipient_cte as (
-            select awarding_agency_id, recipient_hash, sum(generated_pragmatic_obligation) as recipient_amount
-            from transaction_search
-            where fiscal_year={fiscal_year} and
-                awarding_agency_id={agency_id} and
-                action_date >= '2007-10-01'
-            group by awarding_agency_id, recipient_hash
-        )
         select
         count(recipient_hash),
         max(recipient_amount),
         min(recipient_amount),
-        percentile_disc(0.25) within group (order by recipient_cte.recipient_amount) as "25th_percentile",
-        percentile_disc(0.5) within group (order by recipient_cte.recipient_amount) as "50th_percentile",
-        percentile_disc(0.75) within group (order by recipient_cte.recipient_amount) as "75th_percentile"
-        from recipient_cte group by awarding_agency_id;
-        """.format(fiscal_year=self.fiscal_year, agency_id=self.agency_id)
+        percentile_disc(0.25) within group (order by recipient_agency.recipient_amount) as "25th_percentile",
+        percentile_disc(0.5) within group (order by recipient_agency.recipient_amount) as "50th_percentile",
+        percentile_disc(0.75) within group (order by recipient_agency.recipient_amount) as "75th_percentile"
+        from recipient_agency  where fiscal_year={fiscal_year} and toptier_code='{toptier_code}';
+        """.format(fiscal_year=self.fiscal_year, toptier_code=self.toptier_code)
 
         results = execute_sql_to_ordered_dictionary(SQL)
         return {
@@ -50,46 +42,3 @@ class RecipientList(AgencyBase):
             "messages": self.standard_response_messages
         }
         return results
-        # results = (
-        #     TransactionSearch.objects.filter(
-        #         funding_agency_id=self.agency_id,
-        #         fiscal_year=self.fiscal_year,
-        #         recipient_unique_id__isnull=False,
-        #         action_date__gte="2007-10-01",
-        #     )
-        #     .values("recipient_unique_id")
-        #     .annotate(recipient_value=Sum("generated_pragmatic_obligation", output_field=FloatField()))
-        #     .values_list("recipient_value", flat=True)
-        # )
-        # if len(results) > 0:
-        #     count = len(results)
-        #     recipient_max = max(results)
-        #     recipient_min = min(results)
-        #     numpy.amax(count)
-        #     percentile_25 = numpy.percentile(results, 25)
-        #     percentile_50 = numpy.percentile(results, 50)
-        #     percentile_75 = numpy.percentile(results, 75)
-        #     formatted_results = {
-        #         "toptier_code": self.toptier_code,
-        #         "fiscal_year": self.fiscal_year,
-        #         "count": count,
-        #         "max": recipient_max,
-        #         "min": recipient_min,
-        #         "25th_percentile": round(percentile_25, 2),
-        #         "50th_percentile": round(percentile_50, 2),
-        #         "75th_percentile": round(percentile_75, 2),
-        #         "messages": self.standard_response_messages,
-        #     }
-        # else:
-        #     formatted_results = {
-        #         "toptier_code": self.toptier_code,
-        #         "fiscal_year": self.fiscal_year,
-        #         "count": 0,
-        #         "max": None,
-        #         "min": None,
-        #         "25th_percentile": None,
-        #         "50th_percentile": None,
-        #         "75th_percentile": None,
-        #         "messages": self.standard_response_messages,
-        #     }
-        # return formatted_results
