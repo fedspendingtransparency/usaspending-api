729dca1137ddcec5d44215a3437e81010bbc27f4
Emily Brents <brents_emily@bah.com>
2021-06-10 11:37:19 -0400

    [DEV=7359] we don't really need an id field huh

--- usaspending_api/recipient/migrations/0007_recipientagency.py
+++ usaspending_api/recipient/migrations/0007_recipientagency.py
@@ -13,10 +13,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='RecipientAgency',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('fiscal_year', models.IntegerField(db_index=True)),
                 ('toptier_code', models.TextField(db_index=True)),
-                ('recipient_hash', models.UUIDField(db_index=True)),
+                ('recipient_hash', models.UUIDField(primary_key=True, db_index=True)),
                 ('recipient_name', models.TextField()),
                 ('recipient_amount', models.DecimalField(decimal_places=2, max_digits=23)),
             ],
