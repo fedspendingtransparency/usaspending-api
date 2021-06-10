d367f38f12b41e18271bb913b4b5060af79d722b
Emily Brents <brents_emily@bah.com>
2021-06-10 15:32:58 -0400

    Update test_recipients.py

--- usaspending_api/agency/tests/integration/test_recipients.py
+++ usaspending_api/agency/tests/integration/test_recipients.py
@@ -259,8 +259,8 @@ def test_basic_success(client, monkeypatch, recipient_agency_data):
         "max": 5113080.0,
         "min": 1079.1,
         "25th_percentile": 30654.0,
-        "50th_percentile": 11721.0,
-        "75th_percentile": 3465.0,
+        "50th_percentile": 3465.0,
+        "75th_percentile": 11721.0,
         "messages": [],
     }
     assert resp.json() == expected_results
