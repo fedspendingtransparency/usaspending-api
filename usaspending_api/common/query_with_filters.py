634881ee7bb703663a9ef46eace11a4f6d6f2913
Wil Collins <collins_william@bah.com>
2021-06-15 15:37:05 -0400

    [DEV-3021] Updates to use array and adds regex in filter

--- usaspending_api/common/query_with_filters.py
+++ usaspending_api/common/query_with_filters.py
@@ -380,7 +380,9 @@ class _ProgramNumbers(_Filter):
 
         for v in filter_values:
             if query_type == _QueryType.AWARDS:
-                programs_numbers_query.append(ES_Q("match", **{"cfdas.breakdown": v}))
+                escaped_program_number = v.replace(".", "\\.")
+                r = f""".*\\"cfda_number\\": \\"{escaped_program_number}\\".*"""
+                programs_numbers_query.append(ES_Q("regexp", cfdas=r))
             else:
                 programs_numbers_query.append(ES_Q("match", cfda_number=v))
 
