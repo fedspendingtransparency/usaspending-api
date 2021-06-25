a1fea7c62f6edea26b508c151f929535794d85d9
Emily Brents <brents_emily@bah.com>
2021-06-25 14:25:33 -0400

    [DEV-7518] set up office table

---
+++ usaspending_api/references/models/office.py
@@ -0,0 +1,26 @@
+from django.db import models
+from usaspending_api.common.models import DataSourceTrackedModel
+
+
+class Office(DataSourceTrackedModel):
+    # op.create_index(op.f('ix_office_agency_code'), 'office', ['agency_code'], unique=False)
+    # op.create_index(op.f('ix_office_office_code'), 'office', ['office_code'], unique=True)
+    # op.create_index(op.f('ix_office_sub_tier_code'), 'office', ['sub_tier_code'], unique=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    office_code = models.TextField(null=False)
+    office_name = models.TextField(null=False)
+    sub_tier_code = models.TextField(null=False)
+    agency_code = models.TextField(null=False)
+    contract_awards_office = models.BooleanField(null=False)
+    contract_funding_office = models.BooleanField(null=False)
+    financial_assistance_awards_office = models.BooleanField(null=False)
+    financial_assistance_funding_office = models.BooleanField(null=False)
+
+    class Meta:
+        managed = True
+        db_table = "office"
+
+    def __str__(self):
+        return "%s" % self.office_name
