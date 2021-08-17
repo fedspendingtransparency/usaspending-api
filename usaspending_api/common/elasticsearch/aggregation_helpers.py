4918d7587c3d72209daeb2d22c6d03cd61ae848b
Wil Collins <collins_william@bah.com>
2021-08-17 17:01:07 -0400

    [DEV-7501] Fixes null case of create_count_aggregation and adds libc6 to travis apt packages

--- usaspending_api/common/elasticsearch/aggregation_helpers.py
+++ usaspending_api/common/elasticsearch/aggregation_helpers.py
@@ -10,7 +10,7 @@ def create_count_aggregation(field_name):
         "scripted_metric",
         params={"fieldName": field_name},
         init_script="state.list = []",
-        map_script="if(doc[params.fieldName] != null) state.list.add(doc[params.fieldName].value);",
+        map_script="if(doc[params.fieldName].size() > 0) state.list.add(doc[params.fieldName].value);",
         combine_script="return state.list;",
         reduce_script="Map uniqueValueMap = new HashMap(); int count = 0;for(shardList in states) {if(shardList != null) { for(key in shardList) {if(!uniqueValueMap.containsKey(key)) {count +=1;uniqueValueMap.put(key, key); }}}}  return count;",
     )
