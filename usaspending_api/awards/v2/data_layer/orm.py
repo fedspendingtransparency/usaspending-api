70990b9c94e0a4a31cfd20564c9828168e9cedeb
Emily Brents <brents_emily@bah.com>
2021-09-28 09:20:02 -0400

    [DEV-7487] simplify sql

--- usaspending_api/awards/v2/data_layer/orm.py
+++ usaspending_api/awards/v2/data_layer/orm.py
@@ -614,9 +614,9 @@ def fetch_account_details_award(award_id: int) -> dict:
 def fetch_total_outlays(award_id: int) -> dict:
     sql = """
     with date_signed_outlay_amounts (award_id, last_period_total_outlay) as (
-        SELECT faba. award_id, COALESCE(sum(CASE WHEN sa.is_final_balances_for_fy = TRUE THEN (COALESCE(faba.gross_outlay_amount_by_award_cpe,0)
+        SELECT faba. award_id, COALESCE(sum(COALESCE(faba.gross_outlay_amount_by_award_cpe,0)
             + COALESCE(faba.ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe, 0)
-            + COALESCE(faba.ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe, 0)) END), 0) as last_period_total_outlay
+            + COALESCE(faba.ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe, 0)), 0) as last_period_total_outlay
         FROM
             financial_accounts_by_awards faba
         INNER JOIN submission_attributes sa
@@ -635,11 +635,7 @@ def fetch_total_outlays(award_id: int) -> dict:
         financial_accounts_by_awards faba
     INNER JOIN submission_attributes sa
         ON faba.submission_id = sa.submission_id
-    INNER JOIN awards a
-        ON faba.award_id = a.id
-        AND a.date_signed >= '2019-10-01'
-    INNER JOIN date_signed_outlay_amounts o ON faba.award_id = o.award_id AND o.last_period_total_outlay != 0
-    WHERE {award_id_sql};
+    INNER JOIN date_signed_outlay_amounts o ON faba.award_id = o.award_id AND o.last_period_total_outlay != 0;
     """
     results = execute_sql_to_ordered_dictionary(sql.format(award_id_sql=f"faba.award_id = {award_id}"))
     if len(results) > 0:
