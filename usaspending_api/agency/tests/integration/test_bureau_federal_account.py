fd5437174d7cf63ce987260da26b3b9e92ad22f8
Emily Brents <brents_emily@bah.com>
2021-11-29 13:36:22 -0500

    [DEV-8044] tests

--- usaspending_api/agency/tests/integration/test_bureau_federal_account.py
+++ usaspending_api/agency/tests/integration/test_bureau_federal_account.py
@@ -33,6 +33,50 @@ def test_federal_account_list_success(client, monkeypatch, bureau_data, helpers)
             }
         ],
     }
-    print(resp.json())
     assert resp.status_code == status.HTTP_200_OK
     assert resp.json() == expected_result
+
+
+@pytest.mark.django_db
+def test_alternate_year(client, bureau_data):
+    resp = client.get(url.format(toptier_code="001", bureau_slug="test-bureau", query_params="?fiscal_year=2018"))
+    assert resp.status_code == status.HTTP_200_OK
+
+    expected_results = [
+        {
+            "name": "FA 1",
+            "id": "001-0000",
+            "total_obligations": 20.0,
+            "total_outlays": 200.0,
+            "total_budgetary_resources": 2000.0,
+        }
+    ]
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json()["results"] == expected_results
+
+
+@pytest.mark.django_db
+def test_alternate_agency(client, bureau_data):
+    resp = client.get(url.format(toptier_code="002", bureau_slug="test-bureau-2", query_params="?fiscal_year=2018"))
+    assert resp.status_code == status.HTTP_200_OK
+
+    expected_results = [
+        {
+            "name": "FA 2",
+            "id": "002-0000",
+            "total_obligations": 20.0,
+            "total_outlays": 200.0,
+            "total_budgetary_resources": 2000.0,
+        }
+    ]
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json()["results"] == expected_results
+
+
+@pytest.mark.django_db
+def test_invalid_agency(client, bureau_data):
+    resp = client.get(url.format(toptier_code="XXX", bureau_slug="test-bureau", query_params="?fiscal_year=2021"))
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
+
+    resp = client.get(url.format(toptier_code="999", bureau_slug="1234@#$@#", query_params="?fiscal_year=2021"))
+    assert resp.status_code == status.HTTP_404_NOT_FOUND
