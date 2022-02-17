a5acb6eab70ecaa7ccaa50a1d29a08f64bcf3d47
Wil Collins <collins_william@bah.com>
2022-02-17 13:24:55 -0500

    [DEV-8434] Updates url regex to tighten requirements

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r"^duns/$", ListRecipientsByDuns.as_view()),
     url(r"^count/$", RecipientCount.as_view()),
     url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverView.as_view()),
-    url(r"^children/(?P<duns_or_uei>[0-9a-zA-Z]{9,12})/$", ChildRecipients.as_view()),
+    # Regex below accepts 9 numberic char DUNS or 12 alpha-numeric char UEI
+    url(r"^children\/(?P<duns_or_uei>(?:[0-9]{9}|[0-9a-zA-Z]{12}))\/$", ChildRecipients.as_view()),
     url(r"^state/(?P<fips>[0-9]{,2})/$", StateMetaDataViewSet.as_view()),
     url(r"^state/awards/(?P<fips>[0-9]{,2})/$", StateAwardBreakdownViewSet.as_view()),
     url(r"^state/$", ListStates.as_view()),
