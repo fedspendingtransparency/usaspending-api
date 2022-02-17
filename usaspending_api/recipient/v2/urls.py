38d1664af704a30d812859877c2b7f41e8c5ec01
Wil Collins <collins_william@bah.com>
2022-02-17 15:17:03 -0500

    [DEV-8434] Removes extra slashes from url regex

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r"^count/$", RecipientCount.as_view()),
     url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverView.as_view()),
     # Regex below accepts 9 numberic char DUNS or 12 alpha-numeric char UEI
-    url(r"^children\/(?P<duns_or_uei>(?:[0-9]{9}|[0-9a-zA-Z]{12}))\/$", ChildRecipients.as_view()),
+    url(r"^children/(?P<duns_or_uei>(?:[0-9]{9}|[0-9a-zA-Z]{12}))/$", ChildRecipients.as_view()),
     url(r"^state/(?P<fips>[0-9]{,2})/$", StateMetaDataViewSet.as_view()),
     url(r"^state/awards/(?P<fips>[0-9]{,2})/$", StateAwardBreakdownViewSet.as_view()),
     url(r"^state/$", ListStates.as_view()),
