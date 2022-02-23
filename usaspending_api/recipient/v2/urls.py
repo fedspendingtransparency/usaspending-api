66335ab568c478c41c2169ba747cd0f91708fc0b
bkschmidt <94720208+bkschmidt@users.noreply.github.com>
2022-02-23 09:26:31 -0500

    Update urls.py
    updated urls to avoid conflict with qat

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
 from usaspending_api.recipient.v2.views.states import StateMetaDataViewSet, StateAwardBreakdownViewSet, ListStates
-from usaspending_api.recipient.v2.views.recipients import RecipientOverView, RecipientOverViewDuns
+from usaspending_api.recipient.v2.views.recipients import RecipientOverView
 from usaspending_api.recipient.v2.views.recipients import ChildRecipients
-from usaspending_api.recipient.v2.views.list_recipients import ListRecipients, RecipientCount
+from usaspending_api.recipient.v2.views.list_recipients import ListRecipients, ListRecipientsByDuns, RecipientCount
 
 urlpatterns = [
-    url(r"^duns/$", ListRecipients.as_view()),
+    url(r"^$", ListRecipients.as_view()),
+    url(r"^duns/$", ListRecipientsByDuns.as_view()),
     url(r"^count/$", RecipientCount.as_view()),
     url(r"^(?P<recipient_id>.*)/$", RecipientOverView.as_view()),
-    url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverViewDuns.as_view()),
+    url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverView.as_view()),
     url(r"^children/(?P<duns>[0-9]{9})/$", ChildRecipients.as_view()),
     url(r"^state/(?P<fips>[0-9]{,2})/$", StateMetaDataViewSet.as_view()),
     url(r"^state/awards/(?P<fips>[0-9]{,2})/$", StateAwardBreakdownViewSet.as_view()),
