caab9c54a014e9dc06697d1cdfedf9ee5dc4862a
Emily Brents <brents_emily@bah.com>
2022-02-28 17:17:57 -0500

    [DEV_8370] fix agency

--- usaspending_api/recipient/management/sql/recipient_agency.sql
+++ usaspending_api/recipient/management/sql/recipient_agency.sql
@@ -29,8 +29,8 @@ FROM (
 
     GROUP BY toptier_code,  recipient_lookup.legal_business_name, COALESCE(recipient_lookup.recipient_hash, MD5(UPPER(
         CASE
-          WHEN COALESCE(transaction_fpds.awardee_or_recipient_uniqu, transaction_fabs.awardee_or_recipient_uniqu) IS NOT NULL THEN CONCAT('duns-', COALESCE(transaction_fpds.awardee_or_recipient_uniqu, transaction_fabs.awardee_or_recipient_uniqu))
           WHEN COALESCE(transaction_fpds.awardee_or_recipient_uei, transaction_fabs.uei) IS NOT NULL THEN CONCAT('uei-', COALESCE(transaction_fpds.awardee_or_recipient_uei, transaction_fabs.uei))
+          WHEN COALESCE(transaction_fpds.awardee_or_recipient_uniqu, transaction_fabs.awardee_or_recipient_uniqu) IS NOT NULL THEN CONCAT('duns-', COALESCE(transaction_fpds.awardee_or_recipient_uniqu, transaction_fabs.awardee_or_recipient_uniqu))
           ELSE CONCAT('name-', COALESCE(transaction_fpds.awardee_or_recipient_legal, transaction_fabs.awardee_or_recipient_legal)) END
       ))::uuid), fiscal_year
     HAVING SUM(COALESCE(CASE WHEN transaction_normalized.type IN('07','08') THEN transaction_normalized.original_loan_subsidy_cost ELSE transaction_normalized.federal_action_obligation END, 0)) > 0) as recipient_agency_content;
--- usaspending_api/recipient/management/sql/recipient_lookup/070_sam_parents_missing_name.sql
+++ usaspending_api/recipient/management/sql/recipient_lookup/070_sam_parents_missing_name.sql
@@ -11,7 +11,7 @@ INSERT INTO public.temporary_restock_recipient_lookup (
   parent_legal_business_name
 )
 SELECT
-  DISTINCT ON (ultimate_parent_unique_ide)
+  DISTINCT ON (ultimate_parent_unique_ide, ultimate_parent_uei)
   MD5(UPPER(
     CASE WHEN ultimate_parent_uei IS NOT NULL THEN CONCAT('uei-', ultimate_parent_uei)
     ELSE  CONCAT('duns-', ultimate_parent_unique_ide) END
@@ -26,5 +26,5 @@ SELECT
   UPPER(ultimate_parent_legal_enti) AS parent_legal_business_name
 FROM duns
 WHERE ultimate_parent_unique_ide IS NOT NULL
-ORDER BY ultimate_parent_unique_ide, update_date DESC
+ORDER BY ultimate_parent_unique_ide, ultimate_parent_uei, update_date DESC
 ON CONFLICT (recipient_hash) DO NOTHING;
