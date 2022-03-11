5080e3f7b1067897098ee0647f4775381cbb835b
bkschmidt <94720208+bkschmidt@users.noreply.github.com>
2022-03-11 15:02:16 -0500

    Update subcomponents.py
    initial commit

--- usaspending_api/agency/v2/views/subcomponents.py
+++ usaspending_api/agency/v2/views/subcomponents.py
@@ -126,10 +126,12 @@ class SubcomponentList(PaginationMixin, AgencyBase):
             Q(submission__reporting_fiscal_period=latest[0]["max_fiscal_period"]),
         ]
 
+        topcode_exclusion_list =["1125","047","420","075"]
+
         bureau_info_subquery = Subquery(
             BureauTitleLookup.objects.filter(
                 federal_account_code=OuterRef(f"{treasury_account_keyword}__federal_account__federal_account_code")
-            )
+            ).exclude(federal_account_code__in=topcode_exclusion_list)
             .annotate(
                 bureau_info=Case(
                     When(
