8584e9dbd5d8cfeea8a088a410c187607109c1aa
bkschmidt <94720208+bkschmidt@users.noreply.github.com>
2022-03-11 07:28:48 -0500

    Update urls.py
    changed views for old endpoint.

--- usaspending_api/recipient/v2/urls.py
+++ usaspending_api/recipient/v2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from usaspending_api.recipient.v2.views.states import StateMetaDataViewSet, StateAwardBreakdownViewSet, ListStates
-from usaspending_api.recipient.v2.views.recipients import RecipientOverView
+from usaspending_api.recipient.v2.views.recipients import RecipientOverViewDuns, RecipientOverView
 from usaspending_api.recipient.v2.views.recipients import ChildRecipients
 from usaspending_api.recipient.v2.views.list_recipients import ListRecipients, ListRecipientsByDuns, RecipientCount
 
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r"^$", ListRecipients.as_view()),
     url(r"^duns/$", ListRecipientsByDuns.as_view()),
     url(r"^count/$", RecipientCount.as_view()),
-    url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverView.as_view()),
+    url(r"^duns/(?P<recipient_id>.*)/$", RecipientOverViewDuns.as_view()),
     url(r"^children/(?P<duns>[0-9]{9})/$", ChildRecipients.as_view()),
     url(r"^state/(?P<fips>[0-9]{,2})/$", StateMetaDataViewSet.as_view()),
     url(r"^state/awards/(?P<fips>[0-9]{,2})/$", StateAwardBreakdownViewSet.as_view()),
