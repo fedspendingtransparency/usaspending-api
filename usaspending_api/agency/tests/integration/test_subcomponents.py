16fb25c3fd21a2b3cdcb948d7d98a65604d532cd
bkschmidt <94720208+bkschmidt@users.noreply.github.com>
2022-03-30 10:54:54 -0400

    [DEV 8507] Update test case

--- usaspending_api/agency/tests/integration/test_subcomponents.py
+++ usaspending_api/agency/tests/integration/test_subcomponents.py
@@ -70,12 +70,12 @@ def test_invalid_agency(client, bureau_data):
 @pytest.mark.django_db
 def test_exclusion_bureau_codes(client, bureau_data):
     resp = client.get(url.format(toptier_code="0004", filter="?fiscal_year=2021"))
-    assert resp.status_code == status.HTTP_400_OK
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
 
     expected_results = [
         {
             "name": "",
         }
     ]
-    assert resp.status_code == status.HTTP_400_OK
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
     assert resp.json()["results"] == expected_results
