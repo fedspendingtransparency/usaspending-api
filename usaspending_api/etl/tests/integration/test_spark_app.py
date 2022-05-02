1b8386427afe6a457777c4bb7d72fce9eb48f29b
Keith Hickey <hickey_keith@bah.com>
2022-05-02 11:50:16 -0400

    [PIPE-278] Adding fixture to cleanup delta lake unit test data, and asserts in the test that is testing delta lake data creation

--- usaspending_api/etl/tests/integration/test_spark_app.py
+++ usaspending_api/etl/tests/integration/test_spark_app.py
@@ -52,6 +52,7 @@ SPARK_SESSION_JARS = [
     f"io.delta:delta-core_{_SCALA_VERSION}:{_DELTA_VERSION}",
 ]
 
+DELTA_LAKE_UNITTEST_SCHEMA_NAME = "unittest"
 
 @fixture(scope="session")
 def minio_test_data_bucket():
@@ -129,6 +130,21 @@ def spark() -> SparkSession:
     stop_spark_context()
 
 
+@fixture
+def delta_lake_unittest_schema(spark: SparkSession):
+    """Specify which Delta 'SCHEMA' to use (NOTE: 'SCHEMA' and 'DATABASE' are interchangeable in Delta Spark SQL),
+    and cleanup any objects created in the schema after the test run."""
+
+    # Force default usage of the unittest schema in this SparkSession
+    spark.sql(f"USE {DELTA_LAKE_UNITTEST_SCHEMA_NAME}")
+
+    # Yield the name of the db that test delta lake tables and records should be put in.
+    yield DELTA_LAKE_UNITTEST_SCHEMA_NAME
+
+    # Cascade will remove the tables and functions in this SCHEMA
+    spark.sql(f"DROP SCHEMA IF EXISTS {DELTA_LAKE_UNITTEST_SCHEMA_NAME} CASCADE")
+
+
 def test_spark_app_run_local_master(spark: SparkSession):
     """Execute a simple spark app and verify it logged expected output.
     Effectively if it runs without failing, it worked.
@@ -200,72 +216,89 @@ def _transaction_and_award_test_data(db):
 def test_spark_write_to_s3_delta_from_db(
     _transaction_and_award_test_data,
     spark: SparkSession,
+    delta_lake_unittest_schema,
     minio_test_data_bucket,
     request,
 ):
-    PARTITION_ROWS = 10000
-    JDBC_CONN_PROPS = {"driver": "org.postgresql.Driver", "fetchsize": str(PARTITION_ROWS)}
+    """Test that we can read from Postgres DB and write to new delta tables,
+    and the tables are created and data gets there"""
+    jdbc_conn_props = {"driver": "org.postgresql.Driver", "fetchsize": str(CONFIG.PARTITION_SIZE)}
 
     pg_uri = get_database_dsn_string()
     jdbc_url = get_jdbc_url_from_pg_uri(pg_uri)
     if not jdbc_url.startswith("jdbc:postgresql://"):
         raise ValueError("JDBC URL given is not in postgres JDBC URL format (e.g. jdbc:postgresql://...")
 
-    db_name = "default"
+    schema_name = delta_lake_unittest_schema
 
     # ==== transaction_normalized ====
     table_name = "transaction_normalized"
-    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=JDBC_CONN_PROPS)
+    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=jdbc_conn_props)
     # NOTE! NOTE! NOTE! MinIO locally does not support a TRAILING SLASH after object (folder) name
     path = f"s3a://{CONFIG.AWS_S3_BUCKET}/{CONFIG.AWS_S3_OUTPUT_PATH}/{table_name}"
 
     log = get_jvm_logger(spark, request.node.name)
-    log.info(f"Loading {df.count()} rows from DB to Delta table named {db_name}.{table_name} at path {path}")
+    log.info(f"Loading {df.count()} rows from DB to Delta table named {schema_name}.{table_name} at path {path}")
 
     # Create table in the metastore using DataFrame's schema and write data to the table
     df.write.saveAsTable(
         format="delta",
-        name=f"{db_name}.{table_name}",
+        name=f"{table_name}",
         mode="overwrite",
-        # overwriteSchema=True,
-        # partitionBy="fiscal_year",  # optional partition by field, needs overwriteSchema
         path=path,
     )
 
     # ==== transaction_fabs ====
     table_name = "transaction_fabs"
-    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=JDBC_CONN_PROPS)
+    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=jdbc_conn_props)
     # NOTE! NOTE! NOTE! MinIO locally does not support a TRAILING SLASH after object (folder) name
     path = f"s3a://{CONFIG.AWS_S3_BUCKET}/{CONFIG.AWS_S3_OUTPUT_PATH}/{table_name}"
 
     log = get_jvm_logger(spark, request.node.name)
-    log.info(f"Loading {df.count()} rows from DB to Delta table named {db_name}.{table_name} at path {path}")
+    log.info(f"Loading {df.count()} rows from DB to Delta table named {schema_name}.{table_name} at path {path}")
 
     # Create table in the metastore using DataFrame's schema and write data to the table
     df.write.saveAsTable(
         format="delta",
-        name=f"{db_name}.{table_name}",
+        name=f"{table_name}",
         mode="overwrite",
-        # overwriteSchema=True,
-        # partitionBy="fiscal_year",  # optional partition by field, needs overwriteSchema
         path=path,
     )
 
     # ==== transaction_fpds ====
     table_name = "transaction_fpds"
-    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=JDBC_CONN_PROPS)
+    df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=jdbc_conn_props)
     # NOTE! NOTE! NOTE! MinIO locally does not support a TRAILING SLASH after object (folder) name
     path = f"s3a://{CONFIG.AWS_S3_BUCKET}/{CONFIG.AWS_S3_OUTPUT_PATH}/{table_name}"
 
     log = get_jvm_logger(spark, request.node.name)
-    log.info(f"Loading {df.count()} rows from DB to Delta table named {db_name}.{table_name} at path {path}")
+    log.info(f"Loading {df.count()} rows from DB to Delta table named {schema_name}.{table_name} at path {path}")
 
     # Create table in the metastore using DataFrame's schema and write data to the table
     df.write.saveAsTable(
         format="delta",
-        name=f"{db_name}.{table_name}",
+        name=f"{table_name}",
         mode="overwrite",
-        # overwriteSchema=True,
-        # partitionBy="fiscal_year",  # optional partition by field, needs overwriteSchema
         path=path,
     )
+
+    schema_tables = spark.sql(f"show tables in {schema_name}").collect()
+    assert len(schema_tables) == 3
+    schema_table_names = [t.tableName for t in schema_tables]
+    assert "transaction_normalized" in schema_table_names
+    assert "transaction_fabs" in schema_table_names
+    assert "transaction_fpds" in schema_table_names
+
+    # Now assert that we're still by-default using the unittest schema, by way of using that pytest fixture.
+    # i.e. don't tell it what schema to look at
+    tables = spark.sql(f"show tables").collect()
+    assert len(tables) == 3
+    table_names = [t.tableName for t in tables]
+    assert "transaction_normalized" in table_names
+    assert "transaction_fabs" in table_names
+    assert "transaction_fpds" in table_names
+
+    # Assert rows are present
+    assert spark.sql("select count(*) from transaction_normalized").collect()[0][0] == 2
+    assert spark.sql("select count(*) from transaction_fabs").collect()[0][0] == 1
+    assert spark.sql("select count(*) from transaction_fpds").collect()[0][0] == 1
