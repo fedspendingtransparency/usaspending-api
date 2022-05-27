eed3878b93532655e99738c3eab4ff93a9129aef
Wil Collins <collins_william@bah.com>
2022-05-27 12:08:01 -0400

    [PIPE-182] Adds #noqa for import *

--- usaspending_api/etl/tests/conftest.py
+++ usaspending_api/etl/tests/conftest.py
@@ -1,5 +1,5 @@
 # Compose other supporting conftest files
-from usaspending_api.etl.tests.conftest_spark import *
+from usaspending_api.etl.tests.conftest_spark import *  # noqa
 
 # Pulling in specific fixtures elsewhere
 from usaspending_api.etl.tests.data.submissions import submissions
