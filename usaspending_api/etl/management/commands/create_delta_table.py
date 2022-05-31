7f78afd3470eaf2be31d642b34b4807d63b8f2c3
Emily Brents <brents_emily@bah.com>
2022-05-31 13:59:48 -0400

    [PIPE-302] add award search

--- usaspending_api/etl/management/commands/create_delta_table.py
+++ usaspending_api/etl/management/commands/create_delta_table.py
@@ -19,6 +19,7 @@ from usaspending_api.transactions.delta_models import (
     transaction_normalized_sql_string,
     transaction_search_sql_string,
 )
+from usaspending_api.search.delta_models.award_search import award_search_sql_string
 
 
 TABLE_SPEC = {
@@ -86,6 +87,14 @@ TABLE_SPEC = {
         "partition_column_type": None,  # Placeholder for now
         "custom_schema": None,  # Placeholder for now
     },
+    "award_search": {
+        "schema_sql_string": award_search_sql_string,
+        "source_table": "",
+        "destination_database": "rpt",
+        "partition_column": "award_id",
+        "partition_column_type": "numeric",
+        "custom_schema": "",
+    }
 }
 
 
