15103e6e29a9117c07bcfcdde7938e8110ccf5be
Keith Hickey <hickey_keith@bah.com>
2022-06-01 16:09:51 -0400

    [PIPE-10] Needed to add new cleanup fixture to old tests to get rid of their schema after the test

--- usaspending_api/etl/tests/integration/test_create_delta_table.py
+++ usaspending_api/etl/tests/integration/test_create_delta_table.py
@@ -36,21 +36,21 @@ def _verify_delta_table_creation(spark: SparkSession, delta_table_name: str, s3_
     assert the_delta_table["isTemporary"] is False
 
 
-def test_create_delta_table_for_sam_recipient(spark, s3_unittest_data_bucket):
+def test_create_delta_table_for_sam_recipient(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "sam_recipient", s3_unittest_data_bucket)
 
 
-def test_create_delta_table_for_recipient_lookup(spark, s3_unittest_data_bucket):
+def test_create_delta_table_for_recipient_lookup(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "recipient_lookup", s3_unittest_data_bucket)
 
 
-def test_create_delta_table_for_recipient_profile(spark, s3_unittest_data_bucket):
+def test_create_delta_table_for_recipient_profile(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "recipient_profile", s3_unittest_data_bucket)
 
 
-def test_create_delta_table_for_transaction_fabs(spark, s3_unittest_data_bucket):
+def test_create_delta_table_for_transaction_fabs(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "transaction_fabs", s3_unittest_data_bucket)
 
 
-def test_create_delta_table_for_transaction_fpds(spark, s3_unittest_data_bucket):
+def test_create_delta_table_for_transaction_fpds(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "transaction_fpds", s3_unittest_data_bucket)
