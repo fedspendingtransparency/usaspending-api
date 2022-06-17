f2989e537556b307839216c315318033eefb2013
Wil Collins <collins_william@bah.com>
2022-06-17 16:27:12 -0400

    [PIPE-191] Removes manage_databricks.py. Adds touch logs/file to manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+from usaspending_api.settings import APP_DIR
+
 if __name__ == "__main__":
     os.environ.setdefault("DDM_CONTAINER_NAME", "app")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usaspending_api.settings")
@@ -20,4 +22,12 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+
+    # Setup logs dir
+    logs_dir = APP_DIR / "logs"
+    console_log_file_path = logs_dir / "console.log"
+
+    logs_dir.mkdir(parents=True, exist_ok=True)
+    console_log_file_path.touch(exist_ok=True)
+
     execute_from_command_line(sys.argv)
