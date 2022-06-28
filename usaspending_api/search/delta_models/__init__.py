a2256776f7ddb15cd5b8fe7fdb8201e5b28d29a3
Emily Brents <brents_emily@bah.com>
2022-06-28 14:10:18 -0400

    PIPE-288] remove testing definition

--- usaspending_api/search/delta_models/__init__.py
+++ usaspending_api/search/delta_models/__init__.py
@@ -2,10 +2,8 @@ from usaspending_api.search.delta_models.award_search import (
     award_search_create_sql_string,
     award_search_load_sql_string,
 )
-from usaspending_api.search.delta_models.award_search_testing import award_search_testing_sql_string
 
 __all__ = [
     "award_search_create_sql_string",
     "award_search_load_sql_string",
-    "award_search_testing_sql_string",
 ]
