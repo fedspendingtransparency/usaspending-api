14ca6cb5de374d8b7a361fc7f488e014b2ccc9f5
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-07-08 16:44:49 -0400

    Create copy_table_metadata.py

---
+++ usaspending_api/etl/management/commands/copy_table_metadata.py
@@ -0,0 +1,100 @@
+import logging
+from django import db
+from django.core.management.base import BaseCommand
+
+from usaspending_api.etl.management.commands.create_delta_table import TABLE_SPEC
+from usaspending_api.etl.management.commands.combine_transaction_search_chunks import Command as CTSC_Command
+from usaspending_api.database_scripts.matview_generator.chunked_matview_sql_generator import (
+    make_copy_indexes,
+    make_copy_constraints,
+)
+
+logger = logging.getLogger()
+
+
+class Command(BaseCommand):
+
+    help = """
+    This command simply copies the constraints and indexes from one table to another in postgres
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--source-table",
+            type=str,
+            required=True,
+            help="The source Delta Table to read the data",
+            choices=list(TABLE_SPEC),
+        )
+        parser.add_argument(
+            "--dest-table",
+            type=str,
+            required=True,
+            help="The source Delta Table to read the data",
+            choices=list(TABLE_SPEC),
+        )
+        parser.add_argument(
+            "--copy-constraints",
+            action="store_true",
+            help="If provided, copies over the old constraints to the downloaded table.",
+        )
+        parser.add_argument(
+            "--copy-indexes",
+            action="store_true",
+            help="If provided, copies over the old indexes to the downloaded table.",
+        )
+        parser.add_argument(
+            "--max-parallel-maintenance-workers",
+            type=int,
+            required=False,
+            help="Postgres session setting for max parallel workers for creating indexes.",
+        )
+        parser.add_argument(
+            "--maintenance-work-mem",
+            type=int,
+            required=False,
+            help="Postgres session setting for max memory to use for creating indexes (in GBs).",
+        )
+        parser.add_argument(
+            "--index-concurrency",
+            type=int,
+            default=5,
+            help="Concurrency limit for index creation. Only applicable if copy-indexes is provided.",
+        )
+
+    def handle(self, *args, **options):
+        # Resolve Parameters
+        source_table = options["source_table"]
+        dest_table = options["dest_table"]
+        copy_constraints = options["copy_constraints"]
+        copy_indexes = options["copy_indexes"]
+        max_parallel_maintenance_workers = options["max_parallel_maintenance_workers"]
+        maintenance_work_mem = options["maintenance_work_mem"]
+        index_concurrency = options["index_concurrency"]
+
+        logger.info(f"Copying metadata from table {source_table} to a the destination table {dest_table}.")
+
+        copy_index_sql = None
+        with db.connection.cursor() as cursor:
+            if copy_constraints:
+                logger.info(f"Copying constraints over from {source_table}")
+                copy_constraint_sql = make_copy_constraints(
+                    cursor, source_table, dest_table, drop_foreign_keys=True
+                )
+                if copy_constraint_sql:
+                    cursor.execute("; ".join(copy_constraint_sql))
+                logger.info(f"Constraints from {source_table} copied over")
+
+            if copy_indexes:
+                # Ensuring we're using the max cores available when generating indexes
+                if max_parallel_maintenance_workers:
+                    logger.info(f"Setting max_parallel_maintenance_workers to {max_parallel_maintenance_workers}")
+                    cursor.execute(f"SET max_parallel_maintenance_workers = {max_parallel_maintenance_workers}")
+                if maintenance_work_mem:
+                    logger.info(f"Setting maintenance_work_mem to '{maintenance_work_mem}GB'")
+                    cursor.execute(f"SET maintenance_work_mem = '{maintenance_work_mem}GB'")
+                copy_index_sql = make_copy_indexes(cursor, source_table, dest_table)
+        if copy_index_sql:
+            logger.info(f"Copying indexes over from {source_table}.")
+            CTSC_Command().create_indexes(copy_index_sql, index_concurrency)
+            logger.info(f"Indexes from {source_table} copied over.")
