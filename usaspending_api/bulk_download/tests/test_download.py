7918320e9c3ea8b60fb68ae605ab42874acc2ffd
Emily Brents <brents_emily@bah.com>
2022-07-12 11:37:22 -0400

    Update test_download.py

--- usaspending_api/bulk_download/tests/test_download.py
+++ usaspending_api/bulk_download/tests/test_download.py
@@ -635,7 +635,7 @@ def test_download_awards_with_foreign_scope(client, award_data):
 
     assert resp.status_code == status.HTTP_200_OK
     assert resp.json()["total_rows"] == 5
-    assert resp.json()["total_columns"] == 593
+    assert resp.json()["total_columns"] == 595
 
 
 @pytest.mark.django_db
