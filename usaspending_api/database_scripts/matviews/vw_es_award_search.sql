20206c9d48cf6fd95e61c69279184131e446b5ee
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-07-13 14:09:03 -0400

    Updating vw_es_award_search to use award_search

--- usaspending_api/database_scripts/matviews/vw_es_award_search.sql
+++ usaspending_api/database_scripts/matviews/vw_es_award_search.sql
@@ -1,17 +1,8 @@
 DROP VIEW IF EXISTS vw_es_award_search CASCADE;
 
 CREATE VIEW vw_es_award_search AS (
-  SELECT * FROM mv_contract_award_search
-  UNION ALL
-  SELECT * FROM mv_directpayment_award_search
-  UNION ALL
-  SELECT * FROM mv_grant_award_search
-  UNION ALL
-  SELECT * FROM mv_idv_award_search
-  UNION ALL
-  SELECT * FROM mv_loan_award_search
-  UNION ALL
-  SELECT * FROM mv_other_award_search
+  SELECT * FROM award_search
+  WHERE action_date >= '10-01-2007'
 );
 
 GRANT SELECT ON vw_es_award_search TO readonly;
