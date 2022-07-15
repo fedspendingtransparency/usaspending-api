a7e332cc1075fa05bbf98708064308a611d72aed
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-07-15 00:45:26 -0400

    Updating vw_es_award_search to use the proper date format

--- usaspending_api/database_scripts/matviews/vw_es_award_search.sql
+++ usaspending_api/database_scripts/matviews/vw_es_award_search.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS vw_es_award_search CASCADE;
 
 CREATE VIEW vw_es_award_search AS (
   SELECT * FROM award_search
-  WHERE action_date >= '10-01-2007'
+  WHERE action_date >= '2007-10-01'
 );
 
 GRANT SELECT ON vw_es_award_search TO readonly;
