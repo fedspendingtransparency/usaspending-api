9a9002abc032e8c5935608ea053f3f950809123b
bkschmidt <94720208+bkschmidt@users.noreply.github.com>
2022-07-18 17:26:50 -0400

    [Pipe-313]Add summary state view delta create test

--- usaspending_api/etl/tests/integration/test_create_delta_table.py
+++ usaspending_api/etl/tests/integration/test_create_delta_table.py
@@ -69,6 +69,10 @@ def test_create_delta_table_for_recipient_profile(spark, s3_unittest_data_bucket
 
 def test_create_delta_table_for_sam_recipient(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
     _verify_delta_table_creation(spark, "sam_recipient", s3_unittest_data_bucket)
+    
+    
+def test_create_delta_table_for_summary_state_view(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
+    _verify_delta_table_creation(spark, "summary_state_view", s3_unittest_data_bucket)
 
 
 def test_create_delta_table_for_transaction_fabs(spark, s3_unittest_data_bucket, hive_unittest_metastore_db):
