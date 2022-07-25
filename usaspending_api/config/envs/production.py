696cbf3374fe01d4470fd473a799caa8aae680c4
Wil Collins <collins_william@bah.com>
2022-07-25 13:13:33 -0400

    Adds production config for databricks

---
+++ usaspending_api/config/envs/production.py
@@ -0,0 +1,29 @@
+########################################################################################################################
+# Production runtime env configuration
+# - Inherits all defaults from DefaultConfig in default.py
+# - Overrides any default config variables with local runtime env specific values
+# - Users may override local config vars values declared here by putting values in a git-ignored .env file alongside
+#   docker-compose.yml
+# - Set config variables to DefaultConfig.USER_SPECIFIC_OVERRIDE where there is expected to be a
+#   user-provided a config value for a variable (e.g. in the ../.env file)
+########################################################################################################################
+from typing import ClassVar
+
+from usaspending_api.config.envs.default import DefaultConfig
+
+
+class ProductionConfig(DefaultConfig):
+    """Config for a production runtime environment, which inherits and overrides from DefaultConfig
+
+    See Also:
+        Attributes inherited from or overridden from ``DefaultConfig``
+
+    """
+
+    # ==== [Global] ====
+    ENV_CODE: ClassVar[str] = "prd"
+
+    # ==== [AWS] ====
+    AWS_PROFILE: str = None
+    AWS_REGION = ""
+    SPARK_S3_BUCKET = "dti-da-usaspending-spark-prod"
