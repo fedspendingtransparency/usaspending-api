8362d94a943bb9105a351fe7e0213c04b785f8b2
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-07-28 15:34:53 -0400

    broker_subaward delta model

--- usaspending_api/awards/delta_models/__init__.py
+++ usaspending_api/awards/delta_models/__init__.py
@@ -1,4 +1,8 @@
 from usaspending_api.awards.delta_models.awards import AWARDS_COLUMNS, awards_sql_string
+from usaspending_api.awards.delta_models.broker_subawards import (
+    BROKER_SUBAWARDS_COLUMNS,
+    broker_subawards_sql_string
+)
 from usaspending_api.awards.delta_models.financial_accounts_by_awards import (
     FINANCIAL_ACCOUNTS_BY_AWARDS_COLUMNS,
     financial_accounts_by_awards_sql_string,
@@ -9,4 +13,6 @@ __all__ = [
     "awards_sql_string",
     "FINANCIAL_ACCOUNTS_BY_AWARDS_COLUMNS",
     "financial_accounts_by_awards_sql_string",
+    "BROKER_SUBAWARDS_COLUMNS",
+    "broker_subawards_sql_string",
 ]
