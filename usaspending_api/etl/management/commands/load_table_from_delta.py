4a9cd2a3e163d43ff973f172335404b72df491d6
Keith Hickey <hickey_keith@bah.com>
2022-08-08 10:58:38 -0400

    [PIPE-380] Testing import speed at 100k records per CSV file

--- usaspending_api/etl/management/commands/load_table_from_delta.py
+++ usaspending_api/etl/management/commands/load_table_from_delta.py
@@ -357,7 +357,7 @@ class Command(BaseCommand):
         logger.info(f"LOAD: Starting dump of Delta table to temp gzipped CSV files in {s3_bucket_with_csv_path}")
         df_no_arrays = convert_array_cols_to_string(df, is_postgres_array_format=True, is_for_csv_export=True)
         df_no_arrays.write.options(
-            maxRecordsPerFile=CONFIG.SPARK_PARTITION_ROWS,  # TODO temp testing. remove or make a constant
+            maxRecordsPerFile=100000,  # TODO temp testing. remove or make a constant, like CONFIG.SPARK_PARTITION_ROWS
             compression="gzip",
             nullValue=None,
             escape='"',
