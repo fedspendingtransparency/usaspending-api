e2f623bd048df5f98f574a5a126e30fa6e18afe3
Schmidt, Benjamin [USA] <Schmidt_Benjamin@bah.com>
2022-08-12 16:13:46 -0400

    [PIPE 311] Update sam_recipient.sql

--- usaspending_api/recipient/delta_models/__init__.py
+++ usaspending_api/recipient/delta_models/__init__.py
@@ -10,6 +10,12 @@ from usaspending_api.recipient.delta_models.recipient_profile import (
 )
 from usaspending_api.recipient.delta_models.sam_recipient import sam_recipient_sql_string, SAM_RECIPIENT_COLUMNS
 
+from usaspending_api.recipient.delta_models.sam_recipient import (
+    sam_recipient_create_sql_string,
+    sam_recipient_load_sql_string,
+    SAM_RECIPIENT_COLUMNS,
+)
+
 __all__ = [
     "RECIPIENT_LOOKUP_COLUMNS",
     "recipient_lookup_create_sql_string",
