fcea5b29d3672ee22ea9d9929152b084a130b78f
Seth Stoudenmier <stoudenmier_seth@bah.com>
2022-08-15 10:58:19 -0700

    [PIPE-315] add back some ordering of uei and duns

--- usaspending_api/recipient/delta_models/recipient_lookup.py
+++ usaspending_api/recipient/delta_models/recipient_lookup.py
@@ -497,6 +497,8 @@ recipient_lookup_load_sql_string_list = [
                     PARTITION BY recipient_hash
                     ORDER BY
                         priority ASC,
+                        uei ASC NULLS LAST,
+                        duns ASC NULLS LAST,
                         update_date DESC NULLS LAST
                 ) AS row_num_union
             FROM union_all
