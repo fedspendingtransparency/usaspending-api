d7a021bc0f7f9ad78c5fee44e9fcbd158dd7e394
Emily Brents <brents_emily@bah.com>
2022-08-22 15:33:08 -0400

    [PIPE-388] fix tests

--- usaspending_api/etl/management/commands/load_query_to_delta.py
+++ usaspending_api/etl/management/commands/load_query_to_delta.py
@@ -83,7 +83,7 @@ TABLE_SPEC = {
         "delta_table_create_sql": recipient_profile_create_sql_string,
         "source_schema": RECIPIENT_PROFILE_POSTGRES_COLUMNS,
         "custom_schema": "recipient_hash STRING",
-        "column_names": list(RECIPIENT_PROFILE_DELTA_COLUMNS),
+        "column_names": [x for x in list(RECIPIENT_PROFILE_DELTA_COLUMNS) if x != "id"],
     },
     "transaction_search": {
         "model": TransactionSearch,
