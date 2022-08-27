969a40ef634acd07f295b94a767f9741cbf12627
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-08-27 08:52:20 -0400

    Dropping broker_subaward, subaward table, and subaward_view

--- usaspending_api/database_scripts/matviews/drop_old_matviews.sql
+++ usaspending_api/database_scripts/matviews/drop_old_matviews.sql
@@ -1,5 +1,4 @@
 DROP MATERIALIZED VIEW IF EXISTS mv_agency_autocomplete_old;
 DROP MATERIALIZED VIEW IF EXISTS mv_covid_financial_account_old;
-DROP MATERIALIZED VIEW IF EXISTS subaward_view_old;
 DROP MATERIALIZED VIEW IF EXISTS summary_state_view_old;
 DROP MATERIALIZED VIEW IF EXISTS tas_autocomplete_matview_old;
--- usaspending_api/database_scripts/matviews/vacuum_matviews.sql
+++ usaspending_api/database_scripts/matviews/vacuum_matviews.sql
@@ -1,5 +1,4 @@
 VACUUM (ANALYZE, VERBOSE) mv_agency_autocomplete;
 VACUUM (ANALYZE, VERBOSE) mv_covid_financial_account;
-VACUUM (ANALYZE, VERBOSE) subaward_view;
 VACUUM (ANALYZE, VERBOSE) summary_state_view;
 VACUUM (ANALYZE, VERBOSE) tas_autocomplete_matview;
