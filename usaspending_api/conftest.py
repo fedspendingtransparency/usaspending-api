dbc4bc26e85a55cc5a113f3ed9c166a77d5357b6
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-08-30 23:57:57 -0400

    missed a black fix

--- usaspending_api/conftest.py
+++ usaspending_api/conftest.py
@@ -38,7 +38,7 @@ logger = logging.getLogger("console")
 
 # Baker Settings
 # Since baker doesn't support SearchVectorField, we'll pass in a function to return a string in the meantime
-baker.generators.add('django.contrib.postgres.search.SearchVectorField', lambda: "VECTORFIELD")
+baker.generators.add("django.contrib.postgres.search.SearchVectorField", lambda: "VECTORFIELD")
 
 
 def pytest_configure():
