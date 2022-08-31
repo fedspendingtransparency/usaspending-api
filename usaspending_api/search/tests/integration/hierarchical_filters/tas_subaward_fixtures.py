982c583736d70f3853e35a544e576bcd2ecd4fb3
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-08-30 23:46:37 -0400

    Adding tas ids to subaward search models

--- usaspending_api/search/tests/integration/hierarchical_filters/tas_subaward_fixtures.py
+++ usaspending_api/search/tests/integration/hierarchical_filters/tas_subaward_fixtures.py
@@ -1,51 +1,57 @@
 import pytest
 from model_bakery import baker
 from datetime import datetime
+from usaspending_api.search.tests.integration.hierarchical_filters.tas_fixtures import (
+    BASIC_TAS,
+    ATA_TAS,
+    BPOA_TAS,
+    SISTER_TAS,
+)
 
 
 @pytest.fixture
 def subaward_with_tas(db, award_with_tas):
-    subaward(db, 1)
+    subaward(db, 1, [BASIC_TAS])
 
 
 @pytest.fixture
 def subaward_with_ata_tas(db, award_with_ata_tas):
-    subaward(db, 1)
+    subaward(db, 1, [ATA_TAS])
 
 
 @pytest.fixture
 def subaward_with_bpoa_tas(db, award_with_bpoa_tas):
-    subaward(db, 1)
+    subaward(db, 1, [BPOA_TAS])
 
 
 @pytest.fixture
 def subaward_with_unintuitive_agency(db, tas_with_nonintuitive_agency):
-    subaward(db, 1)
+    subaward(db, 1, [BASIC_TAS])
 
 
 @pytest.fixture
 def subaward_with_multiple_tas(db, award_with_multiple_tas):
-    subaward(db, 1)
+    subaward(db, 1, [BASIC_TAS, ATA_TAS])
 
 
 @pytest.fixture
 def subaward_with_no_tas(db, award_without_tas):
-    subaward(db, 2)
+    subaward(db, 2, [])
 
 
 @pytest.fixture
 def multiple_subawards_with_tas(db, multiple_awards_with_tas):
-    subaward(db, 1)
-    subaward(db, 2)
+    subaward(db, 1, [BASIC_TAS])
+    subaward(db, 2, [ATA_TAS])
 
 
 @pytest.fixture
 def multiple_subawards_with_sibling_tas(db, multiple_awards_with_sibling_tas):
-    subaward(db, 1)
-    subaward(db, 2)
+    subaward(db, 1, [SISTER_TAS[0]])
+    subaward(db, 2, [SISTER_TAS[1]])
 
 
-def subaward(db, award_id):
+def subaward(db, award_id, treasury_account_identifiers):
     baker.make(
         "search.SubawardSearch",
         funding_toptier_agency_name="test",
@@ -57,4 +63,5 @@ def subaward(db, award_id):
         subaward_number=11111,
         prime_award_group="procurement",
         subaward_amount=10000,
+        treasury_account_identifiers=treasury_account_identifiers
     )
