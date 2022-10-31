4bea18609dfc69f6fe0ac564346928a01026f9d5
Emily Brents <brents_emily@bah.com>
2022-10-31 11:47:48 -0400

    [PIPE-408] add gold table

--- usaspending_api/search/delta_models/__init__.py
+++ usaspending_api/search/delta_models/__init__.py
@@ -3,6 +3,7 @@ from usaspending_api.search.delta_models.award_search import (
     award_search_load_sql_string,
     AWARD_SEARCH_COLUMNS,
     AWARD_SEARCH_POSTGRES_COLUMNS,
+    AWARD_SEARCH_POSTGRES_GOLD_COLUMNS,
     AWARD_SEARCH_DELTA_COLUMNS,
 )
 from usaspending_api.search.delta_models.subaward_search import (
