658d5751b8a380689ebf74e9e7d3ac874b622359
Emily Brents <brents_emily@bah.com>
2022-12-06 10:38:52 -0500

    Update test_recipient_spending.py

--- usaspending_api/disaster/tests/integration/test_recipient_spending.py
+++ usaspending_api/disaster/tests/integration/test_recipient_spending.py
@@ -172,6 +172,14 @@ def test_correct_response_with_award_type_codes(
     resp = helpers.post_for_spending_endpoint(client, url, def_codes=["L", "M"], award_type_codes=["07", "A", "B"])
     expected_results = [
         {
+            "code": "096354360",
+            "award_count": 1,
+            "description": "MULTIPLE RECIPIENTS",
+            "id": None,
+            "obligation": 20000.0,
+            "outlay": 10000.0,
+        },
+        {
             "code": "987654321",
             "award_count": 1,
             "description": "RECIPIENT, 3",
@@ -194,14 +202,6 @@ def test_correct_response_with_award_type_codes(
             "id": ["5f572ec9-8b49-e5eb-22c7-f6ef316f7689-R"],
             "obligation": 2.0,
             "outlay": 1.0,
-        },
-        {
-            "code": "096354360",
-            "award_count": 1,
-            "description": "MULTIPLE RECIPIENTS",
-            "id": None,
-            "obligation": 20000.0,
-            "outlay": 10000.0,
         },
     ]
     assert resp.status_code == status.HTTP_200_OK
@@ -239,7 +239,7 @@ def test_missing_defc(client, monkeypatch, helpers, elasticsearch_award_index, a
 def test_pagination_page_and_limit(client, monkeypatch, helpers, elasticsearch_award_index, awards_and_transactions):
     setup_elasticsearch_test(monkeypatch, elasticsearch_award_index)
 
-    resp = helpers.post_for_spending_endpoint(client, url, def_codes=["L", "M"], page=2, limit=1, sort="id")
+    resp = helpers.post_for_spending_endpoint(client, url, def_codes=["L", "M"], page=5, limit=1, sort="id")
     expected_results = {
         "totals": {"award_count": 7, "obligation": 2222222.0, "outlay": 1111111.0},
         "results": [
@@ -253,12 +253,12 @@ def test_pagination_page_and_limit(client, monkeypatch, helpers, elasticsearch_a
             }
         ],
         "page_metadata": {
-            "page": 2,
+            "page": 5,
             "total": 5,
             "limit": 1,
-            "next": 3,
-            "previous": 1,
-            "hasNext": True,
+            "next": None,
+            "previous": 4,
+            "hasNext": False,
             "hasPrevious": True,
         },
         "messages": [
