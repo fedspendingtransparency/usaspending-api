7da39c2249a83dcf3b1e49d183cb921ccb970efa
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2022-12-23 14:54:09 -0500

    Accounting for recipient_levels to be None in the agg key

--- usaspending_api/search/v2/views/spending_by_award.py
+++ usaspending_api/search/v2/views/spending_by_award.py
@@ -444,7 +444,10 @@ class SpendingByAwardVisualizationViewSet(APIView):
     def get_recipient_hash_with_level(self, award_doc):
         recipient_info = award_doc.get("recipient_agg_key").split("/")
         recipient_hash = recipient_info[0] if recipient_info else None
-        recipient_levels = literal_eval(recipient_info[1]) if len(recipient_info) > 1 else []
+        if len(recipient_info) > 1 and recipient_info[1].upper() != 'NONE':
+            recipient_levels = literal_eval(recipient_info[1])
+        else:
+            recipient_levels = []
 
         if recipient_hash is None or len(recipient_levels) == 0:
             return None
