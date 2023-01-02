0cfd7621e902779708d701fcb6414e38d119a1a0
Keith Hickey <hickey_keith@bah.com>
2023-01-02 13:15:24 -0500

    [PIPE-347] Reducing module breadth to figure out what is not pickle-able

--- usaspending_api/etl/elasticsearch_loader_helpers/controller_for_spark.py
+++ usaspending_api/etl/elasticsearch_loader_helpers/controller_for_spark.py
@@ -176,8 +176,8 @@ class Controller:
         def show_data(partition_idx: int, partition_data):
             print(f"Hello from lambda partition#{partition_idx}")
             from pprint import pprint
-            print(f"Converting {len(partition_data)} Rows to dict objects for partition #{partition_idx}")
             partition_data_list = list(partition_data)
+            print(f"Converting {len(partition_data_list)} Rows to dict objects for partition #{partition_idx}")
             records = [row.asDict() for row in partition_data_list]
             print(f"Showing 2 records for partition #{partition_idx}")
             pprint(records[0])
