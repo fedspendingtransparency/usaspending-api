647b09a85474092d9753abc2f4ed4a479347afc7
Keith Hickey <hickey_keith@bah.com>
2023-01-04 23:46:54 -0500

    [PIPE-347] Overriding write-alias for testing

--- usaspending_api/etl/management/commands/elasticsearch_indexer.py
+++ usaspending_api/etl/management/commands/elasticsearch_indexer.py
@@ -199,9 +199,10 @@ def parse_cli_args(options: dict, es_client) -> dict:
     )
 
     if config["is_incremental_load"] or config["deletes_only"]:
-        if config["index_name"]:
-            logger.info(format_log(f"Ignoring provided index name, using alias '{config['write_alias']}' for safety"))
-        config["index_name"] = config["write_alias"]
+        # TODO: remove write-alias override after testing
+        # if config["index_name"]:
+        #     logger.info(format_log(f"Ignoring provided index name, using alias '{config['write_alias']}' for safety"))
+        # config["index_name"] = config["write_alias"]
         if not es_client.cat.aliases(name=config["write_alias"]):
             logger.error(f"Write alias '{config['write_alias']}' is missing")
             raise SystemExit(1)
