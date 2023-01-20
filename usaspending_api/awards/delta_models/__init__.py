7ba9076a74bbae210a83303f8e3a8e691b5c1b26
Wil Collins <collins_william@bah.com>
2023-01-20 14:29:22 -0500

    [PIPE-467] Adds code for update_file_c_linkages_in_delta.py

--- usaspending_api/awards/delta_models/__init__.py
+++ usaspending_api/awards/delta_models/__init__.py
@@ -1,6 +1,7 @@
 from usaspending_api.awards.delta_models.awards import AWARDS_COLUMNS, awards_sql_string
 from usaspending_api.awards.delta_models.broker_subawards import BROKER_SUBAWARDS_COLUMNS, broker_subawards_sql_string
 from usaspending_api.awards.delta_models.financial_accounts_by_awards import (
+    c_to_d_linkage_view_sql_strings,
     FINANCIAL_ACCOUNTS_BY_AWARDS_COLUMNS,
     financial_accounts_by_awards_sql_string,
 )
@@ -8,8 +9,9 @@ from usaspending_api.awards.delta_models.financial_accounts_by_awards import (
 __all__ = [
     "AWARDS_COLUMNS",
     "awards_sql_string",
-    "FINANCIAL_ACCOUNTS_BY_AWARDS_COLUMNS",
-    "financial_accounts_by_awards_sql_string",
     "BROKER_SUBAWARDS_COLUMNS",
     "broker_subawards_sql_string",
+    "c_to_d_linkage_view_sql_strings",
+    "FINANCIAL_ACCOUNTS_BY_AWARDS_COLUMNS",
+    "financial_accounts_by_awards_sql_string",
 ]
--- usaspending_api/etl/management/commands/update_file_c_linkages_in_delta.py
+++ usaspending_api/etl/management/commands/update_file_c_linkages_in_delta.py
@@ -1,8 +1,7 @@
 from django.core.management.base import BaseCommand
-from django.db import transaction
 from pyspark.sql import SparkSession
 
-from usaspending_api.common.etl.spark import create_ref_temp_views
+from usaspending_api.awards.delta_models import c_to_d_linkage_view_sql_strings
 from usaspending_api.common.helpers.spark_helpers import (
     configure_spark_session,
     get_active_spark_session,
@@ -13,13 +12,26 @@ from usaspending_api.common.helpers.spark_helpers import (
 class Command(BaseCommand):
 
     help = (
-        "",
-        "",
+        "This command links File C (financial_accounts_by_awards) records to File D (awards) records.",
+        "It creates a new copy of the `financial_accounts_by_awards` table in the `int` schema based",
+        "on the copy in the `raw` schema and applies the updates there. These records are linked to",
+        "the `awards` table in the `int` schema",
     )
 
     spark: SparkSession
 
-    ETL_SQL_FILE_PATH = "usaspending_api/etl/management/sql/"
+    def get_unlinked_count(self, schema):
+        unlinked_sql = f"""
+        SELECT
+            count(*)
+        FROM
+            {schema}.financial_accounts_by_awards
+        WHERE
+            award_id IS NULL;
+        """
+
+        results = self.spark.sql(unlinked_sql)
+        return results.collect()[0][0]
 
     def handle(self, *args, **options):
 
@@ -42,15 +54,34 @@ class Command(BaseCommand):
         # Setup Logger
         logger = get_jvm_logger(self.spark, __name__)
 
-        # Shallow Clone
-        # TODO - Delete existing shallow clone (int.financial_accounts_by_awards)
-        # TODO - Create new shallow clone (int.financial_accounts_by_awards)
+        # Log initial linkage count
+        unlinked_count = self.get_unlinked_count("raw")
+        logger.info(f"Count of unlinked records before updates: {unlinked_count:,}")
 
-        # Populate Temporary Tables
-        # TODO - Rewrite existing c_to_d_linkage sql files as Spark SQL Temp Views
-        # TODO - Add additional Temp View that contains FABA IDs and Null awards for deleted awards
+        # Run Linkage Queries
+        linkage_queries = c_to_d_linkage_view_sql_strings
+        for index, query in enumerate(linkage_queries):
+            logger.info(f"Running query number: {index + 1}\nPreview of query: {query[:100]}")
+            self.spark.sql(query)
 
-        # Merge Temporary Tables into Shallow Clone (int.FABA)
+        # Final MERGE of `union_all_priority` into `int.financial_accounts_by_awards`;
+        # This merge will set the `award_id` field to the `id` of the linked `int.awards` record;
+        # If any previously linked Awards have been deleted, the `award_id` will be set to NULL
+        merge_sql = """
+            MERGE INTO int.financial_accounts_by_awards AS faba
+            USING union_all_priority AS updates
+            ON faba.financial_accounts_by_awards_id = updates.financial_accounts_by_awards_id
+            WHEN MATCHED
+                THEN UPDATE SET faba.award_id = updates.award_id;
+        """
+
+        # Log the number of FABA records updated in the merge and final linkage count
+        results = self.spark.sql(merge_sql)
+        update_count = results.collect()[0][0]
+        logger.info(f"{update_count:,} int.financial_accounts_by_awards records were unlinked or linked to int.awards")
+
+        unlinked_count = self.get_unlinked_count("int")
+        logger.info(f"Count of unlinked records after updates: {unlinked_count:,}")
 
         if spark_created_by_command:
             self.spark.stop()
