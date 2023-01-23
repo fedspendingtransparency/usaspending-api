ff524490b2e72ed7e6ba570dff216aa64a372675
Andrew Guest <andrew.guest@kc.frb.org>
2023-01-22 22:54:22 -0600

    [PIPE-273] SQL file to check for blank awards
    Added SQL files that checks the faba.award_id column for references to any awards that no longer exist on the temp.award_search_temp table. Also added another SQL file to update any rows found with NULL.

---
+++ usaspending_api/etl/management/sql/c_file_linkage/update_faba_award_ids.sql
@@ -0,0 +1,14 @@
+-- If there are any records in the FABA table with an award_id that does NOT exist
+--    in the temp.award_search_temp table then update the FABA.award_id column to NULL.
+
+UPDATE
+    financial_accounts_by_awards faba
+SET
+    faba.award_id = NULL
+WHERE
+    faba.award_id NOT IN (
+        SELECT
+            DISTICT(award_id)
+        FROM
+            temp.award_search_temp
+    );
