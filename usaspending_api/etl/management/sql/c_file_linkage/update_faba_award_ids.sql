0e95229abc773d2e2b90f4048b054eea71fbae20
Andrew Guest <andrew.guest@kc.frb.org>
2023-01-24 11:41:52 -0600

    [PIPE-273] Update SQL to use LEFT EXCLUDING JOIN
    Use a LEFT EXCLUDING JOIN to find the FABA records that do not have a matching row in temp.award_search_temp table and update only those records.

--- usaspending_api/etl/management/sql/c_file_linkage/update_faba_award_ids.sql
+++ usaspending_api/etl/management/sql/c_file_linkage/update_faba_award_ids.sql
@@ -1,14 +1,26 @@
 -- If there are any records in the FABA table with an award_id that does NOT exist
 --    in the temp.award_search_temp table then update the FABA.award_id column to NULL.
 
+WITH award_id_cte AS (
+	SELECT
+		faba.award_id as faba_award_id
+	FROM
+ 		financial_accounts_by_awards faba
+	LEFT JOIN
+		temp.award_search_temp ast
+	ON
+		faba.award_id = ast.award_id
+	WHERE
+		ast.award_id IS NULL
+		AND
+    	faba.award_id IS NOT NULL
+)
+
 UPDATE
-    financial_accounts_by_awards faba
+	financial_accounts_by_awards
 SET
-    faba.award_id = NULL
+	award_id = NULL
+FROM
+	award_id_cte
 WHERE
-    faba.award_id NOT IN (
-        SELECT
-            DISTINCT(award_id)
-        FROM
-            temp.award_search_temp
-    );
+	award_id = award_id_cte.faba_award_id;
