75eeaae3e03db6ad7a4f5864d7c9611f811922e4
Andrew Guest <andrew.guest@kc.frb.org>
2023-01-24 17:06:48 -0600

    [PIPE-273] Update comment and UPDATE statement
    Updated comment and changed UPDATE statement to be more performant.

--- usaspending_api/etl/management/sql/c_file_linkage/update_faba_award_ids.sql
+++ usaspending_api/etl/management/sql/c_file_linkage/update_faba_award_ids.sql
@@ -1,9 +1,9 @@
--- If there are any records in the FABA table with an award_id that does NOT exist
---    in the temp.award_search_temp table then update the FABA.award_id column to NULL.
+-- Unlinks FABA records from Awards that no longer exist
 
 WITH award_id_cte AS (
 	SELECT
-		faba.award_id as faba_award_id
+		faba.award_id as faba_award_id,
+  		faba.financial_accounts_by_awards_id as faba_id
 	FROM
  		financial_accounts_by_awards faba
 	LEFT JOIN
@@ -12,8 +12,8 @@ WITH award_id_cte AS (
 		faba.award_id = ast.award_id
 	WHERE
 		ast.award_id IS NULL
-		AND
-    	faba.award_id IS NOT NULL
+  		AND
+  		faba.award_id IS NOT NULL
 )
 
 UPDATE
@@ -23,4 +23,4 @@ SET
 FROM
 	award_id_cte
 WHERE
-	award_id = award_id_cte.faba_award_id;
+	financial_accounts_by_awards_id = award_id_cte.faba_id;
