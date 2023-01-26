72faafc4d17526a4201b9edddabc1cbcd82c5990
Keith Hickey <hickey_keith@bah.com>
2023-01-26 15:57:14 -0500

    [PIPE-347] fixing imports after module refactor

--- usaspending_api/etl/tests/unit/elasticsearch_loader_helpers/test_controller.py
+++ usaspending_api/etl/tests/unit/elasticsearch_loader_helpers/test_controller.py
@@ -1,4 +1,4 @@
-from usaspending_api.etl.elasticsearch_loader_helpers import PostgresElasticsearchIndexerController
+from usaspending_api.etl.elasticsearch_loader_helpers.controller import PostgresElasticsearchIndexerController
 from math import ceil
 
 
