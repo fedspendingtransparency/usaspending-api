936a53af926e9409d594fad701316d1a1ab6310d
Keith Hickey <hickey_keith@bah.com>
2023-02-21 14:27:15 -0500

    [PIPE-380] Prefix data paths with data/sandbox to not interfere with qat data in the same shared bucket

--- usaspending_api/config/envs/sandbox.py
+++ usaspending_api/config/envs/sandbox.py
@@ -26,3 +26,6 @@ class SandboxConfig(DefaultConfig):
     # ==== [AWS] ====
     AWS_PROFILE: str = None
     SPARK_S3_BUCKET = "dti-da-usaspending-spark-qat"
+    # Prefix data paths with data/sandbox to not interfere with qat data in the same shared bucket
+    DELTA_LAKE_S3_PATH: str = "data/sandbox/delta"
+    SPARK_CSV_S3_PATH: str = "data/sandbox/csv"
