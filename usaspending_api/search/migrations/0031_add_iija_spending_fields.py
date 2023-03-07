2c97a3cb601b36e1416defd5c0b628075bea78e9
aguest-kc <andrew.guest@kc.frb.org>
2023-03-07 16:11:16 -0600

    [DEV-9504] Change indents on JSON file

--- usaspending_api/search/migrations/0031_add_iija_spending_fields.py
+++ usaspending_api/search/migrations/0031_add_iija_spending_fields.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('search', '0028_transaction_search_agency_indexes'),
+        ("search", "0030_force_as_linkage_to_ts.py"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='awardsearch',
-            name='iija_spending_by_defc',
+            model_name="awardsearch",
+            name="iija_spending_by_defc",
             field=models.JSONField(null=True),
         ),
         migrations.AddField(
-            model_name='awardsearch',
-            name='total_iija_obligation',
+            model_name="awardsearch",
+            name="total_iija_obligation",
             field=models.DecimalField(blank=True, decimal_places=2, max_digits=23, null=True),
         ),
         migrations.AddField(
-            model_name='awardsearch',
-            name='total_iija_outlay',
+            model_name="awardsearch",
+            name="total_iija_outlay",
             field=models.DecimalField(blank=True, decimal_places=2, max_digits=23, null=True),
         ),
     ]
