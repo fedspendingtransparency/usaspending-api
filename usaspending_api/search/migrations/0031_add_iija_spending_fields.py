7ecd600e86372fe3406059971572c0b3081c8621
aguest-kc <andrew.guest@kc.frb.org>
2023-03-07 16:44:48 -0600

    [DEV-9504] fix typo in migration dependency name

--- usaspending_api/search/migrations/0031_add_iija_spending_fields.py
+++ usaspending_api/search/migrations/0031_add_iija_spending_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("search", "0030_force_as_linkage_to_ts.py"),
+        ("search", "0030_force_as_linkage_to_ts"),
     ]
 
     operations = [
