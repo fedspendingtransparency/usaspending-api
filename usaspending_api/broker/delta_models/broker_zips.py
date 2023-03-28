d23345be53d9eae9d06840fc4d6f35f96cf4446c
aguest-kc <andrew.guest@kc.frb.org>
2023-03-28 11:23:27 -0500

    [DEV-9534] Refactor to keep consistency

--- usaspending_api/broker/delta_models/broker_zips.py
+++ usaspending_api/broker/delta_models/broker_zips.py
@@ -19,7 +19,7 @@ zips_sql_string = rf"""
     LOCATION 's3a://{{SPARK_S3_BUCKET}}/{{DELTA_LAKE_S3_PATH}}/{{DESTINATION_DATABASE}}/{{DESTINATION_TABLE}}'
 """
 
-zips_load_sql_string = fr"""
+zips_load_sql_string = rf"""
     INSERT OVERWRITE {{DESTINATION_DATABASE}}.{{DESTINATION_TABLE}}
     (
         {", ".join([key for key in ZIPS_DELTA_COLUMNS])}
