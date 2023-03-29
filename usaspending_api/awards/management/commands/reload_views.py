f9d3432b52f94c4c136cd14a6493f991a5c6f1da
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2023-03-29 11:38:38 -0400

    Revert "The recreate sql already replaces it if already exists"
    This reverts commit 5814d1d6be7ba7b8c369baee69fe8fbd9f7492a2.

--- usaspending_api/awards/management/commands/reload_views.py
+++ usaspending_api/awards/management/commands/reload_views.py
@@ -36,4 +36,5 @@ class Command(BaseCommand):
             for view_name, view_sql in self.views.items():
                 if view_name in options.get("views"):
                     self.logger.info(f"Reloading {view_name}")
+                    cursor.execute(f"DROP VIEW IF EXISTS {view_name}")
                     cursor.execute(view_sql)
