f1f118a597eb36a54f8880ecfc0d91cdddccd46c
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-03-30 20:29:29 -0600

    DEV-9339; Adds IIJA columns to covid-19 profile download

--- usaspending_api/disaster/management/sql/disaster_covid19_file_f_contracts.sql
+++ usaspending_api/disaster/management/sql/disaster_covid19_file_f_contracts.sql
@@ -6,6 +6,8 @@ SELECT
     DEFC."disaster_emergency_funds" AS "prime_award_disaster_emergency_fund_codes",
     DEFC."gross_outlay_amount_by_award_cpe" + DEFC."ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe" + DEFC."ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe" AS "prime_award_outlayed_amount_from_COVID-19_supplementals",
     DEFC."transaction_obligated_amount" AS "prime_award_obligated_amount_from_COVID-19_supplementals",
+    IIJA_DEFC."gross_outlay_amount_by_award_cpe" + IIJA_DEFC."ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe" + IIJA_DEFC."ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe" AS "outlayed_amount_from_IIJA_supplemental",
+    IIJA_DEFC."transaction_obligated_amount" AS "obligated_amount_from_IIJA_supplemental",
     "subaward_search"."action_date" AS "prime_award_base_action_date",
     EXTRACT (YEAR FROM ("awards"."date_signed") + INTERVAL '3 months') AS "prime_award_base_action_date_fiscal_year",
     "awards"."certified_date" AS "prime_award_latest_action_date",
@@ -145,6 +147,44 @@ INNER JOIN (
         ) != 0
         OR COALESCE(SUM(faba.transaction_obligated_amount), 0) != 0
 ) DEFC ON (DEFC.award_id = awards.award_id)
+-- Infrastructure Investment and Jobs Act (IIJA) spending
+LEFT JOIN (
+    SELECT
+        faba.award_id,
+        COALESCE(SUM(CASE WHEN sa.is_final_balances_for_fy = TRUE THEN faba.gross_outlay_amount_by_award_cpe END), 0) AS gross_outlay_amount_by_award_cpe,
+        COALESCE(SUM(CASE WHEN sa.is_final_balances_for_fy = TRUE THEN faba.ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe END), 0) AS ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe,
+        COALESCE(SUM(CASE WHEN sa.is_final_balances_for_fy = TRUE THEN faba.ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe END), 0) AS ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe,
+        COALESCE(SUM(faba.transaction_obligated_amount), 0) AS transaction_obligated_amount
+    FROM
+        financial_accounts_by_awards faba
+    INNER JOIN disaster_emergency_fund_code defc
+        ON defc.code = faba.disaster_emergency_fund_code
+        AND defc.group_name = 'infrastructure'
+    INNER JOIN submission_attributes sa
+        ON faba.submission_id = sa.submission_id
+        AND sa.reporting_period_start >= '2020-04-01'
+    INNER JOIN dabs_submission_window_schedule ON (
+        sa."submission_window_id" = dabs_submission_window_schedule."id"
+        AND dabs_submission_window_schedule."submission_reveal_date" <= now()
+    )
+    WHERE faba.award_id IS NOT NULL
+    GROUP BY
+        faba.award_id
+    HAVING
+        COALESCE(
+            SUM(
+                CASE
+                    WHEN sa.is_final_balances_for_fy = TRUE
+                    THEN
+                        COALESCE(faba.gross_outlay_amount_by_award_cpe, 0)
+                        + COALESCE(faba.ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe, 0)
+                        + COALESCE(faba.ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe, 0)
+                END
+            ),
+            0
+        ) != 0
+        OR COALESCE(SUM(faba.transaction_obligated_amount), 0) != 0
+) IIJA_DEFC on (IIJA_DEFC.award_id = "award_search"."award_id")
 WHERE (
     "subaward_search"."prime_award_group" IN ('procurement')
     AND "subaward_search"."sub_action_date" >= '2020-04-01'
