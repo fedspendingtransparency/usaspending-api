042bbb1ae2b650ecb7021b3e3d9b5afe67904624
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-04-14 13:38:13 -0700

    DEV-9625; Removes one more redundant sort

--- usaspending_api/agency/v2/views/tas_object_class_list.py
+++ usaspending_api/agency/v2/views/tas_object_class_list.py
@@ -59,7 +59,6 @@ class TASObjectClassList(PaginationMixin, AgencyBase):
             # Instead of adding another for loop to remove this column from the object class list results
             # Just delete it during an already existing loop over the list
             del response_dict["results"][idx]["id"]
-            child_response_dict = sorted(child_response_dict, key=lambda x: x[self.pagination.sort_key], reverse=order)
             response_dict["results"][idx]["children"] = child_response_dict
 
         return Response(response_dict)
