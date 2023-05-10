320ce3202625eb71b759043bf610a2370f5df41f
Andrew Guest <andrew.guest@kc.frb.org>
2023-05-09 22:24:11 -0500

    [DEV-9650] Add unit test for transaction ES date filtering

---
+++ usaspending_api/search/tests/unit/test_elasticsearch_transactions_defc_date_filter.py
@@ -0,0 +1,65 @@
+import pytest
+from model_bakery import baker
+
+from usaspending_api.common.query_with_filters import QueryWithFilters
+
+
+@pytest.mark.django_db
+def test_transactions_elasticsearch_query():
+    """Test the correct Transactions Elasticsearch query is generated
+    when different DEFC values are provided."""
+
+    baker.make(
+        "references.DisasterEmergencyFundCode",
+        code="A",
+        public_law="PUBLIC LAW FOR CODE A",
+        title="TITLE FOR CODE A",
+        group_name=None,
+        earliest_public_law_enactment_date=None,
+    )
+    baker.make(
+        "references.DisasterEmergencyFundCode",
+        code="B",
+        public_law="PUBLIC LAW FOR CODE B",
+        title="TITLE FOR CODE B",
+        group_name=None,
+        earliest_public_law_enactment_date=None,
+    )
+    baker.make(
+        "references.DisasterEmergencyFundCode",
+        code="L",
+        public_law="PUBLIC LAW FOR CODE L",
+        title="TITLE FOR CODE L",
+        group_name="covid_19",
+        earliest_public_law_enactment_date="2020-03-06",
+    )
+    baker.make(
+        "references.DisasterEmergencyFundCode",
+        code="Z",
+        public_law="PUBLIC LAW FOR CODE Z",
+        title="TITLE FOR CODE Z",
+        group_name="infrastructure",
+        earliest_public_law_enactment_date="2021-11-15",
+    )
+
+    def_code_filters = ["A", "B", "L", "Z"]
+    def_codes_es_query = QueryWithFilters().generate_transactions_elasticsearch_query(
+        filters={"def_codes": def_code_filters}
+    )
+
+    covid_iija_es_queries = (
+        def_codes_es_query.to_dict()["bool"]["must"][0]["bool"]["should"][0]["bool"]["should"][0]["bool"]["must"]
+        + def_codes_es_query.to_dict()["bool"]["must"][0]["bool"]["should"][0]["bool"]["should"][1]["bool"]["must"]
+    )
+
+    other_es_queries = def_codes_es_query.to_dict()["bool"]["must"][0]["bool"]["should"][1]["bool"]["should"]
+
+    # check the COVID-19 and IIJA part of the ES query
+    assert {"match": {"disaster_emergency_fund_codes": "L"}} in covid_iija_es_queries
+    assert {"range": {"action_date": {"gte": "2020-03-06"}}} in covid_iija_es_queries
+    assert {"match": {"disaster_emergency_fund_codes": "Z"}} in covid_iija_es_queries
+    assert {"range": {"action_date": {"gte": "2021-11-15"}}} in covid_iija_es_queries
+
+    # check the non-COVID-19/non-IIJA part of the ES query
+    assert {"match": {"disaster_emergency_fund_codes": "A"}} in other_es_queries
+    assert {"match": {"disaster_emergency_fund_codes": "B"}} in other_es_queries
