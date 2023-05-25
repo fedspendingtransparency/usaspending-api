6421fb939b0bbff4838fc1697e8f2e7e1754799b
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-05-25 09:07:09 -0700

    DEV-9834; Updates tests to reflect new date type member

--- usaspending_api/common/tests/unit/test_time_period.py
+++ usaspending_api/common/tests/unit/test_time_period.py
@@ -47,7 +47,7 @@ def test_transaction_search_time_period():
     assert transaction_search.gte_date_type() == expected_lte_date_type
 
     # Repeating all tests with new_awards_only true
-    time_period_filter = {"new_awards_only": True, "start_date": "2020-10-01", "end_date": "2021-09-30"}
+    time_period_filter = {"date_type": "new_awards_only", "start_date": "2020-10-01", "end_date": "2021-09-30"}
     transaction_search.filter_value = time_period_filter
     expected_start_date = "2020-10-01"
     expected_end_date = "2021-09-30"
@@ -55,14 +55,13 @@ def test_transaction_search_time_period():
     assert transaction_search.end_date() == expected_end_date
 
     time_period_filter = {
-        "new_awards_only": True,
-        "date_type": "date_signed",
+        "date_type": "new_awards_only",
         "start_date": "2020-10-01",
         "end_date": "2021-09-30",
     }
     transaction_search.filter_value = time_period_filter
-    expected_gte_date_type = "award_date_signed"
-    expected_lte_date_type = "award_date_signed"
+    expected_gte_date_type = "new_awards_only"
+    expected_lte_date_type = "new_awards_only"
     assert transaction_search.gte_date_type() == expected_gte_date_type
     assert transaction_search.gte_date_type() == expected_lte_date_type
 
@@ -82,7 +81,7 @@ def test_new_awards_only_transaction_search_time_period():
     transaction_search_decorator.filter_value = time_period_filter
     expected_start_date = "2020-10-01"
     expected_end_date = "2021-09-30"
-    expected_new_awards_only = None
+    expected_new_awards_only = False
     assert transaction_search_decorator.start_date() == expected_start_date
     assert transaction_search_decorator.end_date() == expected_end_date
     assert transaction_search_decorator._new_awards_only() == expected_new_awards_only
@@ -116,25 +115,11 @@ def test_new_awards_only_transaction_search_time_period():
     assert transaction_search_decorator.gte_date_type() == expected_lte_date_type
 
     # Repeating all tests with new_awards_only true
-    time_period_filter = {"new_awards_only": True, "start_date": "2020-10-01", "end_date": "2021-09-30"}
+    time_period_filter = {"date_type": "new_awards_only", "start_date": "2020-10-01", "end_date": "2021-09-30"}
     transaction_search_decorator.filter_value = time_period_filter
     expected_start_date = "2020-10-01"
     expected_end_date = "2021-09-30"
-    expected_new_awards_only = False
+    expected_new_awards_only = True
     assert transaction_search_decorator.start_date() == expected_start_date
     assert transaction_search_decorator.end_date() == expected_end_date
-    assert transaction_search_decorator._new_awards_only() == expected_new_awards_only
-
-    time_period_filter = {
-        "new_awards_only": True,
-        "date_type": "date_signed",
-        "start_date": "2020-10-01",
-        "end_date": "2021-09-30",
-    }
-    transaction_search_decorator.filter_value = time_period_filter
-    expected_gte_date_type = "award_date_signed"
-    expected_lte_date_type = "award_date_signed"
-    expected_new_awards_only = True
-    assert transaction_search_decorator.gte_date_type() == expected_gte_date_type
-    assert transaction_search_decorator.gte_date_type() == expected_lte_date_type
     assert transaction_search_decorator._new_awards_only() == expected_new_awards_only
