ff83c22e1820e7f2a404defeefbb2a7f307979c3
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-05-26 11:22:35 -0700

    DEV-9840; Making sure to remove unsupported date types from subawards time period; if unsupported date type is present let it use default

--- usaspending_api/download/filestreaming/download_generation.py
+++ usaspending_api/download/filestreaming/download_generation.py
@@ -20,6 +20,7 @@ from ddtrace.ext import SpanTypes
 from django.conf import settings
 
 from usaspending_api.download.models.download_job_lookup import DownloadJobLookup
+from usaspending_api.search.filters.time_period.decorators.new_awards_only import NEW_AWARDS_ONLY_KEYWORD
 from usaspending_api.settings import MAX_DOWNLOAD_LIMIT
 from usaspending_api.awards.v2.lookups.lookups import contract_type_mapping, assistance_type_mapping, idv_type_mapping
 from usaspending_api.common.csv_helpers import count_rows_in_delimited_file, partition_large_delimited_file
@@ -190,6 +191,9 @@ def get_download_sources(json_request: dict, download_job: DownloadJob = None, o
                     time_period["lte_date_type"] = time_period.get("date_type", "date_signed")
             if json_request["filters"].get("time_period") is not None and download_type == "sub_awards":
                 for time_period in filters["time_period"]:
+                    # sub awards do not support `new_awards_only` date type
+                    if time_period.get("date_type") == NEW_AWARDS_ONLY_KEYWORD:
+                        del time_period["date_type"]
                     if time_period.get("date_type") == "date_signed":
                         time_period["date_type"] = "action_date"
             if download_type == "elasticsearch_awards" or download_type == "elasticsearch_transactions":
