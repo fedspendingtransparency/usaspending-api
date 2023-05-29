ba83f3369df2de986650a60de7353527687aa01c
Andrew Guest <andrew.guest@kc.frb.org>
2023-05-29 18:40:30 -0500

    [DEV-9762] Add 5 digit FIPS codes to AwardSearch Delta model

--- usaspending_api/search/delta_models/award_search.py
+++ usaspending_api/search/delta_models/award_search.py
@@ -78,6 +78,7 @@ AWARD_SEARCH_COLUMNS = {
     "recipient_location_state_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "recipient_location_county_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "recipient_location_county_name": {"delta": "STRING", "postgres": "TEXT", "gold": False},
+    "recipient_location_county_fips": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "recipient_location_congressional_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "recipient_location_congressional_code_current": {"delta": "STRING", "postgres": "TEXT", "gold": True},
     "recipient_location_zip5": {"delta": "STRING", "postgres": "TEXT", "gold": False},
@@ -92,6 +93,7 @@ AWARD_SEARCH_COLUMNS = {
     "pop_state_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "pop_county_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "pop_county_name": {"delta": "STRING", "postgres": "TEXT", "gold": False},
+    "pop_county_fips": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "pop_city_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "pop_zip5": {"delta": "STRING", "postgres": "TEXT", "gold": False},
     "pop_congressional_code": {"delta": "STRING", "postgres": "TEXT", "gold": False},
@@ -305,6 +307,10 @@ award_search_load_sql_string = rf"""
   RL_STATE_POPULATION.latest_population AS recipient_location_state_population,
   RL_COUNTY_POPULATION.latest_population AS recipient_location_county_population,
   RL_DISTRICT_POPULATION.latest_population AS recipient_location_congressional_population,
+  CONCAT (
+    RL_STATE_LOOKUP.fips,
+    COALESCE(transaction_fpds.legal_entity_county_code, transaction_fabs.legal_entity_county_code)
+  ) AS recipient_location_county_fips,
 
   COALESCE(transaction_fpds.place_of_perf_country_desc, transaction_fabs.place_of_perform_country_n) AS pop_country_name,
   COALESCE(transaction_fpds.place_of_perform_country_c, transaction_fabs.place_of_perform_country_c, 'USA') AS pop_country_code,
@@ -352,6 +358,10 @@ award_search_load_sql_string = rf"""
   POP_STATE_POPULATION.latest_population AS pop_state_population,
   POP_COUNTY_POPULATION.latest_population AS pop_county_population,
   POP_DISTRICT_POPULATION.latest_population AS pop_congressional_population,
+  CONCAT (
+    POP_STATE_LOOKUP.fips,
+    COALESCE(transaction_fpds.place_of_perform_county_co, transaction_fabs.place_of_perform_county_co)
+  ) AS pop_county_fips,
 
   transaction_fabs.cfda_title AS cfda_program_title,
   transaction_fabs.cfda_number AS cfda_number,
