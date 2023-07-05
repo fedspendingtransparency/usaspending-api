4a8bf2b139b06558cc5a5a3dc16c80e8da8a3c5f
aguest-kc <andrew.guest@kc.frb.org>
2023-07-05 13:48:51 -0500

    [DEV-10017] Update test CSV file variable

--- usaspending_api/references/tests/test_load_population_data.py
+++ usaspending_api/references/tests/test_load_population_data.py
@@ -6,7 +6,10 @@ from django.core.management.base import CommandError
 
 from usaspending_api.references.models import PopCounty
 
-TEST_CSV_FILE = str(settings.APP_DIR / "references" / "tests" / "data" / "census_2020_population_county.csv")
+COUNTY_TEST_CSV_FILE = str(settings.APP_DIR / "references" / "tests" / "data" / "census_2020_population_county.csv")
+CD_TEST_CSV_FILE = str(
+    settings.APP_DIR / "references" / "tests" / "data" / "census_2021_population_congressional_district.csv"
+)
 
 
 def test_no_file_provided():
@@ -20,7 +23,7 @@ def test_no_type_provided():
     """Command should throw an error if the `type` argument is not provided"""
 
     with pytest.raises(CommandError):
-        call_command("load_population_data", file=TEST_CSV_FILE)
+        call_command("load_population_data", file=COUNTY_TEST_CSV_FILE)
 
 
 def test_no_file_or_type_provided():
@@ -34,13 +37,13 @@ def test_no_file_or_type_provided():
 def test_load_county_population_data():
     """Test that the load_population_data command successfully imports the CSV file"""
 
-    csv_file_row_count = len(pd.read_csv(TEST_CSV_FILE))
+    csv_file_row_count = len(pd.read_csv(COUNTY_TEST_CSV_FILE))
 
     # Should be 0 objects in this table before the import
     assert PopCounty.objects.count() == 0
 
     # Load the county data then re-check the row count
-    call_command("load_population_data", type="county", file=TEST_CSV_FILE)
+    call_command("load_population_data", type="county", file=COUNTY_TEST_CSV_FILE)
     assert PopCounty.objects.count() == csv_file_row_count
 
 
@@ -48,8 +51,8 @@ def test_load_county_population_data():
 def test_all_states_and_territories_present():
     """Test that all U.S. states and territories are present in the database"""
 
-    df = pd.read_csv(TEST_CSV_FILE)
-    call_command("load_population_data", type="county", file=TEST_CSV_FILE)
+    df = pd.read_csv(COUNTY_TEST_CSV_FILE)
+    call_command("load_population_data", type="county", file=COUNTY_TEST_CSV_FILE)
 
     assert len(df.state_name.unique()) == len(PopCounty.objects.all().distinct("state_name").values("state_name"))
     assert len(df.state_code.unique()) == len(PopCounty.objects.all().distinct("state_code").values("state_code"))
