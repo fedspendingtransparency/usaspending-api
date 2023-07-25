aea0d71b3cb8fb2541c4e90ff2ea9c48369c485d
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-07-25 13:20:44 -0700

    DEV-7066; Typo

--- usaspending_api/spending_explorer/tests/integration/test_spending_explorer.py
+++ usaspending_api/spending_explorer/tests/integration/test_spending_explorer.py
@@ -1087,7 +1087,7 @@ def test_unreported_file_c(client):
     }
     assert expected_results == actual_results
     # "Non-Award Spending" was removed as requested by DEV-7066
-    # This assertion is to ensure its not unintentionally added back
+    # This assertion is to ensure it's not unintentionally added back
     assert "Non-Award Spending" not in actual_results
     # After removing "Non-Award Spending" from recipient aggregation
     # we don't expect object_class and recipient to total to the same number
