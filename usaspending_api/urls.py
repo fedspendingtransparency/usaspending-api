29fb63dbfea6d50949b6ccf21374858d91f7e73b
Keith Hickey <hickey_keith@bah.com>
2023-08-09 08:30:29 -0400

    Fixes for local unit test issues with newer versions of django (and django contrib libs)

--- usaspending_api/urls.py
+++ usaspending_api/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, re_path
     2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, re_path
+from usaspending_api import settings
 from usaspending_api import views as views
 from usaspending_api.common.views import MarkdownView
 
