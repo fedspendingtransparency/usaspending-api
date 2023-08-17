32079d6c4c849533c4bc177fcaf321ca9e134a50
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-08-16 18:22:02 -0700

    PIPE-494; destination shouldn't be a directory

--- usaspending_api/disaster/helpers/covid_download_csv_strategies.py
+++ usaspending_api/disaster/helpers/covid_download_csv_strategies.py
@@ -131,8 +131,7 @@ class SparkCovidToCSVStrategy(AbstractCovidToCSVStrategy):
                 self.spark_created_by_command = True
                 self.spark = configure_spark_session(**extra_conf, spark_context=self.spark)  # type: SparkSession
             df = self.spark.sql(sql_file_path)
-            # Making the file name a directory because spark outputs files in parts
-            record_count = load_csv_file_and_zip(self.spark, df, f"{destination_path}/", logger=self._logger)
+            record_count = load_csv_file_and_zip(self.spark, df, destination_path, logger=self._logger)
             self._logger.info(f"{destination_path} contains {record_count:,} rows of data")
         except Exception:
             self._logger.exception("Exception encountered. See logs")
