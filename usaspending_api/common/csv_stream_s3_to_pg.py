78f03a839b3cb70abaf607667352b4af9c74e4d3
Wil Collins <collins_william@bah.com>
2023-08-31 16:28:30 -0400

    [PIPE-496] Adds retry logic to download_s3_object. Adds _download_and_copy to replace _stream_and_copy.

--- usaspending_api/common/csv_stream_s3_to_pg.py
+++ usaspending_api/common/csv_stream_s3_to_pg.py
@@ -13,17 +13,19 @@ Adding new imports to this module may inadvertently introduce a dependency that 
 As it stands, even if new imports are added to the modules it already imports, it could lead to a problem.
 """
 import boto3
-import time
-import psycopg2
 import codecs
 import gzip
 import logging
+import psycopg2
+import tempfile
+import time
 
 from contextlib import closing
 from typing import Iterable, List
 
 from botocore.client import BaseClient
 
+from usaspending_api.common.helpers.s3_helpers import download_s3_object
 from usaspending_api.common.logging import AbbrevNamespaceUTCFormatter, ensure_logging
 from usaspending_api.config import CONFIG
 from usaspending_api.settings import LOGGING
@@ -94,6 +96,50 @@ def _stream_and_copy(
         raise exc
 
 
+def _download_and_copy(
+    configured_logger: logging.Logger,
+    cursor: psycopg2._psycopg.cursor,
+    s3_client: BaseClient,
+    s3_bucket_name: str,
+    s3_obj_key: str,
+    target_pg_table: str,
+    ordered_col_names: List[str],
+    gzipped: bool,
+    partition_prefix: str = "",
+):
+    start = time.time()
+    configured_logger.info(f"{partition_prefix}Starting write of {s3_obj_key}")
+
+    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+        download_s3_object(s3_bucket_name, s3_obj_key, temp_file.name, s3_client=s3_client)
+
+        try:
+            if gzipped:
+                with gzip.open(temp_file.name, "rb") as csv_file:
+                    cursor.copy_expert(
+                        sql=f"COPY {target_pg_table} ({','.join(ordered_col_names)}) FROM STDIN (FORMAT CSV)",
+                        file=csv_file,
+                    )
+            else:
+                with open(temp_file.name, 'r', encoding='utf-8') as csv_file:
+                    cursor.copy_expert(
+                        sql=f"COPY {target_pg_table} ({','.join(ordered_col_names)}) FROM STDIN (FORMAT CSV)",
+                        file=csv_file,
+                    )
+
+            elapsed = time.time() - start
+            rows_copied = cursor.rowcount
+            configured_logger.info(
+                f"{partition_prefix}Finished writing {rows_copied} row(s) in {elapsed:.3f}s for {s3_obj_key}"
+            )
+            yield rows_copied
+
+        except Exception as exc:
+            configured_logger.error(f"{partition_prefix}ERROR writing {s3_obj_key}")
+            configured_logger.exception(exc)
+            raise exc
+
+
 def copy_csv_from_s3_to_pg(
     s3_bucket_name: str,
     s3_obj_key: str,
@@ -103,7 +149,7 @@ def copy_csv_from_s3_to_pg(
     gzipped: bool = True,
     work_mem_override: int = None,
 ):
-    """Stream a CSV file from S3 into a Postgres table using the SQL bulk COPY command
+    """Download a CSV file from S3 then stream it into a Postgres table using the SQL bulk COPY command
 
     WARNING: See note above in module docstring about this function being pickle-able, and maintaining a lean set of
     outward dependencies on other modules/code that require setup/config.
@@ -116,7 +162,7 @@ def copy_csv_from_s3_to_pg(
                 if work_mem_override:
                     cursor.execute("SET work_mem TO %s", (work_mem_override,))
                 s3_client = _get_boto3_s3_client()
-                results_generator = _stream_and_copy(
+                results_generator = _download_and_copy(
                     configured_logger=logger,
                     cursor=cursor,
                     s3_client=s3_client,
@@ -160,7 +206,7 @@ def copy_csvs_from_s3_to_pg(
                     cursor.execute("SET work_mem TO %s", (work_mem_override,))
                 s3_client = _get_boto3_s3_client()
                 for s3_obj_key in s3_obj_keys:
-                    yield from _stream_and_copy(
+                    yield from _download_and_copy(
                         configured_logger=logger,
                         cursor=cursor,
                         s3_client=s3_client,
