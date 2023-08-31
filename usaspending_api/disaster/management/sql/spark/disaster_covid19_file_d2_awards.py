fa79bed9f3dd9e922166fd9391d0c1f2bd0b5356
Clancy Thomas <clancy.thomas@kc.frb.org>
2023-08-31 14:22:42 -0500

    PIPE-493: Change explode to posexplode in Spark query

--- usaspending_api/disaster/management/sql/spark/disaster_covid19_file_d2_awards.py
+++ usaspending_api/disaster/management/sql/spark/disaster_covid19_file_d2_awards.py
@@ -98,7 +98,7 @@ SELECT
     latest_transaction.place_of_performance_forei AS primary_place_of_performance_foreign_location,
     ARRAY_JOIN(ARRAY(
         (SELECT CONCAT(GET_JSON_OBJECT(TO_JSON(unnest_cfdas), '$.cfda_number'), ': ', GET_JSON_OBJECT(TO_JSON(unnest_cfdas), '$.cfda_program_title'))
-        FROM EXPLODE(award_search.cfdas) AS unnest_cfdas)
+        FROM POSEXPLODE(award_search.cfdas) AS unnest_cfdas)
         ), '; '
     ) AS cfda_numbers_and_titles,
     latest_transaction.type AS assistance_type_code,
