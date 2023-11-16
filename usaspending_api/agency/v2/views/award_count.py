69436f1cd9d98f9134be19838c8a665166297de0
Desmond Porth <Desmond.Porth@kc.frb.org>
2023-11-16 11:01:44 -0800

    DEV-10148; Implements new AC that requires endpoint to be capable of returning more agencies than just cfo

--- usaspending_api/agency/v2/views/award_count.py
+++ usaspending_api/agency/v2/views/award_count.py
@@ -1,11 +1,13 @@
-from typing import Any
+from enum import Enum
+from typing import Any, List
 
 
 from rest_framework.request import Request
 from rest_framework.response import Response
-from usaspending_api.agency.v2.views.agency_base import AgencyBase
+from usaspending_api.agency.v2.views.agency_base import AgencyBase, PaginationMixin
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.elasticsearch.search_wrappers import AwardSearch
+from usaspending_api.common.helpers.generic_helper import get_pagination_metadata
 from usaspending_api.common.query_with_filters import QueryWithFilters
 from usaspending_api.references.models.toptier_agency import ToptierAgency
 from usaspending_api.search.filters.elasticsearch.filter import _QueryType
@@ -20,7 +22,12 @@ from usaspending_api.common.helpers.fiscal_year_helpers import (
 )
 
 
-class AwardCount(AgencyBase):
+class GroupEnum(Enum):
+    ALL = "all"
+    CFO = "cfo"
+
+
+class AwardCount(PaginationMixin, AgencyBase):
     """
     Obtain the count of awards for a specific agency in a single fiscal year.
     """
@@ -28,9 +35,24 @@ class AwardCount(AgencyBase):
     endpoint_doc = "usaspending_api/api_contracts/contracts/v2/agency/awards/count.md"
 
     def __init__(self, *args, **kwargs):
+        self.additional_models = [
+            {
+                "key": "group",
+                "name": "group",
+                "type": "enum",
+                "enum_values": ["all", "cfo"],
+                "optional": True,
+                "default": "all",
+            },
+        ]
+        self.sortable_columns = ["awarding_toptier_agency_name.keyword"]
+        self.default_sort_column = "awarding_toptier_agency_name.keyword"
+        self.params_to_validate = ["fiscal_year", "group"]
         super().__init__(*args, **kwargs)
-        self.params_to_validate = ["fiscal_year"]
-        self._toptier_awarding_agencies_to_include = [
+        # The following are awarding toptier agency names
+        # that will be the only agencies returned when the
+        # cfo filter is used
+        self._toptier_cfo_agency_names = [
             "Agency for International Development",
             "Department of Agriculture",
             "Department of Commerce",
@@ -57,25 +79,26 @@ class AwardCount(AgencyBase):
             "Social Security Administration",
         ]
 
+    @property
+    def group(self):
+        return self._query_params.get("group")
+
     @cache_response()
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         self._fy_end = get_fiscal_year_end_datetime(self.fiscal_year).date()
         self._fy_start = get_fiscal_year_start_datetime(self.fiscal_year).date()
-
         results = self.query_elasticsearch_for_prime_awards({})
+        page_metadata = get_pagination_metadata(len(results), self.pagination.limit, self.pagination.page)
+        results = results[self.pagination.lower_limit : self.pagination.upper_limit],
+        return Response(
+            {
+                "results": results[: self.pagination.limit],
+                "page_metadata": page_metadata,
+                "messages": self.standard_response_messages,
+            }
+        )
 
-        raw_response = {"results": results, "messages": []}
-
-        return Response(raw_response)
-
-    def _generate_filters(self, awarding_toptier_agency_name):
-        return {
-            "agencies": [{"type": "awarding", "tier": "toptier", "name": awarding_toptier_agency_name}],
-            "time_period": [{"start_date": self._fy_start, "end_date": self._fy_end}],
-        }
-
-    def query_elasticsearch_for_prime_awards(self, filters) -> list:
-        toptier_awarding_agencies_to_include_results = []
+    def query_elasticsearch_for_prime_awards(self, filters) -> List:
         filter_options = {}
         time_period_obj = AwardSearchTimePeriod(
             default_end_date=settings.API_MAX_DATE, default_start_date=settings.API_SEARCH_MIN_DATE
@@ -84,31 +107,45 @@ class AwardCount(AgencyBase):
             time_period_obj=time_period_obj, query_type=_QueryType.AWARDS
         )
         filter_options["time_period_obj"] = new_awards_only_decorator
-        for awarding_toptier_agency_name in self._toptier_awarding_agencies_to_include:
-            awarding_toptier_agency_code = (
-                ToptierAgency.objects.filter(name=awarding_toptier_agency_name).first().toptier_code
-            )
-            filters.update(self._generate_filters(awarding_toptier_agency_name))
-            filter_query = QueryWithFilters.generate_awards_elasticsearch_query(filters, **filter_options)
-            s = AwardSearch().filter(filter_query)
+        filters.update({"time_period": [{"start_date": self._fy_start, "end_date": self._fy_end}]})
+        if self.group == GroupEnum.CFO.value:
+                filters.update({"agencies": [{"type": "awarding", "tier": "toptier", "name": awarding_toptier_agency_name} for awarding_toptier_agency_name in self._toptier_cfo_agency_names]})
+        agency_award_count_results = self._get_award_counts_response(filters, filter_options)
+        return agency_award_count_results
 
-            s.aggs.bucket(
-                "types",
-                "filters",
-                filters={category: Q("terms", type=types) for category, types in all_award_types_mappings.items()},
-            )
-            results = s.handle_execute()
+    def _get_award_counts_response(self, filters: List[dict], filter_options: dict) -> List[dict]:
+        """Returns counts of awards filtered by the filter parameters.
 
-            contracts = results.aggregations.types.buckets.contracts.doc_count
-            idvs = results.aggregations.types.buckets.idvs.doc_count
-            grants = results.aggregations.types.buckets.grants.doc_count
-            direct_payments = results.aggregations.types.buckets.direct_payments.doc_count
-            loans = results.aggregations.types.buckets.loans.doc_count
-            other = results.aggregations.types.buckets.other_financial_assistance.doc_count
+        Args:
+            filters: Filters to filter data on
+            filter_options: Additional filters to apply
 
-            response = {
-                "awarding_toptier_agency_name": awarding_toptier_agency_name,
-                "awarding_toptier_agency_code": awarding_toptier_agency_code,
+        Returns:
+            List[dict]: A dictionary containing award types and the number of their awards.
+        """
+        filter_query = QueryWithFilters.generate_awards_elasticsearch_query(filters, **filter_options)
+        record_num = (self.pagination.page - 1) * self.pagination.limit
+        sorts = [{self.default_sort_column: self.pagination.sort_order}]
+        s = AwardSearch().filter(filter_query).sort(*sorts)
+
+        s.aggs.bucket("agencies", "terms", field="awarding_toptier_agency_name.keyword", size=999999)
+        s.aggs["agencies"].bucket(
+            "types",
+            "filters",
+            filters={category: Q("terms", type=types) for category, types in all_award_types_mappings.items()},
+        )
+        results = s.handle_execute()
+
+        response = []
+        for agency in results.aggregations.agencies.buckets:
+            contracts = agency.types.buckets.contracts.doc_count
+            idvs = agency.types.buckets.idvs.doc_count
+            grants = agency.types.buckets.grants.doc_count
+            direct_payments = agency.types.buckets.direct_payments.doc_count
+            loans = agency.types.buckets.loans.doc_count
+            other = agency.types.buckets.other_financial_assistance.doc_count
+            agency_response = {
+                "awarding_toptier_agency_name": agency.key,
                 "contracts": contracts,
                 "direct_payments": direct_payments,
                 "grants": grants,
@@ -116,5 +153,5 @@ class AwardCount(AgencyBase):
                 "loans": loans,
                 "other": other,
             }
-            toptier_awarding_agencies_to_include_results.append(response)
-        return toptier_awarding_agencies_to_include_results
+            response.append(agency_response)
+        return response
