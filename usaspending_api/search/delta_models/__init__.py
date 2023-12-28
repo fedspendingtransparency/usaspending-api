d3eb092ee46f0f2cd1badfa9ada8408022e118d3
Wil Collins <collins_william@bah.com>
2023-12-28 17:13:37 -0500

    [PIPE-512] Fixes __init__ files for removal of gold columns

--- usaspending_api/search/delta_models/__init__.py
+++ usaspending_api/search/delta_models/__init__.py
@@ -1,9 +1,9 @@
 from usaspending_api.search.delta_models.award_search import (
     award_search_create_sql_string,
+    award_search_incremental_load_sql_string,
     award_search_load_sql_string,
     AWARD_SEARCH_COLUMNS,
     AWARD_SEARCH_POSTGRES_COLUMNS,
-    AWARD_SEARCH_POSTGRES_GOLD_COLUMNS,
     AWARD_SEARCH_DELTA_COLUMNS,
 )
 from usaspending_api.search.delta_models.subaward_search import (
@@ -17,10 +17,10 @@ from usaspending_api.search.delta_models.subaward_search import (
 
 __all__ = [
     "award_search_create_sql_string",
+    "award_search_incremental_load_sql_string",
     "award_search_load_sql_string",
     "AWARD_SEARCH_COLUMNS",
     "AWARD_SEARCH_POSTGRES_COLUMNS",
-    "AWARD_SEARCH_POSTGRES_GOLD_COLUMNS",
     "AWARD_SEARCH_DELTA_COLUMNS",
     "subaward_search_create_sql_string",
     "subaward_search_load_sql_string",
