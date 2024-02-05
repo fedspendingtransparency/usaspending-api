004ee3f0b47a25a3c984a29f74a34129c1e8f921
Ayub <ayub.shahab13@gmail.com>
2024-02-05 17:58:13 -0500

    DEV-10428 updated elasticsearch query to use query instead of filter in order to return results based on score relevance

--- usaspending_api/references/tests/integration/test_recipients_autocomplete_v2.py
+++ usaspending_api/references/tests/integration/test_recipients_autocomplete_v2.py
@@ -8,38 +8,73 @@ from model_bakery import baker
 @pytest.fixture
 def recipient_data_fixture(db):
     baker.make(
-        "search.AwardSearch",
-        id=17394129117,
-        recipient_hash="6a9f72ac-1d21-a1c6-e21c-82cde9d0a68eS",
+        "recipient.RecipientProfile",
+        id="01",
         recipient_level="C",
-        recipient_name="EMERZIAN WOODWORKING INC",
-        uei="C111JJBMS328"
+        recipient_hash="521bb024-054c-4c81-8615-372f81629664",
+        uei="UEI-01",
+        recipient_name="Spiderman",
     )
 
     baker.make(
-        "search.AwardSearch",
-        id=17378750196,
-        recipient_hash="3e69fe7a-fd9f-be78-2f2d-fb3d7bf296d9",
+        "recipient.RecipientProfile",
+        id="02",
+        recipient_level="P",
+        recipient_hash="a70b86c3-5a12-4623-963b-9d96c4810163",
+        uei="UEI-02",
+        recipient_name="Batman",
+    )
+
+    baker.make(
+        "recipient.RecipientProfile",
+        id="03",
+        recipient_level="C",
+        recipient_hash="a70b86c3-5a12-4623-963b-9d96c4810345",
+        uei="UEI-03",
+        recipient_name="Batman",
+    )
+
+    baker.make(
+        "recipient.RecipientProfile",
+        id="04",
         recipient_level="R",
-        recipient_name="ADMINISTRATION LOUISIANA DIVISION OF",
-        uei="M9TEQW6ANUJ4"
+        recipient_hash="9159db20-d2f7-42d4-88e2-a69759987520",
+        uei="UEI-04",
+        recipient_name="Superman",
     )
 
 def test_recipient_search_matches_found(client, monkeypatch, recipient_data_fixture, elasticsearch_recipient_index):
     monkeypatch.setattr(
         "usaspending_api.common.elasticsearch.search_wrappers.RecipientSearch._index_name",
-        settings.S_RECIPIENTS_QUERY_ALIAS_PREFIX ,
+        settings.ES_RECIPIENTS_QUERY_ALIAS_PREFIX ,
     )
     elasticsearch_recipient_index.update_index()
     body = {
-        "search_text": "emerzian",
-        "recipient_levels": ["C"],
+        "search_text": "superman",
+        "recipient_levels": ["R"],
         "limit": 20
     }
     response = client.post("/api/v2/autocomplete/recipient", content_type="application/json", data=json.dumps(body))
     assert response.data["count"] == 1
     for entry in response.data["results"]:
-        assert entry["recipient_name"].lower().find("emerzian") > -1
+        assert entry["recipient_name"].lower().find("superman") > -1
+
+def test_recipient_search_multiple_recipient_levels(client, monkeypatch, recipient_data_fixture, elasticsearch_recipient_index):
+    monkeypatch.setattr(
+        "usaspending_api.common.elasticsearch.search_wrappers.RecipientSearch._index_name",
+        settings.ES_RECIPIENTS_QUERY_ALIAS_PREFIX ,
+    )
+    elasticsearch_recipient_index.update_index()
+    body = {
+        "search_text": "batman",
+        "recipient_levels": ["C", "P"],
+        "limit": 20
+    }
+    response = client.post("/api/v2/autocomplete/recipient", content_type="application/json", data=json.dumps(body))
+    assert response.data["count"] == 2
+    for entry in response.data["results"]:
+        assert entry["recipient_name"].lower().find("batman") > -1
+
 
 def test_recipient_search_no_matches(client, monkeypatch, recipient_data_fixture, elasticsearch_recipient_index):
     monkeypatch.setattr(
@@ -59,16 +94,15 @@ def test_recipient_search_no_matches(client, monkeypatch, recipient_data_fixture
 
 def test_recipient_search_special_characters(client, monkeypatch, recipient_data_fixture, elasticsearch_recipient_index):
     monkeypatch.setattr(
-        "usaspending_api.common.elasticsearch.search_wrappers.TransactionSearch._index_name",
-        settings.ES_RECIPIENTS_QUERY_ALIAS_PREFIX,
+        "usaspending_api.common.elasticsearch.search_wrappers.RecipientSearch._index_name",
+        settings.ES_RECIPIENTS_QUERY_ALIAS_PREFIX ,
     )
     elasticsearch_recipient_index.update_index()
     body = {
-        "filter": {"country_code": "USA", "scope": "recipient_location"},
-        "search_text": 'emer+|()[]{}?"<>\\',  # Once special characters are stripped, this should just be 'arl'
-        "limit": 20,
+        "search_text": "batman+()[]{}?<>\\",
+        "recipient_levels": ["C", "P", "R"]
     }
-    response = client.post("/api/v2/autocomplete/city", content_type="application/json", data=json.dumps(body))
-    assert response.data["count"] == 1
+    response = client.post("/api/v2/autocomplete/recipient", content_type="application/json", data=json.dumps(body))
+    assert response.data["count"] == 2
     for entry in response.data["results"]:
-        assert entry["city_name"].lower().find("emer") > -1
+        assert entry["recipient_name"].lower().find("batman") > -1
--- usaspending_api/references/v2/views/recipients.py
+++ usaspending_api/references/v2/views/recipients.py
@@ -1,6 +1,5 @@
 from collections import OrderedDict
 
-from elasticsearch_dsl import A
 from elasticsearch_dsl import Q as ES_Q
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -34,7 +33,7 @@ models = [
         "text_type": "search",
         "items": {"type": "string"},
         "optional": True
-     },
+    },
 ]
 
 @validate_post_request(models)
@@ -50,15 +49,9 @@ class RecipientAutocompleteViewSet(APIView):
     def post(self, request, format=None):
         search_text, recipient_levels = prepare_search_terms(request.data)
         limit = request.data["limit"]
-
-        query = create_es_search(search_text, recipient_levels)
-        print("QUERY: ", query.to_dict())
-        print("\n")
-
-        sorted_results = query_elasticsearch(query)
-        print("SORTED RESULTS :", sorted_results)
-        print("LIMIT", limit)
-        response = OrderedDict([("count", len(sorted_results)), ("results", sorted_results[:limit])])
+        query = create_es_search(search_text, recipient_levels, limit)
+        results = query_elasticsearch(query)
+        response = OrderedDict([("count", len(results)), ("results", results)])
 
         return Response(response)
 
@@ -67,30 +60,53 @@ def prepare_search_terms(request_data):
     fields = [request_data["search_text"], request_data.get("recipient_levels", [])]
     return [es_sanitize(field).upper() if isinstance(field, str) else field for field in fields]
 
- # search_text = "kimberly"  # Test recipient name. There is a recipient with this name and a level of "R"
-def create_es_search(search_text, recipient_levels):
+# search_text = "kimberly"  # Test recipient name. There is a recipient with this name and a level of "R"
+def create_es_search(search_text, recipient_levels, limit):
     ES_RECIPIENT_SEARCH_FIELDS = ["recipient_name", "uei"]
 
+    query = ES_Q()
+    # Build the should_clauses list
     if recipient_levels:
-        should_clauses = [
-            ES_Q("match", recipient_level=level)
-            for level in recipient_levels
+        recipient_should_clause = [
+            ES_Q(
+                    "bool",
+                    should=[
+                    ES_Q("match", recipient_level=level) for level in recipient_levels
+                    ],
+                    minimum_should_match=1
+            )
+        ]
+        search_text_should_clause = [
+            ES_Q(
+                "bool",
+                should=[
+                    ES_Q("query_string", query=search_text, fields=ES_RECIPIENT_SEARCH_FIELDS),
+                    ES_Q("match", recipient_name=search_text),
+                    ES_Q("match", uei=search_text)
+                ],
+                minimum_should_match=1
+            )
         ]
         query = ES_Q(
             "bool",
-            should=should_clauses,
             must=[
-                *should_clauses,
-                ES_Q("query_string", query=f"*{search_text}*", fields=ES_RECIPIENT_SEARCH_FIELDS),
+                ES_Q("bool", should=recipient_should_clause),
+                ES_Q("bool", should=search_text_should_clause)
+            ]
+        )
+    else:
+        query = ES_Q(
+            "bool",
+            should=[
+                ES_Q("query_string", query=search_text, fields=ES_RECIPIENT_SEARCH_FIELDS),
+                ES_Q("match", recipient_name=search_text),
+                ES_Q("match", uei=search_text)
             ],
             minimum_should_match=1
         )
-    else:
-        # If we're NOT filtering on the `recipient_level` field then return all results matching the given search text
-        query = ES_Q("query_string", query=f"*{search_text}*", fields=ES_RECIPIENT_SEARCH_FIELDS)
-
-    search = RecipientSearch().filter(query)
-    return search
+    # .sort({"_score": {"order": "desc" }})
+    query = RecipientSearch().query(query)[:limit]
+    return query
 
 def query_elasticsearch(query):
     hits = query.handle_execute()
@@ -104,7 +120,6 @@ def parse_elasticsearch_response(hits):
     results = []
     for temp in recipients:
         recipient = temp["_source"]
-        print(recipient)
         results.append(
             OrderedDict(
                 [
