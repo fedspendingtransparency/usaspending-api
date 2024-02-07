f4541fe8af3028d1550aada5866a6617786b018f
aguest-kc <andrew.guest@kc.frb.org>
2024-02-07 12:38:29 -0600

    [DEV-10429] Add replacement for global_temp table

--- usaspending_api/etl/elasticsearch_loader_helpers/controller_for_spark.py
+++ usaspending_api/etl/elasticsearch_loader_helpers/controller_for_spark.py
@@ -61,6 +61,7 @@ class DeltaLakeElasticsearchIndexerController(AbstractElasticsearchIndexerContro
         elif self.config["load_type"] == "location":
             # Replace the Postgres regex operator with the Databricks regex operator
             identifier_replacements["~"] = "rlike"
+            identifier_replacements["state_data"] = "global_temp.state_data"
         else:
             raise ValueError(
                 f"Unrecognized load_type {self.config['load_type']}, or this function does not yet support it"
