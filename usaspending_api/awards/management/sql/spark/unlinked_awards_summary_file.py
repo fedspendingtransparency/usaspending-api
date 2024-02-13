53d5ffef464316162882baca348df52dada56433
Desmond Porth <desmond.porth@kc.frb.org>
2024-02-13 09:08:35 -0800

    DEV-10589; Ensuring the join is correct

--- usaspending_api/awards/management/sql/spark/unlinked_awards_summary_file.py
+++ usaspending_api/awards/management/sql/spark/unlinked_awards_summary_file.py
@@ -29,21 +29,19 @@ summary_file = """
         sum(rao.unlinked_procurement_c_awards + rao.unlinked_procurement_d_awards) + sum(rao.unlinked_assistance_c_awards + rao.unlinked_assistance_d_awards) as total_unlinked_awards
     from
         published_dabs_toptier_agency as pdta
-    left join global_temp.reporting_agency_overview rao
+    inner join global_temp.reporting_agency_overview rao
     on
         rao.toptier_code = pdta.toptier_code
-        and pdta.fiscal_year = rao.fiscal_year
-        and pdta.fiscal_period = rao.fiscal_period
     left join global_temp.submission_attributes sa
     on
         sa.toptier_code = pdta.toptier_code
-        and pdta.reporting_fiscal_year = sa.fiscal_year
-        and pdta.reporting_fiscal_period = sa.fiscal_period
+        and sa.reporting_fiscal_year = rao.fiscal_year
+        and sa.reporting_fiscal_period = rao.fiscal_period
     left join global_temp.reporting_agency_missing_tas rmt
     on
         pdta.toptier_code = rmt.toptier_code
-        and pdta.fiscal_year = rmt.fiscal_year
-        and pdta.fiscal_period = rmt.fiscal_period
+        and rao.fiscal_year = rmt.fiscal_year
+        and rao.fiscal_period = rmt.fiscal_period
     group by
         pdta.name,
         pdta.toptier_code,
