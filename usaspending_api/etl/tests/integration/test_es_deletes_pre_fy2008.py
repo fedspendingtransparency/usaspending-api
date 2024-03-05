8cd543d17348b4364a9c7f49fcf248414e501f46
aguest-kc <andrew.guest@kc.frb.org>
2024-03-05 16:34:29 -0600

    [DEV-10453] Update tests

--- usaspending_api/etl/tests/integration/test_es_deletes_pre_fy2008.py
+++ usaspending_api/etl/tests/integration/test_es_deletes_pre_fy2008.py
@@ -1,15 +1,19 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
+from elasticsearch import Elasticsearch
 from model_bakery import baker
 
 from usaspending_api.etl.elasticsearch_loader_helpers.delete_data import (
     _check_awards_for_pre_fy2008,
+    _gather_modified_transactions_pre_fy2008,
+    delete_docs_by_unique_key,
 )
+from usaspending_api.search.tests.data.utilities import setup_elasticsearch_test
 
 
 @pytest.fixture
-def award_search_data_fixture(db):
+def test_data_fixture(db):
     baker.make(
         "search.AwardSearch",
         award_id=1,
@@ -24,10 +28,30 @@ def award_search_data_fixture(db):
         action_date="2020-01-01",
         update_date="2022-01-10 14:02:50.223 -0500",
     )
+    baker.make(
+        "search.TransactionSearch",
+        transaction_id=1,
+        award_id=1,
+        transaction_unique_id="TEST_TRANSACTION_1",
+        is_fpds=False,
+        afa_generated_unique="ABC_123",
+        action_date="2024-01-15",
+        etl_update_date=datetime.now(),
+    )
+    baker.make(
+        "search.TransactionSearch",
+        transaction_id=2,
+        award_id=2,
+        transaction_unique_id="TEST_TRANSACTION_2",
+        is_fpds=True,
+        detached_award_proc_unique="DEF_456",
+        action_date="2020-01-01",
+        etl_update_date=datetime.now() - timedelta(days=1),
+    )
 
 
 @pytest.mark.django_db(transaction=True)
-def test_find_modified_awards_before_fy2008_non_spark(award_search_data_fixture):
+def test_find_modified_awards_before_fy2008(test_data_fixture):
     """Test that we can find any awards that PREVIOUSLY had an `action_date` on or after FY2008 (2007-10-01), but have
     have been recently updated to have an `action_date` before FY2008 now.
     """
@@ -41,16 +65,47 @@ def test_find_modified_awards_before_fy2008_non_spark(award_search_data_fixture)
         update_date=datetime.now(),
     )
 
-    recently_modified_awards_before_fy2008 = _check_awards_for_pre_fy2008(spark=None)
+    recently_modified_awards_before_fy2008 = _check_awards_for_pre_fy2008()
 
     assert len(recently_modified_awards_before_fy2008) == 1
 
 
 @pytest.mark.django_db(transaction=True)
-def test_find_modified_awards_before_fy2008_spark(spark, award_search_data_fixture):
-    """Test that we can find any awards that PREVIOUSLY had an `action_date` on or after FY2008 (2007-10-01), but have
+def test_find_modified_transactions_before_fy2008(test_data_fixture):
+    """Test that we can find any transactions that PREVIOUSLY had an `action_date` on or after FY2008 (2007-10-01), but have
     have been recently updated to have an `action_date` before FY2008 now.
     """
+
+    # Modify the existing DB transactions to now have an `action_date` before 2007-10-01
+    baker.make(
+        "search.TransactionSearch",
+        transaction_id=2,
+        award_id=2,
+        transaction_unique_id="TEST_TRANSACTION_2",
+        is_fpds=True,
+        detached_award_proc_unique="DEF_456",
+        action_date="2004-01-01",
+        etl_update_date=datetime.now(),
+    )
+
+    recently_modified_awards_before_fy2008 = _gather_modified_transactions_pre_fy2008(config={"process_deletes": True})
+
+    assert len(recently_modified_awards_before_fy2008) == 1
+
+
+@pytest.mark.django_db(transaction=True)
+def test_delete_modified_awards_before_fy2008(elasticsearch_award_index, test_data_fixture, monkeypatch):
+    """Test that we can find any awards that PREVIOUSLY had an `action_date` on or after FY2008 (2007-10-01), but have
+    have been recently updated to have an `action_date` before FY2008 now and delete them from Elasticsearch.
+    """
+
+    client: Elasticsearch = elasticsearch_award_index.client
+
+    setup_elasticsearch_test(monkeypatch, elasticsearch_award_index)
+    original_es_count = client.count(index=elasticsearch_award_index.index_name)["count"]
+
+    # We should have both awards at this point, because both have a valid action_date
+    assert original_es_count == 2
 
     # Modify the existing DB award to now have an `action_date` before 2007-10-01
     baker.make(
@@ -61,6 +116,55 @@ def test_find_modified_awards_before_fy2008_spark(spark, award_search_data_fixtu
         update_date=datetime.now(),
     )
 
-    recently_modified_awards_before_fy2008 = _check_awards_for_pre_fy2008(spark)
+    modified_awards_to_delete = _check_awards_for_pre_fy2008()
+    number_of_deleted_awards = delete_docs_by_unique_key(
+        client=client,
+        key="generated_unique_award_id",
+        value_list=modified_awards_to_delete,
+        task_id="task1",
+        index=elasticsearch_award_index.index_name,
+    )
+    assert number_of_deleted_awards == 1
 
-    assert len(recently_modified_awards_before_fy2008) == 1
+    new_es_count = client.count(index=elasticsearch_award_index.index_name)["count"]
+    assert new_es_count == 1
+
+
+@pytest.mark.django_db(transaction=True)
+def test_delete_modified_transactions_before_fy2008(elasticsearch_transaction_index, test_data_fixture, monkeypatch):
+    """Test that we can find any transactions that PREVIOUSLY had an `action_date` on or after FY2008 (2007-10-01),
+    but have have been recently updated to have an `action_date` before FY2008 now and delete them from Elasticsearch.
+    """
+
+    client: Elasticsearch = elasticsearch_transaction_index.client
+
+    setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
+    original_es_count = client.count(index=elasticsearch_transaction_index.index_name)["count"]
+
+    # We should have both transactions at this point, because both have a valid action_date
+    assert original_es_count == 2
+
+    # Modify the existing DB transaction to now have an `action_date` before 2007-10-01
+    baker.make(
+        "search.TransactionSearch",
+        transaction_id=1,
+        award_id=1,
+        transaction_unique_id="TEST_TRANSACTION_1",
+        is_fpds=False,
+        afa_generated_unique="ABC_123",
+        action_date="2004-01-15",
+        etl_update_date=datetime.now(),
+    )
+
+    modified_transactions_to_delete = _gather_modified_transactions_pre_fy2008(config={"process_deletes": True})
+    number_of_deleted_transactions = delete_docs_by_unique_key(
+        client=client,
+        key="generated_unique_transaction_id",
+        value_list=modified_transactions_to_delete,
+        task_id="task1",
+        index=elasticsearch_transaction_index.index_name,
+    )
+    assert number_of_deleted_transactions == 1
+
+    new_es_count = client.count(index=elasticsearch_transaction_index.index_name)["count"]
+    assert new_es_count == 1
