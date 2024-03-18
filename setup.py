d423ca10448d53dd9da26bd065e31b25de5cf24e
Ayub <ayub.shahab13@gmail.com>
2024-03-18 02:07:03 -0400

    DEV-10756 updated config files to ensure compatibility with python version 3.10.13

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         ),
         long_description=(_PROJECT_ROOT_DIR / "README.md").read_text(encoding="utf-8"),
         long_description_content_type="text/markdown",
-        python_requires="==3.8.*",
+        python_requires="==3.10.*",
         license=(_PROJECT_ROOT_DIR / "LICENSE").read_text(encoding="utf-8"),
         packages=find_packages(),
         include_package_data=True,  # see MANIFEST.in for what is included
@@ -49,6 +49,7 @@ if __name__ == "__main__":
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3 :: Only",
         ],
     )
