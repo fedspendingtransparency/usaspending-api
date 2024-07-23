fdb36beb6ef677c4cd993b9954f8f322f827918a
Ayub <ayub.shahab13@gmail.com>
2024-07-23 13:53:35 -0400

    updated recipient_profile_delta_view.sql

--- usaspending_api/database_scripts/etl/recipient_profile_delta_view.sql
+++ usaspending_api/database_scripts/etl/recipient_profile_delta_view.sql
@@ -2,11 +2,15 @@ DROP VIEW IF EXISTS recipient_profile_delta_view;
 
 CREATE VIEW recipient_profile_delta_view AS
 SELECT
-  "id",
-  "recipient_hash",
-  "recipient_name",
-  "uei",
-  "recipient_level",
-  "duns"
+  rp.id,
+  rp.recipient_hash,
+  rp.recipient_name,
+  rp.uei,
+  rp.recipient_level,
+  rl.duns
 FROM
-  "recipient_profile"
+  recipient_profile rp
+JOIN
+  recipient_lookup rl
+ON
+  rp.recipient_hash = rl.recipient_hash;
