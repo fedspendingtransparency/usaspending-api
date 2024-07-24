8e0bd2bc4bcf1569d01752072b8bdfeaffdc0e77
aguest-kc <andrew.guest@kc.frb.org>
2024-07-24 14:32:24 -0500

    [DEV-10914] Update federal account spending endpoint

--- usaspending_api/disaster/v2/views/federal_account/spending.py
+++ usaspending_api/disaster/v2/views/federal_account/spending.py
@@ -1,13 +1,27 @@
 from decimal import Decimal
 from typing import List
 
-from django.db.models import Case, DecimalField, F, Func, IntegerField, OuterRef, Q, Subquery, Sum, Value, When
+from django.db.models import (
+    Case,
+    DecimalField,
+    F,
+    Func,
+    IntegerField,
+    OuterRef,
+    Q,
+    QuerySet,
+    Subquery,
+    Sum,
+    Value,
+    When,
+)
 from django.db.models.functions import Coalesce
 from rest_framework.response import Response
 
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.data_classes import Pagination
 from usaspending_api.common.helpers.generic_helper import get_pagination_metadata
+from usaspending_api.disaster.models import CovidFABASpending
 from usaspending_api.disaster.v2.views.disaster_base import (
     FabaOutlayMixin,
     PaginationMixin,
@@ -61,7 +75,12 @@ class SpendingViewSet(SpendingMixin, FabaOutlayMixin, ElasticsearchAccountDisast
     def post(self, request):
         if self.spending_type == "award":
             self.has_children = True
-            return self.perform_elasticsearch_search()
+            account_db_results = self._get_covid_faba_spending()
+            json_result = self._build_json_result(account_db_results)
+            sorted_json_result = self._sort_json_result(json_result)
+
+            return sorted_json_result
+            # return self.perform_elasticsearch_search()
         else:
             results = list(self.total_queryset)
             extra_columns = ["total_budgetary_resources"]
@@ -71,63 +90,140 @@ class SpendingViewSet(SpendingMixin, FabaOutlayMixin, ElasticsearchAccountDisast
 
         return Response(response)
 
-    def build_elasticsearch_result(self, info_buckets: List[dict]) -> List[dict]:
-        temp_results = {}
-        child_results = []
-        for bucket in info_buckets:
-            child = self._build_child_json_result(bucket)
-            child_results.append(child)
-        for child in child_results:
-            result = self._build_json_result(child)
-            child.pop("parent_data")
-            if result["id"] in temp_results.keys():
-                temp_results[result["id"]] = {
-                    "id": int(result["id"]),
-                    "code": result["code"],
-                    "description": result["description"],
-                    "award_count": temp_results[result["id"]]["award_count"] + result["award_count"],
-                    # the count of distinct awards contributing to the totals
-                    "obligation": temp_results[result["id"]]["obligation"] + result["obligation"],
-                    "outlay": temp_results[result["id"]]["outlay"] + result["outlay"],
+    def _get_covid_faba_spending(self) -> QuerySet:
+        queryset = (
+            CovidFABASpending.objects.filter(spending_type="treasury_account")
+            .filter(defc__in=self.filters["def_codes"])
+            .values(
+                "funding_federal_account_id",
+                "funding_federal_account_code",
+                "funding_federal_account_name",
+                "funding_treasury_account_id",
+                "funding_treasury_account_code",
+                "funding_treasury_account_name",
+            )
+            .annotate(
+                award_count=Sum("award_count"),
+                obligation_sum=Sum("obligation_sum"),
+                outlay_sum=Sum("outlay_sum"),
+            )
+        )
+
+        if self.query is not None:
+            queryset = queryset.filter(
+                Q(funding_federal_account_name__icontains=self.query)
+                | Q(funding_treasury_account_name__icontains=self.query)
+            )
+
+        return queryset
+
+    def _build_json_result(self, queryset: List[QuerySet]) -> dict:
+        """Build the JSON response that will be returned for this endpoint.
+
+        Args:
+            queryset: Database query results.
+
+        Returns:
+            Formatted JSON response.
+        """
+
+        response = {"totals": {"obligation": 0, "outlay": 0, "award_count": 0}, "results": []}
+
+        parent_lookup = {}
+        child_lookup = {}
+
+        for row in queryset:
+            parent_federal_account_id = int(row["funding_federal_account_id"])
+
+            if parent_federal_account_id not in parent_lookup.keys():
+                parent_lookup[parent_federal_account_id] = {
+                    "id": parent_federal_account_id,
+                    "code": row["funding_federal_account_code"],
+                    "description": row["funding_federal_account_name"],
+                    "award_count": 0,
+                    "obligation": Decimal(0),
+                    "outlay": Decimal(0),
+                    # This type of response never has a TBR value
                     "total_budgetary_resources": None,
-                    "children": temp_results[result["id"]]["children"] + result["children"],
+                    "children": [],
                 }
+
+            if parent_federal_account_id not in child_lookup.keys():
+                child_lookup[parent_federal_account_id] = [
+                    {
+                        "id": int(row["funding_treasury_account_id"]),
+                        "code": row["funding_treasury_account_code"],
+                        "description": row["funding_treasury_account_name"],
+                        "award_count": int(row["award_count"]),
+                        "obligation": Decimal(row["obligation_sum"]),
+                        "outlay": Decimal(row["outlay_sum"]),
+                    }
+                ]
             else:
-                temp_results[result["id"]] = result
-        results = [x for x in temp_results.values()]
-        return results
+                child_lookup[parent_federal_account_id].append(
+                    {
+                        "id": int(row["funding_treasury_account_id"]),
+                        "code": row["funding_treasury_account_code"],
+                        "description": row["funding_treasury_account_name"],
+                        "award_count": int(row["award_count"]),
+                        "obligation": Decimal(row["obligation_sum"]),
+                        "outlay": Decimal(row["outlay_sum"]),
+                    }
+                )
 
-    def _build_json_result(self, child):
-        return {
-            "id": child["parent_data"][2],
-            "code": child["parent_data"][1],
-            "description": child["parent_data"][0],
-            "award_count": child["award_count"],
-            # the count of distinct awards contributing to the totals
-            "obligation": child["obligation"],
-            "outlay": child["outlay"],
-            "total_budgetary_resources": None,
-            "children": [child],
-        }
+            response["totals"]["obligation"] += Decimal(row["obligation_sum"])
+            response["totals"]["outlay"] += Decimal(row["outlay_sum"])
+            response["totals"]["award_count"] += row["award_count"]
 
-    def _build_child_json_result(self, bucket: dict):
-        return {
-            "id": int(bucket["key"]),
-            "code": bucket["dim_metadata"]["hits"]["hits"][0]["_source"]["treasury_account_symbol"],
-            "description": bucket["dim_metadata"]["hits"]["hits"][0]["_source"]["treasury_account_title"],
-            # the count of distinct awards contributing to the totals
-            "award_count": int(bucket["count_awards_by_dim"]["award_count"]["value"]),
-            **{
-                key: Decimal(bucket.get(f"sum_{val}", {"value": 0})["value"])
-                for key, val in self.nested_nonzero_fields.items()
-            },
-            "total_budgetary_resources": None,
-            "parent_data": [
-                bucket["dim_metadata"]["hits"]["hits"][0]["_source"]["federal_account_title"],
-                bucket["dim_metadata"]["hits"]["hits"][0]["_source"]["federal_account_symbol"],
-                bucket["dim_metadata"]["hits"]["hits"][0]["_source"]["federal_account_id"],
-            ],
-        }
+        for parent_account_id, children in child_lookup.items():
+            for child_ta_account in children:
+                parent_lookup[parent_account_id]["children"].append(child_ta_account)
+                parent_lookup[parent_account_id]["award_count"] += child_ta_account["award_count"]
+                parent_lookup[parent_account_id]["obligation"] += child_ta_account["obligation"]
+                parent_lookup[parent_account_id]["outlay"] += child_ta_account["outlay"]
+
+        response["results"] = list(parent_lookup.values())
+
+        response["page_metadata"] = get_pagination_metadata(
+            len(response["results"]), self.pagination.limit, self.pagination.page
+        )
+
+        return response
+
+    def _sort_json_result(self, json_result: dict) -> dict:
+        """Sort the JSON by the appropriate field and in the appropriate order before returning it.
+
+        Args:
+            json_result: Unsorted JSON result.
+
+        Returns:
+            Sorted JSON result.
+        """
+
+        if self.pagination.sort_key == "description":
+            sorted_parents = sorted(
+                json_result["results"],
+                key=lambda val: val.get("description", "id").lower(),
+                reverse=self.pagination.sort_order == "desc",
+            )
+        else:
+            sorted_parents = sorted(
+                json_result["results"],
+                key=lambda val: val.get(self.pagination.sort_key, "id"),
+                reverse=self.pagination.sort_order == "desc",
+            )
+
+        if self.has_children:
+            for parent in sorted_parents:
+                parent["children"] = sorted(
+                    parent.get("children", []),
+                    key=lambda val: val.get(self.pagination.sort_key, "id"),
+                    reverse=self.pagination.sort_order == "desc",
+                )
+
+        json_result["results"] = sorted_parents
+
+        return json_result
 
     @property
     def total_queryset(self):
