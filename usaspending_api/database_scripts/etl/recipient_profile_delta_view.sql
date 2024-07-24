3109c683f660003f8dfa108e80dfbd9556549b60
Ayub <ayub.shahab13@gmail.com>
2024-07-24 16:02:28 -0400

    updated failing tests

--- usaspending_api/database_scripts/etl/recipient_profile_delta_view.sql
+++ usaspending_api/database_scripts/etl/recipient_profile_delta_view.sql
@@ -2,15 +2,12 @@ DROP VIEW IF EXISTS recipient_profile_delta_view;
 
 CREATE VIEW recipient_profile_delta_view AS
 SELECT
-  rp.id,
-  rp.recipient_hash,
-  rp.recipient_name,
-  rp.uei,
-  rp.recipient_level,
-  rl.duns
+  "id",
+  "recipient_hash",
+  "recipient_name",
+  "uei",
+  "recipient_level",
+  "recipient_unique_id" AS "duns"
 FROM
-  recipient_profile rp
-JOIN
-  recipient_lookup rl
-ON
-  rp.recipient_hash = rl.recipient_hash;
+  "recipient_profile";
+
