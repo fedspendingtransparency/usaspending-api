c2d770431fb8213baf0b22a4ae2cfd72adb9f9e8
Wil Collins <collins_william@bah.com>
2024-07-29 15:31:10 -0400

    [DEV-11187] Adds new database download list endpoint

---
+++ usaspending_api/download/v2/list_database_downloads.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from usaspending_api.download.helpers.latest_download_file_helpers import get_last_modified_download_file_by_prefix, remove_file_prefix_if_exists
+from usaspending_api.common.cache_decorator import cache_response
+
+class ListDatabaseDownloadsViewSet(APIView):
+    """
+    Returns information about the latest full and subset database download files.
+    """
+
+    redirect_dir = settings.DATABASE_DOWNLOAD_S3_REDIRECT_DIR
+
+    @cache_response()
+    def get(self, request):
+        """Return list of downloads that match the requested params."""
+
+        full_download_prefix = f"{self.redirect_dir}/usaspending-db_"
+        latest_full_download_name = get_last_modified_download_file_by_prefix(full_download_prefix)
+        latest_full_download_name = remove_file_prefix_if_exists(latest_full_download_name, f"{self.redirect_dir}/")
+
+        subset_download_prefix = f"{self.redirect_dir}/usaspending-db-subset_"
+        latest_subset_download_name = get_last_modified_download_file_by_prefix(subset_download_prefix)
+        latest_subset_download_name = remove_file_prefix_if_exists(latest_subset_download_name, f"{self.redirect_dir}/")
+
+        results = {
+            "full_download_file": self._structure_file_response(latest_full_download_name),
+            "subset_download_file": self._structure_file_response(latest_subset_download_name),
+            "messages": [],
+        }
+        return Response(results)
+
+
+    def _structure_file_response(self, file_name):
+        return {
+            "file_name": file_name,
+            "url": (
+                f"{settings.FILES_SERVER_BASE_URL}/{self.redirect_dir}/{file_name}" if file_name is not None else None
+            ),
+        }
