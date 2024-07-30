0863f76875d433cda7dd4d8e8d2ce0fe72519c30
aguest-kc <andrew.guest@kc.frb.org>
2024-07-30 11:57:51 -0500

    [DEV-10915] Update tests

--- usaspending_api/disaster/tests/conftest.py
+++ usaspending_api/disaster/tests/conftest.py
@@ -27,13 +27,7 @@ from usaspending_api.disaster.tests.fixtures.federal_account_data import (
 )
 from usaspending_api.disaster.tests.fixtures.helpers import helpers
 from usaspending_api.disaster.tests.fixtures.object_class_data import (
-    basic_fa_by_object_class_with_multpile_object_class,
-    basic_fa_by_object_class_with_multpile_object_class_of_same_code,
-    basic_fa_by_object_class_with_object_class,
-    basic_fa_by_object_class_with_object_class_but_no_obligations,
     basic_faba_with_object_class,
-    faba_with_object_class_and_two_awards,
-    faba_with_two_object_classes_and_two_awards,
 )
 from usaspending_api.disaster.tests.fixtures.object_class_loan_data import basic_object_class_faba_with_loan_value
 from usaspending_api.disaster.tests.fixtures.overview_data import (
@@ -74,10 +68,6 @@ __all__ = [
     "award_count_submission",
     "award_with_no_outlays",
     "basic_award",
-    "basic_fa_by_object_class_with_object_class",
-    "basic_fa_by_object_class_with_object_class_but_no_obligations",
-    "basic_fa_by_object_class_with_multpile_object_class",
-    "basic_fa_by_object_class_with_multpile_object_class_of_same_code",
     "basic_faba_with_object_class",
     "basic_object_class_faba_with_loan_value",
     "cfda_awards_and_transactions",
@@ -101,8 +91,6 @@ __all__ = [
     "faba_with_toptier_agencies_that_cancel_out_in_outlay",
     "faba_with_non_covid_values",
     "faba_with_values",
-    "faba_with_object_class_and_two_awards",
-    "faba_with_two_object_classes_and_two_awards",
     "fabs_award_with_old_submission",
     "fabs_award_with_quarterly_submission",
     "fabs_award_with_unclosed_submission",
--- usaspending_api/disaster/tests/fixtures/object_class_data.py
+++ usaspending_api/disaster/tests/fixtures/object_class_data.py
@@ -1,227 +1,88 @@
-import pytest
+from decimal import Decimal
 
+import pytest
 from model_bakery import baker
 
-from usaspending_api.references.models import DisasterEmergencyFundCode
-from usaspending_api.submissions.models import SubmissionAttributes
-from usaspending_api.disaster.tests.fixtures.award_count_data import _normal_award
-
 
 @pytest.fixture
-def basic_faba_with_object_class(award_count_sub_schedule, award_count_submission, defc_codes):
-    basic_object_class = major_object_class_with_children("001", [1])
-
-    award = _normal_award(109)
-
-    baker.make(
-        "awards.FinancialAccountsByAwards",
-        parent_award_id="basic award",
-        award=award,
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=basic_object_class[0],
-        transaction_obligated_amount=1,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=0,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=0,
-    )
-
-
-@pytest.fixture
-def basic_fa_by_object_class_with_object_class(award_count_sub_schedule, award_count_submission, defc_codes):
-    basic_object_class = major_object_class_with_children("001", [1])
+def basic_faba_with_object_class():
+    baker.make("references.DisasterEmergencyFundCode", code="A", public_law="Public law for A")
+    baker.make("references.DisasterEmergencyFundCode", code="M", public_law="Public law for M")
+    baker.make("references.DisasterEmergencyFundCode", code="N", public_law="Public law for N")
+    baker.make("references.DisasterEmergencyFundCode", code="O", public_law="Public law for O")
 
     baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=basic_object_class[0],
-        obligations_incurred_by_program_object_class_cpe=19,
-        gross_outlay_amount_by_program_object_class_cpe=0,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-10,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=-10,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=10,
+        "disaster.CovidFABASpending",
+        spending_level="object_class",
+        funding_major_object_class_id="001",
+        funding_major_object_class_code="001",
+        funding_major_object_class_name="001 name",
+        funding_object_class_id=1,
+        funding_object_class_code="0001",
+        funding_object_class_name="0001 name",
+        defc="M",
+        award_type="A",
+        award_count=1,
+        obligation_sum=Decimal(1.0),
+        outlay_sum=Decimal(0.0),
     )
-
-
-@pytest.fixture
-def basic_fa_by_object_class_with_multpile_object_class(
-    award_count_sub_schedule, award_count_quarterly_submission, defc_codes
-):
-    major_object_class_1 = major_object_class_with_children("001", [1, 2, 3])
-
     baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=major_object_class_1[0],
-        obligations_incurred_by_program_object_class_cpe=100,
-        gross_outlay_amount_by_program_object_class_cpe=4,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-90,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=-9,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=7,
+        "disaster.CovidFABASpending",
+        spending_level="object_class",
+        funding_major_object_class_id="002",
+        funding_major_object_class_code="002",
+        funding_major_object_class_name="002 name",
+        funding_object_class_id=2,
+        funding_object_class_code="0002",
+        funding_object_class_name="0002 name",
+        defc="N",
+        award_type="B",
+        award_count=2,
+        obligation_sum=Decimal(1.0),
+        outlay_sum=Decimal(0.0),
     )
-
     baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=major_object_class_1[1],
-        obligations_incurred_by_program_object_class_cpe=10,
-        gross_outlay_amount_by_program_object_class_cpe=200,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-10,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=-50,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=-130,
+        "disaster.CovidFABASpending",
+        spending_level="object_class",
+        funding_major_object_class_id="003",
+        funding_major_object_class_code="003",
+        funding_major_object_class_name="003 name",
+        funding_object_class_id=3,
+        funding_object_class_code="00031",
+        funding_object_class_name="00031 name",
+        defc="O",
+        award_type="B",
+        award_count=2,
+        obligation_sum=Decimal(1.0),
+        outlay_sum=Decimal(10.0),
     )
-
     baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=major_object_class_1[2],
-        obligations_incurred_by_program_object_class_cpe=3,
-        gross_outlay_amount_by_program_object_class_cpe=0,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-2,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=9,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=-9,
+        "disaster.CovidFABASpending",
+        spending_level="object_class",
+        funding_major_object_class_id="003",
+        funding_major_object_class_code="003",
+        funding_major_object_class_name="003 name",
+        funding_object_class_id=4,
+        funding_object_class_code="00032",
+        funding_object_class_name="00032 name",
+        defc="O",
+        award_type="C",
+        award_count=3,
+        obligation_sum=Decimal(1.0),
+        outlay_sum=Decimal(20.0),
     )
-
-
-@pytest.fixture
-def basic_fa_by_object_class_with_multpile_object_class_of_same_code(
-    award_count_sub_schedule, award_count_quarterly_submission, defc_codes
-):
-    class1 = baker.make(
-        "references.ObjectClass",
-        id=9,
-        major_object_class="major",
-        major_object_class_name=f"major name",
-        object_class=f"0001",
-        object_class_name=f"0001 name",
-    )
-
-    class2 = baker.make(
-        "references.ObjectClass",
-        id=10,
-        major_object_class="major",
-        major_object_class_name=f"major name",
-        object_class=f"0001",
-        object_class_name=f"0001 name",
-    )
-
     baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=class1,
-        obligations_incurred_by_program_object_class_cpe=30,
-        gross_outlay_amount_by_program_object_class_cpe=992,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-20,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=-90,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=-900,
+        "disaster.CovidFABASpending",
+        spending_level="object_class",
+        funding_major_object_class_id="003",
+        funding_major_object_class_code="003",
+        funding_major_object_class_name="003 name",
+        funding_object_class_id=5,
+        funding_object_class_code="00033",
+        funding_object_class_name="00033 name",
+        defc="O",
+        award_type="D",
+        award_count=2,
+        obligation_sum=Decimal(1.0),
+        outlay_sum=Decimal(30.0),
     )
-
-    baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=class2,
-        obligations_incurred_by_program_object_class_cpe=100,
-        gross_outlay_amount_by_program_object_class_cpe=5,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-100,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=15,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=0,
-    )
-
-
-@pytest.fixture
-def basic_fa_by_object_class_with_object_class_but_no_obligations(
-    award_count_sub_schedule, award_count_submission, defc_codes
-):
-    basic_object_class = major_object_class_with_children("001", [1])
-
-    baker.make(
-        "financial_activities.FinancialAccountsByProgramActivityObjectClass",
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=basic_object_class[0],
-        obligations_incurred_by_program_object_class_cpe=333,
-        gross_outlay_amount_by_program_object_class_cpe=0,
-        deobligations_recoveries_refund_pri_program_object_class_cpe=-333,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=9,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=-9,
-    )
-
-
-@pytest.fixture
-def faba_with_object_class_and_two_awards(award_count_sub_schedule, award_count_submission, defc_codes):
-    basic_object_class = major_object_class_with_children("001", [1])
-
-    award1 = _normal_award(156)
-    award2 = _normal_award(212)
-
-    baker.make(
-        "awards.FinancialAccountsByAwards",
-        parent_award_id="basic award 1",
-        award=award1,
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=basic_object_class[0],
-        transaction_obligated_amount=1,
-        ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe=0,
-        ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe=0,
-    )
-
-    baker.make(
-        "awards.FinancialAccountsByAwards",
-        parent_award_id="basic award 2",
-        award=award2,
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=basic_object_class[0],
-        transaction_obligated_amount=1,
-    )
-
-
-@pytest.fixture
-def faba_with_two_object_classes_and_two_awards(award_count_sub_schedule, award_count_submission, defc_codes):
-    object_class1 = major_object_class_with_children("001", [1])
-    object_class2 = major_object_class_with_children("002", [2])
-
-    award1 = _normal_award(3)
-    award2 = _normal_award(4)
-
-    baker.make(
-        "awards.FinancialAccountsByAwards",
-        parent_award_id="basic award",
-        award=award1,
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=object_class1[0],
-        transaction_obligated_amount=1,
-    )
-
-    baker.make(
-        "awards.FinancialAccountsByAwards",
-        parent_award_id="basic award",
-        award=award2,
-        disaster_emergency_fund=DisasterEmergencyFundCode.objects.filter(code="M").first(),
-        submission=SubmissionAttributes.objects.all().first(),
-        object_class=object_class2[0],
-        transaction_obligated_amount=1,
-    )
-
-
-def major_object_class_with_children(major_code, minor_codes):
-    retval = []
-    for minor_code in minor_codes:
-        retval.append(
-            baker.make(
-                "references.ObjectClass",
-                id=minor_code,
-                major_object_class=major_code,
-                major_object_class_name=f"{major_code} name",
-                object_class=f"000{minor_code}",
-                object_class_name=f"000{minor_code} name",
-            )
-        )
-    return retval
