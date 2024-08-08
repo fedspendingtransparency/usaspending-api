2dd2c246c9532039d4506fa851463e548dd37dc0
aguest-kc <andrew.guest@kc.frb.org>
2024-08-08 17:27:34 -0500

    [DEV-11345] Don't use the Aurora writer instance for reading

--- usaspending_api/routers/replicas.py
+++ usaspending_api/routers/replicas.py
@@ -1,8 +1,7 @@
-import random
-
 from django.db import DEFAULT_DB_ALIAS
-from usaspending_api.references.models import FilterHash
+
 from usaspending_api.download.models.download_job import DownloadJob
+from usaspending_api.references.models import FilterHash
 
 
 class ReadReplicaRouter:
@@ -25,21 +24,21 @@ class ReadReplicaRouter:
         """
         if model in [FilterHash, DownloadJob]:
             return self.writable_database
-        return random.choice(self.usaspending_databases)
+        return self.read_replicas
 
     def db_for_write(self, model, **hints):
         return self.writable_database
 
     def allow_relation(self, obj1, obj2, **hints):
-        """ Relations are currently only allowed in USAspending.  Cross database relations are not allowed. """
+        """Relations are currently only allowed in USAspending.  Cross database relations are not allowed."""
         return obj1._state.db in self.usaspending_databases and obj2._state.db == obj1._state.db
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        """ Migrations should only run in USAspending against the writable database. """
+        """Migrations should only run in USAspending against the writable database."""
         return db == self.writable_database
 
 
 class DefaultOnlyRouter(ReadReplicaRouter):
-    """ For when only the default connection is used.  Prevents model access/migrations to Broker. """
+    """For when only the default connection is used.  Prevents model access/migrations to Broker."""
 
     read_replicas = []
