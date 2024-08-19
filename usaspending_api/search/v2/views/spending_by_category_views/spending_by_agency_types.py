9c37973137447e5fbcdc77a6e0fe3d2ca5bca815
Kambhampati <J1LXK04@rb.win.frb.org>
2024-08-19 15:58:02 -0500

    DEV-10991; typo

--- usaspending_api/search/v2/views/spending_by_category_views/spending_by_agency_types.py
+++ usaspending_api/search/v2/views/spending_by_category_views/spending_by_agency_types.py
@@ -64,7 +64,7 @@ class AbstractAgencyViewSet(AbstractSpendingByCategoryViewSet, metaclass=ABCMeta
                 for toptier_info in toptier_agency_info_query.all():
                     top_id = toptier_info.pop("toptier_agency")
                     toptier_query = ToptierAgency.objects.filter(toptier_agency_id=top_id).annotate(
-                        top_id=F("toptier_agency_id"), top_code=F("toptier_ code"), top_name=F("name")
+                        top_id=F("toptier_agency_id"), top_code=F("toptier_code"), top_name=F("name")
                     )
                     toptier_query = toptier_query.values("top_id", "top_code", "top_name")
                     current_agency_info[agency_code].append(toptier_query)
