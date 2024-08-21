eb30debbd74b1ed934c785e4b4a309d64d17e3a5
Ayub <ayub.shahab13@gmail.com>
2024-08-21 14:59:13 -0400

    formatting changes

--- usaspending_api/etl/tests/integration/test_load_to_from_delta.py
+++ usaspending_api/etl/tests/integration/test_load_to_from_delta.py
@@ -833,12 +833,7 @@ def test_load_table_to_delta_for_sam_recipient(spark, s3_unittest_data_bucket, p
 def test_load_table_to_delta_for_summary_state_view(
     spark, s3_unittest_data_bucket, populate_usas_data, hive_unittest_metastore_db
 ):
-    tables_to_load = [
-        "transaction_fabs",
-        "transaction_fpds",
-        "transaction_normalized",
-        "award_search"
-    ]
+    tables_to_load = ["transaction_fabs", "transaction_fpds", "transaction_normalized", "award_search"]
     create_and_load_all_delta_tables(spark, s3_unittest_data_bucket, tables_to_load)
     verify_delta_table_loaded_to_delta(
         spark, "summary_state_view", s3_unittest_data_bucket, load_command="load_query_to_delta"
