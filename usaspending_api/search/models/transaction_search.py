7d60ddbd47baaefb90cf45735c7b761b5a7101da
aguest-kc <andrew.guest@kc.frb.org>
2024-09-24 11:49:30 -0500

    [DEV-11488] Add program activity to transactions

--- usaspending_api/search/models/transaction_search.py
+++ usaspending_api/search/models/transaction_search.py
@@ -413,6 +413,8 @@ class TransactionSearch(models.Model):
     veterinary_hospital = models.BooleanField(null=True)
     woman_owned_business = models.BooleanField(null=True)
     women_owned_small_business = models.BooleanField(null=True)
+    program_activity_names = ArrayField(models.TextField(), default=list, null=True)
+    program_activity_codes = ArrayField(models.TextField(), default=list, null=True)
 
     class Meta:
         db_table = "transaction_search"
