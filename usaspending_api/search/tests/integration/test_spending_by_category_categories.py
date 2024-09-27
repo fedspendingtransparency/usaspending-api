f9a5d9425abe6e001aabbe0a3905f79c63cff4d6
Ronald Ong <616554@bah.com>
2024-09-27 17:08:52 -0400

    DEV-11398 update existing tests for spending_by_category to use new spending_level field

--- usaspending_api/search/tests/integration/test_spending_by_category_categories.py
+++ usaspending_api/search/tests/integration/test_spending_by_category_categories.py
@@ -731,10 +731,10 @@ def federal_accounts_test_data(db):
 
 
 @pytest.mark.django_db
-def test_category_awarding_agency_awards(agency_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_awarding_agency_transactions(agency_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "awarding_agency", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "awarding_agency", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = AwardingAgencyViewSet().perform_search(test_payload, {})
 
@@ -753,7 +753,7 @@ def test_category_awarding_agency_awards(agency_test_data, monkeypatch, elastics
 
 @pytest.mark.django_db
 def test_category_awarding_agency_subawards(agency_test_data):
-    test_payload = {"category": "awarding_agency", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "awarding_agency", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = AwardingAgencyViewSet().perform_search(test_payload, {})
 
@@ -769,10 +769,10 @@ def test_category_awarding_agency_subawards(agency_test_data):
 
 
 @pytest.mark.django_db
-def test_category_awarding_subagency_awards(agency_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_awarding_subagency_transactions(agency_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "awarding_subagency", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "awarding_subagency", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = AwardingSubagencyViewSet().perform_search(test_payload, {})
 
@@ -801,7 +801,7 @@ def test_category_awarding_subagency_awards(agency_test_data, monkeypatch, elast
 
 @pytest.mark.django_db
 def test_category_awarding_subagency_subawards(agency_test_data):
-    test_payload = {"category": "awarding_subagency", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "awarding_subagency", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = AwardingSubagencyViewSet().perform_search(test_payload, {})
 
@@ -829,10 +829,10 @@ def test_category_awarding_subagency_subawards(agency_test_data):
 
 
 @pytest.mark.django_db
-def test_category_funding_agency_awards(agency_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_funding_agency_transactions(agency_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "funding_agency", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "funding_agency", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = FundingAgencyViewSet().perform_search(test_payload, {})
 
@@ -849,7 +849,7 @@ def test_category_funding_agency_awards(agency_test_data, monkeypatch, elasticse
 
 @pytest.mark.django_db
 def test_category_funding_agency_subawards(agency_test_data):
-    test_payload = {"category": "funding_agency", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "funding_agency", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = FundingAgencyViewSet().perform_search(test_payload, {})
 
@@ -865,10 +865,10 @@ def test_category_funding_agency_subawards(agency_test_data):
 
 
 @pytest.mark.django_db
-def test_category_funding_subagency_awards(agency_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_funding_subagency_transactions(agency_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "funding_subagency", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "funding_subagency", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = FundingSubagencyViewSet().perform_search(test_payload, {})
 
@@ -897,7 +897,7 @@ def test_category_funding_subagency_awards(agency_test_data, monkeypatch, elasti
 
 @pytest.mark.django_db
 def test_category_funding_subagency_subawards(agency_test_data):
-    test_payload = {"category": "funding_subagency", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "funding_subagency", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = FundingSubagencyViewSet().perform_search(test_payload, {})
 
@@ -925,10 +925,10 @@ def test_category_funding_subagency_subawards(agency_test_data):
 
 
 @pytest.mark.django_db
-def test_category_recipient_awards(recipient_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_recipient_transactions(recipient_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "recipient", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "recipient", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = RecipientViewSet().perform_search(test_payload, {})
 
@@ -961,7 +961,7 @@ def test_category_recipient_awards(recipient_test_data, monkeypatch, elasticsear
 
 @pytest.mark.django_db
 def test_category_recipient_subawards(recipient_test_data):
-    test_payload = {"category": "recipient", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "recipient", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = RecipientViewSet().perform_search(test_payload, {})
 
@@ -992,7 +992,7 @@ def test_category_recipient_subawards(recipient_test_data):
 
 @pytest.mark.django_db
 def test_category_recipient_duns_subawards_deprecated(recipient_test_data):
-    test_payload = {"category": "recipient_duns", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "recipient_duns", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = RecipientDunsViewSet().perform_search(test_payload, {})
 
@@ -1022,10 +1022,10 @@ def test_category_recipient_duns_subawards_deprecated(recipient_test_data):
 
 
 @pytest.mark.django_db
-def test_category_cfda_awards(cfda_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_cfda_transactions(cfda_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "cfda", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "cfda", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = CfdaViewSet().perform_search(test_payload, {})
 
@@ -1042,7 +1042,7 @@ def test_category_cfda_awards(cfda_test_data, monkeypatch, elasticsearch_transac
 
 @pytest.mark.django_db
 def test_category_cfda_subawards(cfda_test_data):
-    test_payload = {"category": "cfda", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "cfda", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = CfdaViewSet().perform_search(test_payload, {})
 
@@ -1062,28 +1062,28 @@ def test_category_defc_subawards(client):
     resp = client.post(
         "/api/v2/search/spending_by_category",
         content_type="application/json",
-        data=json.dumps({"category": "defc", "subawards": True, "page": 1, "limit": 10}),
+        data=json.dumps({"category": "defc", "spending_level": "subawards", "page": 1, "limit": 10}),
     )
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.json().get("results")) == 0
 
 
 @pytest.mark.django_db
-def test_category_defc_awards(client):
+def test_category_defc_transactions(client):
     resp = client.post(
         "/api/v2/search/spending_by_category",
         content_type="application/json",
-        data=json.dumps({"category": "defc", "subawards": False, "page": 1, "limit": 10}),
+        data=json.dumps({"category": "defc", "spending_level": "transactions", "page": 1, "limit": 10}),
     )
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.json().get("results")) == 0
 
 
 @pytest.mark.django_db
-def test_category_psc_awards(psc_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_psc_transactions(psc_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "psc", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "psc", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = PSCViewSet().perform_search(test_payload, {})
 
@@ -1102,10 +1102,10 @@ def test_category_psc_awards(psc_test_data, monkeypatch, elasticsearch_transacti
 
 
 @pytest.mark.django_db
-def test_category_naics_awards(naics_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_naics_transactions(naics_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "naics", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "naics", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = NAICSViewSet().perform_search(test_payload, {})
 
@@ -1124,10 +1124,10 @@ def test_category_naics_awards(naics_test_data, monkeypatch, elasticsearch_trans
 
 
 @pytest.mark.django_db
-def test_category_county_awards(geo_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_county_transactions(geo_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "county", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "county", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = CountyViewSet().perform_search(test_payload, {})
 
@@ -1147,7 +1147,7 @@ def test_category_county_awards(geo_test_data, monkeypatch, elasticsearch_transa
 
 @pytest.mark.django_db
 def test_category_county_subawards(geo_test_data):
-    test_payload = {"category": "county", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "county", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = CountyViewSet().perform_search(test_payload, {})
 
@@ -1166,10 +1166,10 @@ def test_category_county_subawards(geo_test_data):
 
 
 @pytest.mark.django_db
-def test_category_district_awards(geo_test_data, monkeypatch, elasticsearch_transaction_index):
+def test_category_district_transactions(geo_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "district", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "district", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = DistrictViewSet().perform_search(test_payload, {})
 
@@ -1189,7 +1189,7 @@ def test_category_district_awards(geo_test_data, monkeypatch, elasticsearch_tran
 
 @pytest.mark.django_db
 def test_category_district_subawards(geo_test_data):
-    test_payload = {"category": "district", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "district", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = DistrictViewSet().perform_search(test_payload, {})
 
@@ -1211,7 +1211,7 @@ def test_category_district_subawards(geo_test_data):
 def test_category_state_territory(geo_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "state_territory", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "state_territory", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = StateTerritoryViewSet().perform_search(test_payload, {})
 
@@ -1228,7 +1228,7 @@ def test_category_state_territory(geo_test_data, monkeypatch, elasticsearch_tran
 
 @pytest.mark.django_db
 def test_category_state_territory_subawards(geo_test_data):
-    test_payload = {"category": "state_territory", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "state_territory", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = StateTerritoryViewSet().perform_search(test_payload, {})
 
@@ -1247,7 +1247,7 @@ def test_category_state_territory_subawards(geo_test_data):
 def test_category_country(geo_test_data, monkeypatch, elasticsearch_transaction_index):
     setup_elasticsearch_test(monkeypatch, elasticsearch_transaction_index)
 
-    test_payload = {"category": "country", "subawards": False, "page": 1, "limit": 50}
+    test_payload = {"category": "country", "spending_level": "transactions", "page": 1, "limit": 50}
 
     spending_by_category_logic = CountryViewSet().perform_search(test_payload, {})
 
@@ -1264,7 +1264,7 @@ def test_category_country(geo_test_data, monkeypatch, elasticsearch_transaction_
 
 @pytest.mark.django_db
 def test_category_country_subawards(geo_test_data):
-    test_payload = {"category": "country", "subawards": True, "page": 1, "limit": 50}
+    test_payload = {"category": "country", "spending_level": "subawards", "page": 1, "limit": 50}
 
     spending_by_category_logic = CountryViewSet().perform_search(test_payload, {})
 
@@ -1286,7 +1286,7 @@ def test_category_federal_accounts(federal_accounts_test_data, monkeypatch, elas
     test_payload = {
         "category": "federal_account",
         "filters": {"recipient_id": "ab4d44f6-7a16-4ca7-405a-dcb913effbaf-R"},
-        "subawards": False,
+        "spending_level": "transactions",
         "page": 1,
         "limit": 50,
     }
