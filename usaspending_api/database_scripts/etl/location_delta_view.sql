b90b68554fc18a949730afda6f6afda412514da9
Zach Flanders <zachary.flanders@kc.frb.org>
2024-11-29 15:48:38 -0600

    [DEV-11608] - fixing whitespace issue in sql

--- usaspending_api/database_scripts/etl/location_delta_view.sql
+++ usaspending_api/database_scripts/etl/location_delta_view.sql
@@ -227,23 +227,23 @@ WITH locations_cte AS (
 		recipient_location_country_name IS NOT NULL
 )
 SELECT
-    ROW_NUMBER() OVER (ORDER BY country_name, state_name) AS id,
+	ROW_NUMBER() OVER (ORDER BY country_name, state_name) AS id,
 	country_name,
 	state_name,
 	array_agg(DISTINCT(city_name)) FILTER (WHERE city_name IS NOT NULL) AS cities,
 	json_agg(DISTINCT(jsonb_build_object(
-        'name', county_name,
-        'fips', CONCAT(state_fips, county_fips)
-    )))
-    FILTER (
-        WHERE (
-            county_name IS NOT NULL
-            AND
-            state_fips IS NOT NULL
-            AND
-            county_fips IS NOT NULL
-        )
-    )
+		'name', county_name,
+		'fips', CONCAT(state_fips, county_fips)
+	)))
+	FILTER (
+		WHERE (
+			county_name IS NOT NULL
+			AND
+			state_fips IS NOT NULL
+			AND
+			county_fips IS NOT NULL
+			)
+	)
 	AS counties,
 	array_agg(DISTINCT(zip_code)) FILTER (WHERE zip_code IS NOT NULL) AS zip_codes,
 	array_agg(DISTINCT(current_congressional_district)) FILTER (WHERE current_congressional_district IS NOT NULL) AS current_congressional_districts,
