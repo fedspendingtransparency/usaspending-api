d682bb7c91c41714d50bd3f1ecd5491fda38a151
Zach Flanders <zachary.flanders@kc.frb.org>
2024-11-29 09:33:12 -0600

    [DEV-11608] - Add case statement to handle difference with how aggregation filters return null in spark

--- usaspending_api/database_scripts/etl/location_delta_view.sql
+++ usaspending_api/database_scripts/etl/location_delta_view.sql
@@ -231,20 +231,36 @@ SELECT
 	country_name,
 	state_name,
 	array_agg(DISTINCT(city_name)) FILTER (WHERE city_name IS NOT NULL) AS cities,
-	to_json(array_agg(DISTINCT(jsonb_build_object(
-		'name', county_name,
-		'fips', CONCAT(state_fips, county_fips)
-	)))
-	FILTER (
-		WHERE (
-			county_name IS NOT NULL
-			AND
-			state_fips IS NOT NULL
-			AND
-			county_fips IS NOT NULL
-		)
-	))
-	AS counties,
+	CASE
+	    WHEN cardinality(array_agg(DISTINCT(jsonb_build_object(
+            'name', county_name,
+            'fips', CONCAT(state_fips, county_fips)
+        )))
+        FILTER (
+            WHERE (
+                county_name IS NOT NULL
+                AND
+                state_fips IS NOT NULL
+                AND
+                county_fips IS NOT NULL
+            )
+        )) = 0
+	    THEN NULL
+	    ELSE
+	        array_agg(DISTINCT(jsonb_build_object(
+                'name', county_name,
+                'fips', CONCAT(state_fips, county_fips)
+            )))
+            FILTER (
+                WHERE (
+                    county_name IS NOT NULL
+                    AND
+                    state_fips IS NOT NULL
+                    AND
+                    county_fips IS NOT NULL
+                )
+            )
+	END AS counties,
 	array_agg(DISTINCT(zip_code)) FILTER (WHERE zip_code IS NOT NULL) AS zip_codes,
 	array_agg(DISTINCT(current_congressional_district)) FILTER (WHERE current_congressional_district IS NOT NULL) AS current_congressional_districts,
 	array_agg(DISTINCT(original_congressional_district)) FILTER (WHERE original_congressional_district IS NOT NULL) AS original_congressional_districts
