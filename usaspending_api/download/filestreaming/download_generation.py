25c0a873a00deac7c095f25bbcea8b7d1ee27f22
Ayub <ayub.shahab13@gmail.com>
2024-12-01 19:19:13 -0500

    undo temp fix for download_generation test passing

--- usaspending_api/download/filestreaming/download_generation.py
+++ usaspending_api/download/filestreaming/download_generation.py
@@ -14,7 +14,7 @@ import tempfile
 import time
 import traceback
 
-import django
+# import django
 from django.conf import settings
 
 from opentelemetry import trace
@@ -23,8 +23,8 @@ from opentelemetry.trace import SpanKind
 from datetime import datetime, timezone
 from django.apps import apps
 
-if not apps.ready:
-    django.setup()
+# if not apps.ready:
+#     django.setup()
 
 from usaspending_api.download.models.download_job_lookup import DownloadJobLookup
 from usaspending_api.search.filters.time_period.decorators import NEW_AWARDS_ONLY_KEYWORD
