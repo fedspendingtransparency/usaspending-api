1b743cd5c11c8de5477368cd3c6d4ad5ac2d4c7b
Zach Flanders <zachary.flanders@kc.frb.org>
2024-12-17 14:34:46 -0600

    [DEV-10092] - Update load PSC command and test

--- usaspending_api/references/management/commands/load_psc.py
+++ usaspending_api/references/management/commands/load_psc.py
@@ -1,4 +1,6 @@
+import datetime
 import logging
+from typing import Optional, Union, List, Tuple
 from urllib.error import HTTPError
 
 import pandas as pd
@@ -12,31 +14,44 @@ class Command(BaseCommand):
 
     logger = logging.getLogger("script")
     default_filepath = "https://www.acquisition.gov/sites/default/files/manual/PSC%20April%202024.xlsx"
+    default_sheet_name = "PSC for 042022"
 
     def add_arguments(self, parser):
         parser.add_argument(
             "-p", "--path", help="The path to the spreadsheets to load. Can be a url.", default=self.default_filepath
         )
         parser.add_argument(
+            "-s",
+            "--sheet_name",
+            help="The name of the worksheet in the spreadsheet file.",
+            default=self.default_sheet_name,
+        )
+        parser.add_argument(
             "-u", "--update", help="Updates the lengths of any codes that were not in the file.", action="store_true"
         )
 
     def handle(self, *args, **options):
+        num_created, num_updated, errors = load_psc(
+            fullpath=options["path"],
+            sheet_name=options["sheet_name"],
+            update=options["update"],
+        )
+        self.logger.log(
+            20,
+            f"Load PSC command created {num_created} PSCs and updated {num_updated} PSCs with {len(errors)} errors.",
+        )
 
-        load_psc(fullpath=options["path"], update=options["update"])
-        self.logger.log(20, "Loaded PSC codes successfully.")
 
-
-def load_psc(fullpath, update):
+def load_psc(fullpath: str, sheet_name: str, update: bool) -> Tuple[int, int, List]:
     """
     Create/Update Product or Service Code records from an Excel doc of historical data.
     """
+    errors = []
     try:
-        logger = logging.getLogger("script")
         new_pscs = (
             pd.read_excel(
-                "https://www.acquisition.gov/sites/default/files/manual/PSC%20April%202024.xlsx",
-                sheet_name="PSC for 042022",
+                fullpath,
+                sheet_name=sheet_name,
                 usecols=[
                     "PSC CODE",
                     "START DATE",
@@ -55,23 +70,40 @@ def load_psc(fullpath, update):
                     "PSC CODE": "code",
                     "START DATE": "start_date",
                     "END DATE": "end_date",
-                    "PRODUCT AND SERVICE CODE NAME": "full_name",
-                    "PRODUCT AND SERVICE CODE FULL NAME (DESCRIPTION)": "description",
+                    "PRODUCT AND SERVICE CODE FULL NAME (DESCRIPTION)": "full_name",
+                    "PRODUCT AND SERVICE CODE NAME": "description",
                     "PRODUCT AND SERVICE CODE INCLUDES": "includes",
                     "PRODUCT AND SERVICE CODE EXCLUDES": "excludes",
                     "PRODUCT AND SERVICE CODE NOTES": "notes",
                 }
             )
-            # Remove duplicate codes keeping most recent start_date
-            .loc[lambda df: (~df.sort_values(by=["code", "start_date"]).duplicated(subset=["code"], keep="last"))]
+            .loc[
+                lambda df: (
+                    # Remove duplicate codes keeping most recent start_date
+                    ~df.sort_values(by=["code", "start_date"]).duplicated(subset=["code"], keep="last")
+                    # Remove rows with missing descriptions
+                    & df["description"].notnull()
+                )
+            ]
             # Add length column
-            .assign(length=lambda df: df["code"].astype(str).str.len()).to_dict(orient="records")
+            .assign(length=lambda df: df["code"].astype(str).str.len())
+            # Replace missing values with None
+            .where(lambda df: df.notnull(), None)
+            .to_dict(orient="records")
         )
+        logger = logging.getLogger("script")
+        num_created = 0
+        num_updated = 0
         for new_psc in new_pscs:
             psc, created = PSC.objects.get_or_create(code=new_psc["code"])
+            if created:
+                num_created += 1
+            else:
+                num_updated += 1
             psc.length = new_psc["length"]
             psc.description = new_psc["description"]
-            check_start_end_dates(psc, new_psc["start_date"], new_psc["end_date"])
+            psc.start_date = check_dates(psc.start_date, new_psc["start_date"])
+            psc.end_date = check_dates(psc.end_date, new_psc["end_date"])
             psc.full_name = new_psc["full_name"]
             psc.excludes = new_psc["excludes"]
             psc.notes = new_psc["notes"]
@@ -80,28 +112,23 @@ def load_psc(fullpath, update):
         if update:
             update_lengths()
             logger.log(20, "Updated PSC codes.")
-    except IOError:
+    except IOError as e:
         logger.error("Could not open file {}".format(fullpath))
-    except HTTPError:
+        errors.append(e)
+    except HTTPError as e:
         logger.error("Could not open url {}".format(fullpath))
+        errors.append(e)
     except Exception as e:
         logger.error(e)
+        errors.append(e)
+    return num_created, num_updated, errors
 
 
-def check_start_end_dates(psc, start_date, end_date):
-    if psc.start_date:
-        if start_date:
-            if psc.start_date < start_date.date():
-                psc.start_date = start_date
+def check_dates(old_date: Union[datetime.date, None], new_date: pd.Timestamp) -> Optional[datetime.date]:
+    if new_date is not pd.NaT and (old_date is None or old_date < new_date.date()):
+        return new_date
     else:
-        psc.start_date = start_date
-    if psc.end_date:
-        if end_date:
-            if psc.end_date < end_date.date():
-                psc.end_date = end_date
-
-    else:
-        psc.end_date = end_date
+        return old_date
 
 
 def update_lengths():
