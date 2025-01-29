7bc347fbe6d094b735847220945dfad92714d0e6
aguest-kc <andrew.guest@kc.frb.org>
2025-01-29 07:09:18 -0600

    [DEV-11590] Update location ES index structure

--- usaspending_api/database_scripts/etl/location_delta_view.sql
+++ usaspending_api/database_scripts/etl/location_delta_view.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS location_delta_view;
 
 CREATE VIEW location_delta_view AS
-WITH locations_cte AS (
+WITH country_cte AS (
 	SELECT
 		CASE
 			WHEN
@@ -10,98 +10,10 @@ WITH locations_cte AS (
 				'UNITED STATES'
 			ELSE
 				pop_country_name
-		END AS country_name,
-		UPPER(sd.name) AS state_name,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				pop_state_name IS NOT NULL
-				AND
-				pop_city_name ~ '^[a-zA-Z]'
-			THEN
-				pop_city_name
-			ELSE
-				NULL
-		END AS city_name,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				pop_state_name IS NOT NULL
-				AND
-				pop_county_name ~ '^[a-zA-Z]'
-			THEN
-				CONCAT(pop_county_name, ' COUNTY')
-			ELSE
-				NULL
-		END AS county_name,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				pop_state_name IS NOT NULL
-				AND
-				pop_state_fips ~ '^[0-9]{2}$'
-			THEN
-				pop_state_fips
-			ELSE
-				NULL
-		END AS state_fips,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				pop_state_name IS NOT NULL
-				AND
-				pop_county_code ~ '^[0-9]{3}$'
-			THEN
-				pop_county_code
-			ELSE
-				NULL
-		END AS county_fips,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				pop_state_name IS NOT NULL
-			THEN
-				pop_zip5
-			ELSE
-				NULL
-		END AS zip_code,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				(
-					sd.code IS NOT NULL
-					AND
-					pop_congressional_code_current IS NOT NULL
-				)
-			THEN
-				CONCAT(sd.code, pop_congressional_code_current)
-			ELSE
-				NULL
-		END AS current_congressional_district,
-		CASE
-			WHEN
-				pop_country_name = 'UNITED STATES'
-				AND
-				(
-					sd.code IS NOT NULL
-					AND
-					pop_congressional_code IS NOT NULL
-				)
-			THEN
-				CONCAT(sd.code, pop_congressional_code)
-			ELSE
-				NULL
-		END AS original_congressional_district
-	FROM	
+		END AS location_string,
+		'country' AS location_type
+	FROM
 		rpt.transaction_search
-	LEFT JOIN
-		state_data sd ON pop_state_name = UPPER(sd.name)
 	WHERE
 		pop_country_name IS NOT NULL
 	UNION
@@ -112,21 +24,45 @@ WITH locations_cte AS (
 			THEN
 				'UNITED STATES'
 			ELSE
-				recipient_location_country_name 
-		END AS country_name,
-		UPPER(sd.name) AS state_name,
+				recipient_location_country_name
+		END AS location_string,
+		'country' AS location_type
+	FROM
+		rpt.transaction_search
+	WHERE
+		recipient_location_country_name IS NOT NULL
+),
+county_cte AS (
+	SELECT
 		CASE
 			WHEN
-				recipient_location_country_name = 'UNITED STATES'
+				pop_country_name = 'UNITED STATES'
 				AND
-				recipient_location_state_name IS NOT NULL
+				pop_state_name IS NOT NULL
 				AND
-				recipient_location_city_name ~ '^[a-zA-Z]'
+				pop_county_name ~ '^[a-zA-Z]'
 			THEN
-				recipient_location_city_name
-			ELSE
-				NULL
-		END AS city_name,
+				CONCAT(pop_county_name, ' COUNTY', ', ', UPPER(sd.name), ', ', 'UNITED STATES')
+		END AS location_string,
+		'county' AS location_type,
+		CASE
+			WHEN
+				pop_country_name = 'UNITED STATES'
+				AND
+				pop_state_name IS NOT NULL
+				AND
+				pop_county_fips ~ '^[0-9]{5}$'
+			THEN
+				pop_county_fips
+		END AS county_fips
+	FROM
+		rpt.transaction_search
+	LEFT JOIN
+		state_data sd ON pop_state_name = UPPER(sd.name)
+	WHERE
+		pop_country_name IS NOT NULL
+	UNION
+	SELECT
 		CASE
 			WHEN
 				recipient_location_country_name = 'UNITED STATES'
@@ -135,45 +71,70 @@ WITH locations_cte AS (
 				AND
 				recipient_location_county_name ~ '^[a-zA-Z]'
 			THEN
-				CONCAT(recipient_location_county_name, ' COUNTY')
-			ELSE
-				NULL
-		END AS county_name,
+				CONCAT(recipient_location_county_name, ' COUNTY', ', ', UPPER(sd.name), ', ', 'UNITED STATES')
+		END AS location_string,
+		'county' AS location_type,
 		CASE
 			WHEN
 				recipient_location_country_name = 'UNITED STATES'
 				AND
 				recipient_location_state_name IS NOT NULL
 				AND
-				recipient_location_state_fips ~ '^[0-9]{2}$'
+				recipient_location_county_fips ~ '^[0-9]{5}$'
 			THEN
-				recipient_location_state_fips
-			ELSE
-				NULL
-		END AS state_fips,
+				recipient_location_county_fips
+		END AS county_fips
+	FROM
+		rpt.transaction_search
+	LEFT JOIN
+		state_data sd ON recipient_location_state_name = UPPER(sd.name)
+	WHERE
+		recipient_location_country_name IS NOT NULL
+),
+zip_cte AS (
+	SELECT
+		CASE
+			WHEN
+				pop_country_name = 'UNITED STATES'
+				AND
+				sd.name IS NOT NULL
+			    and
+				pop_zip5 IS NOT NULL
+			THEN
+				CONCAT(pop_zip5, ', ', UPPER(sd.name), ', UNITED STATES')
+		END AS location_string,
+		'zip_code' AS location_type
+	FROM
+		rpt.transaction_search
+	LEFT JOIN
+		state_data sd ON pop_state_name = UPPER(sd.name)
+	WHERE
+		pop_county_name IS NOT NULL
+	UNION
+	SELECT
 		CASE
 			WHEN
 				recipient_location_country_name = 'UNITED STATES'
 				AND
-				recipient_location_state_name IS NOT NULL
-				AND
-				recipient_location_county_code ~ '^[0-9]{3}$'
+				sd.name IS NOT NULL
+			    AND
+				recipient_location_zip5 IS NOT NULL
 			THEN
-				recipient_location_county_code
+				CONCAT(recipient_location_zip5, ', ', UPPER(sd.name), ', UNITED STATES')
 			ELSE
 				NULL
-		END AS county_fips,
-		CASE
-			WHEN
-				recipient_location_country_name = 'UNITED STATES'
-				AND
-				recipient_location_state_name IS NOT NULL
-			THEN
-				recipient_location_zip5
-			ELSE
-				NULL
-		END AS zip_code,
-		CASE
+		END AS location_string,
+		'zip_code' AS location_type
+	FROM
+		rpt.transaction_search
+	LEFT JOIN
+		state_data sd ON recipient_location_state_name = UPPER(sd.name)
+	WHERE
+		recipient_location_country_name IS NOT NULL
+),
+current_cd_cte AS (
+    SELECT
+        CASE
 			WHEN
 				recipient_location_country_name = 'UNITED STATES'
 				AND
@@ -183,11 +144,40 @@ WITH locations_cte AS (
 					recipient_location_congressional_code_current IS NOT NULL
 				)
 			THEN
-				CONCAT(sd.code, recipient_location_congressional_code_current)
-			ELSE
-				NULL
-		END AS current_congressional_district,
-		CASE
+				CONCAT(sd.code, recipient_location_congressional_code_current, ', ', UPPER(sd.name), ', UNITED STATES')
+		END AS location_string,
+        'current_congressional_district' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
+		state_data sd ON recipient_location_state_name = UPPER(sd.name)
+	WHERE
+		recipient_location_country_name IS NOT NULL
+	UNION
+	SELECT
+        CASE
+			WHEN
+				pop_country_name = 'UNITED STATES'
+				AND
+				(
+					sd.code IS NOT NULL
+					AND
+					pop_congressional_code_current IS NOT NULL
+				)
+			THEN
+				CONCAT(sd.code, pop_congressional_code_current, ', ', UPPER(sd.name), ', UNITED STATES')
+		END AS location_string,
+        'current_congressional_district' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
+		state_data sd ON pop_state_name = UPPER(sd.name)
+	WHERE
+		pop_country_name IS NOT NULL
+),
+original_cd_cte AS (
+    SELECT
+        CASE
 			WHEN
 				recipient_location_country_name = 'UNITED STATES'
 				AND
@@ -197,49 +187,173 @@ WITH locations_cte AS (
 					recipient_location_congressional_code IS NOT NULL
 				)
 			THEN
-				CONCAT(sd.code, recipient_location_congressional_code)
-			ELSE
-				NULL
-		END AS original_congressional_district
-	FROM
-		rpt.transaction_search
-	LEFT JOIN
+				CONCAT(UPPER(sd.code), recipient_location_congressional_code, ', ', UPPER(sd.name), ', UNITED STATES')
+		END AS location_string,
+        'original_congressional_district' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
 		state_data sd ON recipient_location_state_name = UPPER(sd.name)
 	WHERE
 		recipient_location_country_name IS NOT NULL
+	UNION
+	SELECT
+        CASE
+			WHEN
+				pop_country_name = 'UNITED STATES'
+				AND
+				(
+					sd.code IS NOT NULL
+					AND
+					pop_congressional_code IS NOT NULL
+				)
+			THEN
+				CONCAT(UPPER(sd.code), pop_congressional_code, ', ', UPPER(sd.name), ', UNITED STATES')
+		END AS location_string,
+        'original_congressional_district' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
+		state_data sd ON pop_state_name = UPPER(sd.name)
+	WHERE
+		pop_country_name IS NOT NULL
+),
+state_cte AS (
+    SELECT
+        CONCAT(UPPER(sd.name), ', UNITED STATES') AS location_string,
+        'state' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
+        state_data sd ON recipient_location_state_name = UPPER(sd.name)
+    UNION
+    SELECT
+        CONCAT(UPPER(sd.name), ', UNITED STATES') AS location_string,
+        'state' AS location_type
+    FROM
+        rpt.transaction_search
+    LEFT JOIN
+        state_data sd ON pop_state_name = UPPER(sd.name)
+),
+city_cte AS (
+    SELECT
+        CASE
+            WHEN
+                recipient_location_country_name = 'UNITED STATES'
+                AND
+                (
+                    sd.code IS NOT NULL
+                    AND
+                    recipient_location_city_name IS NOT NULL
+                )
+            THEN
+                CONCAT(UPPER(recipient_location_city_name), ', ', UPPER(sd.name), ', UNITED STATES')
+        END AS location_string,
+        'city' AS location_type
+    FROM rpt.transaction_search
+    LEFT JOIN
+        state_data sd ON recipient_location_state_name = UPPER(sd.name)
+    WHERE recipient_location_country_name IS NOT NULL
+    UNION
+    SELECT
+        CASE
+            WHEN
+                pop_country_name = 'UNITED STATES'
+                AND
+                (
+                    sd.code IS NOT NULL
+                    AND
+                    pop_congressional_code IS NOT NULL
+                )
+            THEN
+                CONCAT(UPPER(pop_city_name), ', ', UPPER(sd.name), ', UNITED STATES')
+        END AS location_string,
+        'city' AS location_type
+    FROM rpt.transaction_search
+    LEFT JOIN
+        state_data sd ON pop_state_name = UPPER(sd.name)
+    WHERE pop_country_name IS NOT NULL
+),
+select_cte AS (
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        country_cte
+    GROUP BY
+        location_string,
+        location_type
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        county_fips
+    FROM
+        county_cte
+    GROUP BY
+        location_string,
+        location_type,
+        county_fips
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        zip_cte
+    GROUP BY
+        location_string,
+        location_type
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        current_cd_cte
+    GROUP BY
+        location_string,
+        location_type
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        original_cd_cte
+    GROUP BY
+        location_string,
+        location_type
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        city_cte
+    GROUP BY
+        location_string,
+        location_type
+    UNION
+    SELECT
+        location_string,
+        location_type,
+        NULL AS county_fips
+    FROM
+        state_cte
+    GROUP BY
+        location_string,
+        location_type
 )
 SELECT
-	ROW_NUMBER() OVER (ORDER BY country_name, state_name) AS id,
-	country_name,
-	state_name,
-	city_name,
-	county_name,
-	CONCAT(state_fips, county_fips) AS county_fips,
-	zip_code,
-	current_congressional_district,
-	original_congressional_district
+    ROW_NUMBER() OVER (ORDER BY location_string, location_type) AS id,
+    location_string,
+    location_type,
+    county_fips
 FROM
-	locations_cte
-WHERE
-	-- require state name for UNITED STATES
-	(
-		country_name = 'UNITED STATES'
-		AND
-		state_name IS NOT NULL
-	)
-    OR
-	-- only need country name for foreign countries since we don't support foreign "states"
-	(
-		country_name != 'UNITED STATES'
-		AND
-		state_name is NULL
-	)
+    select_cte
 GROUP BY
-	country_name,
-	state_name,
-    city_name,
-    county_name,
-    CONCAT(state_fips, county_fips),
-    zip_code,
-    current_congressional_district,
-    original_congressional_district
+    location_string,
+    location_type,
+    county_fips
