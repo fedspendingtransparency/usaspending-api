6c6d6170359ec117fb98ef597c3ab44d095e8db3
Seth Stoudenmier <stoudenmier_seth@bah.com>
2025-02-11 13:40:44 -0500

    set slices to 3

--- usaspending_api/etl/elasticsearch_loader_helpers/delete_data.py
+++ usaspending_api/etl/elasticsearch_loader_helpers/delete_data.py
@@ -118,7 +118,7 @@ def delete_docs_by_unique_key(
                 index=index,
                 body=delete_query,
                 conflicts="proceed",
-                slices=4,
+                slices=3,
             )
             # Some subtle errors come back on the response
             if response["timed_out"]:
