6b07ff43583ce0fedf47f48d3a1014244bd9f4be
aguest-kc <andrew.guest@kc.frb.org>
2025-03-13 10:13:09 -0500

    [DEV-12144] Update location SQL

--- usaspending_api/database_scripts/etl/location_delta_view.sql
+++ usaspending_api/database_scripts/etl/location_delta_view.sql
@@ -20,7 +20,8 @@ CREATE VIEW location_delta_view AS
 		ELSE
             TO_JSONB(
                 JSONB_BUILD_OBJECT(
-                    'country_name', pop_country_name
+                    'country_name', pop_country_name,
+                    'location_type', 'country'
                 )
             )
 	END AS pop_country_json,
@@ -34,7 +35,8 @@ CREATE VIEW location_delta_view AS
             THEN TO_JSONB(
                 JSONB_BUILD_OBJECT(
                     'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'state'
                 )
             )
 	END AS pop_state_json,
@@ -51,7 +53,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'city_name', UPPER(pop_city_name),
 		            'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'city'
                 )
             )
 		WHEN pop_country_name NOT IN ('UNITED STATES OF AMERICA', 'UNITED STATES') AND pop_country_name IS NOT NULL AND pop_city_name IS NOT NULL
@@ -59,7 +62,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'city_name', UPPER(pop_city_name),
 		            'state_name', NULL,
-		            'country_name', UPPER(pop_country_name)
+		            'country_name', UPPER(pop_country_name),
+                    'location_type', 'city'
                 )
             )
 	END AS pop_city_json,
@@ -87,7 +91,8 @@ CREATE VIEW location_delta_view AS
                     'county_name', UPPER(pop_county_name),
 		            'county_fips', CONCAT(pop_state_fips, pop_county_code),
 		            'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'county'
                 )
             )
 	END AS pop_county_json,
@@ -108,7 +113,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'zip_code', pop_zip5,
 		            'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'zip_code'
                 )
             )
 	END AS pop_zip_json,
@@ -133,7 +139,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'current_cd', CONCAT(UPPER(pop_state_code),	'-', pop_congressional_code_current),
 		            'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'current_cd'
                 )
             )
 	END AS pop_current_cd_json,
@@ -158,7 +165,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'current_cd', CONCAT(UPPER(pop_state_code), '-', pop_congressional_code),
 		            'state_name', UPPER(pop_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'original_cd'
                 )
             )
 	END AS pop_original_cd_json,
@@ -171,13 +179,15 @@ CREATE VIEW location_delta_view AS
 		WHEN recipient_location_country_name = 'UNITED STATES OF AMERICA'
             THEN TO_JSONB(
                 JSONB_BUILD_OBJECT(
-                    'country_name', 'UNITED STATES'
+                    'country_name', 'UNITED STATES',
+                    'location_type', 'country'
                 )
             )
 		ELSE
             TO_JSONB(
                 JSONB_BUILD_OBJECT(
-                    'country_name', recipient_location_country_name
+                    'country_name', recipient_location_country_name,
+                    'location_type', 'country'
                 )
             )
 	END AS recipient_location_country_json,
@@ -191,7 +201,8 @@ CREATE VIEW location_delta_view AS
             THEN TO_JSONB(
                 JSONB_BUILD_OBJECT(
                     'state_name', UPPER(recipient_location_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'state'
                 )
             )
 	END AS recipient_location_state_json,
@@ -220,7 +231,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'city_name', UPPER(recipient_location_city_name),
 		            'state_name', UPPER(recipient_location_state_name),
-	                'country_name', 'UNITED STATES'
+	                'country_name', 'UNITED STATES',
+                    'location_type', 'city'
                 )
             )
 		WHEN (
@@ -232,7 +244,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'city_name', UPPER(recipient_location_city_name),
 		            'state_name', NULL,
-		            'country_name', UPPER(recipient_location_country_name)
+		            'country_name', UPPER(recipient_location_country_name),
+                    'location_type', 'city'
                 )
             )
 	END AS recipient_location_city_json,
@@ -260,7 +273,8 @@ CREATE VIEW location_delta_view AS
                     'county_name', UPPER(recipient_location_county_name),
 		            'county_fips', CONCAT(recipient_location_state_fips, recipient_location_county_code),
 		            'state_name', UPPER(recipient_location_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'county'
                 )
             )
 	END AS recipient_location_county_json,
@@ -283,7 +297,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'zip_code', recipient_location_zip5,
 		            'state_name', UPPER(recipient_location_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'zip_code'
                 )
             )
 	END AS recipient_location_zip_json,
@@ -308,7 +323,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'current_cd', CONCAT(UPPER(recipient_location_state_code), '-',	recipient_location_congressional_code_current),
 		            'state_name', UPPER(recipient_location_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'current_cd'
                 )
             )
 	END AS recipient_location_current_cd_json,
@@ -333,7 +349,8 @@ CREATE VIEW location_delta_view AS
                 JSONB_BUILD_OBJECT(
                     'current_cd', CONCAT(UPPER(recipient_location_state_code), '-', recipient_location_congressional_code),
 		            'state_name', UPPER(recipient_location_state_name),
-		            'country_name', 'UNITED STATES'
+		            'country_name', 'UNITED STATES',
+                    'location_type', 'original_cd'
                 )
             )
 	END AS recipient_location_original_cd_json
@@ -343,35 +360,6 @@ WHERE
 	pop_country_name IS NOT NULL
 	OR
     recipient_location_country_name IS NOT NULL
-GROUP BY
-	pop_country_string,
-	pop_country_json,
-	pop_state_string,
-	pop_state_json,
-	pop_city_string,
-	pop_city_json,
-	pop_county_string,
-	pop_county_json,
-	pop_zip_string,
-	pop_zip_json,
-	pop_current_cd_string,
-	pop_current_cd_json,
-	pop_original_cd_string,
-	pop_original_cd_json,
-	recipient_location_country_string,
-	recipient_location_country_json,
-	recipient_location_state_string,
-	recipient_location_state_json,
-	recipient_location_city_string,
-	recipient_location_city_json,
-	recipient_location_county_string,
-	recipient_location_county_json,
-	recipient_location_zip_string,
-	recipient_location_zip_json,
-	recipient_location_current_cd_string,
-	recipient_location_current_cd_json,
-	recipient_location_original_cd_string,
-	recipient_location_original_cd_json
 ),
 normalized_column_names_cte AS (
     SELECT
@@ -459,6 +447,7 @@ normalized_column_names_cte AS (
         transaction_locations_cte
 )
 SELECT
+    ROW_NUMBER() OVER (ORDER BY location, location_json) AS id,
 	location,
 	location_json
 FROM
@@ -466,6 +455,5 @@ FROM
 WHERE
     -- Only include locations that have at least two characters
     location ~ '[A-Z0-9].*[A-Z0-9]'
-GROUP BY
-	location,
-	location_json
+    and
+    location_json IS NOT NULL
