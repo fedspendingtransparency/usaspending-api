c5386bf2b0fe0adfcdfcadb8ea00777087d6ba30
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-05-08 15:44:54 -0500

    [DEV-10024] fix error for upper() on None type

--- usaspending_api/etl/submission_loader_helpers/disaster_emergency_fund_codes.py
+++ usaspending_api/etl/submission_loader_helpers/disaster_emergency_fund_codes.py
@@ -11,11 +11,12 @@ def get_disaster_emergency_fund(row: dict) -> Optional[dict]:
     global DISASTER_EMERGENCY_FUND_CODES
     if not DISASTER_EMERGENCY_FUND_CODES:  # testing for falsy values instead of just null
         DISASTER_EMERGENCY_FUND_CODES = {defc.code: defc for defc in DisasterEmergencyFundCode.objects.all()}
-    disaster_emergency_fund_code = row["disaster_emergency_fund_code"].upper()
+
+    disaster_emergency_fund_code = row["disaster_emergency_fund_code"]
     if not disaster_emergency_fund_code:
         return None
     try:
-        return DISASTER_EMERGENCY_FUND_CODES[disaster_emergency_fund_code]
+        return DISASTER_EMERGENCY_FUND_CODES[disaster_emergency_fund_code.upper()]
     except KeyError:
         raise DisasterEmergencyFundCode.DoesNotExist(
             f"Unable to find disaster emergency fund code for '{disaster_emergency_fund_code}'."
