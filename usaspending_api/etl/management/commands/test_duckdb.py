0b2e06ba8ac6e7dc445f9df5b632e5eab105818e
Wil Collins <collinwr@gmail.com>
2025-05-15 20:48:23 +0000

    [DEV-12498] Creates script for testing duckdb

---
+++ usaspending_api/etl/management/commands/test_duckdb.py
@@ -0,0 +1,39 @@
+import duckdb
+import os
+
+from django.core.management.base import BaseCommand
+
+from usaspending_api.config import CONFIG
+from usaspending_api import settings
+
+SPARK_S3_BUCKET = CONFIG.SPARK_S3_BUCKET
+DELTA_LAKE_S3_PATH = CONFIG.DELTA_LAKE_S3_PATH
+
+S3_DELTA_PATH = f"s3://{SPARK_S3_BUCKET}/{DELTA_LAKE_S3_PATH}/rpt/award_search"
+
+DELTA_EXTENSION_PATH = os.path.abspath("delta.duckdb_extension")
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        # Establish DuckDB connection and install plugins
+        conn = duckdb.connect()
+        conn.query("INSTALL delta")
+        conn.query(f"LOAD '{DELTA_EXTENSION_PATH}'")
+
+        conn.query("INSTALL httpfs")
+        conn.query("LOAD httpfs")
+
+        #conn.execute(f"SET s3_region='{settings.USASPENDING_AWS_REGION}'")
+        conn.execute(f"SET s3_region='us-east-1'")
+        conn.execute(f"SET s3_endpoint='http://localhost:10001';")
+        conn.execute(f"SET s3_use_ssl=false;")
+        conn.execute(f"SET s3_access_key_id='usaspending';")
+        conn.execute(f"SET s3_secret_access_key='usaspender';")
+        conn.execute(f"SET s3_url_style='path';")
+
+        query = f"SELECT * FROM delta_scan('{S3_DELTA_PATH}');"
+        df = conn.execute(query).fetchdf()
+
+        print(df)
