adb527472d4521adb97c8ca89adcf74832eb3758
Wil Collins <collinwr@gmail.com>
2025-05-16 18:55:37 +0000

    [DEV-12498] Manually installs httpfs plugin

--- usaspending_api/etl/management/commands/test_duckdb.py
+++ usaspending_api/etl/management/commands/test_duckdb.py
@@ -12,6 +12,7 @@ DELTA_LAKE_S3_PATH = CONFIG.DELTA_LAKE_S3_PATH
 S3_DELTA_PATH = f"s3://{SPARK_S3_BUCKET}/{DELTA_LAKE_S3_PATH}/rpt/award_search"
 
 DELTA_EXTENSION_PATH = os.path.abspath("/duckdb_plugins/delta.duckdb_extension")
+HTTPFS_EXTENSION_PATH = os.path.abspath("/duckdb_plugins/httpfs.duckdb_extension")
 
 class Command(BaseCommand):
 
@@ -28,10 +29,11 @@ class Command(BaseCommand):
         # Read arguments
         is_local = options["is_local"]
 
-        # Establish DuckDB connection and load plugins
+        # Establish DuckDB connection and install plugins
         conn = duckdb.connect()
         conn.query(f"LOAD '{DELTA_EXTENSION_PATH}'")
-        conn.query("LOAD httpfs")
+        conn.query(f"LOAD '{HTTPFS_EXTENSION_PATH}'")
+
 
         if is_local:
             conn.execute(f"SET s3_region='us-east-1'")
