f1c41e4de69bc66e019f225bf8be78e86bc9fc30
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-05-16 15:06:12 -0500

    [DEV-12586] change log level setting from logging to log4j

--- usaspending_api/etl/management/commands/load_table_from_delta.py
+++ usaspending_api/etl/management/commands/load_table_from_delta.py
@@ -165,7 +165,7 @@ class Command(BaseCommand):
 
         # Setup Logger
         self.logger = get_jvm_logger(spark, __name__)
-        logging.basicConfig(level=logging.INFO)
+        spark.sparkContext.setLogLevel("INFO")
         self.logger.info("test test test")
 
         # Resolve Parameters
