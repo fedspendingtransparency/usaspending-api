105ec3a833dda7819408a1e59c9294bc4a4a4609
Seth Stoudenmier <stoudenmier_seth@bah.com>
2025-05-20 16:39:19 -0400

    Fix limit on spark logging for test suite

--- usaspending_api/tests/conftest_spark.py
+++ usaspending_api/tests/conftest_spark.py
@@ -473,7 +473,7 @@ def _build_usas_data_for_spark():
         action_date="2020-04-01",
         fiscal_year=2020,
         award_amount=0.00,
-        total_obligation=0.00,
+        total_obligation=10.00,  # Temp change to allow testing a failed Spark test
         total_subsidy_cost=0.00,
         total_loan_value=0.00,
         total_obl_bin="<1M",
