448cbfe701ffad8e34a3531b20bcd67283d5814d
aguest-kc <andrew.guest@kc.frb.org>
2025-07-17 19:22:53 -0500

    [DEV-12878] Add create_delta_table test

--- usaspending_api/etl/tests/integration/test_create_delta_table.py
+++ usaspending_api/etl/tests/integration/test_create_delta_table.py
@@ -3,9 +3,8 @@
 NOTE: Uses Pytest Fixtures from immediate parent conftest.py: usaspending_api/etl/tests/conftest.py
 """
 
-from pyspark.sql import SparkSession
-
 from django.core.management import call_command
+from pyspark.sql import SparkSession
 
 from usaspending_api.etl.management.commands.create_delta_table import TABLE_SPEC
 
@@ -106,3 +105,9 @@ def test_create_delta_table_for_recipient_lookup_with_alt_db_and_name(
     _verify_delta_table_creation(
         spark, "recipient_lookup", s3_unittest_data_bucket, alt_db="my_alt_db", alt_name="recipient_lookup_alt_name"
     )
+
+
+def test_create_delta_table_for_object_class_program_activity(
+    spark, s3_unittest_data_bucket, hive_unittest_metastore_db
+):
+    _verify_delta_table_creation(spark, "object_class_program_activity", s3_unittest_data_bucket)
