778ccb871a0ec91fece1870e35984f4c6c10f72a
aguest-kc <andrew.guest@kc.frb.org>
2025-08-11 14:47:02 -0500

    [DEV-12967] [DEV-12966] Add state_operations back to migration

--- usaspending_api/awards/migrations/0112_move_vw_awards_creation_step.py
+++ usaspending_api/awards/migrations/0112_move_vw_awards_creation_step.py
@@ -15,5 +15,13 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql=vw_awards_sql,
             reverse_sql="DROP VIEW IF EXISTS vw_awards",
-        ),
+            # Without this, Django will try to actually change the old table names in another migration
+            # This says we've already done it.
+            state_operations=[
+                migrations.AlterModelTable(
+                    name="award",
+                    table="vw_awards",
+                )
+            ],
+        )
     ]
