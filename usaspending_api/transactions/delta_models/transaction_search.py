2af00c8a79fb33f7983d7c5f69679f209b8d3010
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-08-12 15:44:16 -0500

    [DEV-13063] format

--- usaspending_api/transactions/delta_models/transaction_search.py
+++ usaspending_api/transactions/delta_models/transaction_search.py
@@ -1072,8 +1072,8 @@ _base_load_sql_string = rf"""
             ) AS tas_components,
             COLLECT_SET(
                 TO_JSON(
-                CASE\
-                    WHEN pap.name IS NOT NULL THEN 
+                CASE
+                    WHEN pap.name IS NOT NULL THEN
                         NAMED_STRUCT(
                             'name', UPPER(rpa.program_activity_name),
                             'code', LPAD(rpa.program_activity_code, 4, "0"),
