171589553b28359eebbd06987d94997fa82cf9b4
aguest-kc <andrew.guest@kc.frb.org>
2025-08-26 20:49:01 -0500

    [DEV-12603] Add columns to ocpa download delta table

--- usaspending_api/download/delta_models/object_class_program_activity_download.py
+++ usaspending_api/download/delta_models/object_class_program_activity_download.py
@@ -8,6 +8,7 @@ OBJECT_CLASS_PROGRAM_ACTIVITY_DOWNLOAD_COLUMNS = {
     "federal_account_symbol": {"delta": "STRING", "postgres": "TEXT"},
     "federal_account_name": {"delta": "STRING", "postgres": "TEXT"},
     "agency_identifier_name": {"delta": "STRING", "postgres": "TEXT"},
+    "allocation_transfer_agency_identifier_name": {"delta": "STRING", "postgres": "TEXT"},
     "program_activity_code": {"delta": "STRING", "postgres": "TEXT"},
     "program_activity_name": {"delta": "STRING", "postgres": "TEXT"},
     "object_class_code": {"delta": "STRING", "postgres": "TEXT"},
@@ -156,6 +157,32 @@ OBJECT_CLASS_PROGRAM_ACTIVITY_DOWNLOAD_COLUMNS = {
         "postgres": "NUMERIC(23,2)",
     },
     "last_modified_date": {"delta": "DATE", "postgres": "DATE"},
+    "data_source": {"delta": "STRING", "postgres": "TEXT"},
+    "program_activity_id": {"delta": "INTEGER", "postgres": "INTEGER"},
+    "object_class_id": {"delta": "INTEGER", "postgres": "INTEGER"},
+    "prior_year_adjustment": {"delta": "STRING", "postgres": "TEXT"},
+    "ussgl480110_rein_undel_ord_cpe": {
+        "delta": "NUMERIC(23,2)",
+        "postgres": "NUMERIC(23,2)",
+    },
+    "ussgl490110_rein_deliv_ord_cpe": {
+        "delta": "NUMERIC(23,2)",
+        "postgres": "NUMERIC(23,2)",
+    },
+    "drv_obligations_incurred_by_program_object_class": {
+        "delta": "NUMERIC(23,2)",
+        "postgres": "NUMERIC(23,2)",
+    },
+    "drv_obligations_undelivered_orders_unpaid": {
+        "delta": "NUMERIC(23,2)",
+        "postgres": "NUMERIC(23,2)",
+    },
+    "reporting_period_start": {"delta": "DATE", "postgres": "DATE"},
+    "reporting_period_end": {"delta": "DATE", "postgres": "DATE"},
+    "certified_date": {"delta": "DATE", "postgres": "DATE"},
+    "create_date": {"delta": "DATE", "postgres": "DATE"},
+    "update_date": {"delta": "DATE", "postgres": "DATE"},
+    "treasury_account_id": {"delta": "INTEGER", "postgres": "INTEGER"},
 }
 
 OBJECT_CLASS_PROGRAM_ACTIVITY_DOWNLOAD_DELTA_COLUMNS = {
@@ -183,7 +210,8 @@ object_class_program_activity_download_load_sql_string = rf"""
         ta.name AS owning_agency_name,
         fa.federal_account_code AS federal_account_symbol,
         fa.account_title AS federal_account_name,
-        cgac.agency_name AS agency_identifier_name,
+        cgac_aid.agency_name AS agency_identifier_name,
+        cgac_ata.agency_name AS allocation_transfer_agency_identifier_name,
         rpa.program_activity_code,
         rpa.program_activity_name,
         oc.object_class AS object_class_code,
@@ -251,7 +279,21 @@ object_class_program_activity_download_load_sql_string = rf"""
         fabpaoc.ussgl487200_down_adj_pri_ppaid_undel_orders_oblig_refund_cpe AS USSGL487200_downward_adj_prior_year_prepaid_undeliv_order_oblig,
         fabpaoc.ussgl497200_down_adj_pri_paid_deliv_orders_oblig_refund_cpe AS USSGL497200_downward_adj_of_prior_year_paid_deliv_orders_oblig,
         fabpaoc.deobligations_recoveries_refund_pri_program_object_class_cpe AS deobligations_or_recoveries_or_refunds_from_prior_year,
-        CAST(sa.published_date AS DATE) AS last_modified_date
+        CAST(sa.published_date AS DATE) AS last_modified_date,
+        fabpaoc.data_source,
+        fabpaoc.program_activity_id,
+        fabpaoc.object_class_id,
+        fabpaoc.prior_year_adjustment,
+        fabpaoc.ussgl480110_rein_undel_ord_cpe,
+        fabpaoc.ussgl490110_rein_deliv_ord_cpe,
+        fabpaoc.drv_obligations_incurred_by_program_object_class,
+        fabpaoc.drv_obligations_undelivered_orders_unpaid,
+        fabpaoc.reporting_period_start,
+        fabpaoc.reporting_period_end,
+        fabpaoc.certified_date,
+        fabpaoc.create_date,
+        fabpaoc.update_date,
+        fabpaoc.treasury_account_id
     FROM
         global_temp.financial_accounts_by_program_activity_object_class AS fabpaoc
     INNER JOIN global_temp.submission_attributes AS sa ON
@@ -268,6 +310,8 @@ object_class_program_activity_download_load_sql_string = rf"""
         fabpaoc.object_class_id = oc.id
     LEFT JOIN global_temp.disaster_emergency_fund_code AS defc ON
         fabpaoc.disaster_emergency_fund_code = defc.code
-    LEFT JOIN global_temp.cgac ON
-        taa.agency_id = cgac.cgac_code
+    LEFT JOIN global_temp.cgac AS cgac_aid ON
+        taa.agency_id = cgac_aid.cgac_code
+    LEFT JOIN global_temp.cgac AS cgac_ata ON
+        taa.allocation_transfer_agency_id = cgac_ata.cgac_code
     """
