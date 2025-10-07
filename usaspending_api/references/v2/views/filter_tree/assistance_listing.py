65bed743fc2fa12dce9053fd56518ad4f7efbd78
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-10-07 14:41:59 -0500

    [DEV-13329] refactor for less db calls

--- usaspending_api/references/v2/views/filter_tree/assistance_listing.py
+++ usaspending_api/references/v2/views/filter_tree/assistance_listing.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,7 +12,7 @@ class AssistanceListingViewSet(APIView):
 
     endpoint_doc = "usaspending_api/api_contracts/contracts/v2/references/assistance_listing.md"
 
-    queryset = Cfda.objects.all()
+    all_cfdas = Cfda.objects.all()
 
     def _parse_and_validate_request(self, cfda: int, requested_data) -> dict:
         data = {"code": cfda, "filter": requested_data.query_params.get("filter", None)}
@@ -34,26 +33,31 @@ class AssistanceListingViewSet(APIView):
     def _business_logic(self, cfda_code: int, cfda_filter: str) -> list:
         valid_cfda_code = True if len(str(cfda_code)) == 2 else False
         if valid_cfda_code:
-            self.queryset = Cfda.objects.filter(program_number__startswith=cfda_code)
+            self.all_cfdas = [cfda for cfda in self.all_cfdas if cfda.program_number.startswith(str(cfda_code))]
 
         elif cfda_code is not None:
             raise InvalidParameterException(f"The assistance listing code should be two digits or not provided at all")
 
-        all_cfda_program_numbers = self.queryset.values_list("program_number", flat=True)
+        all_cfda_program_numbers = [cfda.program_number for cfda in self.all_cfdas]
         prefixes = set(cfda.split(".")[0] for cfda in all_cfda_program_numbers if "." in cfda)
         cfdas = []
         for prefix in prefixes:
             filter_query = (
-                Cfda.objects.filter(Q(program_number__contains=cfda_filter) | Q(program_title__contains=cfda_filter))
+                [
+                    {"program_number": cfda.program_number, "program_title": cfda.program_title}
+                    for cfda in self.all_cfdas
+                    if (cfda_filter in cfda.program_title or cfda_filter in cfda.program_number)
+                    and cfda.program_number.startswith(prefix)
+                ]
                 if cfda_filter is not None
-                else Cfda.objects.all()
+                else [
+                    {"program_number": cfda.program_number, "program_title": cfda.program_title}
+                    for cfda in self.all_cfdas
+                    if cfda.program_number.startswith(prefix)
+                ]
             )
-            cfda_with_prefix = filter_query.filter(program_number__startswith=prefix).values(
-                "program_number", "program_title"
-            )
-            children = [
-                {"code": cfda["program_number"], "description": cfda["program_title"]} for cfda in cfda_with_prefix
-            ]
+
+            children = [{"code": cfda["program_number"], "description": cfda["program_title"]} for cfda in filter_query]
             if valid_cfda_code:
                 cfdas.append(
                     {
