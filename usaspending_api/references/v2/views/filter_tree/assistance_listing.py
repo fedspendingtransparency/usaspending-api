12516db0c4fa7ce536e2e7a0a5f0711b3b1a5afd
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-10-08 14:22:20 -0500

    [DEV-13329] pr comment updates

--- usaspending_api/references/v2/views/filter_tree/assistance_listing.py
+++ usaspending_api/references/v2/views/filter_tree/assistance_listing.py
@@ -1,6 +1,10 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models.functions import Cast
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.db.models import Func, F, TextField, Value, Count, JSONField
+from django.db.models.query import Q
 from usaspending_api.common.cache_decorator import cache_response
 from usaspending_api.common.exceptions import InvalidParameterException
 from usaspending_api.common.validator import TinyShield
@@ -14,7 +18,7 @@ class AssistanceListingViewSet(APIView):
 
     all_cfdas = Cfda.objects.all()
 
-    def _parse_and_validate_request(self, cfda: int, requested_data) -> dict:
+    def _parse_and_validate_request(self, cfda: str | None, requested_data: Request) -> dict[str, int | str | None]:
         data = {"code": cfda, "filter": requested_data.query_params.get("filter", None)}
         models = [
             {"key": "code", "name": "code", "type": "integer", "default": None, "allow_nulls": True, "optional": True},
@@ -30,56 +34,41 @@ class AssistanceListingViewSet(APIView):
         ]
         return TinyShield(models).block(data)
 
-    def _business_logic(self, cfda_code: int, cfda_filter: str) -> list:
+    def _business_logic(self, cfda_code: int | None, cfda_filter: str | None) -> list[dict]:
+        filter_condition = (
+            (Q(program_title__contains=cfda_filter) | Q(program_number__contains=cfda_filter)) if cfda_filter else Q()
+        )
+        annotations = {}
+        filters = (Q(program_number__startswith=cfda_code)) if cfda_code else Q()
         valid_cfda_code = True if len(str(cfda_code)) == 2 else False
         if valid_cfda_code:
-            self.all_cfdas = [cfda for cfda in self.all_cfdas if cfda.program_number.startswith(str(cfda_code))]
-
+            annotations["children"] = ArrayAgg(
+                Func(
+                    Cast(Value("code"), TextField()),
+                    F("program_number"),
+                    Cast(Value("description"), TextField()),
+                    F("program_title"),
+                    function="jsonb_build_object",
+                    output_field=JSONField(),
+                )
+            )
         elif cfda_code is not None:
             raise InvalidParameterException(f"The assistance listing code should be two digits or not provided at all")
 
-        all_cfda_program_numbers = [cfda.program_number for cfda in self.all_cfdas]
-        prefixes = set(cfda.split(".")[0] for cfda in all_cfda_program_numbers if "." in cfda)
-        cfdas = []
-        for prefix in prefixes:
-            filter_query = (
-                [
-                    {"program_number": cfda.program_number, "program_title": cfda.program_title}
-                    for cfda in self.all_cfdas
-                    if (cfda_filter in cfda.program_title or cfda_filter in cfda.program_number)
-                    and cfda.program_number.startswith(prefix)
-                ]
-                if cfda_filter is not None
-                else [
-                    {"program_number": cfda.program_number, "program_title": cfda.program_title}
-                    for cfda in self.all_cfdas
-                    if cfda.program_number.startswith(prefix)
-                ]
+        results = (
+            Cfda.objects.filter(filter_condition, filters)
+            .annotate(
+                code=Func(F("program_number"), function="SPLIT_PART", template="%(function)s(%(expressions)s, '.', 1)")
             )
+            .values("code")
+            .annotate(count=Count("code"))
+            .annotate(**annotations)
+        )
 
-            children = [{"code": cfda["program_number"], "description": cfda["program_title"]} for cfda in filter_query]
-            if valid_cfda_code:
-                cfdas.append(
-                    {
-                        "code": prefix,
-                        "description": None,
-                        "count": len(children),
-                        "children": children,
-                    }
-                )
-            else:
-                cfdas.append(
-                    {
-                        "code": prefix,
-                        "description": None,
-                        "count": len(children),
-                    }
-                )
-
-        return sorted(cfdas, key=lambda cfda: cfda["code"])
+        return sorted(results, key=lambda cfda: cfda["code"])
 
     @cache_response()
-    def get(self, request, cfda=None) -> Response:
+    def get(self, request: Request, cfda=None) -> Response:
         requested_data = self._parse_and_validate_request(cfda, request)
         results = self._business_logic(requested_data["code"], requested_data["filter"])
         return Response(results)
