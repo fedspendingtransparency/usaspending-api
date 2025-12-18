8c3bb8eb61d8c4c02a233344d3fcfdb26acc5d10
Lorelei Trimberger <lorelei.trimberger@kc.frb.org>
2025-12-18 11:19:39 -0600

    [DEV-13912] re-add sorted_json_request]

--- usaspending_api/download/v2/base_download_viewset.py
+++ usaspending_api/download/v2/base_download_viewset.py
@@ -37,18 +37,14 @@ class BaseDownloadViewSet(APIView):
         validator = validator_type(request.data)
         json_request = validator.json_request
 
+        sorted_json_request = order_nested_object(json_request)
+
         # Keeps specified columns in the same order
         if "columns" in json_request:
-            columns = json_request["columns"]
-        else:
-            columns = None
-
-        json_request = order_nested_object(json_request)
-        if columns:
-            json_request["columns"] = columns
+            sorted_json_request["columns"] = json_request["columns"]
 
         # Check if download is pre-generated
-        pre_generated_download = json_request.pop("pre_generated_download", None)
+        pre_generated_download = sorted_json_request.pop("pre_generated_download", None)
         if pre_generated_download:
             download_job = (
                 DownloadJob.objects.filter(
@@ -62,23 +58,23 @@ class BaseDownloadViewSet(APIView):
             return self.build_download_response(download_job)
 
         # Check if the same request has been called today
-        ordered_json_request = json.dumps(json_request)
-        cached_download = self._get_cached_download(ordered_json_request, json_request.get("download_types", []))
+        ordered_json_request = json.dumps(sorted_json_request)
+        cached_download = self._get_cached_download(ordered_json_request, sorted_json_request.get("download_types", []))
 
         if cached_download and not settings.IS_LOCAL:
             # By returning the cached files, there should be no duplicates on a daily basis
             write_to_log(message=f"Generating file from cached download job ID: {cached_download.download_job_id}")
             return self.build_download_response(cached_download)
 
-        final_output_zip_name = create_unique_filename(json_request, origination=origination)
+        final_output_zip_name = create_unique_filename(sorted_json_request, origination=origination)
         download_job = DownloadJob.objects.create(
             job_status_id=JOB_STATUS_DICT["ready"],
             file_name=final_output_zip_name,
-            json_request=json.dumps(json_request),
+            json_request=json.dumps(sorted_json_request),
         )
 
         log_new_download_job(request, download_job)
-        self.process_request(download_job, json_request)
+        self.process_request(download_job, sorted_json_request)
 
         return self.build_download_response(download_job)
 
