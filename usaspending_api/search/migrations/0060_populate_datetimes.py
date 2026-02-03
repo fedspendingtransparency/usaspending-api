3e7ffd3bee68b5b56b48924b2a60fea579d94512
Zach Flanders <zachary.flanders@kc.frb.org>
2026-02-03 15:34:55 -0600

    [DEV-14110] - Update migrations - performance updates

--- usaspending_api/search/migrations/0060_populate_datetimes.py
+++ usaspending_api/search/migrations/0060_populate_datetimes.py
@@ -1,61 +1,89 @@
-from functools import partial
 from datetime import datetime, time
+
+from django.apps.registry import Apps
 from django.db import migrations, transaction
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 from django.utils import timezone
 
 
-def populate_column(apps, schema_editor, model_name, column_name):
-    search_model = apps.get_model("search", model_name)
+def populate_transaction_search(
+    apps: Apps, schema_editor: BaseDatabaseSchemaEditor
+) -> None:
+    TransactionSearch = apps.get_model("search", "TransactionSearch")
     while (
-        search_model.objects.exclude(**{f"{column_name}__isnull": True})
-        .filter(**{f"{column_name}time__isnull": True})
+        TransactionSearch.objects.exclude(
+            last_modified_date__isnull=True, initial_report_date__isnull=True
+        )
+        .filter(
+            last_modified_datetime__isnull=True, initial_report_datetime__isnull=True
+        )
         .exists()
     ):
         with transaction.atomic():
-            for row in search_model.objects.filter(
-                **{f"{column_name}time__isnull": True}
-            )[:50_000]:
-                _date = getattr(row, column_name)
-                _datetime = (
+            for row in TransactionSearch.objects.filter(
+                last_modified_datetime__isnull=True,
+                initial_report_datetime__isnull=True,
+            ).values("transaction_id", "last_modified_date", "initial_report_date")[
+                :50_000
+            ]:
+                row["last_modified_datetime"] = (
                     timezone.make_aware(
-                        datetime.combine(_date, time.min),
+                        datetime.combine(row["last_modified_date"], time.min),
                         timezone.get_current_timezone(),
                     )
-                    if _date
+                    if row["last_modified_date"]
                     else None
                 )
-                setattr(row, f"{column_name}time", _datetime)
-                row.save()
+                row["initial_report_datetime"] = (
+                    timezone.make_aware(
+                        datetime.combine(row["initial_report_date"], time.min),
+                        timezone.get_current_timezone(),
+                    )
+                    if row["initial_report_date"]
+                    else None
+                )
+                TransactionSearch(**row).save(
+                    update_fields=["last_modified_datetime", "initial_report_datetime"]
+                )
+
+
+def populate_award_search(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
+    AwardSearch = apps.get_model("search", "AwardSearch")
+    while (
+        AwardSearch.objects.exclude(last_modified_date__isnull=True)
+        .filter(last_modified_datetime__isnull=True)
+        .exists()
+    ):
+        with transaction.atomic():
+            for row in AwardSearch.objects.filter(
+                last_modified_datetime__isnull=True
+            ).values("award_id", "last_modified_date")[:50_000]:
+                row["last_modified_datetime"] = (
+                    timezone.make_aware(
+                        datetime.combine(row["last_modified_date"], time.min),
+                        timezone.get_current_timezone(),
+                    )
+                    if row["last_modified_date"]
+                    else None
+                )
+
+                AwardSearch(**row).save(update_fields=["last_modified_datetime"])
 
 
 class Migration(migrations.Migration):
+    atomic = False
+
     dependencies = [
         ("search", "0059_add_datetimes_drop_views"),
     ]
 
     operations = [
         migrations.RunPython(
-            partial(
-                populate_column,
-                model_name="TransactionSearch",
-                column_name="last_modified_date",
-            ),
+            populate_transaction_search,
             migrations.RunPython.noop,
         ),
         migrations.RunPython(
-            partial(
-                populate_column,
-                model_name="TransactionSearch",
-                column_name="initial_report_date",
-            ),
-            migrations.RunPython.noop,
-        ),
-        migrations.RunPython(
-            partial(
-                populate_column,
-                model_name="AwardSearch",
-                column_name="last_modified_date",
-            ),
+            populate_award_search,
             migrations.RunPython.noop,
         ),
     ]
