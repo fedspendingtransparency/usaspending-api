82aac01a271ecc4aa7e0fd795e62f611fdb42055
David Mikola <david.mikola@kc.frb.org>
2026-03-10 09:35:38 -0500

    fixed faulty match statement

--- usaspending_api/common/query_with_filters.py
+++ usaspending_api/common/query_with_filters.py
@@ -369,7 +369,7 @@ class _RecipientSearchText(_Filter):
 
         for filter_value in filter_values:
 
-            is_exact_match = re.match(r'^".*"nmn$', filter_value) is not None
+            is_exact_match = re.match(r'^".*"$', filter_value) is not None
             search_text = filter_value.upper()  # Recipient Search No longer escapes special characters
 
             if query_type == QueryType.SUBAWARDS:
