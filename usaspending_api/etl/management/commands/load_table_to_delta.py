f4435ffadd718dd7bf9e0f69cf7951510a2f0c5d
James McKay <james.mckay@kc.frb.org>
2026-04-22 19:04:40 +0000

    [DEV-14329] lint

--- usaspending_api/etl/management/commands/load_table_to_delta.py
+++ usaspending_api/etl/management/commands/load_table_to_delta.py
@@ -1,6 +1,6 @@
 import logging
 
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandParser
 
 from usaspending_api.awards.delta_models import (
     AWARDS_COLUMNS,
@@ -54,6 +54,7 @@ from usaspending_api.transactions.delta_models import (
     transaction_search_create_sql_string,
 )
 from usaspending_api.transactions.models import SourceAssistanceTransaction, SourceProcurementTransaction
+
 
 logger = logging.getLogger(__name__)
 
@@ -216,108 +217,107 @@ TABLE_SPEC = {
 # Additional definitions for use in testing;
 # These are copies of Views / Materialized Views / Tables from Postgres to Spark to aid in
 # data comparison between current Postgres data and the data transformed via Spark.
-TABLE_SPEC.update(
-    {
-        "award_search_testing": TableSpec(
-            model=AwardSearch,
-            is_from_broker=False,
-            source_table="award_search",
-            source_database=None,
-            destination_database="rpt",
-            swap_table=None,
-            swap_schema=None,
-            partition_column="award_id",
-            partition_column_type="numeric",
-            is_partition_column_unique=True,
-            delta_table_create_sql=award_search_create_sql_string,
-            source_schema=None,
-            custom_schema="total_covid_outlay NUMERIC(23,2), total_covid_obligation NUMERIC(23,2), recipient_hash "
-            "STRING, federal_accounts STRING, cfdas ARRAY<STRING>, tas_components ARRAY<STRING>",
-            column_names=list(AWARD_SEARCH_COLUMNS),
-            tsvectors=None,
-        ),
-        "recipient_lookup_testing": TableSpec(
-            model=RecipientLookup,
-            is_from_broker=False,
-            source_table="recipient_lookup",
-            source_database="rpt",
-            destination_database="raw",
-            swap_table=None,
-            swap_schema=None,
-            partition_column="id",
-            partition_column_type="numeric",
-            is_partition_column_unique=True,
-            delta_table_create_sql=recipient_lookup_create_sql_string,
-            source_schema=None,
-            custom_schema="recipient_hash STRING",
-            column_names=list(RECIPIENT_LOOKUP_COLUMNS),
-            tsvectors=None,
-        ),
-        "recipient_profile_testing": TableSpec(
-            model=RecipientProfile,
-            is_from_broker=False,
-            source_table="recipient_profile",
-            source_database="rpt",
-            destination_database="raw",
-            swap_table=None,
-            swap_schema=None,
-            partition_column="id",
-            partition_column_type="numeric",
-            delta_table_create_sql=recipient_profile_create_sql_string,
-            is_partition_column_unique=True,
-            source_schema=None,
-            custom_schema="recipient_hash STRING",
-            column_names=list(RECIPIENT_PROFILE_DELTA_COLUMNS),
-            tsvectors=None,
-        ),
-        "sam_recipient_testing": TableSpec(
-            model=DUNS,
-            is_from_broker=False,
-            source_table="duns",
-            source_database="int",
-            destination_database="raw",
-            swap_table=None,
-            swap_schema=None,
-            partition_column=None,
-            partition_column_type=None,
-            is_partition_column_unique=False,
-            delta_table_create_sql=sam_recipient_create_sql_string,
-            source_schema=None,
-            custom_schema="broker_duns_id STRING, business_types_codes ARRAY<STRING>",
-            column_names=list(SAM_RECIPIENT_COLUMNS),
-            tsvectors=None,
-        ),
-        "transaction_search_testing": TableSpec(
-            model=TransactionSearch,
-            is_from_broker=False,
-            source_table="transaction_search",
-            source_database=None,
-            destination_database="test",
-            swap_table=None,
-            swap_schema=None,
-            partition_column="transaction_id",
-            partition_column_type="numeric",
-            is_partition_column_unique=True,
-            delta_table_create_sql=transaction_search_create_sql_string,
-            source_schema=None,
-            custom_schema="recipient_hash STRING, federal_accounts STRING, parent_recipient_hash STRING",
-            column_names=list(TRANSACTION_SEARCH_POSTGRES_COLUMNS),
-            tsvectors=None,
-        ),
-    }
-)
+TABLE_SPEC.update({
+    "award_search_testing": TableSpec(
+        model=AwardSearch,
+        is_from_broker=False,
+        source_table="award_search",
+        source_database=None,
+        destination_database="rpt",
+        swap_table=None,
+        swap_schema=None,
+        partition_column="award_id",
+        partition_column_type="numeric",
+        is_partition_column_unique=True,
+        delta_table_create_sql=award_search_create_sql_string,
+        source_schema=None,
+        custom_schema="total_covid_outlay NUMERIC(23,2), total_covid_obligation NUMERIC(23,2), recipient_hash "
+        "STRING, federal_accounts STRING, cfdas ARRAY<STRING>, tas_components ARRAY<STRING>",
+        column_names=list(AWARD_SEARCH_COLUMNS),
+        tsvectors=None,
+    ),
+    "recipient_lookup_testing": TableSpec(
+        model=RecipientLookup,
+        is_from_broker=False,
+        source_table="recipient_lookup",
+        source_database="rpt",
+        destination_database="raw",
+        swap_table=None,
+        swap_schema=None,
+        partition_column="id",
+        partition_column_type="numeric",
+        is_partition_column_unique=True,
+        delta_table_create_sql=recipient_lookup_create_sql_string,
+        source_schema=None,
+        custom_schema="recipient_hash STRING",
+        column_names=list(RECIPIENT_LOOKUP_COLUMNS),
+        tsvectors=None,
+    ),
+    "recipient_profile_testing": TableSpec(
+        model=RecipientProfile,
+        is_from_broker=False,
+        source_table="recipient_profile",
+        source_database="rpt",
+        destination_database="raw",
+        swap_table=None,
+        swap_schema=None,
+        partition_column="id",
+        partition_column_type="numeric",
+        delta_table_create_sql=recipient_profile_create_sql_string,
+        is_partition_column_unique=True,
+        source_schema=None,
+        custom_schema="recipient_hash STRING",
+        column_names=list(RECIPIENT_PROFILE_DELTA_COLUMNS),
+        tsvectors=None,
+    ),
+    "sam_recipient_testing": TableSpec(
+        model=DUNS,
+        is_from_broker=False,
+        source_table="duns",
+        source_database="int",
+        destination_database="raw",
+        swap_table=None,
+        swap_schema=None,
+        partition_column=None,
+        partition_column_type=None,
+        is_partition_column_unique=False,
+        delta_table_create_sql=sam_recipient_create_sql_string,
+        source_schema=None,
+        custom_schema="broker_duns_id STRING, business_types_codes ARRAY<STRING>",
+        column_names=list(SAM_RECIPIENT_COLUMNS),
+        tsvectors=None,
+    ),
+    "transaction_search_testing": TableSpec(
+        model=TransactionSearch,
+        is_from_broker=False,
+        source_table="transaction_search",
+        source_database=None,
+        destination_database="test",
+        swap_table=None,
+        swap_schema=None,
+        partition_column="transaction_id",
+        partition_column_type="numeric",
+        is_partition_column_unique=True,
+        delta_table_create_sql=transaction_search_create_sql_string,
+        source_schema=None,
+        custom_schema="recipient_hash STRING, federal_accounts STRING, parent_recipient_hash STRING",
+        column_names=list(TRANSACTION_SEARCH_POSTGRES_COLUMNS),
+        tsvectors=None,
+    ),
+})
 
 SPARK_PARTITION_ROWS = CONFIG.SPARK_PARTITION_ROWS
 
 
 class Command(BaseCommand):
+
     help = """
     This command reads data from a Postgres database table and inserts it into a corresponding Delta
     Table. As of now, it only supports a full reload of a table. All existing data will be deleted
     before new data is written.
     """
 
-    def add_arguments(self, parser) -> None:
+    def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
             "--destination-table",
             type=str,
