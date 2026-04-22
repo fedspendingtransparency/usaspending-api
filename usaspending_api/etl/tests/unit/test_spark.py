ad0f3c7164a1915627dab4547fdc7b7dd29db46d
James McKay <james.mckay@kc.frb.org>
2026-04-22 13:54:13 +0000

    refactor

--- usaspending_api/etl/tests/unit/test_spark.py
+++ usaspending_api/etl/tests/unit/test_spark.py
@@ -1,6 +1,6 @@
+from dataclasses import asdict
 from usaspending_api.etl.management.commands.load_query_to_delta import TABLE_SPEC as LOAD_QUERY_TABLE_SPEC
 from usaspending_api.etl.management.commands.load_table_to_delta import TABLE_SPEC as LOAD_TABLE_TABLE_SPEC
-
 
 def test_table_spec_consistency():
     table_spec_config_groups = {
@@ -10,8 +10,8 @@ def test_table_spec_consistency():
     for table_spec_group_name, table_spec_config_group in table_spec_config_groups.items():
         unioned_table_spec_keys = set()
         for table_name, config in table_spec_config_group.items():
-            unioned_table_spec_keys = unioned_table_spec_keys.union(set(list(config.keys())))
+            unioned_table_spec_keys = unioned_table_spec_keys.union(set(asdict(config).keys()))
         for table_name, config in table_spec_config_group.items():
-            diff = unioned_table_spec_keys - set(list(config.keys()))
+            diff = unioned_table_spec_keys - set(asdict(config).keys())
             if diff:
                 raise Exception(f"{table_name} is missing the following {table_spec_group_name} values: {diff}")
