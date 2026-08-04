5136c1134bd03d5a5442774a8fdf2ffd85508c4e
David Mikola <david.mikola@kc.frb.org>
2026-08-04 12:45:10 -0500

    [DEV-15536] Updated Allowed_hosts default behavior

--- usaspending_api/settings.py
+++ usaspending_api/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = os.environ.get("DJANGO_DEBUG", "").lower() in ["true", "1", "yes"]
 
 HOST = os.environ.get("HOST", "localhost:3000")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", [".usaspending.gov", ".localhost", "127.0.0.1", "[::1]"])
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", []).split(",")
 
 # Define local flag to affect location of downloads
 IS_LOCAL = os.environ.get("IS_LOCAL", "").lower() in ["true", "1", "yes"]
