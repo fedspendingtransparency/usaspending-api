79f91b7595ed8754d79a64ca5ab81423fc06430f
David Mikola <david.mikola@kc.frb.org>
2026-09-11 09:18:41 -0500

    [Dev-14781] Updated default job_id val

--- usaspending_api/download/helpers/psql_helpers.py
+++ usaspending_api/download/helpers/psql_helpers.py
@@ -42,7 +42,7 @@ def run_psql_to_file(
     sql_path: str,
     output_path: str,
     env: dict,
-    job_id: int = 0,
+    job_id: int | None = None,
     quiet: bool = True,
     on_error_stop: bool = True,
 ) -> None:
